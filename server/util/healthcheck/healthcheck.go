e0eb3a3131f98d908d79ed62e1b4cc6ebf4621e0
Tyler Williams <williams.tyler@gmail.com>
2020-06-17 13:30:58 -0700

    Support per-service healthchecks that affect readiness state.

--- server/util/healthcheck/healthcheck.go
+++ server/util/healthcheck/healthcheck.go
@@ -10,12 +10,25 @@ import (
 	"sync"
 	"time"
 
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"golang.org/x/sync/errgroup"
 )
 
 const (
 	maxShutdownDuration = 60 * time.Second
+
+	healthCheckPeriod  = 3 * time.Second // The time to wait between health checks.
+	healthCheckTimeout = 2 * time.Second // How long a health check may take, max.
 )
+
+type Checker interface {
+	Check(ctx context.Context) error
+}
+type CheckerFunc func(ctx context.Context) error
+
+func (f CheckerFunc) Check(ctx context.Context) error {
+	return f(ctx)
+}
 
 type ShutDownFunc func(ctx context.Context) error
 
@@ -26,6 +39,7 @@ type HealthChecker struct {
 	shutdownFuncs []ShutDownFunc
 	lock          sync.RWMutex // protects: readyToServe
 	readyToServe  bool
+	checkers      map[string]Checker
 }
 
 func NewHealthChecker(serverType string) *HealthChecker {
@@ -34,11 +48,17 @@ func NewHealthChecker(serverType string) *HealthChecker {
 		done:          make(chan bool),
 		quit:          make(chan os.Signal, 1),
 		shutdownFuncs: make([]ShutDownFunc, 0),
-		// TODO(tylerw): Configure healthchecker to wait on backend service status.
-		readyToServe: true,
+		readyToServe:  true,
+		checkers:      make(map[string]Checker, 0),
 	}
 	signal.Notify(hc.quit, os.Interrupt)
 	go hc.handleShutdownFuncs()
+	go func() {
+		for {
+			hc.runHealthChecks(context.Background())
+			time.Sleep(healthCheckPeriod)
+		}
+	}()
 	return &hc
 }
 
@@ -74,8 +94,43 @@ func (h *HealthChecker) RegisterShutdownFunction(f ShutDownFunc) {
 	h.shutdownFuncs = append(h.shutdownFuncs, f)
 }
 
+func (h *HealthChecker) AddHealthCheck(name string, f Checker) {
+	// Mark the service as unhealthy until the healthcheck runs
+	// and it becomes healthy.
+	h.lock.Lock()
+	h.checkers[name] = f
+	h.readyToServe = false
+	h.lock.Unlock()
+}
+
 func (h *HealthChecker) WaitForGracefulShutdown() {
 	<-h.done
+}
+
+func (h *HealthChecker) runHealthChecks(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, healthCheckTimeout)
+	defer cancel()
+
+	eg, ctx := errgroup.WithContext(ctx)
+	for name, ck := range h.checkers {
+		eg.Go(func() error {
+			if err := ck.Check(ctx); err != nil {
+				return status.UnavailableErrorf("Service %s is unhealthy: %s", name, err)
+			}
+			return nil
+		})
+	}
+	err := eg.Wait()
+	newReadinessState := (err == nil)
+
+	h.lock.Lock()
+	previousReadinessState := h.readyToServe
+	h.readyToServe = newReadinessState
+	h.lock.Unlock()
+
+	if newReadinessState != previousReadinessState {
+		log.Printf("HealthChecker transitioning from ready: %t => ready: %t", previousReadinessState, newReadinessState)
+	}
 }
 
 func (h *HealthChecker) ReadinessHandler() http.Handler {
@@ -83,8 +138,10 @@ func (h *HealthChecker) ReadinessHandler() http.Handler {
 		reqServerType := r.Header.Get("server-type")
 		if reqServerType == h.serverType {
 			h.lock.RLock()
-			defer h.lock.RUnlock()
-			if h.readyToServe {
+			ready := h.readyToServe
+			h.lock.RUnlock()
+
+			if ready {
 				w.WriteHeader(http.StatusOK)
 				w.Write([]byte("OK"))
 			} else {
