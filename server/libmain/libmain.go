83ba293ffc080709b49f8d036b989f381c05e4b7
Tyler Williams <williams.tyler@gmail.com>
2020-06-17 15:04:28 -0700

    Reduce max concurrent streams again to something reasonable

--- server/libmain/libmain.go
+++ server/libmain/libmain.go
@@ -216,7 +216,7 @@ func StartGRPCServiceOrDie(env environment.Env, buildBuddyServer *buildbuddy_ser
 		rpcfilters.GetStreamInterceptor(env),
 		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
 		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
-		grpc.MaxConcurrentStreams(10240),
+		grpc.MaxConcurrentStreams(1024),
 	}
 
 	if credentialOption != nil {
