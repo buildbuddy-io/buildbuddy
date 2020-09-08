842647d66b2ecee4e345db5e7cfa74f620ad687f
Brandon Duffany <brandonduffany@gmail.com>
2020-09-08 13:01:20 -0400

    Simplify setCursorOverride

--- app/lib/mouse.ts
+++ app/lib/mouse.ts
@@ -1,13 +1,9 @@
-import { setClassEnabled, setOrRemoveStyleProperty } from "./dom";
-
 export const NO_BUTTON_BIT = 0;
 export const LEFT_BUTTON_BIT = 1;
 export const RIGHT_BUTTON_BIT = 2;
 export const MIDDLE_BUTTON_BIT = 4;
 
 export function setCursorOverride(cursor: string | null) {
-  setClassEnabled(document.body, "cursorOverride", cursor !== null);
-  setOrRemoveStyleProperty(document.body, "cursor", cursor, {
-    important: true,
-  });
+  document.body.classList.toggle("cursorOverride", cursor !== null);
+  document.body.style.cursor = cursor;
 }
