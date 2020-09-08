a55f8f92434ec577d4a413eeed5334457c45fc2f
Brandon Duffany <brandonduffany@gmail.com>
2020-09-08 14:50:20 -0400

    Remove //app/lib

--- app/lib/mouse.ts
+++
@@ -1,9 +0,0 @@
-export const NO_BUTTON_BIT = 0;
-export const LEFT_BUTTON_BIT = 1;
-export const RIGHT_BUTTON_BIT = 2;
-export const MIDDLE_BUTTON_BIT = 4;
-
-export function setCursorOverride(cursor: string | null) {
-  document.body.classList.toggle("cursorOverride", cursor !== null);
-  document.body.style.cursor = cursor;
-}
