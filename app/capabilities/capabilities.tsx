fe55d382bdef4b5fbee521f924f269e435d53d39
Brandon Duffany <brandonduffany@gmail.com>
2020-09-16 12:29:54 -0400

    Launch flame chart V1

--- app/capabilities/capabilities.tsx
+++ app/capabilities/capabilities.tsx
@@ -8,7 +8,6 @@ export class Capabilities {
   github: boolean;
   auth: string;
   anonymous: boolean;
-  flameChart: boolean;
 
   register(name: string, enterprise: boolean, paths: Array<string>) {
     this.name = name;
@@ -22,8 +21,6 @@ export class Capabilities {
     this.anonymous = window.buildbuddyConfig && window.buildbuddyConfig.anonymous_usage_enabled;
     this.enterprise = enterprise;
     this.paths = new Set(paths);
-    this.flameChart =
-      window.localStorage.getItem("buildbuddy://flags/flame_chart_enabled") === "true";
     window.gtag("set", {
       app_name: this.name,
       app_version: this.version,
