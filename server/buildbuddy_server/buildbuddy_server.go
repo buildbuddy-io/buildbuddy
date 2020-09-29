1aef24fd9371b2430ef744dc865c45d7cd43c93d
Brandon Duffany <brandonduffany@gmail.com>
2020-09-29 14:23:33 -0400

    Fix GetUser to include url_identifier in returned groups

--- server/buildbuddy_server/buildbuddy_server.go
+++ server/buildbuddy_server/buildbuddy_server.go
@@ -72,10 +72,11 @@ func makeGroups(grps []*tables.Group) []*grpb.Group {
 	r := make([]*grpb.Group, 0)
 	for _, g := range grps {
 		r = append(r, &grpb.Group{
-			Id:           g.GroupID,
-			Name:         g.Name,
-			OwnedDomain:  g.OwnedDomain,
-			GithubLinked: g.GithubToken != "",
+			Id:            g.GroupID,
+			Name:          g.Name,
+			OwnedDomain:   g.OwnedDomain,
+			GithubLinked:  g.GithubToken != "",
+			UrlIdentifier: g.URLIdentifier
 		})
 	}
 	return r
