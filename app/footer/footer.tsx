29303254661b973d124f66077b637f415a8f9dd6
Brandon Duffany <brandonduffany@gmail.com>
2020-10-05 12:14:18 -0400

    De-emphasize communication links

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -4,36 +4,34 @@ export default class FooterComponent extends React.Component {
   render() {
     return (
       <div className="footer">
-        <div className="footer-icon-links">
-          <a href="https://github.com/buildbuddy-io/buildbuddy/issues/new" target="_blank">
-            <img src="/image/alert-triangle-white.svg" /> Report an issue
-          </a>
-          <a href="https://slack.buildbuddy.io" target="_blank">
-            <img src="/image/slack-white.svg" /> BuildBuddy Slack
-          </a>
-          <a href="https://twitter.com/buildbuddy_io" target="_blank">
-            <img src="/image/twitter-white.svg" /> Twitter
-          </a>
-          <a href="https://github.com/buildbuddy-io/buildbuddy/" target="_blank">
-            <img src="/image/github-white.svg" /> Github repo
-          </a>
-          <a href="mailto:hello@buildbuddy.io" target="_blank">
-            <img src="/image/message-circle-white.svg" /> Contact us
-          </a>
-        </div>
-        <div>
+        <div className="footer-link-list">
+          <span>&copy; 2020 Iteration, Inc.</span>
           <a href="https://buildbuddy.io/terms" target="_blank">
             Terms
-          </a>{" "}
-          |{" "}
+          </a>
           <a href="https://buildbuddy.io/privacy" target="_blank">
             Privacy
-          </a>{" "}
-          |{" "}
+          </a>
           <a href="https://buildbuddy.io" target="_blank">
             BuildBuddy
-          </a>{" "}
-          | &copy; 2020 Iteration, Inc.
+          </a>
+        </div>
+        <div className="footer-link-list">
+          <a href="https://github.com/buildbuddy-io/buildbuddy/issues/new" target="_blank">
+            Report an issue
+          </a>
+          <a href="https://slack.buildbuddy.io" target="_blank">
+            BuildBuddy Slack
+          </a>
+          <a href="https://twitter.com/buildbuddy_io" target="_blank">
+            Twitter
+          </a>
+          <a href="https://github.com/buildbuddy-io/buildbuddy/" target="_blank">
+            Github repo
+          </a>
+          <a href="mailto:hello@buildbuddy.io" target="_blank">
+            Contact us
+          </a>
         </div>
       </div>
     );
