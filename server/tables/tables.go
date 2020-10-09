3c5af395f82e0f46c17bee1f28b6a272939f3c79
Brandon <brandon@buildbuddy.io>
2020-10-09 17:36:21 -0400

    Fix boolean logic

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -369,7 +369,7 @@ func PreAutoMigrate(db *gorm.DB) error {
 			}
 		}
 		// Before creating a unique index, need to replace empty strings with NULL.
-		if d.HasIndex("Groups", "url_identifier_unique_index") {
+		if !d.HasIndex("Groups", "url_identifier_unique_index") {
 			if err := db.Exec(`UPDATE Groups SET url_identifier = NULL WHERE url_identifier = ""`).Error; err != nil {
 				return err
 			}
