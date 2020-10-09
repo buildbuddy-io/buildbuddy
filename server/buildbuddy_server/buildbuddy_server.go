4bf487609867269f3719ebc8da4f144206f0d127
Brandon <brandon@buildbuddy.io>
2020-10-09 16:12:25 -0400

    Make group slug nullable

--- server/buildbuddy_server/buildbuddy_server.go
+++ server/buildbuddy_server/buildbuddy_server.go
@@ -72,12 +72,16 @@ func (s *BuildBuddyServer) SearchInvocation(ctx context.Context, req *inpb.Searc
 func makeGroups(grps []*tables.Group) []*grpb.Group {
 	r := make([]*grpb.Group, 0)
 	for _, g := range grps {
+		urlIdentifier := ""
+		if g.URLIdentifier != nil {
+			urlIdentifier = *g.URLIdentifier
+		}
 		r = append(r, &grpb.Group{
 			Id:            g.GroupID,
 			Name:          g.Name,
 			OwnedDomain:   g.OwnedDomain,
 			GithubLinked:  g.GithubToken != "",
-			UrlIdentifier: g.URLIdentifier,
+			UrlIdentifier: urlIdentifier,
 		})
 	}
 	return r
@@ -124,8 +128,12 @@ func (s *BuildBuddyServer) GetGroup(ctx context.Context, req *grpb.GetGroupReque
 	if userDB == nil {
 		return nil, status.UnimplementedError("Not Implemented")
 	}
+	urlIdentifier := &req.UrlIdentifier
+	if *urlIdentifier == "" {
+		urlIdentifier = nil
+	}
 	group := &tables.Group{
-		URLIdentifier: req.GetUrlIdentifier(),
+		URLIdentifier: urlIdentifier,
 	}
 	if err := userDB.FillGroup(ctx, group); err != nil {
 		return nil, err
@@ -161,12 +169,13 @@ func (s *BuildBuddyServer) CreateGroup(ctx context.Context, req *grpb.CreateGrou
 	}
 
 	urlIdentifier := strings.TrimSpace(req.GetUrlIdentifier())
-
 	group := &tables.Group{
 		UserID:        user.UserID,
 		Name:          groupName,
-		URLIdentifier: urlIdentifier,
 		OwnedDomain:   groupOwnedDomain,
+	}
+	if urlIdentifier != "" {
+		group.URLIdentifier = &urlIdentifier
 	}
 	groupID, err := userDB.InsertOrUpdateGroup(ctx, group)
 	if err != nil {
--- server/tables/tables.go
+++ server/tables/tables.go
@@ -147,7 +147,7 @@ type Group struct {
 	// An unique URL segment that is displayed in group-related URLs.
 	// e.g. "example-org" in app.buildbuddy.com/join/example-org or
 	// "example-org.buildbuddy.com" if we support subdomains in the future.
-	URLIdentifier string `gorm:"index:url_identifier_index"`
+	URLIdentifier *string `gorm:"unique_index:url_identifier_index"`
 
 	// The "owned" domain. In enterprise/cloud version, we create a
 	// group for a customer's domain, and new users that sign up with an
