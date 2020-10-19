b8aedf8f19c57410f878fb01494bbcb3e8195409
Tyler Williams <williams.tyler@gmail.com>
2020-10-19 16:20:42 -0700

    Print invocation ID in log line if present.

--- server/util/log/log.go
+++ server/util/log/log.go
@@ -8,9 +8,13 @@ import (
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/util/uuid"
+	"github.com/golang/protobuf/proto"
 
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
 
 func formatDuration(dur time.Duration) string {
@@ -34,10 +38,35 @@ func fmtErr(err error) string {
 	return code.String()
 }
 
+func getRequestMetadata(ctx context.Context) *repb.RequestMetadata {
+	if grpcMD, ok := metadata.FromIncomingContext(ctx); ok {
+		rmdVals := grpcMD["build.bazel.remote.execution.v2.requestmetadata-bin"]
+		if len(rmdVals) == 1 {
+			rmd := &repb.RequestMetadata{}
+			if err := proto.Unmarshal([]byte(rmdVals[0]), rmd); err == nil {
+				return rmd
+			}
+		}
+	}
+	return nil
+}
+
+func getInvocationIDFromMD(ctx context.Context) string {
+	iid := ""
+	if rmd := getRequestMetadata(ctx); rmd != nil {
+		iid = rmd.GetToolInvocationId()
+	}
+	return iid
+}
+
 func LogGRPCRequest(ctx context.Context, fullMethod string, dur time.Duration, err error) {
 	reqID, _ := uuid.GetFromContext(ctx) // Ignore error, we're logging anyway.
 	shortPath := "/" + path.Base(fullMethod)
-	log.Printf("%s %s %q %s [%s]", "gRPC", reqID, shortPath, fmtErr(err), formatDuration(dur))
+	if iid := getInvocationIDFromMD(ctx); iid != "" {
+		log.Printf("%s %s %s %s %s [%s]", "gRPC", reqID, iid, shortPath, fmtErr(err), formatDuration(dur))
+	} else {
+		log.Printf("%s %s %s %s [%s]", "gRPC", reqID, shortPath, fmtErr(err), formatDuration(dur))
+	}
 }
 
 func LogHTTPRequest(ctx context.Context, url string, dur time.Duration, err error) {
