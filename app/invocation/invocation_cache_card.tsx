60e5e5929a1ce2c5c2ee28da224a7efaac26a74e
Siggi <siggisim@gmail.com>
2020-10-19 09:15:23 -0700

    Replace cache stats pie charts with real charts

--- app/invocation/invocation_cache_card.tsx
+++ app/invocation/invocation_cache_card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InvocationModel from "./invocation_model";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
 
 interface Props {
   model: InvocationModel;
@@ -33,12 +34,7 @@ export default class CacheCardComponent extends React.Component {
                       <div className="cache-title">Action cache (AC)</div>
                       <div className="cache-subtitle">Maps action hashes to action result metadata</div>
                       <div className="cache-chart">
-                        {this.drawPieChart(
-                          +cacheStat.actionCacheHits,
-                          "#4CAF50",
-                          +cacheStat.actionCacheMisses,
-                          "#f44336"
-                        )}
+                        {this.drawChart(+cacheStat.actionCacheHits, "#4CAF50", +cacheStat.actionCacheMisses, "#f44336")}
                         <div>
                           <div className="cache-chart-label">
                             <span className="color-swatch cache-hit-color-swatch"></span>
@@ -60,7 +56,7 @@ export default class CacheCardComponent extends React.Component {
                       <div className="cache-title">Content addressable store (CAS)</div>
                       <div className="cache-subtitle">Stores files (including logs & profiling info)</div>
                       <div className="cache-chart">
-                        {this.drawPieChart(+cacheStat.casCacheHits, "#4CAF50", +cacheStat.casCacheMisses, "#f44336")}
+                        {this.drawChart(+cacheStat.casCacheHits, "#4CAF50", +cacheStat.casCacheMisses, "#f44336")}
                         <div>
                           <div className="cache-chart-label">
                             <span className="color-swatch cache-hit-color-swatch"></span>
@@ -82,7 +78,7 @@ export default class CacheCardComponent extends React.Component {
                       <div className="cache-title">Volume</div>
                       <div className="cache-subtitle">Total size of files uploaded & downloaded from both caches</div>
                       <div className="cache-chart">
-                        {this.drawPieChart(
+                        {this.drawChart(
                           +cacheStat.totalDownloadSizeBytes,
                           "#03A9F4",
                           +cacheStat.totalUploadSizeBytes,
@@ -111,7 +107,7 @@ export default class CacheCardComponent extends React.Component {
                       <div className="cache-title">Throughput</div>
                       <div className="cache-subtitle">Upload / download speed (per parallelized cache request)</div>
                       <div className="cache-chart">
-                        {this.drawPieChart(downloadThroughput, "#03A9F4", uploadThroughput, "#607D8B")}
+                        {this.drawChart(downloadThroughput, "#03A9F4", uploadThroughput, "#607D8B")}
                         <div>
                           <div className="cache-chart-label">
                             <span className="color-swatch download-color-swatch"></span>
@@ -134,39 +130,28 @@ export default class CacheCardComponent extends React.Component {
     );
   }
 
-  drawPieChart(a: number, colorA: string, b: number, colorB: string) {
-    let total = a + b;
-    let percentA = (100 * a) / total;
-    let percentB = (100 * b) / total;
+  drawChart(a: number, colorA: string, b: number, colorB: string) {
     if (a == 0 && b == 0) {
       colorA = "#eee";
       colorB = "#eee";
     }
 
+    let data = [
+      { value: a, color: colorA },
+      { value: b, color: colorB },
+    ];
+
     return (
       <div className="cache-chart-container">
-        <svg viewBox="0 0 32 32" style={{ borderRadius: "50%", transform: "rotate(-90deg)" }}>
-          <g strokeWidth="16">
-            <circle
-              fill="transparent"
-              r="16"
-              cx="16"
-              cy="16"
-              stroke={colorA}
-              strokeDasharray={`${percentA} 100`}
-              strokeDashoffset="0"
-            />
-            <circle
-              fill="transparent"
-              r="16"
-              cx="16"
-              cy="16"
-              stroke={colorB}
-              strokeDasharray={`${percentB} 100`}
-              strokeDashoffset={`-${percentA}`}
-            />
-          </g>
-        </svg>
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie data={data} dataKey="value" outerRadius={40} innerRadius={20}>
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
       </div>
     );
   }
