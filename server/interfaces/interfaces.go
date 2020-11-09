7f0233206a2b07509d649d0015a51853013b5c7f
Brandon Duffany <brandon@buildbuddy.io>
2020-11-09 15:14:28 -0500

    Rename apiKey parameter

--- server/interfaces/interfaces.go
+++ server/interfaces/interfaces.go
@@ -171,7 +171,7 @@ type UserDB interface {
 	UpdateGroupUsers(ctx context.Context, groupID string, updates []*grpb.UpdateGroupUsersRequest_Update) error
 
 	// API Keys API
-	GetAPIKey(ctx context.Context, apiKey string) (*tables.APIKey, error)
+	GetAPIKey(ctx context.Context, apiKeyID string) (*tables.APIKey, error)
 	GetAPIKeys(ctx context.Context, groupID string) ([]*tables.APIKey, error)
 	CreateAPIKey(ctx context.Context, groupID string) (*tables.APIKey, error)
 	UpdateAPIKey(ctx context.Context, key *tables.APIKey) error
