7b86c5ce9e3b0363ae658c75da930e934e3ace30
Tyler Williams <williams.tyler@gmail.com>
2020-11-12 14:40:53 -0800

    Revert to old-style (simple) hit-tracker.

--- server/remote_cache/hit_tracker/hit_tracker.go
+++ server/remote_cache/hit_tracker/hit_tracker.go
@@ -2,27 +2,23 @@ package hit_tracker
 
 import (
 	"context"
-	"crypto/sha256"
-	"fmt"
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
-	"github.com/buildbuddy-io/buildbuddy/server/tables"
-	"github.com/buildbuddy-io/buildbuddy/server/util/status"
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"
-	"github.com/jinzhu/gorm"
 
 	capb "github.com/buildbuddy-io/buildbuddy/proto/cache"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
-	tspb "github.com/golang/protobuf/ptypes/timestamp"
 )
 
+type CacheMode int
 type counterType int
 
 const (
+	CAS         CacheMode = iota // CAS cache
+	ActionCache                  // Action cache
+
 	Hit counterType = iota
 	Miss
 	Upload
@@ -74,60 +70,49 @@ func counterName(actionCache bool, ct counterType, iid string) string {
 }
 
 type HitTracker struct {
-	iid                string
-	c                  interfaces.MetricsCollector
-	ctx                context.Context
-	env                environment.Env
-	remoteInstanceName string
-	saveActions        bool
+	iid         string
+	c           interfaces.MetricsCollector
+	ctx         context.Context
+	actionCache bool
 }
 
-func NewHitTracker(ctx context.Context, env environment.Env, remoteInstanceName string) *HitTracker {
+func NewHitTracker(ctx context.Context, env environment.Env, actionCache bool) *HitTracker {
 	return &HitTracker{
-		c:                  env.GetMetricsCollector(),
-		ctx:                ctx,
-		env:                env,
-		remoteInstanceName: remoteInstanceName,
-		iid:                digest.GetInvocationIDFromMD(ctx),
-		saveActions:        digest.IsCacheDebuggingEnabled(ctx),
+		c:           env.GetMetricsCollector(),
+		ctx:         ctx,
+		iid:         digest.GetInvocationIDFromMD(ctx),
+		actionCache: actionCache,
 	}
 }
 
-func (h *HitTracker) getCounter(actionCache bool, ct counterType) string {
-	return counterName(actionCache, ct, h.iid)
-}
-
-func (h *HitTracker) casCounterName(ct counterType) string {
-	return counterName(false, ct, h.iid)
-}
-
-func (h *HitTracker) acCounterName(ct counterType) string {
-	return counterName(true, ct, h.iid)
+func (h *HitTracker) counterName(ct counterType) string {
+	return counterName(h.actionCache, ct, h.iid)
 }
 
 // Example Usage:
 //
-// ht := NewHitTracker(ctx, env, instanceName)
+// ht := NewHitTracker(env, invocationID, false /*=actionCache*/)
 // if err := ht.TrackMiss(); err != nil {
 //   log.Printf("Error counting cache miss.")
 // }
-func (h *HitTracker) TrackCASMiss(d *repb.Digest) error {
+func (h *HitTracker) TrackMiss(d *repb.Digest) error {
 	if h.c == nil || h.iid == "" {
 		return nil
 	}
-	_, err := h.c.IncrementCount(h.ctx, h.casCounterName(Miss), 1)
+	_, err := h.c.IncrementCount(h.ctx, h.counterName(Miss), 1)
 	return err
 }
 
-func (h *HitTracker) TrackEmptyCASHit() error {
+func (h *HitTracker) TrackEmptyHit() error {
 	if h.c == nil || h.iid == "" {
 		return nil
 	}
-	_, err := h.c.IncrementCount(h.ctx, h.casCounterName(Hit), 1)
+	_, err := h.c.IncrementCount(h.ctx, h.counterName(Hit), 1)
 	return err
 }
 
 type closeFunction func() error
+
 type transferTimer struct {
 	closeFn closeFunction
 }
@@ -142,13 +127,13 @@ func (h *HitTracker) makeCloseFunc(actionCache bool, d *repb.Digest, start time.
 		if h.c == nil || h.iid == "" {
 			return nil
 		}
-		if _, err := h.c.IncrementCount(h.ctx, h.getCounter(actionCache, actionCounter), 1); err != nil {
+		if _, err := h.c.IncrementCount(h.ctx, h.counterName(actionCounter), 1); err != nil {
 			return err
 		}
-		if _, err := h.c.IncrementCount(h.ctx, h.getCounter(actionCache, sizeCounter), d.GetSizeBytes()); err != nil {
+		if _, err := h.c.IncrementCount(h.ctx, h.counterName(sizeCounter), d.GetSizeBytes()); err != nil {
 			return err
 		}
-		if _, err := h.c.IncrementCount(h.ctx, h.getCounter(actionCache, timeCounter), dur.Microseconds()); err != nil {
+		if _, err := h.c.IncrementCount(h.ctx, h.counterName(timeCounter), dur.Microseconds()); err != nil {
 			return err
 		}
 		return nil
@@ -157,11 +142,11 @@ func (h *HitTracker) makeCloseFunc(actionCache bool, d *repb.Digest, start time.
 
 // Example Usage:
 //
-// ht := NewHitTracker(ctx, env, instanceName)
-// dlt := ht.TrackCASDownload(d)
+// ht := NewHitTracker(env, invocationID, false /*=actionCache*/)
+// dlt := ht.TrackDownload(d)
 // defer dlt.Close()
 // ... body of download logic ...
-func (h *HitTracker) TrackCASDownload(d *repb.Digest) *transferTimer {
+func (h *HitTracker) TrackDownload(d *repb.Digest) *transferTimer {
 	start := time.Now()
 	return &transferTimer{
 		closeFn: h.makeCloseFunc(false, d, start, Hit, DownloadSizeBytes, DownloadUsec),
@@ -170,130 +155,14 @@ func (h *HitTracker) TrackCASDownload(d *repb.Digest) *transferTimer {
 
 // Example Usage:
 //
-// ht := NewHitTracker(ctx, env, instanceName)
-// ult := ht.TrackCASUpload(d)
+// ht := NewHitTracker(env, invocationID, false /*=actionCache*/)
+// ult := ht.TrackUpload(d)
 // defer ult.Close()
 // ... body of download logic ...
-func (h *HitTracker) TrackCASUpload(d *repb.Digest) *transferTimer {
+func (h *HitTracker) TrackUpload(d *repb.Digest) *transferTimer {
 	start := time.Now()
 	return &transferTimer{
 		closeFn: h.makeCloseFunc(false, d, start, Upload, UploadSizeBytes, UploadUsec),
-	}
-}
-
-type actionCloseFn func(actionResult *repb.ActionResult) error
-type actionTimer struct {
-	closeFn actionCloseFn
-}
-
-func (t *actionTimer) Close(actionResult *repb.ActionResult) error {
-	return t.closeFn(actionResult)
-}
-
-func hashString(raw string) string {
-	h := sha256.New()
-	h.Write([]byte(raw))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-func actionResultKey(actionResult *repb.ActionResult) string {
-	for _, of := range actionResult.GetOutputFiles() {
-		return hashString(of.GetPath())
-	}
-	for _, ofs := range actionResult.GetOutputFileSymlinks() {
-		return hashString(ofs.GetPath())
-	}
-	for _, od := range actionResult.GetOutputDirectories() {
-		return hashString(od.GetPath())
-	}
-	for _, ods := range actionResult.GetOutputDirectorySymlinks() {
-		return hashString(ods.GetPath())
-	}
-	return "unknown"
-}
-
-func diffTimeProtos(startPb, endPb *tspb.Timestamp) time.Duration {
-	start, _ := ptypes.Timestamp(startPb)
-	end, _ := ptypes.Timestamp(endPb)
-	return end.Sub(start)
-}
-
-// Example Usage:
-//
-// ht := NewHitTracker(ctx, env, instanceName)
-// dlt := ht.TrackACDownload(d)
-// defer dlt.Close(actionResult)
-// ... body of download logic ...
-func (h *HitTracker) TrackACDownload(d *repb.Digest) *actionTimer {
-	start := time.Now()
-	closeFn := func(actionResult *repb.ActionResult) error {
-		if md := actionResult.GetExecutionMetadata(); md != nil {
-			if h.c != nil && h.iid != "" {
-				actionExecDuration := diffTimeProtos(md.GetExecutionStartTimestamp(), md.GetExecutionCompletedTimestamp())
-				if _, err := h.c.IncrementCount(h.ctx, h.getCounter(false, CachedActionExecUsec), actionExecDuration.Microseconds()); err != nil {
-					return err
-				}
-			}
-		}
-		if h.saveActions {
-			dbHandle := h.env.GetDBHandle()
-			if dbHandle == nil {
-				return status.FailedPreconditionError("No database configured")
-			}
-			if err := h.env.GetDBHandle().Transaction(func(tx *gorm.DB) error {
-				return tx.Create(&tables.CacheLog{
-					JoinKey:            actionResultKey(actionResult),
-					InvocationID:       h.iid,
-					RemoteInstanceName: h.remoteInstanceName,
-					DigestHash:         fmt.Sprintf("%s/%d", d.GetHash(), d.GetSizeBytes()),
-				}).Error
-			}); err != nil {
-				return err
-			}
-		}
-		return h.makeCloseFunc(true, d, start, Hit, DownloadSizeBytes, DownloadUsec)()
-	}
-
-	return &actionTimer{
-		closeFn: closeFn,
-	}
-}
-
-// Example Usage:
-//
-// ht := NewHitTracker(ctx, env, instanceName)
-// ult := ht.TrackACUpload(d)
-// defer ult.Close(actionResult)
-// ... body of download logic ...
-func (h *HitTracker) TrackACUpload(d *repb.Digest) *actionTimer {
-	start := time.Now()
-	closeFn := func(actionResult *repb.ActionResult) error {
-		if h.saveActions {
-			dbHandle := h.env.GetDBHandle()
-			if dbHandle == nil {
-				return status.FailedPreconditionError("No database configured")
-			}
-			blob, err := proto.Marshal(actionResult)
-			if err != nil {
-				return err
-			}
-			if err := h.env.GetDBHandle().Transaction(func(tx *gorm.DB) error {
-				return tx.Create(&tables.CacheLog{
-					JoinKey:            actionResultKey(actionResult),
-					InvocationID:       h.iid,
-					RemoteInstanceName: h.remoteInstanceName,
-					DigestHash:         fmt.Sprintf("%s/%d", d.GetHash(), d.GetSizeBytes()),
-					SerializedProto:    blob,
-				}).Error
-			}); err != nil {
-				return err
-			}
-		}
-		return h.makeCloseFunc(true, d, start, Upload, UploadSizeBytes, UploadUsec)()
-	}
-
-	return &actionTimer{
-		closeFn: closeFn,
 	}
 }
 
