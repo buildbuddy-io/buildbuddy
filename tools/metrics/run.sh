72bb05773624aeb611e6f67bb1f35be0e336ea37
Brandon Duffany <brandon@buildbuddy.io>
2020-11-18 11:42:55 -0500

    Slight improvements to startup script

--- tools/metrics/run.sh
+++ tools/metrics/run.sh
@@ -4,13 +4,13 @@ set -e -o pipefail
 __file__="$0"
 __dir__=$(dirname "$__file__")
 
-# Open grafana webpage in browser when ready
+# Open Grafana dashboard when the server is up and running
 (
   open=$(which open &>/dev/null && echo "open" || echo "xdg-open")
   tries=100
   grafana_url="http://localhost:4500"
   while ! curl "$grafana_url" &>/dev/null ; do
-    sleep 2
+    sleep 0.5
     tries=$(( tries - 1 ))
     if [[ $tries == 0 ]] ; then
       exit 1
@@ -19,5 +19,4 @@ __dir__=$(dirname "$__file__")
   "$open" "http://localhost:4500"
 ) &
 
-# Run docker-compose
 docker-compose up
