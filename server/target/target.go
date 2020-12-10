2727d47b4631b275b04278ea7bbae3fb0de832a4
Tyler Williams <williams.tyler@gmail.com>
2020-12-10 09:42:01 -0800

    Do it all in SQL.

--- server/target/target.go
+++ server/target/target.go
@@ -3,12 +3,9 @@ package target
 import (
 	"context"
 	"fmt"
-	"sort"
-	"strings"
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
-	"github.com/buildbuddy-io/buildbuddy/server/tables"
 	"github.com/buildbuddy-io/buildbuddy/server/util/perms"
 	"github.com/buildbuddy-io/buildbuddy/server/util/query_builder"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
@@ -18,6 +15,10 @@ import (
 	cmpb "github.com/buildbuddy-io/buildbuddy/proto/api/v1/common"
 	"github.com/buildbuddy-io/buildbuddy/proto/build_event_stream"
 	trpb "github.com/buildbuddy-io/buildbuddy/proto/target"
+)
+
+const (
+	sqlite3Dialect = "sqlite3"
 )
 
 func convertToCommonStatus(in build_event_stream.TestStatus) cmpb.Status {
@@ -62,7 +63,7 @@ func GetTarget(ctx context.Context, env environment.Env, req *trpb.GetTargetRequ
 	if et := req.GetEndTimeUsec(); et != 0 {
 		endUsec = et
 	}
-	targetHistory, err := readTargetHistory(ctx, env, req.GetQuery(), startUsec, endUsec)
+	targetHistory, err := readTargets(ctx, env, req.GetQuery(), startUsec, endUsec)
 	if err != nil {
 		return nil, err
 	}
@@ -71,64 +72,41 @@ func GetTarget(ctx context.Context, env environment.Env, req *trpb.GetTargetRequ
 	}, nil
 }
 
-func readTargets(ctx context.Context, env environment.Env, tq *trpb.TargetQuery) ([]*tables.Target, error) {
+func readTargets(ctx context.Context, env environment.Env, tq *trpb.TargetQuery, startUsec, endUsec int64) ([]*trpb.TargetHistory, error) {
 	if env.GetDBHandle() == nil {
 		return nil, status.FailedPreconditionError("database not configured")
 	}
-	q := query_builder.NewQuery(`SELECT * FROM Targets as t`)
-	q.AddWhereClause("t.repo_url = ?", tq.GetRepoUrl())
+	var innerQ *query_builder.Query
+	if env.GetDBHandle().Dialect().GetName() == sqlite3Dialect {
+		innerQ = query_builder.NewQuery(`SELECT invocation_id, MAX(created_at_usec) FROM Invocations`)
+	} else {
+		innerQ = query_builder.NewQuery(`SELECT ANY_VALUE(invocation_id) as invocation_id, MAX(created_at_usec) FROM Invocations`)
+	}
+	// Adds user / permissions check.
+	if err := perms.AddPermissionsCheckToQuery(ctx, env, innerQ); err != nil {
+		return nil, err
+	}
+	innerQ.SetGroupBy("commit_sha")
+	innerQueryStr, innerArgs := innerQ.Build()
+
+	q := query_builder.NewQuery(`SELECT t.target_id, t.label, t.rule_type, ts.target_type,
+                                     ts.test_size, ts.status, ts.start_time_usec, ts.duration_usec,
+                                     i.invocation_id, i.commit_sha, i.repo_url, i.created_at_usec
+                                     FROM Targets as t
+                                     JOIN TargetStatuses AS ts ON t.target_id = ts.target_id
+                                     JOIN Invocations AS i ON ts.invocation_pk = i.invocation_pk
+                                     JOIN (` + innerQueryStr + `) as ci ON i.invocation_id = ci.invocation_id`)
+	// Adds user / permissions to targets (t) table.
 	if err := perms.AddPermissionsCheckToQueryWithTableAlias(ctx, env, q, "t"); err != nil {
 		return nil, err
 	}
-	queryStr, args := q.Build()
-	targets := make([]*tables.Target, 0)
-	err := env.GetDBHandle().Transaction(func(tx *gorm.DB) error {
-		rows, err := tx.Raw(queryStr, args...).Rows()
-		if err != nil {
-			return err
-		}
-		defer rows.Close()
-		for rows.Next() {
-			t := tables.Target{}
-			if err := tx.ScanRows(rows, &t); err != nil {
-				return err
-			}
-			targets = append(targets, &t)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return targets, nil
-}
-
-func readTargetHistory(ctx context.Context, env environment.Env, tq *trpb.TargetQuery, startUsec, endUsec int64) ([]*trpb.TargetHistory, error) {
-	if env.GetDBHandle() == nil {
-		return nil, status.FailedPreconditionError("database not configured")
-	}
-	allTargets, err := readTargets(ctx, env, tq)
-	if err != nil {
-		return nil, err
-	}
-	targets := make(map[int64]*trpb.Target, 0)
-	for _, t := range allTargets {
-		targets[t.TargetID] = &trpb.Target{
-			Id:       fmt.Sprintf("%d", t.TargetID),
-			Label:    t.Label,
-			RuleType: t.RuleType,
-		}
-	}
-
-	q := query_builder.NewQuery(`SELECT ts.target_id, ts.target_type, ts.test_size, ts.status, ts.start_time_usec,
-                                     ts.duration_usec, i.invocation_id, i.commit_sha, i.repo_url, i.created_at_usec
-                                     FROM TargetStatuses AS ts JOIN Invocations AS i ON ts.invocation_pk = i.invocation_pk`)
 	// Adds user / permissions to invocations (i) table.
 	if err := perms.AddPermissionsCheckToQueryWithTableAlias(ctx, env, q, "i"); err != nil {
 		return nil, err
 	}
 	q.AddWhereClause("i.created_at_usec > ?", startUsec)
 	q.AddWhereClause("i.created_at_usec + i.duration_usec < ?", endUsec)
+
 	if repo := tq.GetRepoUrl(); repo != "" {
 		q.AddWhereClause("i.repo_url = ?", repo)
 	}
@@ -147,10 +125,15 @@ func readTargetHistory(ctx context.Context, env environment.Env, tq *trpb.Target
 	if targetType := tq.GetTargetType(); targetType != cmpb.TargetType_TARGET_TYPE_UNSPECIFIED {
 		q.AddWhereClause("ts.target_type = ?", int32(targetType))
 	}
-	queryStr, args := q.Build()
-	statuses := make(map[int64][]*trpb.TargetStatus, 0)
+	q.SetOrderBy("t.label, ts.start_time_usec + ts.duration_usec", true /*=ascending*/)
+	queryStr, outerArgs := q.Build()
+	args := append(innerArgs, outerArgs...)
 
-	err = env.GetDBHandle().Transaction(func(tx *gorm.DB) error {
+	seenTargets := make(map[string]struct{}, 0)
+	targets := make([]*trpb.Target, 0)
+	statuses := make(map[string][]*trpb.TargetStatus, 0)
+
+	err := env.GetDBHandle().Transaction(func(tx *gorm.DB) error {
 		rows, err := tx.Raw(queryStr, args...).Rows()
 		if err != nil {
 			return err
@@ -159,6 +142,8 @@ func readTargetHistory(ctx context.Context, env environment.Env, tq *trpb.Target
 		for rows.Next() {
 			row := struct {
 				TargetID      int64
+				Label         string
+				RuleType      string
 				TargetType    int32
 				TestSize      int32
 				Status        int32
@@ -172,13 +157,20 @@ func readTargetHistory(ctx context.Context, env environment.Env, tq *trpb.Target
 			if err := tx.ScanRows(rows, &row); err != nil {
 				return err
 			}
-			if _, ok := targets[row.TargetID]; ok {
-				targets[row.TargetID].TargetType = cmpb.TargetType(row.TargetType)
-				targets[row.TargetID].TestSize = cmpb.TestSize(row.TestSize)
+			targetID := fmt.Sprintf("%d", row.TargetID)
+			if _, ok := seenTargets[targetID]; !ok {
+				seenTargets[targetID] = struct{}{}
+				targets = append(targets, &trpb.Target{
+					Id:         targetID,
+					Label:      row.Label,
+					RuleType:   row.RuleType,
+					TargetType: cmpb.TargetType(row.TargetType),
+					TestSize:   cmpb.TestSize(row.TestSize),
+				})
 			}
 
 			tsPb, _ := ptypes.TimestampProto(time.Unix(0, row.StartTimeUsec*int64(time.Microsecond)))
-			statuses[row.TargetID] = append(statuses[row.TargetID], &trpb.TargetStatus{
+			statuses[targetID] = append(statuses[targetID], &trpb.TargetStatus{
 				InvocationId: row.InvocationID,
 				CommitSha:    row.CommitSHA,
 				Status:       convertToCommonStatus(build_event_stream.TestStatus(row.Status)),
@@ -195,24 +187,13 @@ func readTargetHistory(ctx context.Context, env environment.Env, tq *trpb.Target
 	}
 
 	targetHistories := make([]*trpb.TargetHistory, 0, len(targets))
-	for targetID, target := range targets {
-		sort.Slice(statuses[targetID], func(i, j int) bool {
-			iStart := statuses[targetID][i].GetTiming().GetStartTime().GetSeconds()
-			jStart := statuses[targetID][j].GetTiming().GetStartTime().GetSeconds()
-			return iStart > jStart
+	for _, target := range targets {
+		targetID := target.Id
+		targetHistories = append(targetHistories, &trpb.TargetHistory{
+			Target:       target,
+			TargetStatus: statuses[targetID],
+			RepoUrl:      tq.GetRepoUrl(),
 		})
-		if len(statuses[targetID]) > 0 {
-			targetHistories = append(targetHistories, &trpb.TargetHistory{
-				Target:       target,
-				TargetStatus: statuses[targetID],
-				RepoUrl:      tq.GetRepoUrl(),
-			})
-		}
 	}
-
-	sort.Slice(targetHistories, func(i, j int) bool {
-		return strings.Compare(targetHistories[i].GetTarget().GetLabel(), targetHistories[j].GetTarget().GetLabel()) < 0
-	})
-
 	return targetHistories, nil
 }
