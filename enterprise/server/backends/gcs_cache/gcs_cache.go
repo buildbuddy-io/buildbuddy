4ed538d351b31b7fbcd7114f5c06a7a6030d3710
Tyler Williams <williams.tyler@gmail.com>
2020-12-16 18:04:03 -0800

    Move gcs_cache, memcache, and pubsub to main repo.

---
+++ enterprise/server/backends/gcs_cache/gcs_cache.go
@@ -0,0 +1,379 @@
+package gcs_cache
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/prefix"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+)
+
+const (
+	numRetries = 3
+)
+
+type GCSCache struct {
+	gcsClient    *storage.Client
+	bucketHandle *storage.BucketHandle
+	projectID    string
+	ttlInDays    int64
+	prefix       string
+}
+
+func NewGCSCache(bucketName, projectID string, ageInDays int64, opts ...option.ClientOption) (*GCSCache, error) {
+	ctx := context.Background()
+	gcsClient, err := storage.NewClient(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+	g := &GCSCache{
+		gcsClient: gcsClient,
+		projectID: projectID,
+		ttlInDays: ageInDays,
+	}
+	if err := g.createBucketIfNotExists(ctx, bucketName); err != nil {
+		return nil, err
+	}
+	if err := g.setBucketTTL(ctx, bucketName, ageInDays); err != nil {
+		return nil, err
+	}
+	log.Printf("Initialized GCS cache with bucket %q, ttl (days): %d", bucketName, ageInDays)
+	return g, nil
+}
+
+func (g *GCSCache) createBucketIfNotExists(ctx context.Context, bucketName string) error {
+	if _, err := g.gcsClient.Bucket(bucketName).Attrs(ctx); err != nil {
+		log.Printf("Creating storage bucket: %s", bucketName)
+		g.bucketHandle = g.gcsClient.Bucket(bucketName)
+		return g.bucketHandle.Create(ctx, g.projectID, nil)
+	}
+	g.bucketHandle = g.gcsClient.Bucket(bucketName)
+	return nil
+}
+
+func (g *GCSCache) setBucketTTL(ctx context.Context, bucketName string, ageInDays int64) error {
+	attrs, err := g.gcsClient.Bucket(bucketName).Attrs(ctx)
+	if err != nil {
+		return err
+	}
+	for _, rule := range attrs.Lifecycle.Rules {
+		if rule.Condition.AgeInDays == ageInDays &&
+			rule.Action.Type == storage.DeleteAction {
+			return nil
+		}
+	}
+	lc := storage.Lifecycle{
+		Rules: []storage.LifecycleRule{
+			storage.LifecycleRule{
+				Condition: storage.LifecycleCondition{
+					AgeInDays: ageInDays,
+				},
+				Action: storage.LifecycleAction{
+					Type: storage.DeleteAction,
+				},
+			},
+		},
+	}
+	// Update the bucket TTL, regardless of whatever value is set.
+	_, err = g.gcsClient.Bucket(bucketName).Update(ctx, storage.BucketAttrsToUpdate{Lifecycle: &lc})
+	return err
+}
+
+func (g *GCSCache) key(ctx context.Context, d *repb.Digest) (string, error) {
+	hash, err := digest.Validate(d)
+	if err != nil {
+		return "", err
+	}
+	userPrefix, err := prefix.UserPrefixFromContext(ctx)
+	if err != nil {
+		return "", err
+	}
+	return userPrefix + g.prefix + hash, nil
+}
+
+func (g *GCSCache) WithPrefix(prefix string) interfaces.Cache {
+	newPrefix := filepath.Join(append(filepath.SplitList(g.prefix), prefix)...)
+	if len(newPrefix) > 0 && newPrefix[len(newPrefix)-1] != '/' {
+		newPrefix += "/"
+	}
+
+	return &GCSCache{
+		gcsClient:    g.gcsClient,
+		bucketHandle: g.bucketHandle,
+		projectID:    g.projectID,
+		ttlInDays:    g.ttlInDays,
+		prefix:       newPrefix,
+	}
+}
+
+func (g *GCSCache) Get(ctx context.Context, d *repb.Digest) ([]byte, error) {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := g.bucketHandle.Object(k).NewReader(ctx)
+	if err != nil {
+		if err == storage.ErrObjectNotExist {
+			return nil, status.NotFoundErrorf("Digest '%s/%d' not found in cache", d.GetHash(), d.GetSizeBytes())
+		}
+		return nil, err
+	}
+	return ioutil.ReadAll(reader)
+}
+
+func (g *GCSCache) GetMulti(ctx context.Context, digests []*repb.Digest) (map[*repb.Digest][]byte, error) {
+	lock := sync.RWMutex{} // protects(foundMap)
+	foundMap := make(map[*repb.Digest][]byte, len(digests))
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, d := range digests {
+		fetchFn := func(d *repb.Digest) {
+			eg.Go(func() error {
+				data, err := g.Get(ctx, d)
+				if err != nil {
+					return err
+				}
+				lock.Lock()
+				defer lock.Unlock()
+				foundMap[d] = data
+				return nil
+			})
+		}
+		fetchFn(d)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	return foundMap, nil
+}
+
+func swallowGCSAlreadyExistsError(err error) error {
+	if err != nil {
+		if gerr, ok := err.(*googleapi.Error); ok {
+			if gerr.Code == http.StatusPreconditionFailed {
+				return nil
+			}
+			// When building with some languages, like Java, certain
+			// files like MANIFEST files which are identical across
+			// all actions can be uploaded. Many concurrent actions
+			// means that these files can be written simultaneously
+			// which triggers http.StatusTooManyRequests. Because
+			// the cache is a CAS, we assume that writing the same
+			// hash over and over again is just writing the same
+			// file, and we swallow the error here.
+			if gerr.Code == http.StatusTooManyRequests {
+				return nil
+			}
+		}
+	}
+	return err
+}
+
+func (g *GCSCache) Set(ctx context.Context, d *repb.Digest, data []byte) error {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return err
+	}
+	for i := 0; i <= numRetries; i += 1 {
+		obj := g.bucketHandle.Object(k)
+		writer := obj.If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
+		setChunkSize(d, writer)
+		if _, err = writer.Write(data); err == nil {
+			err = swallowGCSAlreadyExistsError(writer.Close())
+		}
+		if isRetryableGCSError(err) {
+			continue
+		}
+		break
+	}
+	return err
+}
+
+func (g *GCSCache) SetMulti(ctx context.Context, kvs map[*repb.Digest][]byte) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for d, data := range kvs {
+		setFn := func(d *repb.Digest, data []byte) {
+			eg.Go(func() error {
+				return g.Set(ctx, d, data)
+			})
+		}
+		setFn(d, data)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *GCSCache) Delete(ctx context.Context, d *repb.Digest) error {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return err
+	}
+	return g.bucketHandle.Object(k).Delete(ctx)
+}
+
+func (g *GCSCache) bumpTTLIfStale(ctx context.Context, key string, t time.Time) bool {
+	if int64(time.Since(t).Hours()) < 24*g.ttlInDays/2 {
+		return true
+	}
+	obj := g.bucketHandle.Object(key)
+	_, err := obj.CopierFrom(obj).Run(ctx)
+	if err == storage.ErrObjectNotExist {
+		return false
+	}
+	if err != nil {
+		log.Printf("Error bumping TTL for key %s: %s", key, err.Error())
+	}
+	return true
+}
+
+func (g *GCSCache) Contains(ctx context.Context, d *repb.Digest) (bool, error) {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return false, err
+	}
+	finalErr := error(nil)
+	for i := 0; i <= numRetries; i += 1 {
+		attrs, err := g.bucketHandle.Object(k).Attrs(ctx)
+		finalErr = err
+		if err == storage.ErrObjectNotExist {
+			return false, nil
+		} else if err == nil {
+			return g.bumpTTLIfStale(ctx, k, attrs.Created), nil
+		} else if isRetryableGCSError(err) {
+			log.Printf("Retrying GCS exists, err: %s", err.Error())
+			continue
+		}
+		break
+	}
+	return false, finalErr
+}
+
+func (g *GCSCache) ContainsMulti(ctx context.Context, digests []*repb.Digest) (map[*repb.Digest]bool, error) {
+	lock := sync.RWMutex{} // protects(foundMap)
+	foundMap := make(map[*repb.Digest]bool, len(digests))
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, d := range digests {
+		fetchFn := func(d *repb.Digest) {
+			eg.Go(func() error {
+				exists, err := g.Contains(ctx, d)
+				if err != nil {
+					return err
+				}
+				lock.Lock()
+				defer lock.Unlock()
+				foundMap[d] = exists
+				return nil
+			})
+		}
+		fetchFn(d)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	return foundMap, nil
+}
+
+func (g *GCSCache) Reader(ctx context.Context, d *repb.Digest, offset int64) (io.Reader, error) {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	reader, err := g.bucketHandle.Object(k).NewReader(ctx)
+	if err != nil {
+		if err == storage.ErrObjectNotExist {
+			return nil, status.NotFoundErrorf("Digest '%s/%d' not found in cache", d.GetHash(), d.GetSizeBytes())
+		}
+		return nil, err
+	}
+	return reader, nil
+}
+
+func isRetryableGCSError(err error) bool {
+	if err != nil {
+		if gerr, ok := err.(*googleapi.Error); ok {
+			switch gerr.Code {
+			case http.StatusServiceUnavailable: // 503
+			case http.StatusBadGateway: // 502
+				log.Printf("Saw a retryable error: %s", err.Error())
+				return true
+			default:
+				return false
+			}
+		}
+	}
+	return false
+}
+
+type gcsDedupingWriteCloser struct {
+	io.WriteCloser
+}
+
+func (wc *gcsDedupingWriteCloser) Write(in []byte) (int, error) {
+	n, err := wc.WriteCloser.Write(in)
+
+	for i := 0; isRetryableGCSError(err) && i < numRetries; i += 1 {
+		log.Printf("Retrying GCS write after error: %s", err.Error())
+		n, err = wc.WriteCloser.Write(in)
+	}
+
+	return n, err
+}
+
+func (wc *gcsDedupingWriteCloser) Close() error {
+	return swallowGCSAlreadyExistsError(wc.WriteCloser.Close())
+}
+
+func setChunkSize(d *repb.Digest, w *storage.Writer) {
+	switch size := d.GetSizeBytes(); {
+	case size < 8*1000*1000:
+		{
+			w.ChunkSize = int(size)
+		}
+	default:
+		w.ChunkSize = googleapi.DefaultUploadChunkSize
+	}
+}
+
+func (g *GCSCache) Writer(ctx context.Context, d *repb.Digest) (io.WriteCloser, error) {
+	k, err := g.key(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	obj := g.bucketHandle.Object(k)
+	writer := obj.If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
+	setChunkSize(d, writer)
+	return &gcsDedupingWriteCloser{writer}, nil
+}
+
+func (g *GCSCache) Start() error {
+	return nil
+}
+
+func (g *GCSCache) Stop() error {
+	return nil
+}
---
+++ enterprise/server/backends/pubsub/pubsub.go
@@ -0,0 +1,57 @@
+package pubsub
+
+import (
+	"context"
+
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+
+	redis "github.com/go-redis/redis/v8"
+)
+
+type PubSub struct {
+	rdb *redis.Client
+}
+
+func NewPubSub(redisServer string) *PubSub {
+	return &PubSub{
+		rdb: redis.NewClient(&redis.Options{
+			Addr: redisServer,
+		}),
+	}
+}
+
+func (p *PubSub) Publish(ctx context.Context, channelName string, message string) error {
+	return p.rdb.Publish(ctx, channelName, message).Err()
+}
+
+// To prevent resource leakage, you should close the subscriber when done.
+// For example:
+//  subscriber := ps.Subscribe(ctx, channelName)
+//  defer subscriber.Close()
+//  for m := range subscriber.Chan() {
+//    // GOT CALLBACK!
+//  }
+func (p *PubSub) Subscribe(ctx context.Context, channelName string) interfaces.Subscriber {
+	return &Subscriber{
+		ps: p.rdb.Subscribe(ctx, channelName),
+	}
+}
+
+type Subscriber struct {
+	ps *redis.PubSub
+}
+
+func (s *Subscriber) Close() error {
+	return s.ps.Close()
+}
+
+func (s *Subscriber) Chan() <-chan string {
+	internalChannel := s.ps.Channel()
+	externalChannel := make(chan string)
+	go func() {
+		for m := range internalChannel {
+			externalChannel <- m.Payload
+		}
+	}()
+	return externalChannel
+}
