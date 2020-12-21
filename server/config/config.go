98fa27f81b0c516163a0cdfca5609cb0929e1529
Tyler Williams <williams.tyler@gmail.com>
2020-12-20 21:17:07 -0800

    Allow configuring distributed disk cache backend.

--- server/config/config.go
+++ server/config/config.go
@@ -104,14 +104,22 @@ type S3CacheConfig struct {
 	TTLDays            int64  `yaml:"ttl_days" usage:"The period after which cache files should be TTLd. Disabled if 0."`
 }
 
+type DistributedDiskConfig struct {
+	RootDirectory string `yaml:"root_directory" usage:"The root directory to store all blobs in, if using disk based storage."`
+	ListenAddr    string `yaml:"listen_addr" usage:"The address to listen for local BuildBuddy distributed cache traffic on."`
+	RedisTarget   string `yaml:"redis_target" usage:"A redis target for improved Caching/RBE performance. ** Enterprise only **"`
+	GroupName     string `yaml:"group_name" usage:"A unique name for this distributed cache group. ** Enterprise only **"`
+}
+
 type cacheConfig struct {
-	Disk            DiskConfig     `yaml:"disk"`
-	GCS             GCSCacheConfig `yaml:"gcs"`
-	S3              S3CacheConfig  `yaml:"s3"`
-	InMemory        bool           `yaml:"in_memory" usage:"Whether or not to use the in_memory cache."`
-	MaxSizeBytes    int64          `yaml:"max_size_bytes" usage:"How big to allow the cache to be (in bytes)."`
-	MemcacheTargets []string       `yaml:"memcache_targets" usage:"Deprecated. Use Redis Target instead."`
-	RedisTarget     string         `yaml:"redis_target" usage:"A redis target for improved Caching/RBE performance. ** Enterprise only **"`
+	Disk            DiskConfig            `yaml:"disk"`
+	GCS             GCSCacheConfig        `yaml:"gcs"`
+	S3              S3CacheConfig         `yaml:"s3"`
+	DistributedDisk DistributedDiskConfig `yaml:"distributed_disk"`
+	InMemory        bool                  `yaml:"in_memory" usage:"Whether or not to use the in_memory cache."`
+	MaxSizeBytes    int64                 `yaml:"max_size_bytes" usage:"How big to allow the cache to be (in bytes)."`
+	MemcacheTargets []string              `yaml:"memcache_targets" usage:"Deprecated. Use Redis Target instead."`
+	RedisTarget     string                `yaml:"redis_target" usage:"A redis target for improved Caching/RBE performance. ** Enterprise only **"`
 }
 
 type authConfig struct {
@@ -382,6 +390,13 @@ func (c *Configurator) GetCacheGCSConfig() *GCSCacheConfig {
 func (c *Configurator) GetCacheS3Config() *S3CacheConfig {
 	if c.gc.Cache.S3.Bucket != "" {
 		return &c.gc.Cache.S3
+	}
+	return nil
+}
+
+func (c *Configurator) GetDistributedDiskConfig() *DistributedDiskConfig {
+	if c.gc.Cache.DistributedDisk.RootDirectory != "" {
+		return &c.gc.Cache.DistributedDisk
 	}
 	return nil
 }
