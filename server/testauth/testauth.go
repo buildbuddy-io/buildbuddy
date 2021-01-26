6e82695c01ba29e1e41bad89e67e071ce6b9e84f
Tyler Williams <williams.tyler@gmail.com>
2021-01-25 17:13:35 -0800

    Add testauth, to enable testing of authenticated flows.

---
+++ server/testauth/testauth.go
@@ -0,0 +1,129 @@
+package testauth
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/tables"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+
+	"google.golang.org/grpc/metadata"
+)
+
+// Do not use this authenticator in production. It is useful
+// for *testing* flows that require authentication. In your test,
+// create a new TestAuthenticator with some known set of users
+// and then test that they are used.
+//
+// Example:
+//   authenticator := testauth.NewTestAuthenticator(testauth.TestUsers("USER1", "GROUP1"))
+//   testEnv.SetAuthenticator(authenticator)
+//   ... test code that uses auth ...
+//
+
+const (
+	testAuthenticationHeader = "test-auth-header"
+
+	TestApiKeyHeader = "test-buildbuddy-api-key"
+)
+
+type TestUser struct {
+	UserID        string   `json:"user_id"`
+	GroupID       string   `json:"group_id"`
+	AllowedGroups []string `json:"allowed_groups"`
+}
+
+func (c *TestUser) GetUserID() string          { return c.UserID }
+func (c *TestUser) GetGroupID() string         { return c.GroupID }
+func (c *TestUser) GetAllowedGroups() []string { return c.AllowedGroups }
+func (c *TestUser) IsAdmin() bool              { return false }
+
+// TestUsers creates a map of test users from arguments of the form:
+// user_id1, group_id1, user_id2, group_id2, ..., user_idN, group_idN
+func TestUsers(vals ...string) map[string]*TestUser {
+	if len(vals)%2 != 0 {
+		log.Printf("You're calling TestUsers wrong!")
+	}
+	testUsers := make(map[string]*TestUser, 0)
+	var u *TestUser
+	for i, val := range vals {
+		if i%2 == 0 {
+			u = &TestUser{UserID: val}
+		} else {
+			u.GroupID = val
+			u.AllowedGroups = []string{val}
+			testUsers[u.UserID] = u
+		}
+	}
+	return testUsers
+}
+
+type TestAuthenticator struct {
+	testUsers map[string]*TestUser
+}
+
+func NewTestAuthenticator(testUsers map[string]*TestUser) *TestAuthenticator {
+	return &TestAuthenticator{
+		testUsers: testUsers,
+	}
+}
+
+func (a *TestAuthenticator) AuthenticateHTTPRequest(w http.ResponseWriter, r *http.Request) context.Context {
+	headerVal := r.Header.Get(TestApiKeyHeader)
+	user, ok := a.testUsers[headerVal]
+	if ok {
+		log.Printf("HTTP: Set value in context!")
+		return context.WithValue(r.Context(), testAuthenticationHeader, user)
+	}
+	return r.Context()
+}
+
+func (a *TestAuthenticator) AuthenticateGRPCRequest(ctx context.Context) context.Context {
+	if grpcMD, ok := metadata.FromIncomingContext(ctx); ok {
+		headerVals := grpcMD[TestApiKeyHeader]
+		for _, headerVal := range headerVals {
+			user, ok := a.testUsers[headerVal]
+			if ok {
+				log.Printf("GRPC: Set value in context!")
+				return context.WithValue(ctx, testAuthenticationHeader, user)
+			}
+		}
+	}
+	return ctx
+}
+
+func (a *TestAuthenticator) AuthenticatedUser(ctx context.Context) (interfaces.UserInfo, error) {
+	log.Printf("AuthenticatedUSER")
+	uVal := ctx.Value(testAuthenticationHeader)
+	u, ok := uVal.(interfaces.UserInfo)
+	if ok {
+		return u, nil
+	}
+	return nil, status.PermissionDeniedError("User not found")
+}
+
+func (a *TestAuthenticator) FillUser(ctx context.Context, user *tables.User) error {
+	return nil
+}
+
+func (a *TestAuthenticator) Login(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", 301)
+}
+
+func (a *TestAuthenticator) Auth(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", 301)
+}
+
+func (a *TestAuthenticator) Logout(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", 301)
+}
+
+func (a *TestAuthenticator) ParseAPIKeyFromString(input string) string {
+	return ""
+}
+
+func (a *TestAuthenticator) AuthContextFromAPIKey(ctx context.Context, apiKey string) context.Context {
+	return ctx
+}
