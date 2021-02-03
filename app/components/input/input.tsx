b8e05bd4d3d22c9c5b5d83c7da530898aca76aef
Brandon Duffany <brandon@buildbuddy.io>
2021-02-03 11:52:53 -0500

    Support password in inputs and improve placeholder style

--- app/components/input/input.tsx
+++ app/components/input/input.tsx
@@ -9,9 +9,4 @@ export const TextInput = React.forwardRef((props: TextInputProps, ref: React.Ref
   return <input ref={ref} type={type || "text"} className={`text-input ${className}`} {...rest} />;
 });
 
-export const PasswordInput = React.forwardRef((props: TextInputProps, ref: React.Ref<HTMLInputElement>) => {
-  const { className, ...rest } = props;
-  return <input ref={ref} type="password" className={`text-input ${className}`} {...rest} />;
-});
-
 export default TextInput;
