f8fafabf893334322c44578143ba3723c0df5aa2
Brandon Duffany <brandon@buildbuddy.io>
2021-02-05 16:00:14 -0500

    Improve SQL formatting

--- enterprise/server/auth/auth.go
+++ enterprise/server/auth/auth.go
@@ -370,7 +370,7 @@ func (a *OpenIDAuthenticator) lookupAPIKeyGroupFromAPIKey(apiKey string) (*apiKe
 	akg := &apiKeyGroup{}
 	err := dbHandle.TransactionWithOptions(db.StaleReadOptions(), func(tx *gorm.DB) error {
 		existingRow := tx.Raw(`
-			SELECT ak.value as key, ak.capabilities, g.group_id
+			SELECT ak.value AS key, ak.capabilities, g.group_id
 			FROM Groups AS g, APIKeys AS ak
 			WHERE g.group_id = ak.group_id AND ak.value = ?`,
 			apiKey)
@@ -393,8 +393,8 @@ func (a *OpenIDAuthenticator) lookupAPIKeyGroupFromBasicAuth(login, pass string)
 	akg := &apiKeyGroup{}
 	err := dbHandle.TransactionWithOptions(db.StaleReadOptions(), func(tx *gorm.DB) error {
 		existingRow := tx.Raw(`
-			SELECT ak.value as key, ak.capabilities, g.group_id
-			FROM Groups as g, APIKeys as ak
+			SELECT ak.value AS key, ak.capabilities, g.group_id
+			FROM Groups AS g, APIKeys AS ak
 			WHERE g.group_id = ? AND g.write_token = ? AND g.group_id = ak.group_id`,
 			login, pass)
 		return existingRow.Scan(akg).Error
