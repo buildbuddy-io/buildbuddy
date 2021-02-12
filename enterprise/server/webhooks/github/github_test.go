d9bd03254c0710dac26f34edc30d48055fe51b23
Brandon Duffany <brandon@buildbuddy.io>
2021-02-12 15:25:59 -0500

    Remove PR tests

--- enterprise/server/webhooks/github/github_test.go
+++ enterprise/server/webhooks/github/github_test.go
@@ -3,13 +3,14 @@ package github
 import (
 	"bytes"
 	"fmt"
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/webhooks/webhook_data"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/webhooks/webhook_data"
+	"github.com/stretchr/testify/assert"
 )
 
-func makeRequest(t *testing.T, eventType string, payload []byte) *http.Request {
+func webhookRequest(t *testing.T, eventType string, payload []byte) *http.Request {
 	req, err := http.NewRequest("POST", "https://buildbuddy.io/webhooks/foo", bytes.NewReader(payload))
 	if err != nil {
 		t.Fatal(err)
@@ -23,32 +24,19 @@ func testData(relativePath string) []byte {
 	return Data[fmt.Sprintf("enterprise/server/webhooks/github/%s", relativePath)]
 }
 
-func TestParseRequest_ValidPullRequestEvent_Success(t *testing.T) {
-	req := makeRequest(t, "pull_request", testData("test_data/pull_request_event.txt"))
-
-	data, err := ParseRequest(req)
-
-	assert.NoError(t, err)
-	assert.Equal(t, &webhook_data.WebhookData{
-		Repo: "https://github.com/test/hello_bb_ci.git",
-		SHA:  "21006e203e433034cd4d82859d28d3bc1dbdf9f7",
-	}, data)
-}
-
 func TestParseRequest_ValidPushEvent_Success(t *testing.T) {
-	req := makeRequest(t, "push", testData("test_data/push_event.txt"))
+	req := webhookRequest(t, "push", testData("test_data/push_event.txt"))
 
 	data, err := ParseRequest(req)
 
 	assert.NoError(t, err)
 	assert.Equal(t, &webhook_data.WebhookData{
-		Repo: "https://github.com/test/hello_bb_ci.git",
-		SHA:  "258044d28288d5f6f1c5928b0e22580296fec666",
+		SHA: "258044d28288d5f6f1c5928b0e22580296fec666",
 	}, data)
 }
 
 func TestParseRequest_InvalidEvent_Error(t *testing.T) {
-	req := makeRequest(t, "pull_request", []byte{})
+	req := makeRequest(t, "push", []byte{})
 
 	data, err := ParseRequest(req)
 
