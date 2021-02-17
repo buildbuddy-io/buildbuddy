ea04d83447ee3c827d2f9a53cf0cd88a4728311a
Brandon Duffany <brandon@buildbuddy.io>
2021-02-16 20:36:44 -0500

    Update comment

--- enterprise/server/workflow/workflow.go
+++ enterprise/server/workflow/workflow.go
@@ -327,6 +327,9 @@ func parseRequest(r *http.Request) (*webhook_data.WebhookData, error) {
 	if r.Header.Get("X-Github-Event") != "" {
 		return github.ParseRequest(r)
 	}
+
+	// TODO: Support non-GitHub providers
+
 	return nil, status.UnimplementedErrorf("failed to classify Git provider from webhook request: %+v", r)
 }
 
