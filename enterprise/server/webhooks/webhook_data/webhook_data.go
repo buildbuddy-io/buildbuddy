a6668f7144cec071566d6f3a05c3e532a9f74e61
Brandon Duffany <brandon@buildbuddy.io>
2021-02-17 20:34:58 -0500

    Update documentation

--- enterprise/server/webhooks/webhook_data/webhook_data.go
+++ enterprise/server/webhooks/webhook_data/webhook_data.go
@@ -7,8 +7,6 @@ type WebhookData struct {
 	//
 	// This will be different from the workflow repo if the workflow is run on a forked
 	// repo as part of a pull request.
-	//
-	// If not explicitly provided, the workflow repo URL will be used.
 	RepoURL string
 	// SHA of the commit to be checked out.
 	SHA string
