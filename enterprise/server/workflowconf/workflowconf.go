3e9d90a8b236ea48924e5b9298975bd347a1fe14
Brandon Duffany <brandon@buildbuddy.io>
2021-02-19 17:07:57 -0500

    Add buildbuddy.yaml config schema

---
+++ enterprise/server/workflowconf/workflowconf.go
@@ -0,0 +1,43 @@
+package workflowconf
+
+import (
+	"io"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type BuildBuddyConfig struct {
+	Actions []*Action `yaml:"actions"`
+}
+
+type Action struct {
+	Name          string    `yaml:"name"`
+	Triggers      *Triggers `yaml:"triggers"`
+	BazelCommands []string  `yaml:"bazel_commands"`
+}
+
+type Triggers struct {
+	Push        *PushTrigger        `yaml:"push"`
+	PullRequest *PullRequestTrigger `yaml:"pull_request"`
+}
+
+type PushTrigger struct {
+	Branches []string `yaml:"branches"`
+}
+
+type PullRequestTrigger struct {
+	Branches []string `yaml:"branches"`
+}
+
+func NewConfig(r io.Reader) (*BuildBuddyConfig, error) {
+	byt, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &BuildBuddyConfig{}
+	if err := yaml.Unmarshal(byt, cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
---
+++ enterprise/server/workflowconf/workflowconf_test.go
@@ -0,0 +1,42 @@
+package workflowconf
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testDataReader(t *testing.T, relPath string) io.Reader {
+	data, ok := Data["enterprise/server/workflowconf/"+relPath]
+	if !ok {
+		t.Fatalf("no such file: %q", relPath)
+	}
+	return bytes.NewReader(data)
+}
+
+func TestWorkflowConf_Parse_FullFeaturedConfig_Valid(t *testing.T) {
+	conf, err := NewConfig(testDataReader(t, "test_data/basic.yaml"))
+
+	assert.NoError(t, err)
+	assert.Equal(t, &BuildBuddyConfig{
+		Actions: []*Action{
+			{
+				Name: "CI",
+				Triggers: &Triggers{
+					Push: &PushTrigger{
+						Branches: []string{"main"},
+					},
+					PullRequest: &PullRequestTrigger{
+						Branches: []string{"main"},
+					},
+				},
+				BazelCommands: []string{
+					"build //...",
+					"test //...",
+				},
+			},
+		},
+	}, conf)
+}
