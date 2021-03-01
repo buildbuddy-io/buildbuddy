acc6c6b8293a517b8473e99ac2ac9c33b6d033de
Brandon Duffany <brandon@buildbuddy.io>
2021-03-01 14:18:08 -0500

    Revert webhook_data changes

--- enterprise/server/webhooks/webhook_data/webhook_data.go
+++ enterprise/server/webhooks/webhook_data/webhook_data.go
@@ -1,17 +1,7 @@
 package webhook_data
 
-// EventSource identifies the source associated with the webhook event.
-type EventSource string
-
-const (
-	GitHub EventSource = "github.com"
-)
-
-// WebhookData represents the data extracted from a Webhook event.
+// WebhookData represents the data required to run a workflow.
 type WebhookData struct {
-	// Source identifies the source of the event.
-	Source EventSource
-
 	// RepoURL points to the canonical repo URL containing the sources needed for the
 	// workflow.
 	//
@@ -20,20 +10,4 @@ type WebhookData struct {
 	RepoURL string
 	// SHA of the commit to be checked out.
 	SHA string
-
-	// Event-specific fields. Only one of the following are set, depending on the
-	// event that was received:
-
-	PullRequest *PullRequestEvent
-	Push        *PushEvent
-}
-
-type PullRequestEvent struct {
-	// BaseBranch is the branch in the repo into which the PR is being merged.
-	BaseBranch string
-}
-
-type PushEvent struct {
-	// Branch that was pushed.
-	Branch string
 }
