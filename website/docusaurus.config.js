25bf1aecdd55c62ec8b3d37f7f55aacdc81c24e1
Siggi <siggisim@gmail.com>
2021-03-09 13:29:17 -0800

    Add a little detail to the README

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -175,13 +175,11 @@ module.exports = {
         docs: {
           path: '../docs',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-          'https://github.com/buildbuddy-io/buildbuddy/edit/master/',
+          editUrl: 'https://github.com/buildbuddy-io/buildbuddy/edit/master/',
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            'https://github.com/buildbuddy-io/buildbuddy/edit/master/',
+          editUrl: 'https://github.com/buildbuddy-io/buildbuddy/edit/master/',
         },
         theme: {
           customCss: [
