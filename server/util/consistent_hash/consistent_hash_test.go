dab8b939da0e4b546bc18ceaa76ae4953ffc56ec
Tyler Williams <williams.tyler@gmail.com>
2021-03-17 11:24:34 -0700

    Increase distributed disk heartbeat timeout.

---
+++ server/util/consistent_hash/consistent_hash_test.go
@@ -0,0 +1,46 @@
+package consistent_hash_test
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/random"
+	"github.com/buildbuddy-io/buildbuddy/server/util/consistent_hash"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestNodesetOrderIndependence(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	assert := assert.New(t)
+	ch := consistent_hash.NewConsistentHash()
+
+	hosts := make([]string, 0)
+	for i := 0; i < 10; i++ {
+		r, err := random.RandomString(5)
+		assert.Nil(err)
+		hosts = append(hosts, fmt.Sprintf("%s:%d", r, 1000+i))
+	}
+
+	ch.Set(hosts...)
+
+	mappings := make(map[string]string, 0)
+	for i := 0; i < 1000; i++ {
+		r, err := random.RandomString(64)
+		assert.Nil(err)
+		mappings[r] = ch.Get(r)
+	}
+
+	rand.Shuffle(len(hosts), func(i, j int) {
+		hosts[i], hosts[j] = hosts[j], hosts[i]
+	})
+	ch.Set(hosts...)
+
+	for d, host := range mappings {
+		assert.Equal(host, ch.Get(d))
+		log.Printf("d %q => host %q", d, host)
+	}
+}
