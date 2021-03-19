43c014ef6431e730aa95d77a0ea25679ed754b5c
Vadim Berezniker <vadim@buildbuddy.io>
2021-03-19 11:55:14 -0700

    Rename test digest package to avoid name clash with non-test package.

--- server/backends/disk_cache/disk_cache_test.go
+++ server/backends/disk_cache/disk_cache_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testauth"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testdigest"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
 	"github.com/buildbuddy-io/buildbuddy/server/util/disk"
 	"github.com/buildbuddy-io/buildbuddy/server/util/prefix"
@@ -19,7 +20,6 @@ import (
 	"golang.org/x/sync/errgroup"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
-	testdigest "github.com/buildbuddy-io/buildbuddy/server/testutil/digest"
 )
 
 var (
--- server/testutil/testdigest/testdigest.go
+++ server/testutil/testdigest/testdigest.go
@@ -1,4 +1,4 @@
-package digest
+package testdigest
 
 import (
 	"bytes"
