90a41a28a0f7e21d81bc904ca45915df17a5d720
Brandon Duffany <brandon@buildbuddy.io>
2021-03-22 10:49:16 -0400

    Check whether perl exists, just to be safe

--- workspace_status.sh
+++ workspace_status.sh
@@ -12,7 +12,9 @@
 
 set -eo pipefail # exit immediately if any command fails.
 
-function remove_url_credentials() { perl -pe 's#//.*?:.*?@#//#'; }
+function remove_url_credentials() {
+  which perl >/dev/null && perl -pe 's#//.*?:.*?@#//#' || cat
+}
 
 repo_url=$(git config --get remote.origin.url | remove_url_credentials)
 echo "REPO_URL $repo_url"
