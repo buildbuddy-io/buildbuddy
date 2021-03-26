13e79814deb25d0a60ba39a171a8ea17039e01f4
Vadim Berezniker <vadim@buildbuddy.io>
2021-03-26 16:05:15 -0700

    Add a Redis list-based PubSub implementation.

--- enterprise/server/backends/pubsub/pubsub.go
+++ enterprise/server/backends/pubsub/pubsub.go
@@ -2,6 +2,8 @@ package pubsub
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 
@@ -12,6 +14,9 @@ type PubSub struct {
 	rdb *redis.Client
 }
 
+// NewPubSub creates a PubSub client based on the built-in Redis pubsub commands.
+// Note that this mechanism is "lossy" in the sense that published messages are lost if there are no listeners.
+// See NewListPubSub for a Redis list-based implementation that retains messages even if there are no subscribers.
 func NewPubSub(redisClient *redis.Client) *PubSub {
 	return &PubSub{
 		rdb: redisClient,
@@ -52,4 +57,87 @@ func (s *Subscriber) Chan() <-chan string {
 		}
 	}()
 	return externalChannel
+}
+
+const (
+	listTTL = 15 * time.Minute
+	// Active subscribe will refresh the list TTL periodically at this interval.
+	ttlRefreshInterval = 5 * time.Minute
+)
+
+type ListPubSub struct {
+	rdb *redis.Client
+}
+
+// NewListPubSub creates a PubSub client based on a Redis-list.
+// Publishes are done via RPUSH and subscribes are performed via BLPOP.
+// The underlying list TTL is automatically extended to 15 minutes whenever a new value is published and periodically
+// by active subscribers.
+func NewListPubSub(redisClient *redis.Client) interfaces.PubSub {
+	return &ListPubSub{
+		rdb: redisClient,
+	}
+}
+
+func (p *ListPubSub) Publish(ctx context.Context, channelName string, message string) error {
+	pipe := p.rdb.TxPipeline()
+	pipe.RPush(ctx, channelName, message)
+	pipe.Expire(ctx, channelName, listTTL)
+	_, err := pipe.Exec(ctx)
+	return err
+}
+
+type listSubscriber struct {
+	cancel context.CancelFunc
+	ch     <-chan string
+}
+
+func (s *listSubscriber) Close() error {
+	s.cancel()
+	return nil
+}
+
+func (s *listSubscriber) Chan() <-chan string {
+	return s.ch
+}
+
+func (p *ListPubSub) Subscribe(ctx context.Context, channelName string) interfaces.Subscriber {
+	ctx, cancel := context.WithCancel(ctx)
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		for {
+			vals, err := p.rdb.BLPop(ctx, ttlRefreshInterval, channelName).Result()
+			// Timeout.
+			if err == redis.Nil {
+				if err := p.rdb.Expire(ctx, channelName, listTTL).Err(); err != nil {
+					log.Printf("Was not able to refresh TTL for %q: %v", channelName, err)
+				}
+				continue
+			}
+			if err != nil {
+				if err != context.Canceled {
+					log.Printf("Error executing BLPop: %v", err)
+				}
+				return
+			}
+
+			// Should not happen.
+			if vals == nil || len(vals) != 2 {
+				log.Printf("Wrong number of elemenents returned from BLPop: %v", vals)
+				return
+			}
+			// ... and neither should this.
+			if vals[0] != channelName {
+				log.Printf("Returned key %q did not match expected key %q", vals[0], channelName)
+				return
+			}
+			ch <- vals[1]
+			return
+		}
+	}()
+	return &listSubscriber{
+		cancel: cancel,
+		ch:     ch,
+	}
 }
