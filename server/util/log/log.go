d703d039d81b36d96929c8cccc1d5bf7b034bec4
Brandon Duffany <brandon@buildbuddy.io>
2021-04-05 13:35:28 -0400

    Revert unnecessary diff

--- server/util/log/log.go
+++ server/util/log/log.go
@@ -14,6 +14,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
