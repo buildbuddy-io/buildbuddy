bb477c0a7cb8730824e1286914e8f7b5a4b0ccb3
Brandon Duffany <brandon@buildbuddy.io>
2021-04-28 14:13:12 -0400

    Supporting changes for RBE IO improvements & tests (#361)

--- server/remote_cache/cachetools/cachetools.go
+++ server/remote_cache/cachetools/cachetools.go
@@ -3,14 +3,19 @@ package cachetools
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
+	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/namespace"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/golang/protobuf/proto"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc/codes"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	bspb "google.golang.org/genproto/googleapis/bytestream"
@@ -20,6 +25,7 @@ import (
 
 const (
 	uploadBufSizeBytes = 1000000 // 1MB
+	gRPCMaxSize        = int64(4000000)
 )
 
 // TODO(tylerw): This could probably go into util/ and be used by the BuildBuddy
@@ -267,6 +273,235 @@ func UploadBlobToCAS(ctx context.Context, cache interfaces.Cache, instanceName s
 func UploadProtoToCAS(ctx context.Context, cache interfaces.Cache, instanceName string, in proto.Message) (*repb.Digest, error) {
 	cas := namespace.CASCache(cache, instanceName)
 	return uploadProtoToCache(ctx, cas, instanceName, in)
+}
+
+// BatchCASUploader uploads many files to CAS concurrently, batching small
+// uploads together and falling back to bytestream uploads for large files.
+type BatchCASUploader struct {
+	ctx              context.Context
+	eg               *errgroup.Group
+	cache            interfaces.Cache
+	byteStreamClient bspb.ByteStreamClient
+	casClient        repb.ContentAddressableStorageClient
+	unsentBatchReq   *repb.BatchUpdateBlobsRequest
+	unsentBatchSize  int64
+	instanceName     string
+	uploads          map[digest.Key]struct{}
+}
+
+// NewBatchCASUploader returns an uploader to be used only for the given request
+// context (it should not be used outside the lifecycle of the request).
+func NewBatchCASUploader(ctx context.Context, env environment.Env, instanceName string) (*BatchCASUploader, error) {
+	bsClient := env.GetByteStreamClient()
+	if bsClient == nil {
+		return nil, status.InvalidArgumentError("Missing bytestream client")
+	}
+	casClient := env.GetContentAddressableStorageClient()
+	if casClient == nil {
+		return nil, status.InvalidArgumentError("Missing CAS client")
+	}
+	cache := env.GetCache()
+	if cache == nil {
+		return nil, status.InvalidArgumentError("Missing cache")
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	return &BatchCASUploader{
+		ctx:              ctx,
+		eg:               eg,
+		cache:            cache,
+		byteStreamClient: bsClient,
+		casClient:        casClient,
+		unsentBatchReq:   &repb.BatchUpdateBlobsRequest{InstanceName: instanceName},
+		unsentBatchSize:  0,
+		instanceName:     instanceName,
+		uploads:          make(map[digest.Key]struct{}),
+	}, nil
+}
+
+// Upload adds the given content to the current batch or begins a streaming
+// upload if it exceeds the maximum batch size. It closes r when it is no
+// longer needed.
+func (ul *BatchCASUploader) Upload(d *repb.Digest, r io.ReadSeekCloser) error {
+	// De-dupe uploads by digest.
+	dk := digest.NewKey(d)
+	if _, ok := ul.uploads[dk]; ok {
+		return r.Close()
+	}
+	ul.uploads[dk] = struct{}{}
+
+	r.Seek(0, 0)
+
+	if d.GetSizeBytes() > gRPCMaxSize {
+		ul.eg.Go(func() error {
+			defer r.Close()
+			_, err := UploadFromReader(ul.ctx, ul.byteStreamClient, digest.NewInstanceNameDigest(d, ul.instanceName), r)
+			return err
+		})
+		return nil
+	}
+
+	if ul.unsentBatchSize+d.GetSizeBytes() > gRPCMaxSize {
+		ul.flushCurrentBatch()
+	}
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	if err := r.Close(); err != nil {
+		return err
+	}
+	ul.unsentBatchReq.Requests = append(ul.unsentBatchReq.Requests, &repb.BatchUpdateBlobsRequest_Request{
+		Digest: d,
+		Data:   b,
+	})
+	ul.unsentBatchSize += d.GetSizeBytes()
+	return nil
+}
+
+func (ul *BatchCASUploader) UploadProto(in proto.Message) (*repb.Digest, error) {
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	d, err := digest.Compute(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	if err := ul.Upload(d, NewBytesReadSeekCloser(data)); err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+func (ul *BatchCASUploader) UploadFile(path string) (*repb.Digest, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	d, err := digest.Compute(f)
+	if err != nil {
+		return nil, err
+	}
+	// Note: uploader.Upload will close the file.
+	if err := ul.Upload(d, f); err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+func (ul *BatchCASUploader) flushCurrentBatch() {
+	req := ul.unsentBatchReq
+	ul.unsentBatchReq = &repb.BatchUpdateBlobsRequest{InstanceName: ul.instanceName}
+	ul.unsentBatchSize = 0
+	ul.eg.Go(func() error {
+		rsp, err := ul.casClient.BatchUpdateBlobs(ul.ctx, req)
+		if err != nil {
+			return err
+		}
+		for _, fileResponse := range rsp.GetResponses() {
+			if fileResponse.GetStatus().GetCode() != int32(codes.OK) {
+				return gstatus.Error(codes.Code(fileResponse.GetStatus().GetCode()), fmt.Sprintf("Error uploading file: %v", fileResponse.GetDigest()))
+			}
+		}
+		return nil
+	})
+}
+
+func (ul *BatchCASUploader) Wait() error {
+	if len(ul.unsentBatchReq.GetRequests()) > 0 {
+		ul.flushCurrentBatch()
+	}
+	return ul.eg.Wait()
+}
+
+type bytesReadSeekCloser struct {
+	io.ReadSeeker
+}
+
+func NewBytesReadSeekCloser(b []byte) io.ReadSeekCloser {
+	return &bytesReadSeekCloser{bytes.NewReader(b)}
+}
+func (*bytesReadSeekCloser) Close() error { return nil }
+
+// UploadDirectoryToCAS uploads all the files in a given directory to the CAS
+// as well as the directory structure, and returns the digest of the root
+// Directory proto that can be used to fetch the uploaded contents.
+func UploadDirectoryToCAS(ctx context.Context, env environment.Env, instanceName, rootDirPath string) (*repb.Digest, error) {
+	ul, err := NewBatchCASUploader(ctx, env, instanceName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Recursively find and upload all descendant dirs.
+	visited, rootDirectoryDigest, err := uploadDir(ul, rootDirPath, nil /*=visited*/)
+	if err != nil {
+		return nil, err
+	}
+	if len(visited) == 0 {
+		return nil, status.InternalError("empty directory list after uploading directory tree; this should never happen")
+	}
+	// Upload the tree, which consists of the root dir as well as all descendant
+	// dirs.
+	rootTree := &repb.Tree{Root: visited[0], Children: visited[1:]}
+	if _, err := ul.UploadProto(rootTree); err != nil {
+		return nil, err
+	}
+	if err := ul.Wait(); err != nil {
+		return nil, err
+	}
+	return rootDirectoryDigest, nil
+}
+
+func uploadDir(ul *BatchCASUploader, dirPath string, visited []*repb.Directory) ([]*repb.Directory, *repb.Digest, error) {
+	dir := &repb.Directory{}
+	// Append the directory before doing any other work, so that the root
+	// directory is located at visited[0] at the end of recursion.
+	visited = append(visited, dir)
+	infos, err := os.ReadDir(dirPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	for _, info := range infos {
+		name := info.Name()
+		path := filepath.Join(dirPath, name)
+
+		if info.IsDir() {
+			var d *repb.Digest
+			visited, d, err = uploadDir(ul, path, visited)
+			if err != nil {
+				return nil, nil, err
+			}
+			dir.Directories = append(dir.Directories, &repb.DirectoryNode{
+				Name:   name,
+				Digest: d,
+			})
+		} else if info.Type().IsRegular() {
+			d, err := ul.UploadFile(path)
+			if err != nil {
+				return nil, nil, err
+			}
+			dir.Files = append(dir.Files, &repb.FileNode{
+				Name:         name,
+				Digest:       d,
+				IsExecutable: info.Type()&0100 != 0,
+			})
+		} else if info.Type()&os.ModeSymlink == os.ModeSymlink {
+			target, err := os.Readlink(path)
+			if err != nil {
+				return nil, nil, err
+			}
+			dir.Symlinks = append(dir.Symlinks, &repb.SymlinkNode{
+				Name:   name,
+				Target: target,
+			})
+		}
+	}
+	digest, err := ul.UploadProto(dir)
+	if err != nil {
+		return nil, nil, err
+	}
+	return visited, digest, nil
 }
 
 func UploadProtoToAC(ctx context.Context, cache interfaces.Cache, instanceName string, in proto.Message) (*repb.Digest, error) {
---
+++ server/testutil/testfs/testfs.go
@@ -0,0 +1,115 @@
+package testfs
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/random"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// MakeTempDir creates and returns an empty directory that exists for the scope
+// of a test.
+func MakeTempDir(t testing.TB) string {
+	tmpDir, err := os.MkdirTemp("", "buildbuddy-test-*")
+	if err != nil {
+		assert.FailNow(t, "failed to create temp dir", err)
+	}
+	t.Cleanup(func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			assert.FailNow(t, "failed to clean up temp dir", err)
+		}
+	})
+	return tmpDir
+}
+
+func CopyFile(t testing.TB, src, destRootDir, destPath string) {
+	info, err := os.Stat(src)
+	if err != nil {
+		assert.FailNow(t, "stat failed", err)
+	}
+	b, err := os.ReadFile(src)
+	if err != nil {
+		assert.FailNow(t, "read failed", err)
+	}
+	if err := os.WriteFile(filepath.Join(destRootDir, destPath), b, info.Mode()); err != nil {
+		assert.FailNow(t, "write failed", err)
+	}
+}
+
+func WriteAllFileContents(t testing.TB, rootDir string, contents map[string]string) {
+	for relPath, content := range contents {
+		path := filepath.Join(rootDir, relPath)
+		if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
+			assert.FailNow(t, "failed to create parent dir for file", err)
+		}
+		if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+			assert.FailNow(t, "write failed", err)
+		}
+	}
+}
+
+func WriteRandomString(t testing.TB, rootDir, path string, n int) string {
+	s, err := random.RandomString(n)
+	if err != nil {
+		assert.FailNow(t, "failed to generate random string", err)
+	}
+	if err := os.WriteFile(filepath.Join(rootDir, path), []byte(s), 0644); err != nil {
+		assert.FailNow(t, "write failed", err)
+	}
+	return s
+}
+
+func ReadFileAsString(t testing.TB, rootDir, path string) string {
+	b, err := os.ReadFile(filepath.Join(rootDir, path))
+	if err != nil {
+		assert.FailNow(t, "read failed", err)
+	}
+	return string(b)
+}
+
+func Exists(t testing.TB, rootDir, path string) bool {
+	_, err := os.Stat(filepath.Join(rootDir, path))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		assert.FailNow(t, "stat failed", err)
+	}
+	return true
+}
+
+// AssertExactFileContents checks that the given mapping exactly represents the
+// files in rootDir. The mapping is keyed by path relative to rootDir.
+// Empty dirs and non-regular files (e.g. symlinks) are ignored in the
+// comparison.
+func AssertExactFileContents(t testing.TB, rootDir string, contents map[string]string) {
+	expectedFilePaths := []string{}
+	for k := range contents {
+		expectedFilePaths = append(expectedFilePaths, k)
+	}
+	actualFilePaths := []string{}
+	err := filepath.WalkDir(rootDir, func(path string, entry fs.DirEntry, err error) error {
+		require.NoError(t, err)
+		if !entry.Type().IsRegular() {
+			return nil
+		}
+		relPath := strings.TrimPrefix(path, rootDir+string(os.PathSeparator))
+		actualFilePaths = append(actualFilePaths, relPath)
+		if content, ok := contents[relPath]; ok {
+			actualContent, err := os.ReadFile(path)
+			require.NoError(t, err)
+			assert.Equalf(t, content, string(actualContent), "unexpected contents in %s", relPath)
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	assert.ElementsMatch(
+		t, expectedFilePaths, actualFilePaths,
+		"some files were missing or unexpected files were found",
+	)
+}
