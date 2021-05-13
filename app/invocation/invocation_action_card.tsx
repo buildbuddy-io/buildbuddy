71f2212f7cf3dfbc15f844bc8fd99a981d9b5b6a
Paribartan Parajuli <pari.parajuli@berkeley.edu>
2021-05-13 09:26:26 -0700

    Properly read and decoded Action obj.

--- app/invocation/invocation_action_card.tsx
+++ app/invocation/invocation_action_card.tsx
@@ -2,18 +2,21 @@ import pako from "pako";
 import React from "react";
 import format from "../format/format";
 import InvocationModel from "./invocation_model";
+import { build } from "../../proto/remote_execution_ts_proto";
 import rpcService from "../service/rpc_service";
 
 interface Props {
   model: InvocationModel;
   inProgress: boolean;
-  hash: string;
-  size: Long;
+  search: string;
 }
 
 interface State {
   loading: boolean;
-  contents: string;
+  contents: ArrayBuffer;
+  action_array: Uint8Array;
+  action_obj: build.bazel.remote.execution.v2.Action;
+  error: string;
 }
 
 export default class ActionCardComponent extends React.Component {
@@ -21,29 +24,34 @@ export default class ActionCardComponent extends React.Component {
 
   state: State = {
     loading: false,
-    contents: ""
+    contents: new ArrayBuffer(8),
+    action_array: new Uint8Array(),
+    action_obj: null,
+    error: "",
   };
 
   fetchAction() {
-    let actionFile = "bytestream://localhost:1987/blobs/" + this.props.hash + "/" + String(this.props.size);
+    let actionFile = "bytestream://localhost:1987/blobs/" + this.props.search.substring(1);
+    console.log(actionFile);
     rpcService
-      .fetchBytestreamFile(actionFile, this.props.model.getId())
-      .then((contents: any) => {
-        this.setState({
-            contents: contents,
-            loading: true,
-        })
-      })
-      .catch(() => {
-        console.error("Error loading bytestream timing profile!");
+      .fetchBytestreamFile(actionFile, this.props.model.getId(), "arraybuffer")
+      .then((action_buff: any) => {
+        let temp_array = Uint8Array.from(action_buff);
         this.setState({
           ...this.state,
-          timingLoaded: false,
-          error: "Error loading timing profile. Make sure your cache is correctly configured.",
+          contents: action_buff,
+          action_array: temp_array,
+          action_obj: build.bazel.remote.execution.v2.Action.decode(temp_array),
+          loading: true,
+        });
+      })
+      .catch(() => {
+        console.error("Error loading bytestream action profile!");
+        this.setState({
+          ...this.state,
+          error: "Error loading action profile. Make sure your cache is correctly configured.",
         });
       });
-    
-  
   }
 
   render() {
@@ -51,12 +59,10 @@ export default class ActionCardComponent extends React.Component {
       <div className="card">
         <img className="icon" src="/image/link.svg" />
         <div className="content">
-          <div className="title" onClick={this.fetchAction.bind(this)}>{this.props.hash}/{this.props.size}</div>
-          {this.state.loading && (
-            <div>
-                {this.state.contents}
-            </div>
-          )}
+          <div className="title" onClick={this.fetchAction.bind(this)}>
+            {"bytestream://localhost:1987/blobs/" + this.props.search}
+          </div>
+          {this.state.loading && <div>{build.bazel.remote.execution.v2.Action.verify(this.state.action_obj)}</div>}
         </div>
       </div>
     );
