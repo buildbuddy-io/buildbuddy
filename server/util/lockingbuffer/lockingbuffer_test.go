298b0b3c1cabe3ebfe60af01e45d985324cf2781
Brandon Duffany <brandon@buildbuddy.io>
2021-05-19 20:16:51 -0400

    Add better test for ReadAll

--- server/util/lockingbuffer/lockingbuffer_test.go
+++ server/util/lockingbuffer/lockingbuffer_test.go
@@ -1,7 +1,7 @@
 package lockingbuffer_test
 
 import (
-	"io/ioutil"
+	"io"
 	"math/rand"
 	"regexp"
 	"sync"
@@ -10,10 +10,93 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/server/util/lockingbuffer"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestLockingBuffer_ReadWrite(t *testing.T) {
+	rand.Seed(int64(time.Now().UnixNano()))
+
+	buf := lockingbuffer.New()
+
+	writer := func(val string, writeCount int, done *bool, doneLock *sync.RWMutex) {
+		defer func() {
+			doneLock.Lock()
+			*done = true
+			doneLock.Unlock()
+		}()
+		for i := 0; i < writeCount; i++ {
+			if _, err := buf.Write([]byte(val)); err != nil {
+				t.Fail()
+				return
+			}
+			randDelay()
+		}
+	}
+
+	// Writer A: Write `strACount` copies of `strA` to the buffer
+	strA := "AAA"
+	strACount := 1187
+	writerADone := false
+	doneLock := sync.RWMutex{}
+	go writer(strA, strACount, &writerADone, &doneLock)
+
+	// Writer B: Write `strBCount` copies of `strB` to the buffer
+	strB := "BBBBBBB"
+	strBCount := 919
+	writerBDone := false
+	go writer(strB, strBCount, &writerBDone, &doneLock)
+
+	reader := func(acc *string, done chan struct{}) {
+		defer func() {
+			done <- struct{}{}
+		}()
+		chunk := make([]byte, 128)
+		for {
+			doneLock.RLock()
+			writersDone := writerADone && writerBDone
+			doneLock.RUnlock()
+			n, err := buf.Read(chunk)
+			if err != nil && err != io.EOF {
+				t.Error(err)
+				return
+			}
+			*acc += string(chunk[:n])
+			if writersDone && n == 0 {
+				return
+			}
+			randDelay()
+		}
+	}
+
+	// Readers 1 & 2: continually attempt to read all bytes from the buffer.
+	reader1Acc := ""
+	reader1Done := make(chan struct{}, 1)
+	go reader(&reader1Acc, reader1Done)
+
+	reader2Acc := ""
+	reader2Done := make(chan struct{}, 1)
+	go reader(&reader2Acc, reader2Done)
+
+	<-reader1Done
+	<-reader2Done
+
+	// Assert that we read the correct number of 'A' and 'B'.
+	acc := reader1Acc + reader2Acc
+	assert.Equal(t, strACount*len(strA), charCount(acc, 'A'))
+	assert.Equal(t, strBCount*len(strB), charCount(acc, 'B'))
+	assert.Equal(t, strACount*len(strA)+strBCount*len(strB), len(acc))
+}
+
+func charCount(s string, c byte) int {
+	n := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			n++
+		}
+	}
+	return n
+}
+
+func TestLockingBuffer_ReadAll(t *testing.T) {
 	rand.Seed(int64(time.Now().UnixNano()))
 
 	buf := lockingbuffer.New()
@@ -54,7 +137,7 @@ func TestLockingBuffer_ReadWrite(t *testing.T) {
 			doneLock.RLock()
 			writersDone := writerADone && writerBDone
 			doneLock.RUnlock()
-			b, err := ioutil.ReadAll(buf)
+			b, err := buf.ReadAll()
 			if err != nil {
 				t.Fail()
 				return
@@ -85,24 +168,6 @@ func TestLockingBuffer_ReadWrite(t *testing.T) {
 	assert.Equal(t, strACount, len(regexp.MustCompile(strA).FindAllStringSubmatch(acc, -1)))
 	assert.Equal(t, strBCount, len(regexp.MustCompile(strB).FindAllStringSubmatch(acc, -1)))
 	assert.Equal(t, strACount*len(strA)+strBCount*len(strB), len(acc))
-}
-
-func TestLockingBuffer_ReadAll_DrainsBuffer(t *testing.T) {
-	buf := lockingbuffer.New()
-
-	_, err := buf.Write([]byte("ABC"))
-
-	require.NoError(t, err)
-
-	b, err := buf.ReadAll()
-
-	require.NoError(t, err)
-	require.Equal(t, []byte("ABC"), b)
-
-	b, err = buf.ReadAll()
-
-	require.NoError(t, err)
-	require.Empty(t, b)
 }
 
 func randDelay() {
