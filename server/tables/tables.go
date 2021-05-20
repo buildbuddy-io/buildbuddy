1acf66aa13e595678dadcd399a89b0a9487a03ab
Brandon Duffany <brandon@buildbuddy.io>
2021-05-20 12:38:36 -0400

    task-routing: Add executor instance ID fields to protos and ExecutionNode table (#445)

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -351,6 +351,7 @@ type ExecutionNode struct {
 	SchedulerHostPort     string
 	UserID                string
 	Perms                 int
+	ExecutorID            string
 }
 
 func (n *ExecutionNode) TableName() string {
