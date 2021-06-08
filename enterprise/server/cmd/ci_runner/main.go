2283d8d9fa00f073554af55b8427453e1a7b77f3
Brandon Duffany <brandon@buildbuddy.io>
2021-06-08 11:28:19 -0400

    Fix typo

--- enterprise/server/cmd/ci_runner/main.go
+++ enterprise/server/cmd/ci_runner/main.go
@@ -441,7 +441,7 @@ func (ar *actionRunner) Run(ctx context.Context, startTime time.Time) error {
 		ActionName:          ar.action.Name,
 		ActionTriggerBranch: *triggerBranch,
 		ActionTriggerEvent:  *triggerEvent,
-		WorkflowID:          *workflowID,
+		WorkflowId:          *workflowID,
 	}
 	configuredEvent := &bespb.BuildEvent{
 		Id:      &bespb.BuildEventId{Id: &bespb.BuildEventId_WorkflowConfigured{WorkflowConfigured: &bespb.BuildEventId_WorkflowConfiguredId{}}},
