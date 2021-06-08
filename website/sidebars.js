8ef918335aa9c4492124d928ed84a8a864c12100
Siggi <siggisim@gmail.com>
2021-06-08 16:53:45 -0700

    Add API documentation

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,7 +6,7 @@ module.exports = {
     // TODO: Uncomment when workflows are launched.
     // "Workflows (CI)": ['workflows-setup', 'workflows-config'],
     "Troubleshooting": ['troubleshooting', 'troubleshooting-rbe', 'troubleshooting-slow-upload'],
-    "Enterprise": ['enterprise', 'enterprise-setup', 'enterprise-config', 'enterprise-helm', 'enterprise-rbe', 'enterprise-mac-rbe'],
+    "Enterprise": ['enterprise', 'enterprise-setup', 'enterprise-config', 'enterprise-helm', 'enterprise-rbe', 'enterprise-mac-rbe', 'enterprise-api'],
     "Monitoring": ['prometheus-metrics'],
     "Configuration": ['config', 'config-samples', 'config-app', 'config-database', 'config-storage', 'config-cache', 'config-github', 'config-ssl', 'config-auth', 'config-integrations', 'config-org', 'config-rbe', 'config-misc', 'config-api', 'config-telemetry', 'config-flags'],
   },
