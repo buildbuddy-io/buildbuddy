66ede87a9ab7d88cd5cc332132b9240cd93394e5
Brandon Duffany <brandon@buildbuddy.io>
2021-06-23 14:46:57 -0400

    Remove unnecessary reflection

--- server/util/request_context/request_context.go
+++ server/util/request_context/request_context.go
@@ -2,7 +2,6 @@ package requestcontext
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/golang/protobuf/proto"
 
@@ -25,34 +24,13 @@ func ProtoRequestContextFromContext(ctx context.Context) *ctxpb.RequestContext {
 	return val.(*ctxpb.RequestContext)
 }
 
-func GetProtoRequestContext(req proto.Message) *ctxpb.RequestContext {
-	protoType := reflect.TypeOf(req)
-	for i := 0; i < protoType.NumMethod(); i++ {
-		method := protoType.Method(i)
-		if !isGetRequestContextMethod(method) {
-			continue
-		}
-		args := []reflect.Value{reflect.ValueOf(req)}
-		ctxArr := method.Func.Call(args)
-		return ctxArr[0].Interface().(*ctxpb.RequestContext)
-	}
-	return nil
+type requestContextGetter interface {
+	GetRequestContext() *ctxpb.RequestContext
 }
 
-func isGetRequestContextMethod(m reflect.Method) bool {
-	t := m.Type
-	if t.Kind() != reflect.Func {
-		return false
+func GetProtoRequestContext(req proto.Message) *ctxpb.RequestContext {
+	if req, ok := req.(requestContextGetter); ok {
+		return req.GetRequestContext()
 	}
-	if m.Name != "GetRequestContext" {
-		return false
-	}
-	if t.NumIn() != 1 || t.NumOut() != 1 {
-		return false
-	}
-	// TODO: Figure out why this doesn't work
-	// if !t.Out(0).Implements(reflect.TypeOf((*ctxpb.RequestContext)(nil)).Elem()) {
-	// 	return false
-	// }
-	return true
+	return nil
 }
