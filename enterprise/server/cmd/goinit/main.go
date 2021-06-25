27fec463bd37a807c34ab378616e51a2b0f95690
Tyler Williams <williams.tyler@gmail.com>
2021-06-25 14:57:38 -0700

    comments.

--- enterprise/server/cmd/goinit/main.go
+++ enterprise/server/cmd/goinit/main.go
@@ -9,18 +9,18 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/vsock"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/vmexec"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/rlimit"
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/vmexec"
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/vsock"
-	"google.golang.org/grpc"
 	"golang.org/x/sync/errgroup"
-	
+	"google.golang.org/grpc"
+
 	vmxpb "github.com/buildbuddy-io/buildbuddy/proto/vmexec"
 )
 
 const (
-	logLevel   = "debug"
+	logLevel = "debug"
 
 	commonMountFlags = syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID
 	cgroupMountFlags = syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_RELATIME
@@ -57,7 +57,7 @@ func chroot(path string) error {
 func reapChildren(ctx context.Context) {
 	for {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			break
 		default:
 			// borrowed from https://github.com/driusan/dainit/blob/master/zombie.go
@@ -66,13 +66,13 @@ func reapChildren(ctx context.Context) {
 			// We call it with NOHANG a few times to clear up any backlog, and then make a blocking call until our
 			// next child dies.
 			var status syscall.WaitStatus
-			
+
 			// If there are more than 10 zombies, we likely have other problems.
 			for i := 0; i < 10; i++ {
 				// We don't really care what the pid was that got reaped, or if there's nothing to wait for
 				syscall.Wait4(-1, &status, syscall.WNOHANG, nil)
 			}
-			
+
 			// This blocks, so that we're not spending all of our time reaping processes..
 			syscall.Wait4(-1, &status, 0, nil)
 		}
@@ -83,7 +83,7 @@ func reapChildren(ctx context.Context) {
 // which was very helpful <3!
 func main() {
 	rootContext := context.Background()
-	
+
 	// setup logging
 	opts := log.Opts{
 		Level: logLevel,
@@ -94,10 +94,7 @@ func main() {
 	}
 
 	flag.Parse()
-
-	log.Infof("cmd was: %q", *cmd)
-	log.Infof("path was: %q", *path)
-	log.Infof("args: %s", os.Args)
+	log.Infof("Starting BuildBuddy init (args: %s)", os.Args)
 
 	die(os.MkdirAll("/dev", 0755))
 	die(mount("devtmpfs", "/dev", "devtmpfs", syscall.MS_NOSUID, "mode=0620,gid=5,ptmxmode=666"))
@@ -108,7 +105,7 @@ func main() {
 	die(os.MkdirAll("/newroot/dev", 0755))
 	die(mount("/dev", "/newroot/dev", "", syscall.MS_MOVE, ""))
 
-	log.Printf("switching root!")
+	log.Debugf("switching root!")
 
 	die(chdir("/newroot"))
 	die(mount(".", "/", "", syscall.MS_MOVE, ""))
@@ -205,7 +202,7 @@ func main() {
 		})
 	}
 	eg.Go(func() error {
-		listener, err := vsock.GuestListener(ctx, uint32(*port))
+		listener, err := vsock.NewGuestListener(ctx, uint32(*port))
 		if err != nil {
 			return err
 		}
@@ -215,8 +212,7 @@ func main() {
 		return server.Serve(listener)
 	})
 
-
-	eg.Wait()  // ignore errors.
+	eg.Wait() // ignore errors.
 
 	// Halt the system explicitly to prevent a kernel panic.
 	syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
