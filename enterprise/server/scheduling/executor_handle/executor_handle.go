8084884a500085fdd954f289db0dfc463b310b09
Vadim Berezniker <vadim@buildbuddy.io>
2021-06-28 16:01:05 -0700

    Propagate trace information for streamed task reservations.
    This allows end-to-end tracing for Execute requests in Cloud Trace.

--- enterprise/server/scheduling/executor_handle/executor_handle.go
+++ enterprise/server/scheduling/executor_handle/executor_handle.go
@@ -9,11 +9,13 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/buildbuddy-io/buildbuddy/server/util/tracing"
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 
 	scpb "github.com/buildbuddy-io/buildbuddy/proto/scheduler"
+	tpb "github.com/buildbuddy-io/buildbuddy/proto/trace"
 )
 
 const (
@@ -196,6 +198,13 @@ func (h *registrationAndTasksExecutorHandle) handleTaskReservationResponse(respo
 }
 
 func (h *registrationAndTasksExecutorHandle) EnqueueTaskReservation(ctx context.Context, req *scpb.EnqueueTaskReservationRequest) (*scpb.EnqueueTaskReservationResponse, error) {
+	req, ok := proto.Clone(req).(*scpb.EnqueueTaskReservationRequest)
+	if !ok {
+		log.Errorf("could not clone reservation request")
+		return nil, status.InternalError("could not clone reservation request")
+	}
+	tracing.InjectProtoTraceMetadata(ctx, req.GetTraceMetadata(), func(m *tpb.Metadata) { req.TraceMetadata = m })
+
 	timeout := time.NewTimer(EnqueueTaskReservationTimeout)
 	rspCh := make(chan *scpb.EnqueueTaskReservationResponse, 1)
 	select {
