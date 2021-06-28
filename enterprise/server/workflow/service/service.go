80308cd8c2a898dd55a0b4f43c8c8313885c79b2
Brandon Duffany <brandon@buildbuddy.io>
2021-06-28 17:45:01 -0400

    Fix build

--- enterprise/server/workflow/service/service.go
+++ enterprise/server/workflow/service/service.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/operation"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/platform"
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/webhooks/webhook_data"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/metrics"
