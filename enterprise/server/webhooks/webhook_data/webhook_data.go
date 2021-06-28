20df71f231dd9cd4affd11b931a901f3e10f8158
Brandon Duffany <brandon@buildbuddy.io>
2021-06-28 17:38:46 -0400

    Update IsTrusted logic

--- enterprise/server/webhooks/webhook_data/webhook_data.go
+++ enterprise/server/webhooks/webhook_data/webhook_data.go
@@ -1,7 +1,6 @@
 package webhook_data
 
 import (
-	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 )
 
 var (
@@ -15,9 +14,4 @@ var (
 func init() {
 	EventName.Push = "push"
 	EventName.PullRequest = "pull_request"
-}
-
-// IsTrusted returns whether the event came from a trusted actor.
-func IsTrusted(wd *interfaces.WebhookData) bool {
-	return wd.IsRepoPrivate
 }
