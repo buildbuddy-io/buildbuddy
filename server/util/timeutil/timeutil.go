982fe064ae0afad08209785dc3d97f45d4d583dc
Brandon Duffany <brandon@buildbuddy.io>
2021-07-01 19:43:58 -0400

    Make clearer

--- server/util/timeutil/timeutil.go
+++ server/util/timeutil/timeutil.go
@@ -7,22 +7,22 @@ import (
 
 // FromMillis converts a Unix timestamp in milliseconds to a Time.
 func FromMillis(timestampMillis int64) time.Time {
-	return time.Unix(0, timestampMillis*int64(time.Millisecond))
+	return time.Unix(0, timestampMillis*1e6)
 }
 
 // FromUsec converts a Unix timestamp in microseconds to a Time.
 func FromUsec(timestampUsec int64) time.Time {
-	return time.Unix(0, timestampUsec*int64(time.Microsecond))
+	return time.Unix(0, timestampUsec*1e3)
 }
 
 // ToMillis converts a Time to a timestamp in milliseconds since the Unix epoch.
 func ToMillis(t time.Time) int64 {
-	return t.UnixNano() / int64(time.Millisecond)
+	return t.UnixNano() / 1e6
 }
 
 // ToUsec converts a Time to a timestamp in microseconds since the Unix epoch.
 func ToUsec(t time.Time) int64 {
-	return t.UnixNano() / int64(time.Microsecond)
+	return t.UnixNano() / 1e3
 }
 
 // ShortFormatDuration returns a human-readable, short, and approximate
