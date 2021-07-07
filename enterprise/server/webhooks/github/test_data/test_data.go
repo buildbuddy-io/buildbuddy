9d6f753d781ae91f551ccf9de6f15c6b906d4745
Brandon Duffany <brandon@buildbuddy.io>
2021-07-07 16:51:11 -0400

    workflows: Make test data more readable (#660)

--- enterprise/server/webhooks/github/test_data/test_data.go
+++ enterprise/server/webhooks/github/test_data/test_data.go
@@ -2,8 +2,8 @@ package test_data
 
 import _ "embed"
 
-//go:embed push_event.txt
+//go:embed push_event.json
 var PushEvent []byte
 
-//go:embed pull_request_event.txt
+//go:embed pull_request_event.json
 var PullRequestEvent []byte
