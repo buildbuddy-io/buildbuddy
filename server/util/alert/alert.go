775693c088aa5f51c41a717e9299c51ea005cccc
Vadim Berezniker <vadim@buildbuddy.io>
2021-07-12 16:48:19 -0700

    Add an unexpected event metric.
    https://github.com/buildbuddy-io/buildbuddy-internal/issues/342

---
+++ server/util/alert/alert.go
@@ -0,0 +1,16 @@
+package alert
+
+import (
+	"github.com/buildbuddy-io/buildbuddy/server/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// UnexpectedEvent is used to indicate that something unexpected happened within the application.
+// This causes a Prometheus counter to be incremented which in turn generates an alert.
+//
+// `name` is used as a metric label and this should be a constant string. Do not include dynamic information.
+//
+// Use this for cases where it's worth knowing about the event, but not worth introducing a standalone metric & alert.
+func UnexpectedEvent(name string) {
+	metrics.UnexpectedEvent.With(prometheus.Labels{metrics.EventName: name}).Inc()
+}
