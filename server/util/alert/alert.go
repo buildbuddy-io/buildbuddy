a7408f1103dd00ab91857518395f3d16a6a8b79d
Vadim Berezniker <vadim@buildbuddy.io>
2021-07-13 16:59:50 -0700

    Add support for struct slice flags in configs. (#707)
    They are not expected to be used often but are useful for specifying
    more complex configuration data.

--- server/util/alert/alert.go
+++ server/util/alert/alert.go
@@ -1,7 +1,10 @@
 package alert
 
 import (
+	"fmt"
+
 	"github.com/buildbuddy-io/buildbuddy/server/metrics"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -11,6 +14,23 @@ import (
 // `name` is used as a metric label and thus should be a constant string. Do not include dynamic information.
 //
 // Use this for cases where it's worth knowing about the event, but not worth introducing a standalone metric & alert.
-func UnexpectedEvent(name string) {
+//
+// Example usages:
+// 	alert.UnexpectedEvent("cannot_unmarshal_proto")
+//	alert.UnexpectedEvent("cannot_unmarshal_proto", "invocation_id %s err: %s", invocation_id, err)
+func UnexpectedEvent(name string, msgAndArgs ...interface{}) {
 	metrics.UnexpectedEvent.With(prometheus.Labels{metrics.EventName: name}).Inc()
+	logMsg := fmt.Sprintf("Unexpected event %q", name)
+	if len(msgAndArgs) == 1 {
+		logMsg += fmt.Sprintf(": %s", msgAndArgs[0])
+	} else if len(msgAndArgs) > 1 {
+		logMsg += ": "
+		msg, ok := msgAndArgs[0].(string)
+		if ok {
+			logMsg += fmt.Sprintf(msg, msgAndArgs[1:]...)
+		} else {
+			logMsg += "<invalid args to UnexpectedEvent>"
+		}
+	}
+	log.Warning(logMsg)
 }
