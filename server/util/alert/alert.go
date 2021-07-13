d882f4e9f9fcf5cef05a38dd051d3fb0e4d479b9
Vadim Berezniker <vadim@buildbuddy.io>
2021-07-13 10:16:49 -0700

    fix typo

--- server/util/alert/alert.go
+++ server/util/alert/alert.go
@@ -8,7 +8,7 @@ import (
 // UnexpectedEvent is used to indicate that something unexpected happened within the application.
 // This causes a Prometheus counter to be incremented which in turn generates an alert.
 //
-// `name` is used as a metric label and this should be a constant string. Do not include dynamic information.
+// `name` is used as a metric label and thus should be a constant string. Do not include dynamic information.
 //
 // Use this for cases where it's worth knowing about the event, but not worth introducing a standalone metric & alert.
 func UnexpectedEvent(name string) {
