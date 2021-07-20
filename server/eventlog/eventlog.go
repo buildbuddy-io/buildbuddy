6feafe73cbbc83e64e378bffeb139ff9603dd4ba
Zoey Greer <zoey@buildbuddy.io>
2021-07-20 16:21:43 -0400

    Address review comments

--- server/eventlog/eventlog.go
+++ server/eventlog/eventlog.go
@@ -11,8 +11,10 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/backends/chunkstore"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 
 	elpb "github.com/buildbuddy-io/buildbuddy/proto/eventlog"
+	inpb "github.com/buildbuddy-io/buildbuddy/proto/invocation"
 	ansi "github.com/leaanthony/go-ansi-parser"
 )
 
@@ -48,7 +50,13 @@ func GetEventLogChunk(ctx context.Context, env environment.Env, req *elpb.GetEve
 		startingIndex, _ := chunkstore.ChunkIndexAsUint16(inv.LastChunkId)
 		var err error
 		if intChunkId, err = c.GetLastChunkIndex(ctx, eventLogPath, startingIndex); err != nil {
-			return nil, err
+			if status.IsNotFoundError(err) && inv.InvocationStatus != int64(inpb.Invocation_PARTIAL_INVOCATION_STATUS) {
+				return &elpb.GetEventLogChunkResponse{
+					Chunk: &elpb.GetEventLogChunkResponse_Chunk{
+						Buffer: make([]byte, 0),
+					},
+				}, nil
+			}
 		}
 	}
 
@@ -85,7 +93,8 @@ func GetEventLogChunk(ctx context.Context, env environment.Env, req *elpb.GetEve
 		}
 		if lineCount >= int(req.MinLines) {
 			break
-		} else if req.ReadBackward || req.ChunkId == "" {
+		}
+		if req.ReadBackward || req.ChunkId == "" {
 			// If the client specified a backwards Read or the client requested the tail of the log
 			if intChunkId == 0 {
 				rsp.PreviousChunkId = ""
@@ -160,6 +169,9 @@ func (w *EventLogWriter) WriteLines(p []byte) (int, error) {
 	return bytesWritten, err
 }
 
+// Split ANSI text by line, rewriting ANSI control sequences when necessary such
+// that any line printed individually is still formatted as it was in the
+// original buffer
 func (w *EventLogWriter) WriteLinesANSI(p []byte) (int, error) {
 	s := string(p)
 	if !ansi.HasEscapeCodes(s) {
