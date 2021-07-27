882e06370557509ec3289010e2e1aa404857abe8
Siggi Simonarson <siggisim@gmail.com>
2021-07-27 16:34:35 -0700

    Use hash of js bundle in the `js_entry_point_path` (#772)
    * Add a sha rule, calculate the sha of our app bundle, and replace it in the js entrypoint
    
    * Update index.bzl
    
    * Fix filename
    
    * Add missing close quote
    
    * Only find files
    
    * Add health checker
    
    * Address review feedback

--- server/static/static.go
+++ server/static/static.go
@@ -1,6 +1,7 @@
 package static
 
 import (
+	"context"
 	"flag"
 	"html/template"
 	"io/fs"
@@ -11,6 +12,7 @@ import (
 
 	"github.com/bazelbuild/rules_go/go/tools/bazel"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/buildbuddy-io/buildbuddy/server/version"
 
 	cfgpb "github.com/buildbuddy-io/buildbuddy/proto/config"
@@ -21,7 +23,7 @@ const (
 )
 
 var (
-	jsEntryPointPath = flag.String("js_entry_point_path", "/app/app_bundle/app.js", "Absolute URL path of the app JS entry point")
+	jsEntryPointPath = flag.String("js_entry_point_path", "/app/app_bundle/app.js?hash={APP_BUNDLE_HASH}", "Absolute URL path of the app JS entry point")
 	disableGA        = flag.Bool("disable_ga", false, "If true; ga will be disabled")
 )
 
@@ -43,7 +45,7 @@ type StaticFileServer struct {
 
 // NewStaticFileServer returns a new static file server that will serve the
 // content in relpath, optionally stripping the prefix.
-func NewStaticFileServer(env environment.Env, fs fs.FS, rootPaths []string) (*StaticFileServer, error) {
+func NewStaticFileServer(env environment.Env, fs fs.FS, rootPaths []string, appBundleHash string) (*StaticFileServer, error) {
 	// Handle "/static/*" requests by serving those static files out of the bundled runfiles.
 	handler := http.FileServer(http.FS(fs))
 	if len(rootPaths) > 0 {
@@ -52,7 +54,16 @@ func NewStaticFileServer(env environment.Env, fs fs.FS, rootPaths []string) (*St
 			return nil, err
 		}
 
-		handler = handleRootPaths(env, rootPaths, template, version.AppVersion(), handler)
+		jsPath := *jsEntryPointPath
+		if appBundleHash != "" {
+			jsPath = strings.ReplaceAll(jsPath, "{APP_BUNDLE_HASH}", appBundleHash)
+		}
+
+		if strings.HasPrefix(jsPath, "http://") || strings.HasPrefix(jsPath, "https://") {
+			env.GetHealthChecker().AddHealthCheck("app_static_file_server", &healthChecker{jsPath: jsPath})
+		}
+
+		handler = handleRootPaths(env, rootPaths, template, version.AppVersion(), jsPath, handler)
 	}
 	return &StaticFileServer{
 		handler: handler,
@@ -64,7 +75,7 @@ func (s *StaticFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.handler.ServeHTTP(w, r)
 }
 
-func handleRootPaths(env environment.Env, rootPaths []string, template *template.Template, version string, h http.Handler) http.Handler {
+func handleRootPaths(env environment.Env, rootPaths []string, template *template.Template, version string, jsPath string, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		for _, rootPath := range rootPaths {
 			if strings.HasPrefix(r.URL.Path, rootPath) {
@@ -73,7 +84,7 @@ func handleRootPaths(env environment.Env, rootPaths []string, template *template
 		}
 
 		if r.URL.Path == "/" {
-			serveIndexTemplate(env, template, version, w)
+			serveIndexTemplate(env, template, version, jsPath, w)
 			return
 		}
 
@@ -81,7 +92,7 @@ func handleRootPaths(env environment.Env, rootPaths []string, template *template
 	})
 }
 
-func serveIndexTemplate(env environment.Env, tpl *template.Template, version string, w http.ResponseWriter) {
+func serveIndexTemplate(env environment.Env, tpl *template.Template, version string, jsPath string, w http.ResponseWriter) {
 	issuers := make([]string, 0)
 	ssoEnabled := false
 	// Assemble a slice of the supported issuers. Omit "private" issuers, which have a slug,
@@ -120,10 +131,30 @@ func serveIndexTemplate(env environment.Env, tpl *template.Template, version str
 	}
 	err := tpl.ExecuteTemplate(w, indexTemplateFilename, &cfgpb.FrontendTemplateData{
 		Config:           &config,
-		JsEntryPointPath: *jsEntryPointPath,
+		JsEntryPointPath: jsPath,
 		GaEnabled:        !*disableGA,
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func AppBundleHash(bundleFS fs.FS) (string, error) {
+	hashBytes, err := fs.ReadFile(bundleFS, "sha.sum")
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(hashBytes)), nil
+}
+
+type healthChecker struct {
+	jsPath string
+}
+
+func (c *healthChecker) Check(ctx context.Context) error {
+	resp, err := http.Get(c.jsPath)
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return status.UnavailableErrorf("Failed to fetch static app js content from url %s. HTTP error code: %s", c.jsPath, resp.Status)
+	}
+	return err
 }
