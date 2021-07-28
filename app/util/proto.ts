b62e689a7b3a04b0afd4882c362faa0059cb24f6
Brandon Duffany <brandon@buildbuddy.io>
2021-07-28 15:40:07 -0400

    Fix floating point issues

--- app/util/proto.ts
+++ app/util/proto.ts
@@ -1,8 +1,9 @@
 import { google } from "../../proto/timestamp_ts_proto";
 
 export function dateToTimestamp(date: Date): google.protobuf.Timestamp {
-  const timestampSeconds = date.getTime() / 1000;
-  const seconds = Math.floor(timestampSeconds);
-  const nanos = Math.floor((timestampSeconds - seconds) * 1e9);
-  return new google.protobuf.Timestamp({ seconds: seconds as any, nanos });
+  const timestampMillis = date.getTime();
+  return new google.protobuf.Timestamp({
+    seconds: Math.floor(timestampMillis / 1000) as any,
+    nanos: (timestampMillis % 1000) * 1e6,
+  });
 }
