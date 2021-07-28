4056e3c792c9338284081f9ee295b2b603de945d
Brandon Duffany <brandon@buildbuddy.io>
2021-07-28 10:43:18 -0400

    Address PR feedback

--- server/util/redact/redact_test.go
+++ server/util/redact/redact_test.go
@@ -3,40 +3,54 @@ package redact_test
 import (
 	"testing"
 
-	"github.com/buildbuddy-io/buildbuddy/proto/build_event_stream"
 	"github.com/buildbuddy-io/buildbuddy/proto/command_line"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
 	"github.com/buildbuddy-io/buildbuddy/server/util/redact"
 	"github.com/stretchr/testify/assert"
+
+	bespb "github.com/buildbuddy-io/buildbuddy/proto/build_event_stream"
 )
 
-func singleFile() *build_event_stream.File {
-	return &build_event_stream.File{
-		Name: "afile",
-		File: &build_event_stream.File_Uri{
-			Uri: "213wZJyTUyhXkj381312@uri",
-		},
+func fileWithURI(uri string) *bespb.File {
+	return &bespb.File{
+		Name: "foo.txt",
+		File: &bespb.File_Uri{Uri: uri},
 	}
 }
 
-func singleFiles() []*build_event_stream.File {
-	return []*build_event_stream.File{
-		singleFile(),
-	}
-}
-
-func TestRedact(t *testing.T) {
+func TestRedactMetadata_BuildStarted_StripsSecrets(t *testing.T) {
 	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
-
-	buildStarted := &build_event_stream.BuildStarted{
+	buildStarted := &bespb.BuildStarted{
 		OptionsDescription: "213wZJyTUyhXkj381312@foo",
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_Started{Started: buildStarted},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_Started{Started: buildStarted},
 	})
 
 	assert.Equal(t, "foo", buildStarted.OptionsDescription)
+}
 
+func TestRedactMetadata_UnstructuredCommandLine_RemovesArgs(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	unstructuredCommandLine := &bespb.UnstructuredCommandLine{
+		Args: []string{"foo"},
+	}
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_UnstructuredCommandLine{
+			UnstructuredCommandLine: unstructuredCommandLine,
+		},
+	})
+
+	assert.Equal(t, []string{}, unstructuredCommandLine.Args)
+}
+
+func TestRedactMetadata_StructuredCommandLine_RedactsEnvVars(t *testing.T) {
+	// TODO(bduffany): Migrate env redaction to redactor and enable.
+	t.Skip()
+
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
 	shellOption := &command_line.Option{
 		CombinedForm: "--client_env=SHELL=/bin/bash",
 		OptionName:   "client_env",
@@ -63,103 +77,122 @@ func TestRedact(t *testing.T) {
 			},
 		},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_StructuredCommandLine{StructuredCommandLine: structuredCommandLine},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_StructuredCommandLine{StructuredCommandLine: structuredCommandLine},
 	})
 
-	// TODO(bduffs): Migrate env redaction to redactor and enable these assertions.
-	// assert.Equal(t, "--client_env=SECRET=<REDACTED>", secretOption.OptionValue)
-	// assert.Equal(t, "secret=<REDACTED>", secretOption.OptionValue)
+	assert.Equal(t, "--client_env=SECRET=<REDACTED>", secretOption.OptionValue)
+	assert.Equal(t, "secret=<REDACTED>", secretOption.OptionValue)
+}
 
-	optionsParsed := &build_event_stream.OptionsParsed{
+func TestRedactMetadata_OptionsParsed_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	optionsParsed := &bespb.OptionsParsed{
 		CmdLine:         []string{"213wZJyTUyhXkj381312@foo"},
 		ExplicitCmdLine: []string{"213wZJyTUyhXkj381312@explicit"},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_OptionsParsed{OptionsParsed: optionsParsed},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_OptionsParsed{OptionsParsed: optionsParsed},
 	})
 
 	assert.Equal(t, []string{"foo"}, optionsParsed.CmdLine)
 	assert.Equal(t, []string{"explicit"}, optionsParsed.ExplicitCmdLine)
+}
 
-	actionExecuted := &build_event_stream.ActionExecuted{
-		Stdout:             singleFile(),
-		Stderr:             singleFile(),
-		PrimaryOutput:      singleFile(),
-		ActionMetadataLogs: singleFiles(),
+func TestRedactMetadata_ActionExecuted_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	actionExecuted := &bespb.ActionExecuted{
+		Stdout:             fileWithURI("213wZJyTUyhXkj381312@uri"),
+		Stderr:             fileWithURI("213wZJyTUyhXkj381312@uri"),
+		PrimaryOutput:      fileWithURI("213wZJyTUyhXkj381312@uri"),
+		ActionMetadataLogs: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_Action{Action: actionExecuted},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_Action{Action: actionExecuted},
 	})
 
 	assert.Equal(t, "uri", actionExecuted.Stdout.GetUri())
 	assert.Equal(t, "uri", actionExecuted.Stderr.GetUri())
 	assert.Equal(t, "uri", actionExecuted.PrimaryOutput.GetUri())
 	assert.Equal(t, "uri", actionExecuted.ActionMetadataLogs[0].GetUri())
+}
 
-	namedSetOfFiles := &build_event_stream.NamedSetOfFiles{
-		Files: singleFiles(),
+func TestRedactMetadata_NamedSetOfFiles_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	namedSetOfFiles := &bespb.NamedSetOfFiles{
+		Files: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_NamedSetOfFiles{NamedSetOfFiles: namedSetOfFiles},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_NamedSetOfFiles{NamedSetOfFiles: namedSetOfFiles},
 	})
 
 	assert.Equal(t, "uri", namedSetOfFiles.Files[0].GetUri())
+}
 
-	targetComplete := &build_event_stream.TargetComplete{
+func TestRedactMetadata_TargetComplete_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	targetComplete := &bespb.TargetComplete{
 		Success:         true,
-		ImportantOutput: singleFiles(),
+		ImportantOutput: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_Completed{Completed: targetComplete},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_Completed{Completed: targetComplete},
 	})
 
 	assert.Equal(t, "uri", targetComplete.ImportantOutput[0].GetUri())
+}
 
-	testResult := &build_event_stream.TestResult{
-		Status:           build_event_stream.TestStatus_PASSED,
-		TestActionOutput: singleFiles(),
+func TestRedactMetadata_TestResult_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	testResult := &bespb.TestResult{
+		Status:           bespb.TestStatus_PASSED,
+		TestActionOutput: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_TestResult{TestResult: testResult},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_TestResult{TestResult: testResult},
 	})
 
 	assert.Equal(t, "uri", testResult.TestActionOutput[0].GetUri())
+}
 
-	testSummary := &build_event_stream.TestSummary{
-		Passed: singleFiles(),
-		Failed: singleFiles(),
+func TestRedactMetadata_TestSummary_StripsURLSecrets(t *testing.T) {
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	testSummary := &bespb.TestSummary{
+		Passed: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
+		Failed: []*bespb.File{fileWithURI("213wZJyTUyhXkj381312@uri")},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_TestSummary{TestSummary: testSummary},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_TestSummary{TestSummary: testSummary},
 	})
 
 	assert.Equal(t, "uri", testSummary.Passed[0].GetUri())
 	assert.Equal(t, "uri", testSummary.Failed[0].GetUri())
+}
 
-	buildFinished := &build_event_stream.BuildFinished{
-		FinishTimeMillis: 1,
-		ExitCode: &build_event_stream.BuildFinished_ExitCode{
-			Name: "Success",
-			Code: 0,
-		},
-	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_Finished{Finished: buildFinished},
-	})
+func TestRedactMetadata_BuildMetadata_StripsURLSecrets(t *testing.T) {
+	// TODO(bduffany): Migrate BuildMetadata redaction from event_parser to redactor
+	// and enable.
+	t.Skip()
 
-	buildMetadata := &build_event_stream.BuildMetadata{
+	redactor := redact.NewStreamingRedactor(testenv.GetTestEnv(t))
+	buildMetadata := &bespb.BuildMetadata{
 		Metadata: map[string]string{
 			"ALLOW_ENV": "SHELL",
 			"ROLE":      "METADATA_CI",
 			"REPO_URL":  "https://USERNAME:PASSWORD@github.com/buildbuddy-io/metadata_repo_url",
 		},
 	}
-	redactor.RedactMetadata(&build_event_stream.BuildEvent{
-		Payload: &build_event_stream.BuildEvent_BuildMetadata{BuildMetadata: buildMetadata},
+
+	redactor.RedactMetadata(&bespb.BuildEvent{
+		Payload: &bespb.BuildEvent_BuildMetadata{BuildMetadata: buildMetadata},
 	})
 
-	// TODO(bduffs): Migrate BuildMetadata redaction from event_parser to redactor
-	// and enable this assertion.
-	// assert.Equal(t, "https://github.com/buildbuddy-io/metadata_repo_url", buildMetadata.Metadata["REPO_URL"])
+	assert.Equal(t, "https://github.com/buildbuddy-io/metadata_repo_url", buildMetadata.Metadata["REPO_URL"])
 }
