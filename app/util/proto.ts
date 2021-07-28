721110cba14822b49aaf8ece2a170fac7e391919
Brandon Duffany <brandon@buildbuddy.io>
2021-07-28 15:41:28 -0400

    Consistent notation

--- app/util/proto.ts
+++ app/util/proto.ts
@@ -3,7 +3,7 @@ import { google } from "../../proto/timestamp_ts_proto";
 export function dateToTimestamp(date: Date): google.protobuf.Timestamp {
   const timestampMillis = date.getTime();
   return new google.protobuf.Timestamp({
-    seconds: Math.floor(timestampMillis / 1000) as any,
-    nanos: (timestampMillis % 1000) * 1e6,
+    seconds: Math.floor(timestampMillis / 1e3) as any,
+    nanos: (timestampMillis % 1e3) * 1e6,
   });
 }
