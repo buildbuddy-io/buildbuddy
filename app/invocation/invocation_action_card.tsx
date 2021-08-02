216a4e684152ce1b0b69675c8820315df82e6b5c
Paribartan Parajuli <pari.parajuli@berkeley.edu>
2021-08-02 15:50:42 -0400

    Created linked output files

--- app/invocation/invocation_action_card.tsx
+++ app/invocation/invocation_action_card.tsx
@@ -104,6 +104,14 @@ export default class InvocationActionCardComponent extends React.Component<Props
     );
   }
 
+  handleOutputFileClicked(file: build.bazel.remote.execution.v2.IOutputFile) {
+    rpcService.downloadBytestreamFile(
+      file.path,
+      "bytestream://" + this.getCacheAddress() + "/blobs/" + file.digest.hash + "/" + file.digest.sizeBytes,
+      this.props.model.getId()
+    );
+  }
+
   getCacheAddress() {
     let address = this.props.model.optionsMap.get("remote_executor").replace("grpc://", "");
     address = address.replace("grpcs://", "");
@@ -252,7 +260,7 @@ export default class InvocationActionCardComponent extends React.Component<Props
                         {this.state.command.environmentVariables.map((variable) => (
                           <div>
                             <span className="prop-name">{variable.name}</span>
-                            <span className="prop-value">={variable.value}</span>
+                            <span className="metadata-detail">={variable.value}</span>
                           </div>
                         ))}
                       </div>
@@ -332,7 +340,11 @@ export default class InvocationActionCardComponent extends React.Component<Props
                         <div className="action-list">
                           {this.state.actionResult.outputFiles.map((file) => (
                             <div>
-                              <span className="prop-value">{file.path}</span>
+                              <span
+                                className="prop-link clickable"
+                                onClick={this.handleOutputFileClicked.bind(this, file)}>
+                                {file.path}
+                              </span>
                               {file.isExecutable && <span className="detail"> (executable)</span>}
                             </div>
                           ))}
@@ -347,7 +359,7 @@ export default class InvocationActionCardComponent extends React.Component<Props
                         <div className="action-list">
                           {this.state.actionResult.outputDirectories.map((dir) => (
                             <div>
-                              <span className="prop-value">{dir.path}</span>
+                              <span>{dir.path}</span>
                             </div>
                           ))}
                         </div>
