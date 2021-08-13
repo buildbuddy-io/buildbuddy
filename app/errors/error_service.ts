5133549d9cc330d938ffd03b74d86ab28c9b95b1
Siggi <siggisim@gmail.com>
2021-08-13 11:54:54 -0700

    Display errors passed through the `?error=` url query param using the error service

--- app/errors/error_service.ts
+++ app/errors/error_service.ts
@@ -1,7 +1,17 @@
-import { BuildBuddyError } from "../../app/util/errors";
+import router from "../router/router";
+import { BuildBuddyError } from "../util/errors";
 import alertService from "../alert/alert_service";
 
+const ERROR_SEARCH_PARAM = "error";
 export class ErrorService {
+  register() {
+    let searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has(ERROR_SEARCH_PARAM)) {
+      this.handleError(searchParams.get(ERROR_SEARCH_PARAM));
+    }
+    router.replaceParams({ ERROR_SEARCH_PARAM: undefined });
+  }
+
   handleError(e: any) {
     console.error(e);
 
