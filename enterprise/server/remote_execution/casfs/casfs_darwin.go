8f80fb9399c4e32bc819442e90cbdbe3960fd04c
Vadim Berezniker <vadim@buildbuddy.io>
2021-08-26 18:31:39 -0700

    fix imports

--- enterprise/server/remote_execution/casfs/casfs_darwin.go
+++ enterprise/server/remote_execution/casfs/casfs_darwin.go
@@ -2,8 +2,9 @@ package casfs
 
 import (
 	"context"
-	"github.com/hanwen/go-fuse/v2/fs"
 	"syscall"
+
+	"github.com/hanwen/go-fuse/v2/fs"
 )
 
 func (n *Node) CopyFileRange(ctx context.Context, fhIn fs.FileHandle, offIn uint64, out *fs.Inode, fhOut fs.FileHandle, offOut uint64, len uint64, flags uint64) (uint32, syscall.Errno) {
