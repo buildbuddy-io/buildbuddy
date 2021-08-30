0ec1a3a21084abeb4ea5dfae373983fa32766a7c
Vadim Berezniker <vadim@buildbuddy.io>
2021-08-30 10:18:44 -0700

    Make casfs lib build on Mac. (#927)
    * Make casfs lib build on Mac.

--- enterprise/server/remote_execution/casfs/casfs.go
+++ enterprise/server/remote_execution/casfs/casfs.go
@@ -18,7 +18,6 @@ import (
 	"github.com/docker/go-units"
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
-	"golang.org/x/sys/unix"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
@@ -557,24 +556,6 @@ func (n *Node) Unlink(ctx context.Context, name string) syscall.Errno {
 	}
 
 	return syscall.ENOTSUP
-}
-
-func (n *Node) CopyFileRange(ctx context.Context, fhIn fs.FileHandle, offIn uint64, out *fs.Inode, fhOut fs.FileHandle, offOut uint64, len uint64, flags uint64) (uint32, syscall.Errno) {
-	lfIn, ok := fhIn.(*instrumentedLoopbackFile)
-	if !ok {
-		log.Warningf("[%s] In is not a *instrumentedLoopbackFile", n.cfs.taskID())
-		return 0, syscall.ENOSYS
-	}
-	lfOut, ok := fhOut.(*instrumentedLoopbackFile)
-	if !ok {
-		log.Warningf("[%s] Out is not a *instrumentedLoopbackFile", n.cfs.taskID())
-		return 0, syscall.ENOSYS
-	}
-
-	rOffset := int64(offIn)
-	wOffset := int64(offOut)
-	bytesCopied, err := unix.CopyFileRange(lfIn.fd, &rOffset, lfOut.fd, &wOffset, int(len), int(flags))
-	return uint32(bytesCopied), fs.ToErrno(err)
 }
 
 type instrumentedReadResult struct {
