4708374a409ceba79dacb5d217bdc7b3a94c4e06
Brandon Duffany <brandon@buildbuddy.io>
2021-09-02 12:20:18 -0400

    Implement usage page RPC and UI view

--- enterprise/app/usage/usage.tsx
+++ enterprise/app/usage/usage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import errorService from "../../../app/errors/error_service";
 import rpcService from "../../../app/service/rpc_service";
 import { usage } from "../../../proto/usage_ts_proto";
+import Select, { Option } from "../../../app/components/select/select";
 
 export interface UsageProps {}
 
 interface State {
   response?: usage.GetUsageResponse;
+  selectedPeriod?: string;
   loading?: boolean;
 }
 
@@ -19,21 +21,59 @@ export default class UsageComponent extends React.Component<UsageProps, State> {
     this.setState({ loading: true });
     rpcService.service
       .getUsage(new usage.GetUsageRequest())
-      .then((response) => this.setState({ response }))
+      .then((response) => {
+        this.setState({ response, selectedPeriod: response.usage[0].period });
+      })
       .catch((e) => errorService.handleError(e))
       .finally(() => this.setState({ loading: false }));
   }
 
+  private onChangePeriod(e: React.ChangeEvent) {
+    const selectedPeriod = (e.target as HTMLOptionElement).value;
+    this.setState({ selectedPeriod });
+  }
+
   render() {
+    const usage = this.state.response
+      ? this.state.response.usage.find((usage) => usage.period === this.state.selectedPeriod)
+      : null;
+
     return (
       <div className="usage-page">
-        <div className="container">
+        <div className="container usage-page-container">
           <div className="usage-header">
             <div className="usage-title">Usage</div>
           </div>
           {this.state.loading && <div className="loading" />}
-          {/* TODO(bduffany): render the data nicely */}
-          {this.state.response && <pre>{JSON.stringify(this.state.response, null, 2)}</pre>}
+          {this.state.response && (
+            <div className="card usage-card">
+              <div className="content">
+                <div className="usage-period-header">
+                  <div className="selected-period-label">
+                    Usage for <span className="usage-period">{usage.period} (UTC)</span>
+                  </div>
+                  <Select title="Usage period" onChange={this.onChangePeriod.bind(this)}>
+                    {this.state.response.usage.map((usage, i) => (
+                      <Option key={usage.period} value={usage.period}>
+                        {usage.period}
+                        {i === 0 ? " (Current period)" : ""}
+                      </Option>
+                    ))}
+                  </Select>
+                </div>
+                <div className="usage-period-table">
+                  <div className="usage-resource-name">Invocations</div>
+                  <div className="usage-value">{usage.totalNumBuilds}</div>
+                  <div className="usage-resource-name">Action cache hits</div>
+                  <div className="usage-value">{usage.actionCacheHits}</div>
+                  <div className="usage-resource-name">Content addressable storage cache hits</div>
+                  <div className="usage-value">{usage.casCacheHits}</div>
+                  <div className="usage-resource-name">Total bytes downloaded from cache</div>
+                  <div className="usage-value">{usage.totalDownloadSizeBytes}</div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
--- enterprise/server/usage_service/usage_service.go
+++ enterprise/server/usage_service/usage_service.go
@@ -2,10 +2,18 @@ package usage_service
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/timeutil"
 
 	usagepb "github.com/buildbuddy-io/buildbuddy/proto/usage"
+)
+
+const (
+	// Allow the user to view this many months of usage data.
+	maxNumMonthsOfUsageToReturn = 6
 )
 
 type usageService struct {
@@ -17,5 +25,81 @@ func New(env environment.Env) *usageService {
 }
 
 func (s *usageService) GetUsage(ctx context.Context, req *usagepb.GetUsageRequest) (*usagepb.GetUsageResponse, error) {
-	return &usagepb.GetUsageResponse{}, nil
+	db := s.env.GetDBHandle()
+
+	// Get the timestamp corresponding to the start of the current month in UTC,
+	// with a month offset so that we return the desired number of months of
+	// usage.
+	now := time.Now().UTC()
+	maxNumHistoricalMonths := maxNumMonthsOfUsageToReturn - 1
+	createdAfter := addCalendarMonths(startOfMonth(now), -maxNumHistoricalMonths)
+	createdBefore := addCalendarMonths(startOfMonth(now), 1)
+
+	rows, err := db.Raw(`
+	SELECT `+db.UTCMonthFromUsecTimestamp("created_at_usec")+` AS period,
+	COUNT(1) AS total_num_builds,
+	SUM(action_cache_hits) AS action_cache_hits,
+	SUM(cas_cache_hits) AS cas_cache_hits,
+	SUM(total_download_size_bytes) AS total_download_size_bytes
+	FROM Invocations
+	WHERE created_at_usec >= ? AND created_at_usec < ?
+	GROUP BY period
+	ORDER BY period ASC
+	`, timeutil.ToUsec(createdAfter), timeutil.ToUsec(createdBefore)).Rows()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	usages := []*usagepb.Usage{}
+	for rows.Next() {
+		usage := &usagepb.Usage{}
+		if err := db.ScanRows(rows, usage); err != nil {
+			return nil, err
+		}
+		usages = append(usages, usage)
+	}
+	// Build the response list from scanned rows, inserting explicit zeroes for months with no data.
+	rsp := &usagepb.GetUsageResponse{}
+	for t := createdAfter; !(t.Equal(createdBefore) || t.After(createdBefore)); t = addCalendarMonths(t, 1) {
+		period := fmt.Sprintf("%d-%02d", t.Year(), t.Month())
+		if len(usages) > 0 && usages[0].Period == period {
+			rsp.Usage = append(rsp.Usage, usages[0])
+			usages = usages[1:]
+		} else {
+			rsp.Usage = append(rsp.Usage, &usagepb.Usage{
+				Period: period,
+			})
+		}
+	}
+
+	// Return in reverse-chronological order.
+	reverseUsageSlice(rsp.Usage)
+
+	return rsp, nil
+}
+
+func startOfMonth(t time.Time) time.Time {
+	// Set day to 1 and time to 0:00:00.000
+	return time.Date(
+		t.Year(), t.Month(), 1, /*=day*/
+		0, 0, 0, 0, /*=hour,min,sec,nsec*/
+		t.Location())
+}
+
+func addCalendarMonths(t time.Time, months int) time.Time {
+	// Note: the month arithmetic works because Go allows passing month values
+	// outside their usual range. For example, a month value of 0 corresponds to
+	// December of the preceding year.
+	return time.Date(
+		t.Year(), time.Month(int(t.Month())+months), t.Day(),
+		t.Hour(), t.Minute(), t.Second(), t.Nanosecond(),
+		t.Location())
+}
+
+func reverseUsageSlice(a []*usagepb.Usage) {
+	for i := 0; i < len(a)/2; i++ {
+		j := len(a) - i - 1
+		a[i], a[j] = a[j], a[i]
+	}
 }
