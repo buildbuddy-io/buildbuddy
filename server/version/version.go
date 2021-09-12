273cdc2fec550c3afbe55cb76ea04dbcffcd5897
Siggi <siggisim@gmail.com>
2021-09-12 12:49:04 -0700

    Don't pull in full bundle just to get app version

--- server/version/version.go
+++ server/version/version.go
@@ -2,16 +2,9 @@ package version
 
 import (
 	"fmt"
-	"io/fs"
-	"os"
-	"path/filepath"
 	"runtime"
-	"strings"
 
-	"github.com/bazelbuild/rules_go/go/tools/bazel"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
-
-	bundle "github.com/buildbuddy-io/buildbuddy"
 )
 
 const (
@@ -21,9 +14,11 @@ const (
 
 // This is set by x_defs in the BUILD file.
 //    x_defs = {
+//        "version": "{VERSION}",
 //        "commitSha": "{COMMIT_SHA}",
 //    },
 var commitSha string
+var version string
 
 func Print() {
 	appVersion := fmt.Sprintf("BuildBuddy %s", AppVersion())
@@ -34,23 +29,10 @@ func Print() {
 }
 
 func AppVersion() string {
-	var versionBytes []byte
-	if rfp, err := bazel.RunfilesPath(); err == nil {
-		versionFile := filepath.Join(rfp, versionFilename)
-		if b, err := os.ReadFile(versionFile); err == nil {
-			versionBytes = b
-		}
+	if commitSha != "{VERSION}" {
+		return commitSha
 	}
-	if versionBytes == nil {
-		if bundleFS, err := bundle.Get(); err == nil {
-			if data, err := fs.ReadFile(bundleFS, versionFilename); err == nil {
-				versionBytes = data
-			}
-		}
-	}
-	if versionBytes != nil {
-		return strings.TrimSpace(string(versionBytes))
-	}
+
 	return unknownValue
 }
 
