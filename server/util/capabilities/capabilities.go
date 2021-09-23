3b86908446dc24588fd91c1b823badf3b688ad28
Brandon Duffany <brandon@buildbuddy.io>
2021-09-23 12:06:54 -0400

    Clean up a few anonymous user checks

--- server/util/capabilities/capabilities.go
+++ server/util/capabilities/capabilities.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
-	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/buildbuddy-io/buildbuddy/server/util/perms"
 
 	akpb "github.com/buildbuddy-io/buildbuddy/proto/api_key"
 )
@@ -43,18 +43,10 @@ func ToInt(caps []akpb.ApiKey_Capability) int32 {
 }
 
 func IsGranted(ctx context.Context, env environment.Env, cap akpb.ApiKey_Capability) (bool, error) {
-	authIsRequired := !env.GetConfigurator().GetAnonymousUsageEnabled()
-	a := env.GetAuthenticator()
-	if a == nil {
-		if authIsRequired {
-			return false, status.UnimplementedError("Not Implemented")
-		}
-		return int32(cap)&AnonymousUserCapabilitiesMask > 0, nil
-	}
-	user, err := a.AuthenticatedUser(ctx)
+	user, err := perms.AuthenticatedUser(ctx, env)
 	if err != nil {
-		if isAnonymousUser := status.IsPermissionDeniedError(err) || status.IsUnauthenticatedError(err); isAnonymousUser {
-			if authIsRequired {
+		if perms.IsAnonymousUserError(err) {
+			if !env.GetConfigurator().GetAnonymousUsageEnabled() {
 				return false, nil
 			}
 			return int32(cap)&AnonymousUserCapabilitiesMask > 0, nil
