e7de75af2cb0788a8c5fb278692d7b837ab397f7
Siggi Simonarson <siggisim@gmail.com>
2021-09-28 15:42:39 -0400

    Add option to force darwin executions to run on user-owned executors (#1060)
    * Force darwin
    
    * Clean up
    
    * Fix
    
    * Disable anonymous
    
    * Respond to review comments
    
    * Please checkstyle
    
    * Rm whitespace

--- enterprise/app/org/org_form.tsx
+++ enterprise/app/org/org_form.tsx
@@ -168,7 +168,7 @@ export default abstract class OrgForm<T extends GroupRequest> extends React.Comp
               name="useGroupOwnedExecutors"
               checked={request.useGroupOwnedExecutors}
             />
-            <span>Use self-hosted executors</span>
+            <span>Use self-hosted Linux executors</span>
           </label>
         )}
       </>
