8026d5d1bf25bb7e8df425366da648a159c574de
Brandon Duffany <brandon@buildbuddy.io>
2021-10-04 14:55:17 -0400

    Remove readback from MetricsCollector.Increment

--- server/backends/redis_metrics_collector/redis_metrics_collector.go
+++ server/backends/redis_metrics_collector/redis_metrics_collector.go
@@ -14,7 +14,7 @@ func New(rdb *redis.Client) *collector {
 	return &collector{rdb}
 }
 
-func (c *collector) IncrementCount(ctx context.Context, counterName string, n int64) (int64, error) {
+func (c *collector) IncrementCount(ctx context.Context, counterName string, n int64) error {
 	return c.rdb.IncrBy(ctx, counterName, n).Result()
 }
 
--- server/interfaces/interfaces.go
+++ server/interfaces/interfaces.go
@@ -452,7 +452,7 @@ type PubSub interface {
 // evicted from the backing store that maintains them (usually memcache or
 // redis), so they should *not* be used in critical path code.
 type MetricsCollector interface {
-	IncrementCount(ctx context.Context, counterName string, n int64) (int64, error)
+	IncrementCount(ctx context.Context, counterName string, n int64) error
 	ReadCount(ctx context.Context, counterName string) (int64, error)
 }
 
