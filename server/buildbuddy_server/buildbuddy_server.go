652a0e8384deeb6268afe377b50575df846e40e0
Brandon Duffany <brandon@buildbuddy.io>
2021-10-07 18:01:55 -0400

    Use pointer types for group fields that can be null (#1087)

--- server/buildbuddy_server/buildbuddy_server.go
+++ server/buildbuddy_server/buildbuddy_server.go
@@ -125,15 +125,19 @@ func makeGroups(grps []*tables.Group) []*grpb.Group {
 		if g.URLIdentifier != nil {
 			urlIdentifier = *g.URLIdentifier
 		}
+		githubToken := ""
+		if g.GithubToken != nil {
+			githubToken = *g.GithubToken
+		}
 		r = append(r, &grpb.Group{
 			Id:                     g.GroupID,
 			Name:                   g.Name,
 			OwnedDomain:            g.OwnedDomain,
-			GithubLinked:           g.GithubToken != "",
-			GithubToken:            g.GithubToken,
+			GithubLinked:           githubToken != "",
+			GithubToken:            githubToken,
 			UrlIdentifier:          urlIdentifier,
 			SharingEnabled:         g.SharingEnabled,
-			UseGroupOwnedExecutors: g.UseGroupOwnedExecutors,
+			UseGroupOwnedExecutors: g.UseGroupOwnedExecutors != nil && *g.UseGroupOwnedExecutors,
 		})
 	}
 	return r
@@ -195,7 +199,7 @@ func (s *BuildBuddyServer) GetGroup(ctx context.Context, req *grpb.GetGroupReque
 		// info should not be exposed here.
 		Name:        group.Name,
 		OwnedDomain: group.OwnedDomain,
-		SsoEnabled:  group.SamlIdpMetadataUrl != "",
+		SsoEnabled:  group.SamlIdpMetadataUrl != nil && *group.SamlIdpMetadataUrl != "",
 	}, nil
 }
 
@@ -251,12 +255,13 @@ func (s *BuildBuddyServer) CreateGroup(ctx context.Context, req *grpb.CreateGrou
 		groupOwnedDomain = userEmailDomain
 	}
 
+	useGroupOwnedExecutors := req.GetUseGroupOwnedExecutors()
 	group := &tables.Group{
 		UserID:                 user.UserID,
 		Name:                   groupName,
 		OwnedDomain:            groupOwnedDomain,
 		SharingEnabled:         req.GetSharingEnabled(),
-		UseGroupOwnedExecutors: req.GetUseGroupOwnedExecutors(),
+		UseGroupOwnedExecutors: &useGroupOwnedExecutors,
 	}
 	urlIdentifier := strings.TrimSpace(req.GetUrlIdentifier())
 
@@ -325,7 +330,8 @@ func (s *BuildBuddyServer) UpdateGroup(ctx context.Context, req *grpb.UpdateGrou
 		group.OwnedDomain = ""
 	}
 	group.SharingEnabled = req.GetSharingEnabled()
-	group.UseGroupOwnedExecutors = req.GetUseGroupOwnedExecutors()
+	useGroupOwnedExecutors := req.GetUseGroupOwnedExecutors()
+	group.UseGroupOwnedExecutors = &useGroupOwnedExecutors
 	if _, err := userDB.InsertOrUpdateGroup(ctx, group); err != nil {
 		return nil, err
 	}
