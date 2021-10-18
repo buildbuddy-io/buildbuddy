2cf650e04cc75ebf073693b0a9428aeefd58fe11
Brandon Duffany <brandon@buildbuddy.io>
2021-10-18 14:56:34 -0400

    Fix a few more issues

--- enterprise/app/org/org_form.tsx
+++ enterprise/app/org/org_form.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import capabilities from "../../../app/capabilities/capabilities";
 import { User } from "../../../app/auth/auth_service";
-import { grp } from "../../../proto/group_ts_proto";
+import { BuildBuddyError } from "../../../app/util/errors";
 
 export type FormProps = {
   user: User;
@@ -14,7 +14,7 @@ export type FormState<T extends GroupRequest> = {
   initialRequest: T;
   // Fields that have received focus at least once.
   touched: Set<string>;
-  error?: string;
+  error?: BuildBuddyError;
   submitting?: boolean;
   submitted?: boolean;
   dirty?: boolean;
@@ -37,7 +37,7 @@ export default abstract class OrgForm<T extends GroupRequest> extends React.Comp
   async onSubmit(e: any) {
     e.preventDefault();
 
-    this.setState({ submitting: true, error: "" });
+    this.setState({ submitting: true, error: null });
     try {
       await this.submitRequest();
       this.setState({
@@ -46,8 +46,8 @@ export default abstract class OrgForm<T extends GroupRequest> extends React.Comp
         touched: new Set(),
         initialRequest: Object.assign(this.newRequest(), this.state.request),
       });
-    } catch (error) {
-      this.setState({ error });
+    } catch (error: any) {
+      this.setState({ error: BuildBuddyError.parse(error) });
     } finally {
       this.setState({ submitting: false });
     }
@@ -79,7 +79,7 @@ export default abstract class OrgForm<T extends GroupRequest> extends React.Comp
   }
 
   renderError() {
-    return this.state.error && <div className="form-error">{parseErrorDescription(this.state.error)}</div>;
+    return this.state.error && <div className="form-error">{this.state.error.description}</div>;
   }
 
   renderFields() {
@@ -187,14 +187,6 @@ export function getChangedFormState(changeEvent: React.ChangeEvent) {
 
 function getDomainFromEmail(email: string) {
   return email.split("@").pop();
-}
-
-function parseErrorDescription(message: string) {
-  if (!message?.includes("desc = ")) {
-    return "An unknown error occurred. Try again later.";
-  }
-
-  return message.split("desc = ").pop();
 }
 
 export function makeSlug(value: string) {
