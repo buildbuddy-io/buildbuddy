03af0054127038ac503c23e6028ec1ef974dc165
Brandon Duffany <brandon@buildbuddy.io>
2021-10-20 12:05:11 -0400

    Change default user role to Developer (#1120)

--- server/util/role/role.go
+++ server/util/role/role.go
@@ -20,10 +20,7 @@ const (
 
 	// DefaultRole is the role assigned to users when joining a group they did
 	// not create.
-	// TODO(bduffany): Change this to DeveloperRole once we have a way to manage
-	// roles via the UI (otherwise, there would be no easy way to promote new
-	// users to admins in the meantime).
-	Default = Admin
+	Default = Developer
 )
 
 // Role represents a user's role within a group.
