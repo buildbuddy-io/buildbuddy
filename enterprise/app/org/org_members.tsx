8944e6d55b3a6ed8d2eb755ed19a02fcbc41417d
Brandon Duffany <brandon@buildbuddy.io>
2021-10-26 17:03:26 -0400

    Update comment

--- enterprise/app/org/org_members.tsx
+++ enterprise/app/org/org_members.tsx
@@ -137,8 +137,9 @@ export default class OrgMembersComponent extends React.Component<OrgMembersProps
         })
       )
       .then(() => {
-        // After changing your own role within an org, refresh the page to trigger
-        // group reselection or login page as appropriate.
+        // After changing your own role within an org, refresh the page to
+        // trigger a user refresh and redirect, in case this settings page is no
+        // longer accessible.
         if (this.state.selectedUserIds.has(this.props.user.displayUser.userId.id)) {
           window.location.reload();
           return;
