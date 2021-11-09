cca99071a041541c572603de0a097c8dfa5cc5eb
Siggi Simonarson <siggisim@gmail.com>
2021-11-08 17:00:41 -0800

    deps

--- server/util/prefix/prefix.go
+++ server/util/prefix/prefix.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/util/authutil"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 )
