d1a5439a90746211318379ab1465bc8988f085da
Tyler Williams <williams.tyler@gmail.com>
2021-11-12 09:26:36 -0800

    Add a simple rangemap, with tests.

---
+++ server/util/rangemap/rangemap.go
@@ -0,0 +1,193 @@
+package rangemap
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"sort"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+)
+
+var (
+	RangeOverlapError      = errors.New("Range overlap")
+	RangeDoesNotExistError = errors.New("Range does not exist")
+)
+
+// Ranges are [inclusive,exclusive)
+type Range struct {
+	Left  []byte
+	Right []byte
+
+	Val interface{}
+}
+
+func (r *Range) String() string {
+	return fmt.Sprintf("[%s, %s)", string(r.Left), string(r.Right))
+}
+
+func (r *Range) Contains(key []byte) bool {
+	// bytes.Compare(a,b) does:
+	//  0 if a==b, -1 if a < b, and +1 if a > b
+	greaterThanOrEqualToLeft := bytes.Compare(key, r.Left) > -1
+	lessThanRight := bytes.Compare(key, r.Right) == -1
+
+	contained := greaterThanOrEqualToLeft && lessThanRight
+	log.Debugf("Checking if %s contains %q returned %t", r, string(key), contained)
+	return contained
+}
+
+type RangeMap struct {
+	ranges []*Range
+}
+
+func New() *RangeMap {
+	return &RangeMap{
+		ranges: make([]*Range, 0),
+	}
+}
+
+func (rm *RangeMap) Add(left, right []byte, value interface{}) (*Range, error) {
+	insertIndex := sort.Search(len(rm.ranges), func(i int) bool {
+		//  0 if a==b, -1 if a < b, and +1 if a > b
+		c := bytes.Compare(rm.ranges[i].Left, right)
+		b := c >= 0
+		return b
+	})
+
+	// if we're inserting anywhere but the very beginning, ensure that
+	// we don't overlap with the range before us.
+	prevRangeIndex := insertIndex - 1
+	if len(rm.ranges) > 0 && prevRangeIndex >= 0 {
+		if bytes.Compare(rm.ranges[prevRangeIndex].Right, left) > 0 {
+			return nil, RangeOverlapError
+		}
+	}
+
+	newRange := &Range{
+		Left:  left,
+		Right: right,
+		Val:   value,
+	}
+
+	if insertIndex >= len(rm.ranges) {
+		rm.ranges = append(rm.ranges, newRange)
+	} else {
+		rm.ranges = append(rm.ranges[:insertIndex+1], rm.ranges[insertIndex:]...)
+		rm.ranges[insertIndex] = newRange
+	}
+	log.Debugf("Added new range: %s", newRange)
+	return newRange, nil
+}
+
+func (rm *RangeMap) Remove(left, right []byte) error {
+	deleteIndex := -1
+	for i, r := range rm.ranges {
+		if bytes.Compare(left, r.Left) == 0 && bytes.Compare(right, r.Right) == 0 {
+			deleteIndex = i
+			break
+		}
+	}
+	if deleteIndex == -1 {
+		return RangeDoesNotExistError
+	}
+	rm.ranges = append(rm.ranges[:deleteIndex], rm.ranges[deleteIndex+1:]...)
+	return nil
+}
+
+func (rm *RangeMap) Get(left, right []byte) *Range {
+	if len(rm.ranges) == 0 {
+		return nil
+	}
+
+	// Search returns the smallest i for which func returns true.
+	// We want the smallest range that is bigger than this key
+	// aka, starts AFTER this key, and then we'll go one left of it
+	i := sort.Search(len(rm.ranges), func(i int) bool {
+		//  0 if a==b, -1 if a < b, and +1 if a > b
+		return bytes.Compare(rm.ranges[i].Left, left) > 0
+	})
+
+	// This is safe anyway because of how sort.Search works, but
+	// be clear so readers see this won't hit an out of range panic.
+	if i > 0 {
+		i -= 1
+	}
+
+	r := rm.ranges[i]
+	leftEq := bytes.Compare(r.Left, left) == 0
+	rightEq := bytes.Compare(r.Right, right) == 0
+	if leftEq && rightEq {
+		return r
+	}
+	return nil
+}
+
+func (rm *RangeMap) GetOverlapping(left, right []byte) []*Range {
+	if len(rm.ranges) == 0 {
+		return nil
+	}
+
+	// Search returns the smallest i for which func returns true.
+	// We want the smallest range that is bigger than this key
+	// aka, starts AFTER this key, and then we'll go one left of it
+	leftIndex := sort.Search(len(rm.ranges), func(i int) bool {
+		//  0 if a==b, -1 if a < b, and +1 if a > b
+		return bytes.Compare(rm.ranges[i].Left, left) > 0
+	})
+
+	// Search returns the smallest i for which func returns true.
+	// We want the smallest range that is bigger than this key
+	// aka, starts AFTER this key, and then we'll go one left of it
+	rightIndex := sort.Search(len(rm.ranges), func(i int) bool {
+		//  0 if a==b, -1 if a < b, and +1 if a > b
+		return bytes.Compare(rm.ranges[i].Left, right) > 0
+	})
+
+	return rm.ranges[leftIndex-1 : rightIndex]
+}
+
+func (rm *RangeMap) Lookup(key []byte) interface{} {
+	if len(rm.ranges) == 0 {
+		return nil
+	}
+
+	// Search returns the smallest i for which func returns true.
+	// We want the smallest range that is bigger than this key
+	// aka, starts AFTER this key, and then we'll go one left of it
+	i := sort.Search(len(rm.ranges), func(i int) bool {
+		//  0 if a==b, -1 if a < b, and +1 if a > b
+		return bytes.Compare(rm.ranges[i].Left, key) > 0
+	})
+
+	// This is safe anyway because of how sort.Search works, but
+	// be clear so readers see this won't hit an out of range panic.
+	if i > 0 {
+		i -= 1
+	}
+	if rm.ranges[i].Contains(key) {
+		return rm.ranges[i].Val
+	}
+
+	return nil
+
+}
+
+func (rm *RangeMap) String() string {
+	buf := "RangeMap:\n"
+	for i, r := range rm.ranges {
+		buf += r.String()
+		if i != len(rm.ranges)-1 {
+			buf += "\n"
+		}
+	}
+	return buf
+}
+
+func (rm *RangeMap) Ranges() []*Range {
+	return rm.ranges
+}
+
+func (rm *RangeMap) Clear() {
+	rm.ranges = make([]*Range, 0)
+}
---
+++ server/util/rangemap/rangemap_test.go
@@ -0,0 +1,209 @@
+package rangemap_test
+
+import (
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/rangemap"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddOrdering(t *testing.T) {
+	r := rangemap.New()
+
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	addRange("c", "d", 2)
+	addRange("a", "b", 1)
+	addRange("g", "h", 4)
+	addRange("m", "n", 7)
+	addRange("k", "l", 6)
+	addRange("e", "f", 3)
+	addRange("o", "pp", 8)
+	addRange("i", "j", 5)
+
+	ranges := r.Ranges()
+
+	require.Equal(t, 8, len(ranges))
+	require.Equal(t, ranges[0].Val, 1)
+	require.Equal(t, ranges[1].Val, 2)
+	require.Equal(t, ranges[2].Val, 3)
+	require.Equal(t, ranges[3].Val, 4)
+	require.Equal(t, ranges[4].Val, 5)
+	require.Equal(t, ranges[5].Val, 6)
+	require.Equal(t, ranges[6].Val, 7)
+	require.Equal(t, ranges[7].Val, 8)
+}
+
+func TestAddOverlapError(t *testing.T) {
+	r := rangemap.New()
+
+	var err error
+	_, err = r.Add([]byte("a"), []byte("c"), 1)
+	require.Nil(t, err)
+
+	_, err = r.Add([]byte("b"), []byte("f"), 2)
+	require.Equal(t, rangemap.RangeOverlapError, err)
+	r.Clear()
+
+	_, err = r.Add([]byte("a"), []byte("z"), 1)
+	require.Nil(t, err)
+
+	_, err = r.Add([]byte("bbbb"), []byte("cccc"), 2)
+	require.Equal(t, rangemap.RangeOverlapError, err)
+}
+
+func TestAddDuplicateError(t *testing.T) {
+	r := rangemap.New()
+
+	var err error
+	_, err = r.Add([]byte("a"), []byte("c"), 1)
+	require.Nil(t, err)
+
+	_, err = r.Add([]byte("a"), []byte("c"), 1)
+	require.Equal(t, rangemap.RangeOverlapError, err)
+}
+
+func TestLookupSingleRange(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	// Single range
+	addRange("a", "z", 1)
+	require.Equal(t, 1, r.Lookup([]byte("a")))
+}
+
+func TestLookupMultiRange(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	// Lots of small ranges
+	addRange("a", "b", 1)
+	addRange("b", "c", 2)
+	addRange("c", "d", 3)
+	addRange("d", "e", 4)
+	addRange("e", "f", 5)
+	addRange("f", "g", 6)
+	addRange("g", "h", 7)
+	addRange("h", "iiii", 8)
+
+	require.Equal(t, 1, r.Lookup([]byte("a")))
+	require.Equal(t, 2, r.Lookup([]byte("b")))
+	require.Equal(t, 3, r.Lookup([]byte("c")))
+	require.Equal(t, 4, r.Lookup([]byte("d")))
+	require.Equal(t, 5, r.Lookup([]byte("e")))
+	require.Equal(t, 6, r.Lookup([]byte("f")))
+	require.Equal(t, 7, r.Lookup([]byte("g")))
+	require.Equal(t, 8, r.Lookup([]byte("h")))
+	require.Equal(t, 8, r.Lookup([]byte("i")))
+	require.Equal(t, nil, r.Lookup([]byte("z")))
+}
+
+func TestLookupSparseRange(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	// Sparse ranges
+	addRange("a", "e", 1)
+	// f-m is missing
+	addRange("m", "z", 2)
+
+	require.Equal(t, 1, r.Lookup([]byte("d")))
+	require.Equal(t, nil, r.Lookup([]byte("g")))
+	require.Equal(t, 2, r.Lookup([]byte("m")))
+}
+
+func TestLookupNarrowRange(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	addRange("aaaa", "aaac", 1)
+	addRange("aaad", "ffff", 2)
+
+	require.Equal(t, 1, r.Lookup([]byte("aaab")))
+	require.Equal(t, nil, r.Lookup([]byte("aaac")))
+	require.Equal(t, 2, r.Lookup([]byte("aaae")))
+	require.Equal(t, 2, r.Lookup([]byte("bbbb")))
+}
+
+func TestGet(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	addRange("aaaa", "aaac", 1)
+	addRange("aaad", "ffff", 2)
+
+	r1 := r.Get([]byte("aaaa"), []byte("aaac"))
+	r2 := r.Get([]byte("aaad"), []byte("ffff"))
+	require.Equal(t, 1, r1.Val)
+	require.Equal(t, 2, r2.Val)
+	require.Nil(t, r.Get([]byte("ffff"), []byte("z")))
+}
+
+func TestGetOverlapping(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	addRange("a", "e", 1)
+	addRange("e", "i", 2)
+	addRange("m", "q", 3)
+
+	overlap := r.GetOverlapping([]byte("d"), []byte("m"))
+
+	require.Equal(t, 1, overlap[0].Val)
+	require.Equal(t, 2, overlap[1].Val)
+	require.Equal(t, 3, overlap[2].Val)
+}
+
+func TestRemove(t *testing.T) {
+	r := rangemap.New()
+	addRange := func(left, right string, id int) {
+		_, err := r.Add([]byte(left), []byte(right), id)
+		require.Nil(t, err)
+	}
+
+	// Sparse ranges
+	addRange("a", "z", 1)
+
+	require.Equal(t, 1, r.Lookup([]byte("d")))
+
+	err := r.Remove([]byte("a"), []byte("z"))
+	require.Nil(t, err)
+
+	err = r.Remove([]byte("a"), []byte("z"))
+	require.NotNil(t, err)
+
+	require.Equal(t, nil, r.Lookup([]byte("d")))
+}
+
+func TestClear(t *testing.T) {
+	r := rangemap.New()
+
+	var err error
+	_, err = r.Add([]byte("a"), []byte("c"), 1)
+	require.Nil(t, err)
+	require.Equal(t, 1, len(r.Ranges()))
+
+	r.Clear()
+	require.Equal(t, 0, len(r.Ranges()))
+}
