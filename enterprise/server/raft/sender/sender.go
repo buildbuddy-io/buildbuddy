7dba73de05f96b09d610af7d4b11508c84c917e5
Tyler Williams <williams.tyler@gmail.com>
2021-12-15 16:40:08 -0800

    Improve sender algorithm to respect cached node if present

--- enterprise/server/raft/sender/sender.go
+++ enterprise/server/raft/sender/sender.go
@@ -2,6 +2,7 @@ package sender
 
 import (
 	"context"
+	"sync"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/raft/client"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/raft/constants"
@@ -10,6 +11,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/raft/rbuilder"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/raft/registry"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/retry"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/golang/protobuf/proto"
 
@@ -26,13 +28,18 @@ type Sender struct {
 
 	// Keeps track of connections to other machines.
 	apiClient *client.APIClient
+
+	// Map of connections for eache rangeID and mutex to protec.
+	mu             sync.RWMutex
+	cachedReplicas map[uint64]*rfpb.ReplicaDescriptor
 }
 
 func New(rangeCache *rangecache.RangeCache, nodeRegistry *registry.DynamicNodeRegistry, apiClient *client.APIClient) *Sender {
 	return &Sender{
-		rangeCache:   rangeCache,
-		nodeRegistry: nodeRegistry,
-		apiClient:    apiClient,
+		rangeCache:     rangeCache,
+		nodeRegistry:   nodeRegistry,
+		apiClient:      apiClient,
+		cachedReplicas: make(map[uint64]*rfpb.ReplicaDescriptor),
 	}
 }
 
@@ -97,7 +104,7 @@ func (s *Sender) fetchRangeDescriptorFromMetaRange(ctx context.Context, key []by
 	return nil, status.UnavailableErrorf("Error finding range descriptor for %q", key)
 }
 
-func (s *Sender) getOrCreateRangeDescriptor(ctx context.Context, key []byte) (*rfpb.RangeDescriptor, error) {
+func (s *Sender) lookupRangeDescriptor(ctx context.Context, key []byte) (*rfpb.RangeDescriptor, error) {
 	rangeDescriptor := s.rangeCache.Get(key)
 	// TODO(tylerw): loop until available or context timeout?
 	if rangeDescriptor == nil {
@@ -114,7 +121,7 @@ func (s *Sender) getOrCreateRangeDescriptor(ctx context.Context, key []byte) (*r
 }
 
 func (s *Sender) GetAllNodes(ctx context.Context, key []byte) ([]string, error) {
-	rangeDescriptor, err := s.getOrCreateRangeDescriptor(ctx, key)
+	rangeDescriptor, err := s.lookupRangeDescriptor(ctx, key)
 	if err != nil {
 		return nil, err
 	}
@@ -131,38 +138,78 @@ func (s *Sender) GetAllNodes(ctx context.Context, key []byte) ([]string, error) 
 	return allNodes, nil
 }
 
-func (s *Sender) Run(ctx context.Context, key []byte, fn func(c rfspb.ApiClient, h *rfpb.Header) error) error {
-	rangeDescriptor, err := s.getOrCreateRangeDescriptor(ctx, key)
-	if err != nil {
-		return err
+func (s *Sender) orderReplicas(rd *rfpb.RangeDescriptor) []*rfpb.ReplicaDescriptor {
+	s.mu.RLock()
+	cachedReplica, ok := s.cachedReplicas[rd.GetRangeId()]
+	s.mu.RUnlock()
+
+	if !ok {
+		return rd.GetReplicas()
 	}
 
-	var lastErr error
-	for _, replica := range rangeDescriptor.GetReplicas() {
-		addr, _, err := s.nodeRegistry.ResolveGRPC(replica.GetClusterId(), replica.GetNodeId())
-		if err != nil {
-			log.Printf("ResolveGRPC returned err: %s, continuing", err)
-			lastErr = err
+	replicas := make([]*rfpb.ReplicaDescriptor, 0, len(rd.GetReplicas()))
+	replicas = append(replicas, cachedReplica)
+
+	for _, replica := range rd.GetReplicas() {
+		if replica.GetClusterId() == cachedReplica.GetClusterId() &&
+			replica.GetNodeId() == cachedReplica.GetNodeId() {
 			continue
 		}
-		client, err := s.apiClient.Get(ctx, addr)
-		if err != nil {
-			lastErr = err
-			continue
-		}
-		header := &rfpb.Header{
-			Replica: replica,
-			RangeId: rangeDescriptor.GetRangeId(),
-		}
-		lastErr = fn(client, header)
-		if lastErr != nil {
-			if status.IsOutOfRangeError(lastErr) || status.IsUnavailableError(lastErr) {
-				continue
-			}
-		}
-		return lastErr
+		replicas = append(replicas, replica)
 	}
-	return status.UnavailableErrorf("Run failed. (%d attempts): %s", len(rangeDescriptor.GetReplicas()), lastErr)
+	return replicas
+}
+
+func (s *Sender) Run(ctx context.Context, key []byte, fn func(c rfspb.ApiClient, h *rfpb.Header) error) error {
+	retrier := retry.DefaultWithContext(ctx)
+	var lastErr error
+	for retrier.Next() {
+		rangeDescriptor, err := s.lookupRangeDescriptor(ctx, key)
+		if err != nil {
+			lastErr = err
+			continue
+		}
+		replicas := s.orderReplicas(rangeDescriptor)
+		for _, replica := range replicas {
+			addr, _, err := s.nodeRegistry.ResolveGRPC(replica.GetClusterId(), replica.GetNodeId())
+			if err != nil {
+				// If we can't resolve the node, probably best
+				// to back off and wait for it.
+				log.Printf("ResolveGRPC returned err: %s, breaking", err)
+				//return err
+				lastErr = err
+				break
+			}
+			client, err := s.apiClient.Get(ctx, addr)
+			if err != nil {
+				log.Printf("apiClient.Get returned err: %s, breaking", err)
+				//return err
+				lastErr = err
+				break
+			}
+
+			header := &rfpb.Header{
+				Replica: replica,
+				RangeId: rangeDescriptor.GetRangeId(),
+			}
+			lastErr = fn(client, header)
+			if lastErr != nil {
+				if status.IsOutOfRangeError(lastErr) || status.IsUnavailableError(lastErr) {
+					log.Printf("sender.Run got outOfRange or Unavailable on %+v, continuing to next replica", replica)
+					continue
+				}
+			} else {
+				// TODO(tylerw): Make this lock free?
+				s.mu.Lock()
+				if _, ok := s.cachedReplicas[rangeDescriptor.GetRangeId()]; !ok {
+					s.cachedReplicas[rangeDescriptor.GetRangeId()] = replica
+				}
+				s.mu.Unlock()
+			}
+			return lastErr
+		}
+	}
+	return lastErr
 }
 
 func (s *Sender) SyncPropose(ctx context.Context, key []byte, batchCmd *rfpb.BatchCmdRequest) (*rfpb.BatchCmdResponse, error) {
