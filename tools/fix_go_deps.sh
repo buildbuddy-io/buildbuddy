0a881d1d105eb74191d3e321c99b30568edfe20d
Brandon Duffany <brandon@buildbuddy.io>
2022-01-26 21:10:51 -0500

    Add CI check and fix script for go deps

---
+++ tools/fix_go_deps.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+: "${GAZELLE_PATH:=}"
+
+GAZELLE_COMMAND=(bazelisk run //:gazelle --)
+if [[ "$GAZELLE_PATH" ]]; then
+  GAZELLE_COMMAND=("$GAZELLE_PATH")
+fi
+
+DIFF_MODE=0
+if [[ "${1:-}" == "-d" ]] || [[ "${1:-}" == "--diff" ]]; then
+  DIFF_MODE=1
+fi
+
+if ((DIFF_MODE)); then
+  if ! git diff --quiet; then
+    echo >&2 "Git working tree is dirty. To run in diff mode, 'check_go_deps.sh' must be run from a clean tree."
+    git status --short --untracked=no 1>&2
+    exit 1
+  fi
+fi
+
+tmp_log_file=$(mktemp)
+cleanup() {
+  if ((DIFF_MODE)); then
+    git restore go.mod go.sum deps.bzl
+  fi
+  rm -r "$tmp_log_file"
+}
+trap cleanup EXIT
+
+# go mod tidy fails if generated sources are not checked into the repo,
+# and we don't want to require that (yet, at least). So use the `-e`
+# option to ask `go mod tidy` to proceed even if it encounters errors
+# loading packages.
+if ! go mod tidy -e &>"$tmp_log_file"; then
+  echo "Command 'go mod tidy -e' failed. Logs:" >&2
+  cat "$tmp_log_file" >&2
+  exit 1
+fi
+
+# Update deps.bzl (using Gazelle)
+if ! "${GAZELLE_COMMAND[@]}" update-repos -from_file=go.mod \
+  -to_macro=deps.bzl%install_buildbuddy_dependencies &>"$tmp_log_file"; then
+  echo "Auto-updating 'deps.bzl' failed. Logs:" >&2
+  cat "$tmp_log_file" >&2
+  exit 1
+fi
+
+if ((DIFF_MODE)); then
+  git diff
+fi
