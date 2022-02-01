519d77397278e825c6a76b91ceb74ef794affe7e
Brandon Duffany <brandon@buildbuddy.io>
2022-02-01 16:44:57 -0500

    Remove APIKey dep from fetchWorkflowConfig

--- enterprise/server/workflow/service/service.go
+++ enterprise/server/workflow/service/service.go
@@ -411,7 +411,7 @@ func (ws *workflowService) ExecuteWorkflow(ctx context.Context, req *wfpb.Execut
 	if err != nil {
 		return nil, err
 	}
-	cfg, err := ws.fetchWorkflowConfig(ctx, apiKey, gitProvider, wf, wd)
+	cfg, err := ws.fetchWorkflowConfig(ctx, gitProvider, wf, wd)
 	if err != nil {
 		return nil, err
 	}
@@ -717,7 +717,7 @@ func (ws *workflowService) checkStartWorkflowPreconditions(ctx context.Context) 
 
 // fetchWorkflowConfig returns the BuildBuddyConfig from the repo, or the
 // default BuildBuddyConfig if one is not set up.
-func (ws *workflowService) fetchWorkflowConfig(ctx context.Context, key *tables.APIKey, gitProvider interfaces.GitProvider, workflow *tables.Workflow, webhookData *interfaces.WebhookData) (*config.BuildBuddyConfig, error) {
+func (ws *workflowService) fetchWorkflowConfig(ctx context.Context, gitProvider interfaces.GitProvider, workflow *tables.Workflow, webhookData *interfaces.WebhookData) (*config.BuildBuddyConfig, error) {
 	b, err := gitProvider.GetFileContents(ctx, workflow.AccessToken, webhookData.PushedRepoURL, config.FilePath, webhookData.SHA)
 	if err != nil {
 		if status.IsNotFoundError(err) {
@@ -754,7 +754,7 @@ func (ws *workflowService) startWorkflow(webhookID string, r *http.Request) erro
 	}
 	// Fetch the workflow config (buildbuddy.yaml) and start a CI runner execution
 	// for each action matching the webhook event.
-	cfg, err := ws.fetchWorkflowConfig(ctx, apiKey, gitProvider, wf, webhookData)
+	cfg, err := ws.fetchWorkflowConfig(ctx, gitProvider, wf, webhookData)
 	if err != nil {
 		return err
 	}
