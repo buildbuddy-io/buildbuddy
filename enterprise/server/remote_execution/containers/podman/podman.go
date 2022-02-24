e6d7fbadd671ce39cd4ddd8f2a6db22032212d5b
Lulu Zhang <lulu@buildbuddy.io>
2022-02-24 14:12:30 -0800

    Podman Prototype and Unit test (#1635)
    * podman prototype and unit-test
    
    * address comments
    
    * Use podman to run remote_execution (#1636)
    
    Tested locally.
    
    * fix build

---
+++ enterprise/server/remote_execution/containers/podman/podman.go
@@ -0,0 +1,170 @@
+package podman
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/commandutil"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/container"
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/util/background"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/random"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+)
+
+var (
+	// Additional time used to kill the container if the command doesn't exit cleanly
+	containerFinalizationTimeout = 10 * time.Second
+)
+
+// podmanCommandContainer containerizes a command's execution using a Podman container.
+// between containers.
+type podmanCommandContainer struct {
+	image       string
+	hostRootDir string
+	// workDir is the path to the workspace directory mounted to the container.
+	workDir string
+}
+
+func NewPodmanCommandContainer(image, hostRootDir string) container.CommandContainer {
+	return &podmanCommandContainer{
+		image:       image,
+		hostRootDir: hostRootDir,
+	}
+}
+
+func (c *podmanCommandContainer) Run(ctx context.Context, command *repb.Command, workDir string, creds container.PullCredentials) *interfaces.CommandResult {
+	result := &interfaces.CommandResult{
+		CommandDebugString: fmt.Sprintf("(podman) %s", command.GetArguments()),
+		ExitCode:           commandutil.NoExitCode,
+	}
+	containerName, err := generateContainerName()
+	if err != nil {
+		result.Error = status.UnavailableErrorf("failed to generate podman container name: %s", err)
+		return result
+	}
+	if err := c.PullImageIfNecessary(ctx); err != nil {
+		result.Error = status.UnavailableErrorf("failed to pull docker image: %s", err)
+		return result
+	}
+
+	podmanRunArgs := []string{
+		"--hostname",
+		"localhost",
+		"--workdir",
+		workDir,
+		"--name",
+		containerName,
+		"--rm",
+		"--network=none",
+		"--volume",
+		fmt.Sprintf(
+			"%s:%s",
+			filepath.Join(c.hostRootDir, filepath.Base(workDir)),
+			workDir,
+		),
+	}
+
+	for _, envVar := range command.GetEnvironmentVariables() {
+		podmanRunArgs = append(podmanRunArgs, "--env", fmt.Sprintf("%s=%s", envVar.GetName(), envVar.GetValue()))
+	}
+	podmanRunArgs = append(podmanRunArgs, c.image)
+	podmanRunArgs = append(podmanRunArgs, command.Arguments...)
+	result = runPodman(ctx, "run", "", nil, nil, podmanRunArgs...)
+	if exitedCleanly := result.ExitCode >= 0; !exitedCleanly {
+		err = killContainerIfRunning(ctx, containerName)
+	}
+	if err != nil {
+		log.Warningf("Failed to shut down docker container: %s\n", err.Error())
+	}
+	return result
+}
+
+func (c *podmanCommandContainer) Create(ctx context.Context, workDir string) error {
+	c.workDir = workDir
+	return nil
+}
+
+func (c *podmanCommandContainer) Exec(ctx context.Context, cmd *repb.Command, stdin io.Reader, stdout io.Writer) *interfaces.CommandResult {
+	return runPodman(ctx, "run" /*workDir=*/, "", stdin, stdout, cmd.Arguments...)
+}
+
+func (c *podmanCommandContainer) IsImageCached(ctx context.Context) (bool, error) {
+	// Try to avoid the `pull` command which results in a network roundtrip.
+	listResult := runPodman(ctx, "images", "", nil, nil, "--filter=reference="+c.image, "--format={{.ID}}")
+	if listResult.Error != nil {
+		return false, listResult.Error
+	}
+	if strings.TrimSpace(string(listResult.Stdout)) != "" {
+		// Found at least one image matching the ref; `docker run` should succeed
+		// without pulling the image.
+		return true, nil
+	}
+	return false, nil
+}
+
+func (c *podmanCommandContainer) PullImage(ctx context.Context, creds container.PullCredentials) error {
+	pullResult := runPodman(ctx, "pull", "", nil, nil, c.image)
+	if pullResult.Error != nil {
+		return pullResult.Error
+	}
+	return nil
+}
+func (c *podmanCommandContainer) Start(ctx context.Context) error   { return nil }
+func (c *podmanCommandContainer) Remove(ctx context.Context) error  { return nil }
+func (c *podmanCommandContainer) Pause(ctx context.Context) error   { return nil }
+func (c *podmanCommandContainer) Unpause(ctx context.Context) error { return nil }
+
+func (c *podmanCommandContainer) Stats(ctx context.Context) (*container.Stats, error) {
+	return &container.Stats{}, nil
+}
+
+func runPodman(ctx context.Context, subCommand string, workDir string, stdin io.Reader, stdout io.Writer, args ...string) *interfaces.CommandResult {
+	command := []string{"podman", subCommand}
+
+	command = append(command, args...)
+	result := commandutil.Run(ctx, &repb.Command{Arguments: command}, workDir, stdin, stdout)
+	return result
+}
+
+func generateContainerName() (string, error) {
+	suffix, err := random.RandomString(20)
+	if err != nil {
+		return "", err
+	}
+	return "buildbuddy_exec_" + suffix, nil
+}
+
+// TODO(luluz): replace this function with container.PullImageIfNecessary
+func (r *podmanCommandContainer) PullImageIfNecessary(ctx context.Context) error {
+	isCached, err := r.IsImageCached(ctx)
+	if err != nil {
+		return err
+	}
+	if isCached {
+		return nil
+	}
+
+	return r.PullImage(ctx, container.PullCredentials{})
+}
+
+func killContainerIfRunning(ctx context.Context, containerName string) error {
+	ctx, cancel := background.ExtendContextForFinalization(ctx, containerFinalizationTimeout)
+	defer cancel()
+
+	result := runPodman(ctx, "kill", "", nil, nil, containerName)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.ExitCode == 0 || strings.Contains(string(result.Stderr), "No such container: "+containerName) {
+		return nil
+	}
+	return status.UnknownErrorf("podman kill failed: %s", string(result.Stderr))
+}
---
+++ enterprise/server/remote_execution/containers/podman/podman_test.go
@@ -0,0 +1,58 @@
+package podman_test
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/container"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/containers/podman"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testfs"
+	"github.com/stretchr/testify/assert"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+)
+
+func writeFile(t *testing.T, parentDir, fileName, content string) {
+	path := filepath.Join(parentDir, fileName)
+	if err := ioutil.WriteFile(path, []byte(content), 0660); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func makeTempDirWithWorldTxt(t *testing.T) string {
+	dir := testfs.MakeTempDir(t)
+	workDir := testfs.MakeDirAll(t, dir, "work")
+	writeFile(t, workDir, "world.txt", "world")
+	return dir
+}
+
+func TestHelloWorld(t *testing.T) {
+	ctx := context.Background()
+	rootDir := makeTempDirWithWorldTxt(t)
+	cmd := &repb.Command{
+		EnvironmentVariables: []*repb.Command_EnvironmentVariable{
+			&repb.Command_EnvironmentVariable{Name: "GREETING", Value: "Hello"},
+		},
+		Arguments: []string{"sh", "-c", `printf "$GREETING $(cat world.txt)!"`},
+	}
+	// Need to give enough time to download the Docker image.
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+	defer cancel()
+
+	podman := podman.NewPodmanCommandContainer("docker.io/library/busybox", rootDir)
+	result := podman.Run(ctx, cmd, "/work", container.PullCredentials{})
+
+	if result.Error != nil {
+		t.Fatal(result.Error)
+	}
+	assert.Regexp(t, "^(/usr)?/bin/podman\\s", result.CommandDebugString, "sanity check: command should be run bare")
+	assert.Equal(t, "Hello world!", string(result.Stdout),
+		"stdout should equal 'Hello world!' ('$GREETING' env var should be replaced with 'Hello', and "+
+			"tempfile containing 'world' should be readable.)",
+	)
+	assert.Empty(t, string(result.Stderr), "stderr should be empty")
+	assert.Equal(t, 0, result.ExitCode, "should exit with success")
+}
