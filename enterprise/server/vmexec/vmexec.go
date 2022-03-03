c06d3c74f754eb0805a82bf25378085f184ee820
Brandon Duffany <brandon@buildbuddy.io>
2022-03-03 15:19:23 -0500

    Use consts

--- enterprise/server/vmexec/vmexec.go
+++ enterprise/server/vmexec/vmexec.go
@@ -17,6 +17,14 @@ import (
 	vmxpb "github.com/buildbuddy-io/buildbuddy/proto/vmexec"
 )
 
+const (
+	// workspaceDevice is the path to the hot-swappable workspace block device.
+	workspaceDevice = "/dev/vdc"
+	// workspaceMountPath is the path where the hot-swappable workspace block
+	// device is mounted.
+	workspaceMountPath = "/workspace"
+)
+
 type execServer struct {
 	reapMutex *sync.RWMutex
 }
@@ -77,14 +85,14 @@ func (x *execServer) Initialize(ctx context.Context, req *vmxpb.InitializeReques
 }
 
 func (x *execServer) UnmountWorkspace(ctx context.Context, req *vmxpb.UnmountWorkspaceRequest) (*vmxpb.UnmountWorkspaceResponse, error) {
-	if err := syscall.Unmount("/workspace", 0); err != nil {
+	if err := syscall.Unmount(workspaceMountPath, 0); err != nil {
 		return nil, status.InternalErrorf("unmount failed: %s", err)
 	}
 	return &vmxpb.UnmountWorkspaceResponse{}, nil
 }
 
 func (x *execServer) MountWorkspace(ctx context.Context, req *vmxpb.MountWorkspaceRequest) (*vmxpb.MountWorkspaceResponse, error) {
-	if err := syscall.Mount("/dev/vdc", "/workspace", "ext4", syscall.MS_RELATIME, ""); err != nil {
+	if err := syscall.Mount(workspaceDevice, workspaceMountPath, "ext4", syscall.MS_RELATIME, ""); err != nil {
 		return nil, err
 	}
 	return &vmxpb.MountWorkspaceResponse{}, nil
