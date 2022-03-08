76b748bd73fe0e6d31d0a0a77332b79002e6f746
Brandon Duffany <brandon@buildbuddy.io>
2022-03-08 12:51:04 -0500

    Add filecache file size metric

--- enterprise/server/remote_execution/filecache/filecache.go
+++ enterprise/server/remote_execution/filecache/filecache.go
@@ -218,6 +218,7 @@ func (c *fileCache) AddFile(node *repb.FileNode, existingFilePath string) {
 		sizeBytes:   node.GetDigest().GetSizeBytes(),
 		value:       fp,
 	}
+	metrics.FileCacheFileSizeBytes.Observe(float64(e.sizeBytes))
 	c.l.Add(key(node), e)
 }
 
