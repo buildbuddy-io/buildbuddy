9bdbc2cb41c40ba867e0af6119f90170cc847729
Lulu Zhang <lulu@buildbuddy.io>
2022-03-09 11:15:02 -0800

    fix test

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -772,17 +772,31 @@ func postMigrateInvocationUUIDForSQLite(db *gorm.DB) error {
 // Manual migration called after auto-migration.
 func PostAutoMigrate(db *gorm.DB) error {
 	indexes := map[string]string{
-		"invocations_trends_query_index":            "(`group_id`, `updated_at_usec`)",
-		"invocations_trends_query_role_index":       "(`group_id`, `role`, `updated_at_usec`)",
-		"invocations_stats_group_id_index":          "(`group_id`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_user_index":              "(`group_id`, `user`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_host_index":              "(`group_id`, `host`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_repo_index":              "(`group_id`, `repo_url`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_branch_index":            "(`group_id`, `branch_name`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_commit_index":            "(`group_id`, `commit_sha`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_stats_role_index":              "(`group_id`, `role`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
-		"invocations_test_grid_query_command_index": "(`group_id` (25), `role` (10), `repo_url`, `command` (10), `created_at_usec` DESC)",
+		"invocations_trends_query_index":      "(`group_id`, `updated_at_usec`)",
+		"invocations_trends_query_role_index": "(`group_id`, `role`, `updated_at_usec`)",
+		"invocations_stats_group_id_index":    "(`group_id`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_user_index":        "(`group_id`, `user`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_host_index":        "(`group_id`, `host`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_repo_index":        "(`group_id`, `repo_url`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_branch_index":      "(`group_id`, `branch_name`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_commit_index":      "(`group_id`, `commit_sha`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
+		"invocations_stats_role_index":        "(`group_id`, `role`, `action_count`, `duration_usec`, `updated_at_usec`, `success`, `invocation_status`)",
 	}
+	prefixIndicesByDialect := map[string]map[string]string{
+		mysqlDialect: map[string]string{
+			"invocations_test_grid_query_command_index": "(`group_id` (25), `role` (10), `repo_url`, `command` (10), `created_at_usec` DESC)",
+		},
+		sqliteDialect: map[string]string{
+			"invocations_test_grid_query_command_index": "(`group_id`, `role`, `repo_url`, `command` , `created_at_usec` DESC)",
+		},
+	}
+	prefixIndexes, ok := prefixIndicesByDialect[db.Dialector.Name()]
+	if ok {
+		for name, cols := range prefixIndexes {
+			indexes[name] = cols
+		}
+	}
+
 	m := db.Migrator()
 	if m.HasTable("Invocations") {
 		for indexName, cols := range indexes {
