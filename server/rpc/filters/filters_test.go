c4ca29bbf2cba429a55fbc9816d398bba981616a
Tyler Williams <williams.tyler@gmail.com>
2022-04-11 12:09:15 -0700

    Benchmark interceptors

---
+++ server/rpc/filters/filters_test.go
@@ -0,0 +1,109 @@
+package filters_test
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testport"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_server"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/random"
+	"google.golang.org/grpc"
+
+	pspb "github.com/buildbuddy-io/buildbuddy/proto/ping_service"
+)
+
+type pingServer struct{}
+func (p *pingServer) Ping(ctx context.Context, req *pspb.PingRequest) (*pspb.PingResponse, error) {
+	return &pspb.PingResponse{
+		Tag: req.GetTag(),
+	}, nil
+}
+
+func BenchmarkBare(b *testing.B) {
+	ctx := context.Background()
+	listenAddr := fmt.Sprintf("localhost:%d", testport.FindFree(b))	
+	grpcServer := grpc.NewServer()
+	ps := &pingServer{}
+	pspb.RegisterApiServer(grpcServer, ps)
+
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	go func() {
+		grpcServer.Serve(lis)
+	}()
+
+	conn, err := grpc.Dial(listenAddr, grpc.WithInsecure())
+	if err != nil {
+		b.Fatal(err)
+	}
+	client := pspb.NewApiClient(conn)
+
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+                req := &pspb.PingRequest{
+			Tag: random.RandUint64(),
+		}
+                b.StartTimer()
+		rsp, err := client.Ping(ctx, req)
+		if err != nil {
+                        b.Fatal(err)
+		}
+		if rsp.GetTag() != req.GetTag() {
+			b.Fatal("tag mismatch")
+		}
+        }
+	grpcServer.Stop()
+}
+
+
+func BenchmarkInstrumented(b *testing.B) {
+	*log.LogLevel = "error"
+	*log.IncludeShortFileName = true
+	log.Configure()
+
+	ctx := context.Background()
+	listenAddr := fmt.Sprintf("localhost:%d", testport.FindFree(b))	
+	
+	env := testenv.GetTestEnv(b)
+	grpcOptions := grpc_server.CommonGRPCServerOptions(env)
+	grpcServer := grpc.NewServer(grpcOptions...)
+	ps := &pingServer{}
+	pspb.RegisterApiServer(grpcServer, ps)
+
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	go func() {
+		grpcServer.Serve(lis)
+	}()
+	conn, err := grpc_client.DialTarget("grpc://"+listenAddr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	client := pspb.NewApiClient(conn)
+
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+                req := &pspb.PingRequest{
+			Tag: random.RandUint64(),
+		}
+                b.StartTimer()
+		rsp, err := client.Ping(ctx, req)
+		if err != nil {
+                        b.Fatal(err)
+		}
+		if rsp.GetTag() != req.GetTag() {
+			b.Fatal("tag mismatch")
+		}
+        }
+	grpcServer.Stop()
+}
+
