e753ffa527b2fb96b4095d4ee6acb29c32dfbc0f
Brandon Duffany <brandon@buildbuddy.io>
2022-06-06 15:43:29 -0400

    Add a target history button (#2100)

--- app/components/button/link_button.tsx
+++ app/components/button/link_button.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Link from "../link/link";
 
-export type LinkButtonProps = JSX.IntrinsicElements["a"];
+export type LinkButtonProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const LinkButton = React.forwardRef((props: LinkButtonProps, ref: React.Ref<HTMLAnchorElement>) => {
   const { className, ...rest } = props;
-  return <a role="button" ref={ref} className={`base-button filled-button link-button ${className || ""}`} {...rest} />;
+  return (
+    <Link role="button" ref={ref} className={`base-button filled-button link-button ${className || ""}`} {...rest} />
+  );
 });
 
 export const OutlinedLinkButton = React.forwardRef((props: LinkButtonProps, ref: React.Ref<HTMLAnchorElement>) => {
   const { className, ...rest } = props;
   return (
-    <a role="button" ref={ref} className={`base-button outlined-button link-button ${className || ""}`} {...rest} />
+    <Link role="button" ref={ref} className={`base-button outlined-button link-button ${className || ""}`} {...rest} />
   );
 });
 
