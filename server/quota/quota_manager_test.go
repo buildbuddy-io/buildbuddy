8e115c19c9e89ee05d13ad5bb30bf6f92fc79ca3
Lulu Zhang <lulu@buildbuddy.io>
2022-06-13 13:40:06 -0700

    add tests

---
+++ server/quota/quota_manager_test.go
@@ -0,0 +1,96 @@
+package quota
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/server/environment"
+	"github.com/buildbuddy-io/buildbuddy/server/tables"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testBucket struct {
+	config *tables.QuotaBucket
+}
+
+func (tb *testBucket) Config() *tables.QuotaBucket {
+	return tb.config
+}
+
+func (tb *testBucket) Allow(ctx context.Context, key string, quantity int64) (bool, error) {
+	return true, nil
+}
+
+func createTestBucket(env environment.Env, config *tables.QuotaBucket) (Bucket, error) {
+	return &testBucket{
+		config: config,
+	}, nil
+}
+
+func TestQuotaManagerFindRateLimiter(t *testing.T) {
+	env := testenv.GetTestEnv(t)
+	ctx := context.Background()
+
+	db := env.GetDBHandle().DB(ctx)
+	buckets := []*tables.QuotaBucket{
+		{
+			QuotaBucketID:      "abc123",
+			Namespace:          "remote_execution",
+			Prefix:             "default",
+			NumRequests:        100,
+			PeriodDurationUsec: int64(time.Second / time.Microsecond),
+			MaxBurst:           105,
+		},
+		{
+			QuotaBucketID:      "abc124",
+			Namespace:          "remote_execution",
+			Prefix:             "restricted",
+			NumRequests:        10,
+			PeriodDurationUsec: int64(time.Second / time.Microsecond),
+			MaxBurst:           12,
+		},
+	}
+
+	quotaGroup := &tables.QuotaGroup{
+		QuotaBucketID: "abc124",
+		QuotaKey:      "GR123456",
+	}
+	result := db.Create(&buckets)
+	require.NoError(t, result.Error)
+	result = db.Create(quotaGroup)
+	require.NoError(t, result.Error)
+
+	qm, err := newQuotaManager(env, createTestBucket)
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name       string
+		quotaKey   string
+		namespace  string
+		wantConfig *tables.QuotaBucket
+	}{
+		{
+			name:       "restricted bucket",
+			quotaKey:   "GR123456",
+			namespace:  "remote_execution",
+			wantConfig: buckets[1],
+		},
+		{
+			name:       "default bucket",
+			quotaKey:   "GR0000",
+			namespace:  "remote_execution",
+			wantConfig: buckets[0],
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			bucket := qm.findBucket(tc.namespace, tc.quotaKey)
+			config := bucket.Config()
+			assert.Equal(t, config, tc.wantConfig)
+		})
+	}
+}
