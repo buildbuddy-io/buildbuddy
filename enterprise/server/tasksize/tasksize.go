a1ee8890f575cbad21e9e466270e900c39358388
Brandon Duffany <brandon@buildbuddy.io>
2022-06-22 12:23:18 -0400

    Add flag to store last recorded task size in Redis

--- enterprise/server/tasksize/tasksize.go
+++ enterprise/server/tasksize/tasksize.go
@@ -2,15 +2,27 @@ package tasksize
 
 import (
 	"context"
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/platform"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/redisutil"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/perms"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/go-redis/redis/v8"
+	"github.com/gogo/protobuf/proto"
 
 	espb "github.com/buildbuddy-io/buildbuddy/proto/execution_stats"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	scpb "github.com/buildbuddy-io/buildbuddy/proto/scheduler"
+)
+
+var (
+	adaptiveSizingEnabled = flag.Bool("remote_execution.adaptive_task_sizing_enabled", false, "Whether to adapt task sizing estimates based on task usage stats.")
 )
 
 const (
@@ -52,6 +64,23 @@ const (
 
 	// The fraction of an executor's allocatable resources to make available for task sizing.
 	MaxResourceCapacityRatio = 0.8
+
+	// The expiration for all task-sizing keys stored in Redis.
+	redisKeyExpiration = 12 * time.Hour
+	// Redis key prefix used for holding current task size estimates.
+	redisKeyPrefix = "taskSize"
+	// Redis hash key used to hold the CPU estimate for a task (in milli-CPU).
+	// The hash is stored under a key derived from the task parameters.
+	redisCPUHashKey = "milliCPU"
+	// Redis hash key used to hold the memory estimate (in bytes). The hash is
+	// stored under a key derived from the task.
+	redisMemHashKey = "memBytes"
+
+	// When using adaptive task sizing, multiply the stat-based memory estimate
+	// by this much in order to determine effective the memory estimate. This
+	// allows some wiggle room for new tasks to use more memory than the
+	// previously recorded task.
+	adaptiveSizingMemoryMultiplier = 1.10
 )
 
 // Register registers the task sizer with the env.
@@ -64,20 +93,141 @@ func Register(env environment.Env) error {
 	return nil
 }
 
-type taskSizer struct{}
+type taskSizer struct {
+	env environment.Env
+	rdb redis.UniversalClient
+}
 
 func NewSizer(env environment.Env) (*taskSizer, error) {
-	return &taskSizer{}, nil
+	var rdb redis.UniversalClient
+	if *adaptiveSizingEnabled {
+		rdb = env.GetRemoteExecutionRedisClient()
+		if rdb == nil {
+			return nil, status.FailedPreconditionError("missing Redis client configuration")
+		}
+	}
+	return &taskSizer{
+		env: env,
+		rdb: rdb,
+	}, nil
 }
 
 func (s *taskSizer) Estimate(ctx context.Context, task *repb.ExecutionTask) *scpb.TaskSize {
-	// TODO(bduffany): Use previous execution stats
-	return Estimate(task)
+	defaultSize := Estimate(task)
+	if !*adaptiveSizingEnabled {
+		return defaultSize
+	}
+	recordedSize, err := s.lastRecordedSize(ctx, task)
+	if err != nil {
+		log.Warningf("Failed to read task size from Redis; falling back to default size estimate: %s", err)
+		return defaultSize
+	}
+	if recordedSize == nil {
+		return defaultSize
+	}
+	return &scpb.TaskSize{
+		EstimatedMemoryBytes: int64(float64(recordedSize.EstimatedMemoryBytes) * adaptiveSizingMemoryMultiplier),
+		EstimatedMilliCpu:    recordedSize.EstimatedMilliCpu,
+	}
 }
 
 func (s *taskSizer) Update(ctx context.Context, cmd *repb.Command, summary *espb.ExecutionSummary) error {
-	// TODO(bduffany): Implement
-	return status.UnimplementedError("not implemented")
+	if !*adaptiveSizingEnabled {
+		return nil
+	}
+	// If we are missing CPU/memory stats, do nothing. This is expected for
+	// tasks that either completed too quickly to get a sample of their CPU/mem
+	// usage, or tasks where the workload isolation type doesn't yet support the
+	// Stats() API.
+	stats := summary.GetUsageStats()
+	if stats.GetCpuNanos() == 0 || stats.GetPeakMemoryBytes() == 0 {
+		return nil
+	}
+	md := summary.GetExecutedActionMetadata()
+	execDuration := md.GetExecutionCompletedTimestamp().AsTime().Sub(md.GetExecutionStartTimestamp().AsTime())
+	// If execution duration is missing or invalid, we won't be able to compute
+	// milli-CPU usage.
+	if execDuration <= 0 {
+		return status.InvalidArgumentErrorf("execution duration is missing or invalid")
+	}
+	// Compute milliCPU as CPU-milliseconds used per second of execution time.
+	milliCPU := int64((float64(stats.GetCpuNanos()) / 1e6) / execDuration.Seconds())
+	key, err := s.taskSizeKey(ctx, cmd)
+	if err != nil {
+		return err
+	}
+	pipe := s.rdb.TxPipeline()
+	pipe.HSet(ctx, key, redisMemHashKey, stats.GetPeakMemoryBytes())
+	pipe.HSet(ctx, key, redisCPUHashKey, milliCPU)
+	pipe.Expire(ctx, key, redisKeyExpiration)
+	_, err = pipe.Exec(ctx)
+	return err
+}
+
+func (s *taskSizer) lastRecordedSize(ctx context.Context, task *repb.ExecutionTask) (*scpb.TaskSize, error) {
+	key, err := s.taskSizeKey(ctx, task.GetCommand())
+	if err != nil {
+		return nil, err
+	}
+	m, err := s.rdb.HGetAll(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+	// Note: if the key is not found, HGETALL returns an empty map, not a
+	// redis.Nil error.
+	if len(m) == 0 {
+		return nil, nil
+	}
+	mInt64, err := redisutil.Int64Values(m)
+	if err != nil {
+		return nil, err
+	}
+	if mInt64[redisMemHashKey] == 0 || mInt64[redisCPUHashKey] == 0 {
+		return nil, status.InternalError("found invalid task size stored in Redis")
+	}
+	return &scpb.TaskSize{
+		EstimatedMemoryBytes: mInt64[redisMemHashKey],
+		EstimatedMilliCpu:    mInt64[redisCPUHashKey],
+	}, nil
+}
+
+func (s *taskSizer) taskSizeKey(ctx context.Context, cmd *repb.Command) (string, error) {
+	// Get group ID (task sizing is segmented by group)
+	u, err := perms.AuthenticatedUser(ctx, s.env)
+	if err != nil {
+		if !perms.IsAnonymousUserError(err) || !s.env.GetAuthenticator().AnonymousUsageEnabled() {
+			return "", err
+		}
+	}
+	groupKey := "ANON"
+	if u != nil {
+		groupKey = u.GetGroupID()
+	}
+	// For now, associate stats with the exact command, including the full
+	// command line, env vars, and platform.
+	// Note: This doesn't account for platform overrides for now
+	// (--remote_header=x-buildbuddy-platform.NAME=VALUE).
+	cmdKey, err := commandKey(cmd)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/%s/%s", redisKeyPrefix, groupKey, cmdKey), nil
+}
+
+func commandKey(cmd *repb.Command) (string, error) {
+	// Include the command executable name in the key for easier debugging.
+	arg0 := "?"
+	if len(cmd.Arguments) > 0 {
+		arg0 = cmd.Arguments[0]
+		if len(arg0) > 64 {
+			arg0 = arg0[:64] + "..."
+		}
+	}
+	b, err := proto.Marshal(cmd)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/%x", arg0, sha256.Sum256(b)), nil
 }
 
 func testSize(testSize string) (int64, int64) {
--- enterprise/server/usage/usage.go
+++ enterprise/server/usage/usage.go
@@ -452,13 +452,9 @@ func countsToMap(tu *tables.UsageCounts) (map[string]int64, error) {
 // stringMapToCounts converts a Redis hashmap containing usage counts to
 // tables.UsageCounts.
 func stringMapToCounts(h map[string]string) (*tables.UsageCounts, error) {
-	hInt64 := make(map[string]int64, len(h))
-	for k, v := range h {
-		count, err := strconv.ParseInt(v, 10, 64)
-		if err != nil {
-			return nil, status.InvalidArgumentErrorf("Invalid usage count in Redis hash: %q => %q", k, v)
-		}
-		hInt64[k] = count
+	hInt64, err := redisutil.Int64Values(h)
+	if err != nil {
+		return nil, err
 	}
 	return &tables.UsageCounts{
 		Invocations:                hInt64["invocations"],
