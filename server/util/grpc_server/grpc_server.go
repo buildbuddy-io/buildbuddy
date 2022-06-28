4833b68fc0ff8b5775740908c7f0b103571d33d2
Tyler Williams <williams.tyler@gmail.com>
2022-06-27 20:38:03 -0700

    Put prometheus time histograms behind a flag (default enabled)

--- server/util/grpc_server/grpc_server.go
+++ server/util/grpc_server/grpc_server.go
@@ -34,6 +34,8 @@ var (
 
 	gRPCPort  = flag.Int("grpc_port", 1985, "The port to listen for gRPC traffic on")
 	gRPCSPort = flag.Int("grpcs_port", 1986, "The port to listen for gRPCS traffic on")
+
+	prometheusHistogramsEnabled = flag.Bool("app.enable_prometheus_histograms", true, "If true, collect prometheus histograms for all RPCs")
 )
 
 type RegisterServices func(server *grpc.Server, env environment.Env)
@@ -97,7 +99,9 @@ func NewGRPCServer(env environment.Env, port int, credentialOption grpc.ServerOp
 
 	// DISABLED in prod: enabling these causes unnecessary allocations
 	// that substantially (50%+ QPS) impact performance.
-	// grpc_prometheus.EnableHandlingTimeHistogram()
+	if *prometheusHistogramsEnabled {
+		grpc_prometheus.EnableHandlingTimeHistogram()
+	}
 
 	// Register health check service.
 	hlpb.RegisterHealthServer(grpcServer, env.GetHealthChecker())
