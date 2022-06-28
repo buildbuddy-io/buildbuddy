554a467654a7787f55b92c55458a2d1403e05e0d
Tyler Williams <williams.tyler@gmail.com>
2022-06-28 10:16:43 -0700

    Remove outdated comment

--- server/util/grpc_server/grpc_server.go
+++ server/util/grpc_server/grpc_server.go
@@ -97,8 +97,6 @@ func NewGRPCServer(env environment.Env, port int, credentialOption grpc.ServerOp
 	// Support prometheus grpc metrics.
 	grpc_prometheus.Register(grpcServer)
 
-	// DISABLED in prod: enabling these causes unnecessary allocations
-	// that substantially (50%+ QPS) impact performance.
 	if *enablePrometheusHistograms {
 		grpc_prometheus.EnableHandlingTimeHistogram()
 	}
