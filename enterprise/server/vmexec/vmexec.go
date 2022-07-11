0db1d0fd8f8ceab993e9ab3f793a001ee4d12ba5
Brandon Duffany <brandon@buildbuddy.io>
2022-07-11 15:04:01 -0400

    Add API protos for stream-based Firecracker execution

--- enterprise/server/vmexec/vmexec.go
+++ enterprise/server/vmexec/vmexec.go
@@ -117,17 +117,6 @@ func (x *execServer) Exec(ctx context.Context, req *vmxpb.ExecRequest) (*vmxpb.E
 		cmd.Dir = req.GetWorkingDirectory()
 	}
 
-	// TODO(tylerw): implement this.
-	if req.GetStdinVsockPort() != 0 {
-		return nil, status.UnimplementedError("Vsock stdin not implemented")
-	}
-	if req.GetStdoutVsockPort() != 0 {
-		return nil, status.UnimplementedError("Vsock stdout not implemented")
-	}
-	if req.GetStderrVsockPort() != 0 {
-		return nil, status.UnimplementedError("Vsock stderr not implemented")
-	}
-
 	// TODO(tylerw): use syncfs or something better here.
 	defer unix.Sync()
 
@@ -151,4 +140,8 @@ func (x *execServer) Exec(ctx context.Context, req *vmxpb.ExecRequest) (*vmxpb.E
 	rsp.Stdout = stdoutBuf.Bytes()
 	rsp.Stderr = stderrBuf.Bytes()
 	return rsp, nil
+}
+
+func (x *execServer) ExecStreamed(stream vmxpb.Exec_ExecStreamedServer) error {
+	return status.UnimplementedError("not implemented")
 }
