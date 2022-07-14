8e6903f8b0ade96dc453064db3a4408ebb9b580b
Tyler Williams <williams.tyler@gmail.com>
2022-07-13 21:16:09 -0700

    Build FIX

--- tools/repro/repro.go
+++ tools/repro/repro.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"sync"
 	"time"
-	
+
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/cachetools"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
 	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
@@ -18,18 +18,18 @@ import (
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc/metadata"
 
+	flagtypes "github.com/buildbuddy-io/buildbuddy/server/util/flagutil/types"
 	bspb "google.golang.org/genproto/googleapis/bytestream"
-	flagtypes "github.com/buildbuddy-io/buildbuddy/server/util/flagutil/types"	
 )
 
 var (
-	cacheTarget  = flag.String("cache_target", "localhost:1985", "Cache target to connect to.")
+	cacheTarget = flag.String("cache_target", "localhost:1985", "Cache target to connect to.")
 
 	concurrency  = flag.Int("concurrency", 10, "Number of concurrent workers to use")
 	sizeBytes    = flag.Int64("size_bytes", 1e8, "Size of blob to upload")
 	instanceName = flag.String("instance_name", "loadtest", "An optional Remote Instance name.")
-	randomSeed         = flag.Int64("random_seed", 0, "Random seed.")
-	headers            = flagtypes.Slice("headers", []string{}, "A list of headers to set (format: 'key=val'")
+	randomSeed   = flag.Int64("random_seed", 0, "Random seed.")
+	headers      = flagtypes.Slice("headers", []string{}, "A list of headers to set (format: 'key=val'")
 )
 
 type randomDataMaker struct {
@@ -63,26 +63,25 @@ func main() {
 	randomSrc := &randomDataMaker{rand.NewSource(seed)}
 	ctx := context.Background()
 
-        headersToSet := make([]string, 0)
-        for _, header := range *headers {
-                pair := strings.SplitN(header, "=", 2)
-                if len(pair) != 2 {
-                        log.Fatalf("Target headers must be of form key=val, got: %q", *headers)
-                }
-                headersToSet = append(headersToSet, pair[0])
-                headersToSet = append(headersToSet, pair[1])
-        }
-        if len(headersToSet) > 0 {
-                ctx = metadata.AppendToOutgoingContext(ctx, headersToSet...)
-        }
-	
-	
+	headersToSet := make([]string, 0)
+	for _, header := range *headers {
+		pair := strings.SplitN(header, "=", 2)
+		if len(pair) != 2 {
+			log.Fatalf("Target headers must be of form key=val, got: %q", *headers)
+		}
+		headersToSet = append(headersToSet, pair[0])
+		headersToSet = append(headersToSet, pair[1])
+	}
+	if len(headersToSet) > 0 {
+		ctx = metadata.AppendToOutgoingContext(ctx, headersToSet...)
+	}
+
 	conn, err := grpc_client.DialTarget(*cacheTarget)
 	if err != nil {
 		log.Fatalf("Error dialing: %s", err)
 	}
 	bsClient := bspb.NewByteStreamClient(conn)
-	
+
 	f, err := os.CreateTemp("", "example")
 	if err != nil {
 		log.Fatalf("Error making tempdir: %s", err)
@@ -91,7 +90,7 @@ func main() {
 		os.Remove(f.Name())
 		log.Printf("Cleaned up file %q", f.Name())
 	}()
-	
+
 	n, err := io.CopyN(f, randomSrc, *sizeBytes)
 	if err != nil {
 		log.Fatalf("Error copying bytes to file: %s", err)
@@ -110,7 +109,7 @@ func main() {
 	quitChan := make(chan struct{})
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-	go func(){
+	go func() {
 		<-c
 		close(quitChan)
 	}()
@@ -134,7 +133,7 @@ func main() {
 				mu.Lock()
 
 				dur := time.Since(start)
-				tput := (float64(d.GetSizeBytes())/1e6) / float64(dur.Seconds())
+				tput := (float64(d.GetSizeBytes()) / 1e6) / float64(dur.Seconds())
 				log.Printf("Thread %d downloaded %d bytes in %s (%f MB/sec)", i, d.GetSizeBytes(), dur, tput)
 				mu.Unlock()
 			}
@@ -145,4 +144,3 @@ func main() {
 		log.Fatalf("WaitGroup err: %s", err)
 	}
 }
-
