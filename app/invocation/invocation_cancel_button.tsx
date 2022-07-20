7ca89a9a056ec5d2f571240cad7beea31c9b1e82
Maggie Lou <margaretlou2019@u.northwestern.edu>
2022-07-20 09:12:05 -0700

    Address PR comments
    * Add UI elements - loading, error handling
    * Add integration test

--- app/invocation/invocation_cancel_button.tsx
+++ app/invocation/invocation_cancel_button.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { Slash as SlashIcon } from "lucide-react";
+import Spinner from "../components/spinner/spinner";
 import { OutlinedButton } from "../components/button/button";
 import rpcService from "../service/rpc_service";
 import { invocation } from "../../proto/invocation_ts_proto";
+import errorService from "../errors/error_service";
 
 export interface InvocationCancelButtonComponentProps {
   invocationId: string;
 }
 
+type State = {
+  isLoading?: boolean;
+};
+
 export default class InvocationCancelButtonComponent extends React.Component<InvocationCancelButtonComponentProps> {
+  state: State = {};
+
   private onClick() {
-    rpcService.service.cancelInvocation(
-      new invocation.CancelInvocationRequest({ invocationId: this.props.invocationId })
-    );
+    this.setState({ isLoading: true });
+    rpcService.service
+      .cancelInvocation(new invocation.CancelInvocationRequest({ invocationId: this.props.invocationId }))
+      .catch((e) => errorService.handleError(e))
+      .finally(() => this.setState({ isLoading: false }));
   }
 
   render() {
+    const isLoading = this.state.isLoading;
     return (
       <div className="invocation-cancel-button-container">
-        <OutlinedButton onClick={this.onClick.bind(this)}>
-          <SlashIcon className="icon" />
+        <OutlinedButton disabled={isLoading} onClick={this.onClick.bind(this)}>
+          {isLoading ? <Spinner className="icon" /> : <SlashIcon className="icon" />}
           <div>Cancel</div>
         </OutlinedButton>
       </div>
