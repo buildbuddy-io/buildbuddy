032606b4fef38b022e7239d9c4662a92b346f3bd
Tyler Williams <williams.tyler@gmail.com>
2022-08-05 10:09:33 -0700

    Make it clear that FileWriter is being used.

--- enterprise/server/backends/pebble_cache/pebble_cache.go
+++ enterprise/server/backends/pebble_cache/pebble_cache.go
@@ -1197,7 +1197,7 @@ func (p *PebbleCache) Writer(ctx context.Context, d *repb.Digest) (io.WriteClose
 	if d.GetSizeBytes() < *maxInlineFileSizeBytes {
 		wcm = filestore.InlineWriter(ctx, d.GetSizeBytes())
 	} else {
-		fw, err := filestore.NewWriter(ctx, p.blobDir(), db.NewBatch(), fileRecord)
+		fw, err := filestore.FileWriter(ctx, p.blobDir(), fileRecord)
 		if err != nil {
 			return nil, err
 		}
