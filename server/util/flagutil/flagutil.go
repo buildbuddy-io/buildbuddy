e38e1aad8f0cbc841e38a6561ae1a452743ccddc
Zoey Greer <zoey@buildbuddy.io>
2022-08-10 13:02:57 -0400

    Add flag type registry (#2347)
    * Add flag type registry
    
    * expose autoflags via flagutil

--- server/util/flagutil/flagutil.go
+++ server/util/flagutil/flagutil.go
@@ -2,6 +2,7 @@ package flagutil
 
 import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/flagutil/common"
+	"github.com/buildbuddy-io/buildbuddy/server/util/flagutil/types/autoflags"
 )
 
 // SetValueForFlagName sets the value for a flag by name. setFlags is the set of
@@ -17,4 +18,33 @@ var SetValueForFlagName = common.SetValueForFlagName
 // a given flag name.
 func GetDereferencedValue[T any](name string) (T, error) {
 	return common.GetDereferencedValue[T](name)
+}
+
+// New declares a new flag named `name` with the specified value `defaultValue`
+// of type `T` and the help text `usage`. It returns a pointer to where the
+// value is stored.
+func New[T any](name string, defaultValue T, usage string) *T {
+	return autoflags.New(name, defaultValue, usage)
+}
+
+// Var declares a new flag named `name` with the specified value `defaultValue`
+// of type `T` stored at the pointer `value` and the help text `usage`.
+func Var[T any](value *T, name string, defaultValue T, usage string) {
+	autoflags.Var(value, name, defaultValue, usage)
+}
+
+// Deprecated declares a new deprecated flag named `name` with the specified
+// value `defaultValue` of type `T`, the help text `usage`, and a
+// `migrationPlan` explaining to the user how to migrate from the deprecated
+// functionality. It returns a pointer to where the value is stored.
+func Deprecated[T any](name string, defaultValue T, usage string, migrationPlan string) *T {
+	return autoflags.Deprecated(name, defaultValue, usage, migrationPlan)
+}
+
+// DeprecatedVar declares a new deprecated flag named `name` with the specified
+// value `defaultValue` of type `T` stored at the pointer `value`, the help text
+// `usage`, and a `migrationPlan` explaining to the user how to migrate from the
+// deprecated functionality.
+func DeprecatedVar[T any](value *T, name string, defaultValue T, usage string, migrationPlan string) {
+	autoflags.DeprecatedVar(value, name, defaultValue, usage, migrationPlan)
 }
