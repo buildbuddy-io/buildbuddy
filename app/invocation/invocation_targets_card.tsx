9c7e33e5d8f16d149a49ce73e4291a47eaec280a
Brandon Duffany <brandon@buildbuddy.io>
2022-08-25 13:56:32 -0700

    Show a badge for root-cause targets (#2452)

--- app/invocation/invocation_targets_card.tsx
+++ app/invocation/invocation_targets_card.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import InvocationModel from "./invocation_model";
-import router from "../router/router";
 import { build_event_stream } from "../../proto/build_event_stream_ts_proto";
 import { Copy } from "lucide-react";
 import { copyToClipboard } from "../util/clipboard";
 import alert_service from "../alert/alert_service";
+import Link from "../components/link/link";
 
 interface Props {
   model: InvocationModel;
   icon: JSX.Element;
-  buildEvents: build_event_stream.BuildEvent[];
+  buildEvents: build_event_stream.IBuildEvent[];
   pastVerb: string;
   presentVerb: string;
   pageSize: number;
@@ -26,16 +26,11 @@ export default class TargetsCardComponent extends React.Component<Props, State> 
     numPages: 1,
   };
 
-  handleMoreClicked() {
-    this.setState({ ...this.state, numPages: this.state.numPages + 1 });
+  private handleMoreClicked() {
+    this.setState({ numPages: this.state.numPages + 1 });
   }
 
-  handleTargetClicked(label: string, event: MouseEvent) {
-    event.preventDefault();
-    router.navigateToQueryParam("target", label);
-  }
-
-  handleCopyClicked(label: string) {
+  private handleCopyClicked(label: string) {
     copyToClipboard(label);
     alert_service.success("Target labels copied to clipboard!");
   }
@@ -45,9 +40,16 @@ export default class TargetsCardComponent extends React.Component<Props, State> 
       (target) =>
         !this.props.filter || target.id.targetCompleted.label.toLowerCase().includes(this.props.filter.toLowerCase())
     );
+    const rootCauseEvents = events.filter((event) =>
+      this.props.model.rootCauseTargetLabels.has(event.id.targetCompleted.label)
+    );
+    const otherEvents = events.filter(
+      (event) => !this.props.model.rootCauseTargetLabels.has(event.id.targetCompleted.label)
+    );
+    events = [...rootCauseEvents, ...otherEvents];
 
     return (
-      <div className={`card ${this.props.className}`}>
+      <div className={`card ${this.props.className} invocation-targets-card`}>
         <div className="icon">{this.props.icon}</div>
         <div className="content">
           <div className="title">
@@ -62,27 +64,32 @@ export default class TargetsCardComponent extends React.Component<Props, State> 
             />
           </div>
           <div className="details">
-            {events
-              .slice(0, (this.props.pageSize && this.state.numPages * this.props.pageSize) || undefined)
-              .map((target) => (
-                <a
-                  className="list-grid"
-                  href={`?target=${encodeURIComponent(target.id.targetCompleted.label)}`}
-                  onClick={this.handleTargetClicked.bind(this, target.id.targetCompleted.label)}>
-                  <div
-                    title={`${
-                      this.props.model.configuredMap.get(target.id.targetCompleted.label)?.buildEvent.configured
-                        .targetKind
-                    } ${this.props.model.getTestSize(
-                      this.props.model.configuredMap.get(target.id.targetCompleted.label)?.buildEvent.configured
-                        .testSize
-                    )}`}
-                    className={"clickable target"}>
-                    <span className="target-status-icon">{this.props.icon}</span> {target.id.targetCompleted.label}
-                  </div>
-                  <div>{this.props.model.getRuntime(target.id.targetCompleted.label)}</div>
-                </a>
-              ))}
+            <div className="targets-table">
+              {events
+                .slice(0, (this.props.pageSize && this.state.numPages * this.props.pageSize) || undefined)
+                .map((target) => (
+                  <Link className="target-row" href={`?target=${encodeURIComponent(target.id.targetCompleted.label)}`}>
+                    <div
+                      title={`${
+                        this.props.model.configuredMap.get(target.id.targetCompleted.label)?.buildEvent.configured
+                          .targetKind
+                      } ${this.props.model.getTestSize(
+                        this.props.model.configuredMap.get(target.id.targetCompleted.label)?.buildEvent.configured
+                          .testSize
+                      )}`}
+                      className="target">
+                      <span className="target-status-icon">{this.props.icon}</span>{" "}
+                      <span className="target-label">{target.id.targetCompleted.label}</span>{" "}
+                      {this.props.model.rootCauseTargetLabels.has(target.id.targetCompleted.label) && (
+                        <span className="root-cause-badge">Root cause</span>
+                      )}
+                    </div>
+                    <div className="target-duration">
+                      {this.props.model.getRuntime(target.id.targetCompleted.label)}
+                    </div>
+                  </Link>
+                ))}
+            </div>
           </div>
           {this.props.pageSize && events.length > this.props.pageSize * this.state.numPages && !!this.state.numPages && (
             <div className="more" onClick={this.handleMoreClicked.bind(this)}>
