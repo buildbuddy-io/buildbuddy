c99911c1acc6f88d9ea19073640abb9d9ed612a1
Siggi Simonarson <siggisim@gmail.com>
2022-09-16 16:33:37 -0700

    Update auth_service.ts

--- app/auth/auth_service.ts
+++ app/auth/auth_service.ts
@@ -59,7 +59,7 @@ export class AuthService {
   }
 
   getCookie(name: string) {
-    var match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
+    let match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
     if (match) return match[2];
   }
 
