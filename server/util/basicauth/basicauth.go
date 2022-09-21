f60046b796b4e212a8448dd399fa8a3b5e9d03b0
Brandon Duffany <brandon@buildbuddy.io>
2022-09-21 13:17:20 -0400

    Add optional basic auth to monitoring endpoint

---
+++ server/util/basicauth/basicauth.go
@@ -0,0 +1,37 @@
+package basicauth
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"net/http"
+)
+
+// DefaultRealm is a reasonable default realm value for basic auth.
+const DefaultRealm = "Restricted"
+
+// Middleware returns basic auth middleware with the given credentials.
+func Middleware(realm string, creds map[string]string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user, pass, ok := r.BasicAuth()
+			if !ok {
+				basicAuthFailed(w, realm)
+				return
+			}
+
+			credPass, credUserOk := creds[user]
+			if !credUserOk || subtle.ConstantTimeCompare([]byte(pass), []byte(credPass)) != 1 {
+				basicAuthFailed(w, realm)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func basicAuthFailed(w http.ResponseWriter, realm string) {
+	w.Header().Add("WWW-Authenticate", fmt.Sprintf(`Basic realm="%s"`, realm))
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte("Unauthorized\n"))
+}
