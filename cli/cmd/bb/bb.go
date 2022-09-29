fe5f04da2d3c4f0dcb961727db5ecff8fa5f4d29
Siggi Simonarson <siggisim@gmail.com>
2022-09-29 16:26:36 -0700

    Add `login` command to CLI (#2625)
    * Add `login` command
    
    * Add TODO

--- cli/cmd/bb/bb.go
+++ cli/cmd/bb/bb.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/cli/arg"
 	"github.com/buildbuddy-io/buildbuddy/cli/bazelisk"
+	"github.com/buildbuddy-io/buildbuddy/cli/login"
 	"github.com/buildbuddy-io/buildbuddy/cli/parser"
 	"github.com/buildbuddy-io/buildbuddy/cli/remotebazel"
 	"github.com/buildbuddy-io/buildbuddy/cli/sidecar"
@@ -23,10 +24,12 @@ func main() {
 	// Fiddle with args
 	args = tooltag.ConfigureToolTag(args)
 	args = sidecar.ConfigureSidecar(args)
+	args = login.ConfigureAPIKey(args)
 
 	// Handle commands
 	args = remotebazel.HandleRemoteBazel(args)
 	args = version.HandleVersion(args)
+	args = login.HandleLogin(args)
 
 	// Remove any args that don't need to be on the command line
 	args = arg.RemoveExistingArgs(args, rcFileArgs)
---
+++ cli/login/login.go
@@ -0,0 +1,102 @@
+package login
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/buildbuddy-io/buildbuddy/cli/arg"
+	"github.com/buildbuddy-io/buildbuddy/cli/log"
+	"github.com/buildbuddy-io/buildbuddy/cli/storage"
+)
+
+const (
+	apiKeyFileName = "apikey"
+	apiKeyHeader   = "remote_header=x-buildbuddy-api-key"
+)
+
+func HandleLogin(args []string) []string {
+	if arg.GetCommand(args) != "login" {
+		return args
+	}
+	log.Printf("Enter your api key from https://app.buildbuddy.io/settings/org/api-keys")
+
+	var apiKey string
+	fmt.Scanln(&apiKey)
+
+	log.Printf("You are now logged in!")
+
+	dir, err := storage.Dir()
+	if err != nil {
+		log.Fatalf("error getting storage directory: %s", err)
+	}
+	apiKeyFile := filepath.Join(dir, apiKeyFileName)
+	err = os.WriteFile(apiKeyFile, []byte(apiKey), 0644)
+	if err != nil {
+		log.Fatalf("error saving api key: %s", err)
+	}
+	os.Exit(0)
+	return args
+}
+
+func ConfigureAPIKey(args []string) []string {
+	if !isSupportedCommand(arg.GetCommand(args)) {
+		return args
+	}
+
+	dir, err := storage.Dir()
+	if err != nil {
+		log.Fatalf("error getting storage directory: %s", err)
+	}
+	apiKeyFile := filepath.Join(dir, apiKeyFileName)
+	apiKeyBytes, err := os.ReadFile(apiKeyFile)
+	if err != nil {
+		fmt.Print(err)
+		return args
+	}
+
+	// TODO(siggisim): find a more graceful way of finding headers if we change the way we parse flags.
+	if !arg.Has(args, apiKeyHeader) {
+		return append(args, "--"+apiKeyHeader+"="+string(apiKeyBytes))
+	}
+	return args
+}
+
+// Commands that support the `--remote_header` bazel flag
+func isSupportedCommand(command string) bool {
+	switch command {
+	case "aquery":
+		fallthrough
+	case "build":
+		fallthrough
+	case "clean":
+		fallthrough
+	case "config":
+		fallthrough
+	case "coverage":
+		fallthrough
+	case "cquery":
+		fallthrough
+	case "canonicalize-flags":
+		fallthrough
+	case "fetch":
+		fallthrough
+	case "info":
+		fallthrough
+	case "mobile-install":
+		fallthrough
+	case "print_action":
+		fallthrough
+	case "query":
+		fallthrough
+	case "remote":
+		fallthrough
+	case "run":
+		fallthrough
+	case "sync":
+		fallthrough
+	case "test":
+		return true
+	}
+	return false
+}
