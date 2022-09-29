6ae4c5dd19a2cad36a71dbd71b873943c92f6811
Siggi Simonarson <siggisim@gmail.com>
2022-09-29 16:02:01 -0700

    Add a little shout-out (#2626)

--- cli/tooltag/tooltag.go
+++ cli/tooltag/tooltag.go
@@ -2,9 +2,11 @@ package tooltag
 
 import (
 	"github.com/buildbuddy-io/buildbuddy/cli/arg"
+	"github.com/buildbuddy-io/buildbuddy/cli/log"
 )
 
 func ConfigureToolTag(args []string) []string {
+	log.Printf("🚀 \033[1mBUILT WITH BUILDBUDDY\033[0m 🚀")
 	if !arg.Has(args, "tool_tag") {
 		return append(args, "--tool_tag=buildbuddy-cli")
 	}
