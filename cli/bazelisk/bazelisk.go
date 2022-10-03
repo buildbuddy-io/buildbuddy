da282d008e4e80ebc1595137841c79d39b316042
Brandon Duffany <brandon@buildbuddy.io>
2022-10-03 13:48:31 -0700

    CLI: Wire up plugins in `main` (#2631)

--- cli/bazelisk/bazelisk.go
+++ cli/bazelisk/bazelisk.go
@@ -2,6 +2,7 @@ package bazelisk
 
 import (
 	"context"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,7 +15,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 )
 
-func Run(args []string) {
+func Run(args []string) (int, *Output, error) {
 	// If we were already invoked via bazelisk, then set the bazel version to
 	// the next version appearing in the .bazelversion file so that bazelisk
 	// doesn't just invoke us again (resulting in an infinite loop).
@@ -29,12 +30,62 @@ func Run(args []string) {
 	// Fetch releases, release candidates and Bazel-at-commits from GCS, forks from GitHub
 	repos := core.CreateRepositories(gcs, gcs, gitHub, gcs, gitHub, true)
 
+	output, err := os.CreateTemp("", "bazelisk-output-*")
+	if err != nil {
+		return 0, nil, status.FailedPreconditionErrorf("failed to create output file: %s", err)
+	}
+
+	// Temporarily redirect stdout/stderr so that we can capture the bazelisk
+	// output.
+	// Note, we might be able to use Bazel's "command.log" file instead, but
+	// would need to find a way to avoid a race condition if another Bazel
+	// command is run concurrently (since it will clear out command.log as
+	// soon as our bazelisk invocation below is complete and it is able to
+	// grab the workspace lock).
+	originalStdout, originalStderr := os.Stdout, os.Stderr
+	defer func() {
+		os.Stdout, os.Stderr = originalStdout, originalStderr
+	}()
+	stdoutPipe, closeStdoutPipe, err := makePipeWriter(io.MultiWriter(output, os.Stdout))
+	if err != nil {
+		return 0, nil, err
+	}
+	defer closeStdoutPipe()
+	os.Stdout = stdoutPipe
+	stderrPipe, closeStderrPipe, err := makePipeWriter(io.MultiWriter(output, os.Stderr))
+	if err != nil {
+		return 0, nil, err
+	}
+	defer closeStderrPipe()
+	os.Stderr = stderrPipe
+
 	exitCode, err := core.RunBazelisk(args, repos)
 	if err != nil {
 		log.Fatalf("error running bazelisk: %s", err)
 	}
+	return exitCode, &Output{output}, nil
+}
 
-	os.Exit(exitCode)
+// makePipeWriter adapts a writer to an *os.File by using an os.Pipe().
+// The returned file should not be closed; instead the returned closeFunc
+// should be called to ensure that all data from the pipe is flushed to the
+// underlying writer.
+func makePipeWriter(w io.Writer) (pw *os.File, closeFunc func(), err error) {
+	pr, pw, err := os.Pipe()
+	if err != nil {
+		return
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(w, pr)
+		close(done)
+	}()
+	closeFunc = func() {
+		pw.Close()
+		// Wait until the pipe contents are flushed to w.
+		<-done
+	}
+	return
 }
 
 func setBazelVersion() error {
@@ -64,4 +115,14 @@ func setBazelVersion() error {
 	}
 
 	return os.Setenv("USE_BAZEL_VERSION", parts[0])
+}
+
+// Output points to the stdout/stderr written by bazelisk. It is like a regular
+// file, but the Close() method also deletes the file to avoid accumulating too
+// many logs on disk.
+type Output struct{ *os.File }
+
+func (o *Output) Close() error {
+	o.File.Close()
+	return os.Remove(o.File.Name())
 }
--- cli/cmd/bb/bb.go
+++ cli/cmd/bb/bb.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/buildbuddy-io/buildbuddy/cli/arg"
 	"github.com/buildbuddy-io/buildbuddy/cli/bazelisk"
+	"github.com/buildbuddy-io/buildbuddy/cli/log"
 	"github.com/buildbuddy-io/buildbuddy/cli/login"
 	"github.com/buildbuddy-io/buildbuddy/cli/parser"
+	"github.com/buildbuddy-io/buildbuddy/cli/plugin"
 	"github.com/buildbuddy-io/buildbuddy/cli/remotebazel"
 	"github.com/buildbuddy-io/buildbuddy/cli/sidecar"
+	"github.com/buildbuddy-io/buildbuddy/cli/terminal"
 	"github.com/buildbuddy-io/buildbuddy/cli/tooltag"
 	"github.com/buildbuddy-io/buildbuddy/cli/version"
 )
 
 func main() {
 	flag.Parse()
+	exitCode, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(exitCode)
+}
+
+func run() (exitCode int, err error) {
+	// Load plugins
+	plugins, err := plugin.LoadAll()
+	if err != nil {
+		return -1, err
+	}
 
 	// Parse args
 	commandLineArgs := flag.Args()
@@ -22,9 +39,19 @@ func main() {
 	args := append(commandLineArgs, rcFileArgs...)
 
 	// Fiddle with args
+	// TODO(bduffany): model these as "built-in" plugins
 	args = tooltag.ConfigureToolTag(args)
 	args = sidecar.ConfigureSidecar(args)
 	args = login.ConfigureAPIKey(args)
+	args = terminal.ConfigureOutputMode(args)
+
+	// Run plugin pre-bazel hooks
+	for _, p := range plugins {
+		args, err = p.PreBazel(args)
+		if err != nil {
+			return -1, err
+		}
+	}
 
 	// Handle commands
 	args = remotebazel.HandleRemoteBazel(args)
@@ -35,5 +62,18 @@ func main() {
 	args = arg.RemoveExistingArgs(args, rcFileArgs)
 
 	// Actually run bazel
-	bazelisk.Run(args)
+	exitCode, output, err := bazelisk.Run(args)
+	if err != nil {
+		return -1, err
+	}
+	defer output.Close()
+
+	// Run plugin post-bazel hooks
+	for _, p := range plugins {
+		if err := p.PostBazel(output.Name()); err != nil {
+			return -1, err
+		}
+	}
+
+	return exitCode, nil
 }
