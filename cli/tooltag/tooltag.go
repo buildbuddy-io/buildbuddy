24a377e44f8743614f8e9137ae4422625cc08afb
Siggi Simonarson <siggisim@gmail.com>
2022-10-03 11:25:01 -0700

    Don't apply `tool_tag` if there is no command present (#2641)

--- cli/tooltag/tooltag.go
+++ cli/tooltag/tooltag.go
@@ -7,7 +7,7 @@ import (
 
 func ConfigureToolTag(args []string) []string {
 	log.Printf("🚀 \033[1mBUILT WITH BUILDBUDDY\033[0m 🚀")
-	if !arg.Has(args, "tool_tag") {
+	if arg.GetCommand(args) != "" && !arg.Has(args, "tool_tag") {
 		return append(args, "--tool_tag=buildbuddy-cli")
 	}
 	return args
