3b06b94a97cd3b76e55fc85d4456f3607e523d43
Brandon Duffany <brandon@buildbuddy.io>
2022-10-11 09:54:38 -0400

    Fix memory leak in fastcopy_darwin (#2686)

--- server/util/fastcopy/fastcopy_darwin.go
+++ server/util/fastcopy/fastcopy_darwin.go
@@ -5,16 +5,23 @@ package fastcopy
 
 /*
 #include <stdint.h> // for uint32_t
+#include <stdlib.h> // for free
 #include <sys/clonefile.h>
 */
 import "C"
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 func FastCopy(source, destination string) error {
-	ret, err := C.clonefile(C.CString(source), C.CString(destination), C.uint32_t(0))
+	src := C.CString(source)
+	defer C.free(unsafe.Pointer(src))
+	dst := C.CString(destination)
+	defer C.free(unsafe.Pointer(dst))
+
+	ret, err := C.clonefile(src, dst, C.uint32_t(0))
 	if errno, ok := err.(syscall.Errno); ret == -1 && ok && errno != syscall.EEXIST {
 		return err
 	}
