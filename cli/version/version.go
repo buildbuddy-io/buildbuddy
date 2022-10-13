2846fc10dd3f58c473803ab7ca2d8c3631bbd4b5
Brandon Duffany <brandon@buildbuddy.io>
2022-10-13 13:49:24 -0400

    Fix bb version command (#2708)

--- cli/version/version.go
+++ cli/version/version.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/buildbuddy-io/buildbuddy/cli/arg"
-	"github.com/buildbuddy-io/buildbuddy/server/version"
 )
 
 func HandleVersion(args []string) []string {
 	if arg.GetCommand(args) != "version" {
 		return args
 	}
-	fmt.Printf("bb %s\n", version.AppVersion())
+	// The "version" var is generated in this package according to the
+	// Bazel flag value --//cli/version:cli_version
+	fmt.Printf("bb %s\n", cliVersionFlag)
 	return args
 }
