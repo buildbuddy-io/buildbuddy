50705123792450fec40c09e32855440b6c8dd474
Lulu Zhang <lulu@buildbuddy.io>
2022-10-20 14:55:16 -0700

    Use CtxInfo in clickhouse logging (#2756)
    * Use CtxInfo in clickhouse logging
    
    Also fix a logging bug.
    
    * fix

--- server/build_event_protocol/build_event_handler/build_event_handler.go
+++ server/build_event_protocol/build_event_handler/build_event_handler.go
@@ -266,7 +266,7 @@ func (r *statsRecorder) flushInvocationStatsToOLAPDB(ctx context.Context, ij *in
 	err = r.env.GetOLAPDBHandle().FlushInvocationStats(ctx, inv)
 	// Temporary logging for debugging clickhouse missing data.
 	if err == nil {
-		log.Infof("successfully wrote invocation %q to clickhouse", inv.InvocationID)
+		log.CtxInfo(ctx, "successfully wrote invocation to clickhouse")
 	}
 	return err
 }
