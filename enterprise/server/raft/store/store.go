0b37200227d857f3116236a6df19111d56365d2c
Tyler Williams <williams.tyler@gmail.com>
2022-11-04 15:36:22 -0700

    Clean up /statusz formatting for store (#2862)

--- enterprise/server/raft/store/store.go
+++ enterprise/server/raft/store/store.go
@@ -113,41 +113,42 @@ func New(rootDir string, nodeHost *dragonboat.NodeHost, gossipManager *gossip.Go
 	return s
 }
 
-func (s *Store) replicaString(r *replica.Replica) string {
-	ru, err := r.Usage()
-	if err != nil {
-		return "UNKNOWN"
-	}
-	clusterString := fmt.Sprintf("(c%dn%d)", ru.GetReplica().GetClusterId(), ru.GetReplica().GetNodeId())
-	rangeLeaseString := ""
-	if rd := s.lookupRange(ru.GetReplica().GetClusterId()); rd != nil {
-		clusterString = fmt.Sprintf("%d: [%q %q)\t", rd.GetRangeId(), rd.GetLeft(), rd.GetRight()) + clusterString
-		if rlIface, ok := s.leases.Load(rd.GetRangeId()); ok {
-			if rl, ok := rlIface.(*rangelease.Lease); ok {
-				rangeLeaseString = rl.String()
-			}
-		}
-	}
-	mbUsed := ru.GetEstimatedDiskBytesUsed() / 1e6
-	return fmt.Sprintf("\t%s Usage: %dMB, Lease: %s\n", clusterString, mbUsed, rangeLeaseString)
-}
-
 func (s *Store) Statusz(ctx context.Context) string {
 	buf := "<pre>"
 	buf += fmt.Sprintf("NHID: %s\n", s.nodeHost.ID())
 	buf += fmt.Sprintf("Liveness lease: %s\n", s.liveness)
 
-	replicaStrings := make([]string, 0)
+	replicas := make([]*replica.Replica, 0)
 	s.replicas.Range(func(key, value any) bool {
 		if r, ok := value.(*replica.Replica); ok {
-			replicaStrings = append(replicaStrings, s.replicaString(r))
+			replicas = append(replicas, r)
 		}
 		return true
 	})
+	sort.Slice(replicas, func(i, j int) bool {
+		return replicas[i].ClusterID < replicas[j].ClusterID
+	})
 	buf += "Replicas:\n"
-	sort.Strings(replicaStrings)
-	for _, replicaString := range replicaStrings {
-		buf += replicaString
+	for _, r := range replicas {
+		cluster := fmt.Sprintf("(c%03dn%03d)", r.ClusterID, r.NodeID)
+
+		usage, err := r.Usage()
+		if err != nil {
+			buf += fmt.Sprintf("\t%s error: %s\n", cluster, err)
+			continue
+		}
+
+		mbUsed := usage.GetEstimatedDiskBytesUsed() / 1e6
+		extra := ""
+		if r.IsSplitting() {
+			extra += "(Splitting) "
+		}
+		if rd := s.lookupRange(r.ClusterID); rd != nil {
+			if rlIface, ok := s.leases.Load(rd.GetRangeId()); ok && rlIface.(*rangelease.Lease).Valid() {
+				extra += "Leaseholder"
+			}
+		}
+		buf += fmt.Sprintf("\t%s Usage: %4dMB %s\n", cluster, mbUsed, extra)
 	}
 	buf += "</pre>"
 	return buf
