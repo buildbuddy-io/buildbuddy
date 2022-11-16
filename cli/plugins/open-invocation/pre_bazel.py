e9d7b108f57290b926d349d4f0e528cc596e29bd
Brandon Duffany <brandon@buildbuddy.io>
2022-11-16 11:44:28 -0500

    CLI: Fix flag parsing in open-invocation plugin (#2939)

--- cli/plugins/open-invocation/pre_bazel.py
+++ cli/plugins/open-invocation/pre_bazel.py
@@ -8,14 +8,23 @@ def main():
     with open(args_file_path, 'r') as f:
         args = f.read().split('\n')
 
-    bes_results_url = get_flag_value(args, 'bes_results_url')
 
     # TODO: More robust parsing (handle --open false, --noopen, --open=1 etc.)
-    if '--open' not in args or not bes_results_url:
+    if '--open' not in args:
         return
+    args = [a for a in args if a != '--open']
 
-    args.remove('--open')
+    bes_results_url = get_flag_value(args, 'bes_results_url')
+    if bes_results_url:
+        write_invocation_url(args, bes_results_url)
+    else:
+        sys.stderr.write("\x1b[33mWarning: --bes_results_url is not configured; --open flag will be ignored\x1b[m\n")
 
+    # Write the new args.
+    with open(args_file_path, 'w') as f:
+        f.write('\n'.join(args))
+
+def write_invocation_url(args, bes_results_url):
     # Add --invocation_id flag if not already set.
     invocation_id = get_flag_value(args, 'invocation_id')
     if not invocation_id:
@@ -28,17 +37,15 @@ def main():
     with open(url_file_path, 'w') as f:
         f.write(bes_results_url + invocation_id)
 
-    # Write the new args.
-    with open(args_file_path, 'w') as f:
-        f.write('\n'.join(args))
-
 
 # TODO: Provide a library function for this?
 def get_flag_value(flags, name):
+    """Returns the last occurrence of the given flag name."""
+    value = None
     for flag in flags:
         if flag.startswith('--' + name + '='):
-            return '='.join(flag.split('=')[1:])
-    return None
+            value = '='.join(flag.split('=')[1:])
+    return value
 
 
 if __name__ == "__main__":
