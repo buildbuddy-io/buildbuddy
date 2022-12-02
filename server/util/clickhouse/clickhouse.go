712ee5365159c702c281149d697410dbfe7dfe9d
Lulu Zhang <lulu@buildbuddy.io>
2022-12-02 15:40:29 -0800

    add success and invocation_status to Executions table in clickhouse

--- server/util/clickhouse/clickhouse.go
+++ server/util/clickhouse/clickhouse.go
@@ -208,14 +208,16 @@ type Execution struct {
 	DoNotCache   bool
 
 	// Fields from Invocations
-	User       string
-	Host       string
-	Pattern    string
-	Role       string
-	BranchName string
-	CommitSHA  string
-	RepoURL    string
-	Command    string
+	User             string
+	Host             string
+	Pattern          string
+	Role             string
+	BranchName       string
+	CommitSHA        string
+	RepoURL          string
+	Command          string
+	InvocationStatus int64
+	Success          bool
 }
 
 func (e *Execution) TableName() string {
@@ -248,6 +250,8 @@ func (e *Execution) AdditionalFields() []string {
 		"CommitSHA",
 		"RepoURL",
 		"Command",
+		"InvocationStatus",
+		"Success",
 		"InvocationLinkType",
 	}
 }
