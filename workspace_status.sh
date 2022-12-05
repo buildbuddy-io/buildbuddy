15cbafe4533e63717c6cbacddf375d4e253450db
Brandon Duffany <brandon@buildbuddy.io>
2022-12-05 18:28:26 -0500

    Embed version info using workspace_status.sh

--- workspace_status.sh
+++ workspace_status.sh
@@ -28,3 +28,9 @@ echo "GIT_BRANCH $git_branch"
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
 
+# Note: the "STABLE_" suffix causes these to be part of the "stable" workspace
+# status, which may trigger rebuilds of certain targets if these values change
+# and you're building with the "--stamp" flag.
+latest_version_tag=$(./tools/latest_version_tag.sh)
+echo "STABLE_VERSION_TAG $latest_version_tag"
+echo "STABLE_COMMIT_SHA $commit_sha"
