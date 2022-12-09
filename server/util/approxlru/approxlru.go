871596ae6a20637a878280fda32b58ceca32fbcb
Vadim Berezniker <vadim@buildbuddy.io>
2022-12-09 11:58:06 -0800

    Add an approximate LRU implementation. (#3045)

---
+++ server/util/approxlru/approxlru.go
@@ -0,0 +1,309 @@
+package approxlru
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+)
+
+const (
+	// sampleN is the number of random files to sample when adding a new
+	// deletion candidate to the sample pool. Increasing this number
+	// makes eviction slower but improves sampled-LRU accuracy.
+	sampleN = 10
+
+	// samplePoolSize is the number of deletion candidates to maintain in
+	// memory at a time. Increasing this number uses more memory but
+	// improves sampled-LRU accuracy.
+	samplePoolSize = 100
+
+	// evictCheckPeriod is how often the janitor thread will wake up to
+	// check the cache size.
+	evictCheckPeriod = 1 * time.Second
+)
+
+type Key interface {
+	// String() is used for information logging.
+	fmt.Stringer
+	// The ID is used to filter out duplicate samples and is stored within the
+	// eviction pool.
+	ID() string
+}
+
+type Sample[T Key] struct {
+	Key       T
+	SizeBytes int64
+	Timestamp time.Time
+}
+
+// OnEvict requests that the given key be evicted. The callback may return
+// skip=true to indicate that the given sample is no longer valid and should be
+// removed from the eviction pool.
+type OnEvict[T any] func(ctx context.Context, key T) (skip bool, err error)
+
+// OnSample requests that n random samples be provided from the underlying data.
+type OnSample[T Key] func(ctx context.Context, n int) ([]*Sample[T], error)
+
+// OnRefresh requests the latest access timestamp for the given key. The
+// callback may return skip=true to indicate that the given sample is no longer
+// valid and should be removed from the eviction pool.
+type OnRefresh[T any] func(ctx context.Context, key T) (skip bool, timestamp time.Time, error error)
+
+// LRU implements a thread safe fixed size LRU cache using sampled eviction.
+//
+// The LRU does not store information about the full set of keys or values that
+// is subject to eviction, instead relying on user provided callbacks to perform
+// sampling of the full universe of data.
+//
+// An internal eviction pool is maintained consisting of random keys sampled
+// by the user-provided callback. When LRU capacity is exceeded, the LRU evicts
+// the oldest entries in the pool and resamples to maintain the pool size.
+//
+// For more details, see:
+//
+//	https://github.com/redis/redis/blob/unstable/src/evict.c#L118 and
+//	http://antirez.com/news/109
+type LRU[T Key] struct {
+	maxSizeBytes int64
+	onEvict      OnEvict[T]
+	onSample     OnSample[T]
+	onRefresh    OnRefresh[T]
+
+	samplePool []*Sample[T]
+
+	mu              sync.Mutex
+	ctx             context.Context
+	cancelCtx       context.CancelFunc
+	globalSizeBytes int64
+	localSizeBytes  int64
+	lastRun         time.Time
+	lastEvicted     *Sample[T]
+}
+
+type Opts[T Key] struct {
+	MaxSizeBytes int64
+	OnEvict      OnEvict[T]
+	OnSample     OnSample[T]
+	OnRefresh    OnRefresh[T]
+}
+
+func New[T Key](opts *Opts[T]) (*LRU[T], error) {
+	if opts.MaxSizeBytes == 0 {
+		return nil, status.FailedPreconditionError("max size is required")
+	}
+	if opts.OnEvict == nil {
+		return nil, status.FailedPreconditionError("eviction callback is required")
+	}
+	if opts.OnSample == nil {
+		return nil, status.FailedPreconditionError("sample callback is required")
+	}
+	if opts.OnRefresh == nil {
+		return nil, status.FailedPreconditionError("refresh callback is required")
+	}
+	l := &LRU[T]{
+		maxSizeBytes: opts.MaxSizeBytes,
+		onEvict:      opts.OnEvict,
+		onSample:     opts.OnSample,
+		onRefresh:    opts.OnRefresh,
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	l.ctx = ctx
+	l.cancelCtx = cancel
+	go l.evictor()
+	return l, nil
+}
+
+func (l *LRU[T]) Stop() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if l.cancelCtx != nil {
+		l.cancelCtx()
+		l.cancelCtx = nil
+	}
+}
+
+func (l *LRU[T]) LocalSizeBytes() int64 {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.localSizeBytes
+}
+
+func (l *LRU[T]) MaxSizeBytes() int64 {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.maxSizeBytes
+}
+
+func (l *LRU[T]) UpdateLocalSizeBytes(localSizeBytes int64) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.localSizeBytes = localSizeBytes
+}
+
+func (l *LRU[T]) UpdateGlobalSizeBytes(globalSizeBytes int64) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.globalSizeBytes = globalSizeBytes
+}
+
+func (l *LRU[T]) UpdateSizeBytes(sizeBytes int64) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.globalSizeBytes = sizeBytes
+	l.localSizeBytes = sizeBytes
+}
+
+func (l *LRU[T]) resampleK(k int) error {
+	seen := make(map[string]struct{}, len(l.samplePool))
+	for _, entry := range l.samplePool {
+		seen[entry.Key.ID()] = struct{}{}
+	}
+
+	// read new entries to put in the pool.
+	additions := make([]*Sample[T], 0, k*sampleN)
+	for i := 0; i < k; i++ {
+		entries, err := l.onSample(l.ctx, sampleN)
+		if err != nil {
+			return err
+		}
+		for _, e := range entries {
+			if _, ok := seen[e.Key.ID()]; ok {
+				continue
+			}
+			seen[e.Key.ID()] = struct{}{}
+			additions = append(additions, e)
+		}
+	}
+
+	filtered := make([]*Sample[T], 0, len(l.samplePool))
+
+	// refresh all the entries already in the pool.
+	for _, sample := range l.samplePool {
+		skip, timestamp, err := l.onRefresh(l.ctx, sample.Key)
+		if err != nil {
+			log.Warningf("Could not refresh timestamp for %q: %s", sample.Key, err)
+		}
+		if skip {
+			continue
+		}
+		sample.Timestamp = timestamp
+		filtered = append(filtered, sample)
+	}
+
+	l.samplePool = append(filtered, additions...)
+
+	if len(l.samplePool) > 0 {
+		sort.Slice(l.samplePool, func(i, j int) bool {
+			return l.samplePool[i].Timestamp.UnixNano() < l.samplePool[j].Timestamp.UnixNano()
+		})
+	}
+
+	if len(l.samplePool) > samplePoolSize {
+		l.samplePool = l.samplePool[:samplePoolSize]
+	}
+
+	return nil
+}
+
+func (l *LRU[T]) evict() (*Sample[T], error) {
+	// Resample every time we evict a key
+	if err := l.resampleK(1); err != nil {
+		return nil, err
+	}
+
+	for {
+		for i, sample := range l.samplePool {
+			log.Infof("Evictor attempting to evict %q", sample.Key)
+			skip, err := l.onEvict(l.ctx, sample.Key)
+			if err != nil {
+				log.Warningf("Could not evict %q: %s", sample.Key, err)
+				continue
+			}
+
+			l.mu.Lock()
+			l.localSizeBytes -= sample.SizeBytes
+			// Assume eviction on remote servers is happening at the same
+			// rate as local eviction. It's fine to be wrong as we expect the
+			// actual sizes to be periodically to be reset to the true numbers
+			// using UpdateSizeBytes from data received from other servers.
+			l.globalSizeBytes -= int64(float64(sample.SizeBytes) * float64(l.globalSizeBytes) / float64(l.localSizeBytes))
+			l.mu.Unlock()
+
+			l.samplePool = append(l.samplePool[:i], l.samplePool[i+1:]...)
+
+			if skip {
+				continue
+			}
+
+			return sample, nil
+		}
+
+		// If no candidates were evictable in the whole pool, resample
+		// the pool.
+		l.samplePool = l.samplePool[:0]
+		if err := l.resampleK(samplePoolSize); err != nil {
+			return nil, err
+		}
+	}
+}
+
+func (l *LRU[T]) ttl() error {
+	for {
+		l.mu.Lock()
+		globalSizeBytes := l.globalSizeBytes
+		localSizeBytes := l.localSizeBytes
+		l.mu.Unlock()
+
+		if globalSizeBytes <= l.maxSizeBytes || localSizeBytes == 0 {
+			break
+		}
+
+		select {
+		case <-l.ctx.Done():
+			return nil
+		default:
+			break
+		}
+
+		lastEvicted, err := l.evict()
+		if err != nil {
+			return err
+		}
+
+		l.mu.Lock()
+		l.lastRun = time.Now()
+		l.lastEvicted = lastEvicted
+		l.mu.Unlock()
+	}
+	return nil
+}
+
+func (l *LRU[T]) evictor() {
+	for {
+		select {
+		case <-l.ctx.Done():
+			return
+		case <-time.After(evictCheckPeriod):
+			if err := l.ttl(); err != nil {
+				log.Warningf("could not evict: %s", err)
+			}
+		}
+	}
+}
+
+func (l *LRU[T]) LastRun() time.Time {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.lastRun
+}
+
+func (l *LRU[T]) LastEvicted() *Sample[T] {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.lastEvicted
+}
---
+++ server/util/approxlru/approxlru_test.go
@@ -0,0 +1,168 @@
+package approxlru_test
+
+import (
+	"context"
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/approxlru"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/stretchr/testify/require"
+)
+
+type entry struct {
+	id        string
+	sizeBytes int64
+	atime     time.Time
+}
+
+func (l *entry) String() string {
+	return l.id
+}
+
+func (l *entry) ID() string {
+	return l.id
+}
+
+type testCache struct {
+	mu        sync.Mutex
+	data      []*entry
+	evictions []*entry
+}
+
+func (tc *testCache) Add(e *entry) {
+	tc.data = append(tc.data, e)
+}
+
+func (tc *testCache) evict(ctx context.Context, key *entry) (skip bool, err error) {
+	tc.mu.Lock()
+	defer tc.mu.Unlock()
+
+	for i, v := range tc.data {
+		if v.ID() == key.ID() {
+			tc.evictions = append(tc.evictions, key)
+			tc.data[i] = tc.data[len(tc.data)-1]
+			tc.data = tc.data[:len(tc.data)-1]
+			return false, nil
+		}
+	}
+
+	return false, status.InvalidArgumentErrorf("key %q not in cache", key.ID())
+}
+
+func (tc *testCache) sample(ctx context.Context, n int) ([]*approxlru.Sample[*entry], error) {
+	tc.mu.Lock()
+	defer tc.mu.Unlock()
+
+	var samples []*approxlru.Sample[*entry]
+	for i := 0; i < n; i++ {
+		s := tc.data[rand.Intn(len(tc.data))]
+		samples = append(samples, &approxlru.Sample[*entry]{
+			Key:       s,
+			SizeBytes: s.sizeBytes,
+			Timestamp: s.atime,
+		})
+	}
+	return samples, nil
+}
+
+func (tc *testCache) refresh(ctx context.Context, key *entry) (skip bool, timestamp time.Time, err error) {
+	return false, key.atime, nil
+}
+
+func waitForEviction(t *testing.T, l *approxlru.LRU[*entry]) {
+	start := time.Now()
+	for l.LocalSizeBytes() > l.MaxSizeBytes() {
+		time.Sleep(1 * time.Second)
+		if time.Since(start) > 10*time.Second {
+			require.FailNow(t, "eviction did not finish")
+		}
+	}
+}
+
+func newCache(t *testing.T, maxSizeBytes int64) (*testCache, *approxlru.LRU[*entry]) {
+	c := &testCache{}
+	l, err := approxlru.New(&approxlru.Opts[*entry]{
+		MaxSizeBytes: maxSizeBytes,
+		OnEvict: func(ctx context.Context, key *entry) (skip bool, err error) {
+			return c.evict(ctx, key)
+		},
+		OnSample: func(ctx context.Context, n int) ([]*approxlru.Sample[*entry], error) {
+			return c.sample(ctx, n)
+		},
+		OnRefresh: func(ctx context.Context, key *entry) (skip bool, timestamp time.Time, err error) {
+			return c.refresh(ctx, key)
+		},
+	})
+	require.NoError(t, err)
+	return c, l
+}
+
+func fillCache(t *testing.T, c *testCache, n int, sizeBytes int64) {
+	atime, err := time.Parse(time.RFC3339, "2022-01-01T00:00:00-07:00")
+	require.NoError(t, err)
+
+	for i := 0; i < n; i++ {
+		c.Add(&entry{
+			id:        strconv.Itoa(i),
+			sizeBytes: sizeBytes,
+			atime:     atime,
+		})
+		atime = atime.Add(1 * time.Hour)
+	}
+}
+
+func TestPartialEviction(t *testing.T) {
+	c, l := newCache(t, 9_000 /*=maxSizeBytes*/)
+
+	fillCache(t, c, 1000, 10 /*=sizeBytes*/)
+
+	// Inform the LRU about the new size of the underlying cache.
+	// Eviction should kick in and evict until the cache is down to 9_000.
+	l.UpdateSizeBytes(10_000)
+	waitForEviction(t, l)
+
+	oldest := c.data[0].atime
+
+	for _, e := range c.evictions {
+		if !e.atime.Before(oldest) {
+			require.FailNowf(t, "early eviction", "evicted item %+v is not older than entries still in the cache", e)
+		}
+	}
+}
+
+func TestRefresh(t *testing.T) {
+	c, l := newCache(t, 9_000 /*=maxSizeBytes*/)
+
+	fillCache(t, c, 1000, 10 /*=sizeBytes*/)
+
+	// Inform the LRU about the new size of the underlying cache.
+	// Eviction should kick in and evict until the cache is down to 9_000.
+	l.UpdateSizeBytes(10_000)
+	waitForEviction(t, l)
+
+	// Update atimes on the oldest atimes to avoid evicting them.
+	var refreshedIDs []string
+	c.mu.Lock()
+	for i := 0; i < 10; i++ {
+		c.data[i].atime = time.Now()
+		refreshedIDs = append(refreshedIDs, c.data[i].id)
+	}
+	c.mu.Unlock()
+
+	// Tell the LRU that the cache is back to 10,000 so it evicts 1_000 more
+	// bytes.
+	l.UpdateSizeBytes(10_000)
+	waitForEviction(t, l)
+
+	c.mu.Lock()
+	for i := 0; i < 10; i++ {
+		if c.data[i].id != refreshedIDs[i] {
+			require.FailNowf(t, "early eviction", "entry %q should not have been evicted yet", refreshedIDs[i])
+		}
+	}
+	c.mu.Unlock()
+}
