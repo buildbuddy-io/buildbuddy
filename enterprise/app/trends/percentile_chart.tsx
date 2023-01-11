df84086d66300c7f11f6377191a775fdaf53fed9
Jim Hollenbach <james.d.hollenbach@gmail.com>
2023-01-11 11:58:29 -0800

    make PercentilesChartTooltip into a real life component

--- enterprise/app/trends/percentile_chart.tsx
+++ enterprise/app/trends/percentile_chart.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 import * as format from "../../../app/format/format";
-import { ResponsiveContainer, ComposedChart, CartesianGrid, XAxis, YAxis, Line, Legend, Tooltip } from "recharts";
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Line,
+  Legend,
+  Tooltip,
+  TooltipProps,
+} from "recharts";
 
 export interface PercentilesChartProps {
   title: string;
@@ -94,9 +104,7 @@ export default class PercentilesChartComponent extends React.Component<Percentil
   }
 }
 
-interface PercentilesChartTooltipProps {
-  active?: boolean;
-  payload?: any[];
+interface PercentilesChartTooltipProps extends TooltipProps<any, any> {
   labelFormatter: (datum: string) => string;
   extractP50: (datum: string) => number;
   extractP75: (datum: string) => number;
@@ -106,30 +114,32 @@ interface PercentilesChartTooltipProps {
   triggerCallback: (datum: string) => void;
 }
 
-function PercentilesChartTooltip({
-  active,
-  payload,
-  labelFormatter,
-  extractP50,
-  extractP75,
-  extractP90,
-  extractP95,
-  extractP99,
-  triggerCallback,
-}: PercentilesChartTooltipProps) {
-  if (!active) return null;
-  let data = payload[0].payload;
-  triggerCallback(data);
-  return (
-    <div className="trend-chart-hover">
-      <div className="trend-chart-hover-label">{labelFormatter(data)}</div>
-      <div className="trend-chart-hover-value">
-        <div>p50: {format.durationSec(extractP50(data))}</div>
-        <div>p75: {format.durationSec(extractP75(data))}</div>
-        <div>p90: {format.durationSec(extractP90(data))}</div>
-        <div>p95: {format.durationSec(extractP95(data))}</div>
-        <div>p99: {format.durationSec(extractP99(data))}</div>
+class PercentilesChartTooltip extends React.Component<PercentilesChartTooltipProps> {
+  componentDidUpdate(prevProps: PercentilesChartTooltipProps) {
+    if (this.props.payload && this.props.payload.length > 0) {
+      this.props.triggerCallback(this.props.payload[0].payload);
+    }
+  }
+
+  render() {
+    if (!this.props.active || !this.props.payload || this.props.payload.length < 1) return null;
+
+    const data = this.props.payload[0].payload;
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <div className="trend-chart-hover">
+        <div className="trend-chart-hover-label">{this.props.labelFormatter(data)}</div>
+        <div className="trend-chart-hover-value">
+          <div>p50: {format.durationSec(this.props.extractP50(data))}</div>
+          <div>p75: {format.durationSec(this.props.extractP75(data))}</div>
+          <div>p90: {format.durationSec(this.props.extractP90(data))}</div>
+          <div>p95: {format.durationSec(this.props.extractP95(data))}</div>
+          <div>p99: {format.durationSec(this.props.extractP99(data))}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
