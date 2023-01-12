c1eab888aec4649875481becf9ef71d769f3a30b
Brandon Duffany <brandon@buildbuddy.io>
2023-01-12 13:36:44 -0500

    CLI: Highlight TypeScript errors too.

--- cli/example_plugins/go-highlight/handle_bazel_output.py
+++ cli/example_plugins/go-highlight/handle_bazel_output.py
@@ -6,5 +6,15 @@ if __name__ == "__main__":
         m = re.search(r"^(.*?\.go:\d+:\d+:)(.*)", line)
         if m:
             print("\x1b[33m" + m.group(1) + "\x1b[0m" + m.group(2))
-        else:
-            print(line, end="")
+            continue
+
+        # TypeScript formats line numbers like "path.tsx(line,col)". In addition
+        # to highlighting, reformat as "path:line:col:" since VSCode allows
+        # Ctrl+Clicking to jump to that exact location in the code when it's
+        # formatted that way.
+        m = re.search(r"^(.*?\.tsx?)\((\d+),(\d+)\)(:)(.*)", line)
+        if m:
+            print("\x1b[33m%s:%s:%s%s\x1b[m%s" % m.groups())
+            continue
+
+        print(line, end="")
