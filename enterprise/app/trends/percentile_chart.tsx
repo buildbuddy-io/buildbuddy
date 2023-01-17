67b643174fd354601aed6f43e22c1e26abf92c63
jim hollenbach <james.d.hollenbach@gmail.com>
2023-01-17 11:52:23 -0800

    Allow clicking on percentile charts (#3207)

--- enterprise/app/trends/percentile_chart.tsx
+++ enterprise/app/trends/percentile_chart.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 import * as format from "../../../app/format/format";
-import { ResponsiveContainer, ComposedChart, CartesianGrid, XAxis, YAxis, Line, Legend, Tooltip } from "recharts";
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Line,
+  Legend,
+  Tooltip,
+  TooltipProps,
+} from "recharts";
 
 export interface PercentilesChartProps {
   title: string;
@@ -13,15 +23,28 @@ export interface PercentilesChartProps {
   extractP90: (datum: string) => number;
   extractP95: (datum: string) => number;
   extractP99: (datum: string) => number;
+  onRowClicked?: (datum: string) => void;
 }
 
 export default class PercentilesChartComponent extends React.Component<PercentilesChartProps> {
+  private lastDataFromHover: string;
+
+  handleRowClick() {
+    if (!this.props.onRowClicked || !this.lastDataFromHover) {
+      return;
+    }
+    this.props.onRowClicked(this.lastDataFromHover);
+  }
+
   render() {
     return (
       <div className="trend-chart">
         <div className="trend-chart-title">{this.props.title}</div>
         <ResponsiveContainer width="100%" height={300}>
-          <ComposedChart data={this.props.data}>
+          <ComposedChart
+            data={this.props.data}
+            style={this.props.onRowClicked ? { cursor: "pointer" } : {}}
+            onClick={this.handleRowClick.bind(this)}>
             <CartesianGrid strokeDasharray="3 3" />
             <Legend />
             <XAxis dataKey={this.props.extractLabel} />
@@ -35,6 +58,7 @@ export default class PercentilesChartComponent extends React.Component<Percentil
                   extractP90={this.props.extractP90}
                   extractP95={this.props.extractP95}
                   extractP99={this.props.extractP99}
+                  triggerCallback={(data) => (this.lastDataFromHover = data)}
                 />
               }
             />
@@ -80,41 +104,42 @@ export default class PercentilesChartComponent extends React.Component<Percentil
   }
 }
 
-interface PercentilesChartTooltipProps {
-  active?: boolean;
-  payload?: any[];
+interface PercentilesChartTooltipProps extends TooltipProps<any, any> {
   labelFormatter: (datum: string) => string;
   extractP50: (datum: string) => number;
   extractP75: (datum: string) => number;
   extractP90: (datum: string) => number;
   extractP95: (datum: string) => number;
   extractP99: (datum: string) => number;
+  triggerCallback: (datum: string) => void;
 }
 
-function PercentilesChartTooltip({
-  active,
-  payload,
-  labelFormatter,
-  extractP50,
-  extractP75,
-  extractP90,
-  extractP95,
-  extractP99,
-}: PercentilesChartTooltipProps) {
-  if (!active) return null;
-  let data = payload[0].payload;
-  return (
-    <div className="trend-chart-hover">
-      <div className="trend-chart-hover-label">{labelFormatter(data)}</div>
-      <div className="trend-chart-hover-value">
-        <div>p50: {format.durationSec(extractP50(data))}</div>
-        <div>p75: {format.durationSec(extractP75(data))}</div>
-        <div>p90: {format.durationSec(extractP90(data))}</div>
-        <div>p95: {format.durationSec(extractP95(data))}</div>
-        <div>p99: {format.durationSec(extractP99(data))}</div>
-      </div>
-    </div>
-  );
+class PercentilesChartTooltip extends React.Component<PercentilesChartTooltipProps> {
+  componentDidUpdate(prevProps: PercentilesChartTooltipProps) {
+    if (this.props.payload && this.props.payload.length > 0) {
+      this.props.triggerCallback(this.props.payload[0].payload);
+    }
+  }
 
-  return null;
+  render() {
+    if (!this.props.active || !this.props.payload || this.props.payload.length < 1) return null;
+
+    const data = this.props.payload[0].payload;
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <div className="trend-chart-hover">
+        <div className="trend-chart-hover-label">{this.props.labelFormatter(data)}</div>
+        <div className="trend-chart-hover-value">
+          <div>p50: {format.durationSec(this.props.extractP50(data))}</div>
+          <div>p75: {format.durationSec(this.props.extractP75(data))}</div>
+          <div>p90: {format.durationSec(this.props.extractP90(data))}</div>
+          <div>p95: {format.durationSec(this.props.extractP95(data))}</div>
+          <div>p99: {format.durationSec(this.props.extractP99(data))}</div>
+        </div>
+      </div>
+    );
+  }
 }
