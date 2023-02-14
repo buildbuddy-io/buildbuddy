185e1294cb66b4fb9dd35ee0016385c81b105acf
Son Luong Ngoc <sluongng@gmail.com>
2023-02-14 15:03:17 +0100

    terraform: start experimenting with AWS deployment
    We want to experiement with deploying BuildBuddy helm chart on AWS.
    
    Initial approach includes using Terraform with EKS module and Helm
    provider to apply the BuildBuddy chart onto a newly provisioned EKS
    cluster.
    
    We use:
    - AWS's EBS CSI driver to automatically provisioning EBS volumes for
      k8s Persistent Volume Claims.
    - AWS's LoadBalancer controller to automatically provision ALB/ELB for
      k8s' Service and/or Ingress.
    
    Future works:
    - Swapping out components in the helm chart for AWS-native components:
      - S3 for object storage
      - Replace K8s MySQL with RDS
      - Use AWS's ClickHouse DB
    - Provide additonal examples:
      - Setup authentication provider
      - Deploy BuildBuddy Executor
      - Configure distributed cache

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
---
+++ terraform/aws/vpc.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "bb-aws-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
