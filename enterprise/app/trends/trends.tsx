babacdf013eba076ab1f2f9eef112dd59c6f1189
Jim Hollenbach <james.d.hollenbach@gmail.com>
2023-02-15 09:45:08 -0800

    don't let the drilldowns tab get rendered when the flag isn't enabled--harmless but annoying.

--- enterprise/app/trends/trends.tsx
+++ enterprise/app/trends/trends.tsx
@@ -217,7 +217,7 @@ export default class TrendsComponent extends React.Component<Props, State> {
   }
 
   showingDrilldown(): boolean {
-    return this.props.hash === "#drilldown";
+    return capabilities.config.trendsHeatmapEnabled && this.props.hash === "#drilldown";
   }
 
   render() {
