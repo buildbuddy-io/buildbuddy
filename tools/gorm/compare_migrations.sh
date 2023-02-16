7567d0f21459cc48c0ef1728e5f9ae3855276e4e
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-02-15 16:05:53 -0800

    Export diff to a variable

--- tools/gorm/compare_migrations.sh
+++ tools/gorm/compare_migrations.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
-
 # This script can be used to determine whether there will be any gorm db schema changes from a git branch
 #
-# It assumes that the input data source has the db schema from the master branch
-# It generates the gorm auto-migration SQL statements on the master branch, then compares these statements to the
-# SQL statements on the input branch and prints any diff
+# It assumes that the schema of the data source is already up-to-date with what is defined on the current branch,
+# so the current branch can serve as a benchmark for the current schema of the db
+#
+# The script generates the gorm auto-migration SQL statements for the current branch, then compares these statements to the
+# auto-migration SQL statements for the input branch and prints any diff
 # It does not execute any migrations on the db
 
 git_branch=$1
 data_source=$2
 
-git checkout master
 tmpfile="$(mktemp /tmp/auto_migration.XXXXXXXXX)"
 bazel run print_auto_migration -- --data_source "$data_source" --output_path "$tmpfile"
 
@@ -20,7 +19,9 @@ git checkout "$git_branch"
 tmpfile2="$(mktemp /tmp/auto_migration.XXXXXXXXX)"
 bazel run print_auto_migration -- --data_source "$data_source" --output_path "$tmpfile2"
 
-#TODO: Delete tmp files
+DIFF=$(diff "$tmpfile" "$tmpfile2")
+echo "$DIFF"
+export DIFF
 
-diff "$tmpfile" "$tmpfile2"
-
+git checkout -
+rm "$tmpfile" "$tmpfile2"
