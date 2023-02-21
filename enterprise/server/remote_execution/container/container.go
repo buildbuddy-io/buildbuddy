8373f90999be473b5a08af338956bb60ae1f4c58
Brandon Duffany <brandon@buildbuddy.io>
2023-02-21 17:36:44 -0500

    refactor: Move IsAnonymousUserError to authutil (#3455)

--- enterprise/server/remote_execution/container/container.go
+++ enterprise/server/remote_execution/container/container.go
@@ -12,6 +12,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/metrics"
+	"github.com/buildbuddy-io/buildbuddy/server/util/authutil"
 	"github.com/buildbuddy-io/buildbuddy/server/util/flagutil"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/perms"
@@ -267,7 +268,7 @@ func NewImageCacheToken(ctx context.Context, env environment.Env, creds PullCred
 	groupID := ""
 	u, err := perms.AuthenticatedUser(ctx, env)
 	if err != nil {
-		if !perms.IsAnonymousUserError(err) {
+		if !authutil.IsAnonymousUserError(err) {
 			return ImageCacheToken{}, err
 		}
 	} else {
