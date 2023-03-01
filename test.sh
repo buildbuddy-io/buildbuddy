ea4f80b288d154ca66f4f2cb7bfbecc0b7c11419
Son Luong Ngoc <sluongng@gmail.com>
2023-03-01 15:38:57 +0100

    Fix working dir

--- test.sh
+++ test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-/root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc info
+(
+  cd $BUILD_WORKSPACE_DIRECTORY
 
-echo 'Testing once'
-/root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc test //... --config=workflows --test_tag_filters=-performance,-webdriver,-docker
+  /root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc info
 
-echo 'Testing again'
-/root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc test //... --config=workflows --test_tag_filters=-performance,-webdriver,-docker
+  echo 'Testing once'
+  /root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc test //... --config=workflows --test_tag_filters=-performance,-webdriver,-docker
 
-/root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc info
+  echo 'Testing again'
+  /root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc test //... --config=workflows --test_tag_filters=-performance,-webdriver,-docker
+
+  /root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc info
+)
