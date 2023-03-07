1158a3d68876370eb28cef9b46e161b795f4c3b9
Son Luong Ngoc <sluongng@gmail.com>
2023-03-07 15:45:23 +0100

    wrong output base

--- get-log.sh
+++ get-log.sh
@@ -2,5 +2,5 @@
 
 (
   cd ${BUILD_WORKING_DIRECTORY}
-  cp "$(bazel info output_base | sed 's/\r//')/java.log" .
+  cp "$(/root/workspace/bazelisk --output_base=/root/workspace/output-base --bazelrc=/root/workspace/buildbuddy.bazelrc --noworkspace_rc --bazelrc=.bazelrc --bazelrc=custom.bazelrc info output_base | sed 's/\r//')/java.log" .
 )
