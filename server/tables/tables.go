22d18f65748ab1ce6925e1ec9f988b7f4580a534
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-03-08 08:38:42 -0800

    Change default type from false -> 0 (#3509)

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -200,7 +200,7 @@ type Group struct {
 	Model
 
 	SharingEnabled       bool `gorm:"default:1;type:tinyint(1)"`
-	UserOwnedKeysEnabled bool `gorm:"not null;default:false;type:tinyint(1)"`
+	UserOwnedKeysEnabled bool `gorm:"not null;default:0;type:tinyint(1)"`
 
 	// If enabled, builds for this group will always use their own executors instead of the installation-wide shared
 	// executors.
