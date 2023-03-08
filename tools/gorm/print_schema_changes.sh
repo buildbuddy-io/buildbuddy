85efc9e1e6896818fd9513016c38834b84121d14
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-03-07 16:30:49 -0800

    Rebase off simplified script

--- tools/gorm/print_schema_changes.sh
+++ tools/gorm/print_schema_changes.sh
@@ -8,9 +8,35 @@
 
 git_branch=$1
 db_conn_string=$2
+# TODO: Remove when buildbuddy-internal#2096 is fixed
+# baseline_git_branch is only needed when using the script for clickhouse
+# When auto-migrating clickhouse tables, gorm runs MODIFY COLUMN on all columns even if their types have not changed
+# To determine whether a meaningful migration is taking place, we need to compare the migration diff against the
+# baseline
+baseline_git_branch=$3
 
-git checkout "$git_branch" >/dev/null 2>&1
-schema_changes=$(bazel run //enterprise/server -- --database.data_source="$db_conn_string" --database.print_schema_changes_and_exit=true 2>/dev/null)
+# If running on a Mac, you may need to uncomment the following for the string parsing to work correctly:
+# alias sed="gsed"
+
+# Parse db connection string
+db_driver=$(echo "$db_conn_string" | sed -n "s/\(\S*\):\/\/.*$/\1/p")
+if [[ "$db_driver" == "clickhouse" ]]; then
+  if [[ "$baseline_git_branch" == "" ]]; then
+    echo "When running this script with clickhouse, you must pass in a baseline git branch that contains the current
+    schema of the database in order to generate a meaningful diff. See buildbuddy-internal#2096 for more info."
+    exit 1
+  fi
+
+  git checkout "$baseline_git_branch" >/dev/null 2>&1
+  baseline_migration_diff=$(bazel run //enterprise/server -- --olap_database.data_source="$db_conn_string" --olap_database.print_schema_changes_and_exit=true 2>/dev/null)
+  git checkout "$git_branch" >/dev/null 2>&1
+  new_migration_diff=$(bazel run //enterprise/server -- --olap_database.data_source="$db_conn_string" --olap_database.print_schema_changes_and_exit=true 2>/dev/null)
+
+  schema_changes=$(diff <(echo "$baseline_migration_diff") <(echo "$new_migration_diff"))
+else
+  git checkout "$git_branch" >/dev/null 2>&1
+  schema_changes=$(bazel run //enterprise/server -- --database.data_source="$db_conn_string" --database.print_schema_changes_and_exit=true 2>/dev/null)
+fi
 
 if [[ "$schema_changes" != "" ]]; then
   echo "$schema_changes"
