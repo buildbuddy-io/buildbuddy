d35bd9988d2f9a6a5959fc7c2f4bd1ec2a217770
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-03-17 15:12:27 -0700

    Add script to poll whether all release assets have been uploaded

---
+++ tools/check_release_assets_uploaded.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import requests
+import sys
+import time
+
+def die(message):
+    print(message)
+    sys.exit(1)
+
+def get_assets_for_release(version_tag):
+    github_token = os.environ.get('GITHUB_TOKEN')
+    query_url = "https://api.github.com/graphql"
+    json = {"query": "query {repository(owner: \"buildbuddy-io\", name: \"buildbuddy\") { release(tagName:\"" + version_tag+ "\"){ releaseAssets(last:10) { edges {node {downloadUrl}}}}}}"}
+    headers = {'Authorization': f'token {github_token}'}
+    r = requests.post(query_url, json=json, headers=headers)
+    if r.status_code == 401:
+        die("Invalid github credentials. Did you set the GITHUB_TOKEN environment variable?")
+
+    release = r.json()["data"]["repository"]["release"]
+    if release is None:
+        die(f"Release with tag {version_tag} not found.")
+
+    asset_objs = release["releaseAssets"]["edges"]
+    asset_urls = [o["node"]["downloadUrl"] for o in asset_objs]
+    return asset_urls
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--polling_interval_sec', type=int, default=60)
+    parser.add_argument('--max_timeout_sec', type=int, default=600)
+    parser.add_argument('--release_version_tag', required=True)
+    args = parser.parse_args()
+
+    expected_assets = [
+        "executor-enterprise-darwin-arm64",
+        "buildbuddy-enterprise-linux-amd64",
+        "buildbuddy-linux-amd64",
+        "executor-enterprise-linux-amd64",
+        "buildbuddy-enterprise-darwin-amd64",
+        "executor-enterprise-darwin-amd64",
+        "buildbuddy-darwin-amd64"
+    ]
+
+    time_passed = 0
+    while time_passed < args.max_timeout_sec:
+        asset_urls = get_assets_for_release(args.release_version_tag)
+        missing_assets = []
+        for expected_asset in expected_assets:
+            asset_uploaded = any(expected_asset in uploaded_asset for uploaded_asset in asset_urls)
+            if not asset_uploaded:
+                missing_assets.append(expected_asset)
+        expected_assets = missing_assets
+
+        if len(expected_assets) == 0:
+            break
+
+        time.sleep(args.polling_interval_sec)
+        time_passed += args.polling_interval_sec
+
+    if len(expected_assets) > 0:
+        print(f"Missing assets {expected_assets}")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
