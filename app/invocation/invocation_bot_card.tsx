7fd6fb081b7958c642ef8487fed3897e01b77dbf
Siggi Simonarson <siggisim@gmail.com>
2023-03-16 20:00:22 -0700

    Only return a single element

--- app/invocation/invocation_bot_card.tsx
+++ app/invocation/invocation_bot_card.tsx
@@ -6,15 +6,19 @@ export interface Props {
 }
 
 export function InvocationBotCard({ suggestions }: Props) {
-  return suggestions.map((suggestion) => (
-    <div className="card card-suggestion card-suggestion-bot">
-      <Bot className="icon" />
-      <div className="content">
-        <div className="details">
-          <div className="card-suggestion-message">{suggestion.trim()}</div>
-          <div className="card-suggestion-reason">Based on the error message in your build logs.</div>
+  return (
+    <>
+      {suggestions.map((suggestion) => (
+        <div className="card card-suggestion card-suggestion-bot">
+          <Bot className="icon" />
+          <div className="content">
+            <div className="details">
+              <div className="card-suggestion-message">{suggestion.trim()}</div>
+              <div className="card-suggestion-reason">Based on the error message in your build logs.</div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  ));
+      ))}
+    </>
+  );
 }
