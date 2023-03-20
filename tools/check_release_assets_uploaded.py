48d3f29fcf1a0b26100218aff03b10fd8c77ad8d
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-03-20 16:15:21 -0700

    PR feedback

--- tools/check_release_assets_uploaded.py
+++ tools/check_release_assets_uploaded.py
@@ -9,6 +9,11 @@ def die(message):
     print(message)
     sys.exit(1)
 
+# get_assets_for_release returns a list of URL strings representing the assets uploaded to the release with the given
+# version tag
+# Example output:
+# ['https://github.com/buildbuddy-io/buildbuddy/releases/download/v2.0.0/executor-enterprise-darwin-arm64',
+# 'https://github.com/buildbuddy-io/buildbuddy/releases/download/v2.0.0/buildbuddy-enterprise-linux-amd64']
 def get_assets_for_release(version_tag):
     github_token = os.environ.get('GITHUB_TOKEN')
     query_url = "https://api.github.com/graphql"
@@ -48,7 +53,12 @@ def main():
         asset_urls = get_assets_for_release(args.release_version_tag)
         missing_assets = []
         for expected_asset in expected_assets:
-            asset_uploaded = any(expected_asset in uploaded_asset for uploaded_asset in asset_urls)
+            asset_uploaded = False
+            for uploaded_asset in asset_urls:
+                if expected_asset in uploaded_asset:
+                    asset_uploaded = True
+                    break
+
             if not asset_uploaded:
                 missing_assets.append(expected_asset)
         expected_assets = missing_assets
@@ -58,9 +68,10 @@ def main():
 
         time.sleep(args.polling_interval_sec)
         time_passed += args.polling_interval_sec
+        print(f"Still missing release assets {expected_assets}. {time_passed}s have passed...")
 
     if len(expected_assets) > 0:
-        print(f"Missing assets {expected_assets}")
+        print(f"Missing assets {expected_assets}", file=sys.stderr)
         exit(1)
 
 if __name__ == "__main__":
