5865dfea99272ee04b2ba22827d797cc4027cd93
Maggie Lou <margaretlou2019@u.northwestern.edu>
2023-03-20 16:48:58 -0700

    Simplify script

--- tools/gorm/print_schema_changes.sh
+++ tools/gorm/print_schema_changes.sh
@@ -3,42 +3,27 @@
 # This script can be used to determine whether there will be any gorm db schema changes from a git branch
 #
 # Example usage:
+# Passwords can be found in buildbuddy-internal/enterprise/config/buildbuddy.dev.yaml
 # # Port-forward dev mysql server so it can be accessed locally
 # kubectl --namespace=tools-dev port-forward deployment/sqlproxy 3308:3306
-# # Run script for mysql: Dev mysql password can be found in buildbuddy-internal/enterprise/config/buildbuddy.dev.yaml
+# # Run script for mysql
 # schema_changes=$(./print_schema_changes.sh MY_GIT_BRANCH "mysql://buildbuddy-dev:PASSWORD@tcp(127.0.0.1:3308)/buildbuddy_dev")
 #
 # # Port-forward dev clickhouse server so it can be accessed locally
 # kubectl -n clickhouse-operator-dev port-forward chi-repl-dev-replicated-0-0-0 9001:9000
-# # Run script for clickhouse: Dev clickhouse password can be found in buildbuddy-internal/enterprise/config/buildbuddy.dev.yaml
-# schema_changes=$(./print_schema_changes.sh MY_GIT_BRANCH "clickhouse://buildbuddy_dev:PASSWORD@localhost:9001/buildbuddy_dev" master)
+# # Run script for clickhouse
+# schema_changes=$(./print_schema_changes.sh MY_GIT_BRANCH "clickhouse://buildbuddy_dev:PASSWORD@localhost:9001/buildbuddy_dev")
 
 git_branch=$1
 db_conn_string=$2
-# TODO: Remove when buildbuddy-internal#2096 is fixed
-# baseline_git_branch is only needed when using the script for clickhouse
-# When auto-migrating clickhouse tables, gorm runs MODIFY COLUMN on all columns even if their types have not changed
-# To determine whether a meaningful migration is taking place, we need to compare the migration diff against the
-# baseline
-baseline_git_branch=$3
+
+git checkout "$git_branch" >/dev/null 2>&1
 
 # Parse db connection string
 db_driver=$(echo "$db_conn_string" | sed -n "s/\(\S*\):\/\/.*$/\1/p")
 if [[ "$db_driver" == "clickhouse" ]]; then
-  if [[ "$baseline_git_branch" == "" ]]; then
-    echo "When running this script with clickhouse, you must pass in a baseline git branch that contains the current
-    schema of the database in order to generate a meaningful diff. See buildbuddy-internal#2096 for more info."
-    exit 1
-  fi
-
-  git checkout "$baseline_git_branch" >/dev/null 2>&1
-  baseline_migration_diff=$(bazel run //enterprise/server -- --olap_database.data_source="$db_conn_string" --olap_database.print_schema_changes_and_exit=true 2>/dev/null)
-  git checkout "$git_branch" >/dev/null 2>&1
-  new_migration_diff=$(bazel run //enterprise/server -- --olap_database.data_source="$db_conn_string" --olap_database.print_schema_changes_and_exit=true 2>/dev/null)
-
-  schema_changes=$(diff <(echo "$baseline_migration_diff") <(echo "$new_migration_diff"))
+  schema_changes=$(bazel run //enterprise/server -- --olap_database.data_source="$db_conn_string" --olap_database.print_schema_changes_and_exit=true 2>/dev/null)
 else
-  git checkout "$git_branch" >/dev/null 2>&1
   schema_changes=$(bazel run //enterprise/server -- --database.data_source="$db_conn_string" --database.print_schema_changes_and_exit=true 2>/dev/null)
 fi
 
