8a8ba14ee79d2db279a107205db43ac3917e2ce9
Son Luong Ngoc <sluongng@gmail.com>
2023-04-05 21:18:59 +0700

    cli/version: simplified w. embedsrcs (#3700)
    Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

---
+++ cli/version/version_test.go
@@ -0,0 +1,33 @@
+package version_test
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/cli/version"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEmbeddedVersion(t *testing.T) {
+	require.Contains(t, version.String(), "unknown")
+}
+
+func TestHandleVersion(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = w
+
+	exitCode, err := version.HandleVersion([]string{"version", "--cli"})
+	require.NoError(t, err)
+	require.Equal(t, 0, exitCode)
+
+	err = w.Close()
+	require.NoError(t, err)
+	out, err := io.ReadAll(r)
+	require.NoError(t, err)
+	os.Stdout = rescueStdout
+
+	require.Contains(t, string(out), "unknown")
+}
