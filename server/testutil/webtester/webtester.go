4e66fe34df03b396afe81b0ba2f8800aff49d097
Iain Macdonald <iain@buildbuddy.io>
2023-05-03 13:04:50 -0700

    Wait for the sidebar to appear after logging in to account for possible delay during webdriver tests (#3904)

--- server/testutil/webtester/webtester.go
+++ server/testutil/webtester/webtester.go
@@ -281,11 +281,13 @@ func Login(wt *WebTester, target Target) {
 		wt.FindByDebugID("sso-button").Click()
 		wt.FindByDebugID("sso-slug").SendKeys(target.SSOSlug())
 		wt.FindByDebugID("sso-button").Click()
-		return
+	} else {
+		// Otherwise attempt self-auth.
+		wt.FindByDebugID("login-button").Click()
 	}
 
-	// Otherwise attempt self-auth.
-	wt.FindByDebugID("login-button").Click()
+	// Login can have a delay. Wait for the sidebar before proceeding.
+	wt.Find(".sidebar-footer")
 }
 
 // Logout logs out of the app. It expects that a user is currently logged in,
