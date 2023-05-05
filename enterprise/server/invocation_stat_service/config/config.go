f5789b34e32eb47e44641bf688e6d6beb901bc0d
jim hollenbach <james.d.hollenbach@gmail.com>
2023-05-05 13:47:25 -0700

    Flip a bunch of flags that have been on in dev and prod for a long time. (#3918)

--- enterprise/server/invocation_stat_service/config/config.go
+++ enterprise/server/invocation_stat_service/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import "flag"
 
-var trendsHeatmapEnabled = flag.Bool("app.trends_heatmap_enabled", false, "If set, enable a fancy heatmap UI for exploring build trends.")
+var trendsHeatmapEnabled = flag.Bool("app.trends_heatmap_enabled", true, "If set, enable a fancy heatmap UI for exploring build trends.")
 
 func TrendsHeatmapEnabled() bool {
 	return *trendsHeatmapEnabled
