853a38001f2491191ca1a25a0fd8e84d8bb0991d
Son Luong Ngoc <sluongng@gmail.com>
2023-05-05 11:27:01 +0200

    Rebase, move to new func, fix url in doc

--- enterprise/server/remote_execution/dirtools/dirtools.go
+++ enterprise/server/remote_execution/dirtools/dirtools.go
@@ -289,6 +289,76 @@ func uploadFiles(uploader *cachetools.BatchCASUploader, fc interfaces.FileCache,
 	return nil
 }
 
+// handleSymlink adds the symlink to the directory and actionResult proto so that
+// they could be recreated on the Bazel client side if needed.
+func handleSymlink(dirHelper *DirHelper, rootDir string, cmd *repb.Command, actionResult *repb.ActionResult, directory *repb.Directory, fqfn string) error {
+	target, err := os.Readlink(fqfn)
+	if err != nil {
+		return err
+	}
+	symlink := &repb.OutputSymlink{
+		Path:   trimPathPrefix(fqfn, rootDir),
+		Target: target,
+	}
+	directory.Symlinks = append(directory.Symlinks, &repb.SymlinkNode{
+		Name:   symlink.Path,
+		Target: symlink.Target,
+	})
+
+	// REAPI specification:
+	//   `output_symlinks` will only be populated if the command `output_paths` field
+	//   was used, and not the pre v2.1 `output_files` or `output_directories` fields.
+	//
+	// Check whether the current client is using REAPI version before or after v2.1.
+	if len(cmd.OutputPaths) > 0 && len(cmd.OutputFiles) == 0 && len(cmd.OutputDirectories) == 0 {
+		// REAPI >= v2.1
+		if dirHelper.IsOutputPath(fqfn) {
+			actionResult.OutputSymlinks = append(actionResult.OutputSymlinks, symlink)
+
+			// REAPI specification:
+			//   Servers that wish to be compatible with v2.0 API should still
+			//   populate `output_file_symlinks` and `output_directory_symlinks`
+			//   in addition to `output_symlinks`.
+			//
+			// TODO(sluongng): Since v6.0.0, all the output directories are included in
+			// Action.input_root_digest. So we should be able to save a `stat()` call by
+			// checking wherether the symlink target was included as a directory inside
+			// input root or not.
+			//
+			// Reference:
+			//   https://github.com/bazelbuild/bazel/commit/4310aeb36c134e5fc61ed5cdfdf683f3e95f19b7
+			symlinkInfo, err := os.Stat(fqfn)
+			if err != nil {
+				// When encounter a dangling symlink, skip setting it to legacy fields.
+				// TODO(sluongng): do we care to log this?
+				log.Warningf("Could not find symlink %q's target: %s, skip legacy fields", fqfn, err)
+				return nil
+			}
+			if symlinkInfo.IsDir() {
+				actionResult.OutputDirectorySymlinks = append(actionResult.OutputDirectorySymlinks, symlink)
+			} else {
+				actionResult.OutputFileSymlinks = append(actionResult.OutputFileSymlinks, symlink)
+			}
+		}
+		return nil
+	}
+
+	// REAPI < v2.1
+	for _, expectedFile := range cmd.OutputFiles {
+		if symlink.Path == expectedFile {
+			actionResult.OutputFileSymlinks = append(actionResult.OutputFileSymlinks, symlink)
+			break
+		}
+	}
+	for _, expectedDir := range cmd.OutputDirectories {
+		if symlink.Path == expectedDir {
+			actionResult.OutputDirectorySymlinks = append(actionResult.OutputDirectorySymlinks, symlink)
+			break
+		}
+	}
+	return nil
+}
+
 func UploadTree(ctx context.Context, env environment.Env, dirHelper *DirHelper, instanceName string, digestFunction repb.DigestFunction_Value, rootDir string, cmd *repb.Command, actionResult *repb.ActionResult) (*TransferInfo, error) {
 	txInfo := &TransferInfo{}
 	startTime := time.Now()
@@ -357,67 +427,9 @@ func UploadTree(ctx context.Context, env environment.Env, dirHelper *DirHelper, 
 				txInfo.BytesTransferred += fileNode.GetDigest().GetSizeBytes()
 				directory.Files = append(directory.Files, fileNode)
 			} else if info.Mode()&os.ModeSymlink == os.ModeSymlink {
-				target, err := os.Readlink(fqfn)
-				if err != nil {
+				if err := handleSymlink(dirHelper, rootDir, cmd, actionResult, directory, fqfn); err != nil {
 					return nil, err
 				}
-				symlink := &repb.OutputSymlink{
-					Path:   trimPathPrefix(fqfn, rootDir),
-					Target: target,
-				}
-
-				// REAPI specification:
-				//   `output_symlinks` will only be populated if the command `output_paths` field
-				//   was used, and not the pre v2.1 `output_files` or `output_directories` fields.
-				//
-				// Check whether the current client is using REAPI version before or after v2.1.
-				if len(cmd.OutputPaths) > 0 && len(cmd.OutputFiles) == 0 && len(cmd.OutputDirectories) == 0 {
-					// >= v2.1
-					if dirHelper.IsOutputPath(fqfn) {
-						actionResult.OutputSymlinks = append(actionResult.OutputSymlinks, symlink)
-
-						// Servers that wish to be compatible with v2.0 API should still
-						// populate `output_file_symlinks` and `output_directory_symlinks`
-						// in addition to `output_symlinks`.
-						if symlinkInfo, err := os.Stat(fqfn); err != nil {
-							// When encounter a dangling symlink, skip setting it to legacy fields.
-							// TODO(sluongng): do we want to log this?
-							log.Warningf("Could not find symlink %q's target: %s, skip legacy fields", fqfn, err)
-						} else {
-							// TODO(sluongng): Since v6.0.0, all the output directories are included in
-							// Action.input_root_digest. So we should be able to save a `stat()` call by
-							// checking wherether the symlink target was included as a directory inside
-							// input root or not.
-							//
-							// Reference:
-							//   https://github.com/sluongng/bazel/commit/4310aeb36c134e5fc61ed5cdfdf683f3e95f19b7
-							if symlinkInfo.IsDir() {
-								actionResult.OutputDirectorySymlinks = append(actionResult.OutputDirectorySymlinks, symlink)
-							} else {
-								actionResult.OutputFileSymlinks = append(actionResult.OutputFileSymlinks, symlink)
-							}
-						}
-					}
-				} else {
-					// < v2.1
-					for _, expectedFile := range cmd.OutputFiles {
-						if symlink.Path == expectedFile {
-							actionResult.OutputFileSymlinks = append(actionResult.OutputFileSymlinks, symlink)
-							break
-						}
-					}
-					for _, expectedDir := range cmd.OutputDirectories {
-						if symlink.Path == expectedDir {
-							actionResult.OutputDirectorySymlinks = append(actionResult.OutputDirectorySymlinks, symlink)
-							break
-						}
-					}
-				}
-
-				directory.Symlinks = append(directory.Symlinks, &repb.SymlinkNode{
-					Name:   symlink.Path,
-					Target: symlink.Target,
-				})
 			}
 		}
 
