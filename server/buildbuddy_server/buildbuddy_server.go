466dab9451d2b39d9613495110a04acb30b5d84c
Maggie Lou <maggie@buildbuddy.io>
2023-05-09 11:17:57 -0700

    PR feedback

--- server/buildbuddy_server/buildbuddy_server.go
+++ server/buildbuddy_server/buildbuddy_server.go
@@ -887,7 +887,7 @@ func (s *BuildBuddyServer) GetWorkflows(ctx context.Context, req *wfpb.GetWorkfl
 func (s *BuildBuddyServer) ExecuteWorkflow(ctx context.Context, req *wfpb.ExecuteWorkflowRequest) (*wfpb.ExecuteWorkflowResponse, error) {
 	if wfs := s.env.GetWorkflowService(); wfs != nil {
 		// Set the workflow ID if it's not on the request
-		// Note: This will only work for workflows created with the githup app integration and not the legacy approach
+		// Note: This will only work for workflows created with the github app integration and not the legacy approach
 		if req.GetWorkflowId() == "" {
 			auth := s.env.GetAuthenticator()
 			if auth == nil {
