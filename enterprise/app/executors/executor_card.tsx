db4c397e06d631f4f6bc9bbc4b683eaaeef9d4e5
Jim Hollenbach <james.d.hollenbach@gmail.com>
2023-05-10 16:54:24 -0700

    strictify enterprise/app/executors/...

--- enterprise/app/executors/executor_card.tsx
+++ enterprise/app/executors/executor_card.tsx
@@ -4,44 +4,42 @@ import format from "../../../app/format/format";
 import { Cloud } from "lucide-react";
 
 interface Props {
-  executor: scheduler.GetExecutionNodesResponse.IExecutor;
+  node: scheduler.ExecutionNode;
+  isDefault: boolean;
 }
 
 export default class ExecutorCardComponent extends React.Component<Props> {
   render() {
-    const node = this.props.executor.node;
-    const isDefault = this.props.executor.isDefault;
-
     return (
-      <div className={`card ${isDefault ? "card-success" : "card-neutral"}`}>
+      <div className={`card ${this.props.isDefault ? "card-success" : "card-neutral"}`}>
         <Cloud className="icon" />
         <div className="content">
           <div className="details">
             <div className="executor-section">
               <div className="executor-section-title">Address:</div>
               <div>
-                {node.host}:{node.port}
+                {this.props.node.host}:{this.props.node.port}
               </div>
             </div>
             <div className="executor-section">
               <div className="executor-section-title">Executor Host ID:</div>
-              <div>{node.executorHostId}</div>
+              <div>{this.props.node.executorHostId}</div>
             </div>
             <div className="executor-section">
               <div className="executor-section-title">Assignable Memory:</div>
-              <div>{format.bytes(+node.assignableMemoryBytes)}</div>
+              <div>{format.bytes(+this.props.node.assignableMemoryBytes)}</div>
             </div>
             <div className="executor-section">
               <div className="executor-section-title">Assignable Milli CPU:</div>
-              <div>{node.assignableMilliCpu}</div>
+              <div>{this.props.node.assignableMilliCpu}</div>
             </div>
             <div className="executor-section">
               <div className="executor-section-title">Version:</div>
-              <div>{node.version}</div>
+              <div>{this.props.node.version}</div>
             </div>
             <div className="executor-section">
               <div className="executor-section-title">Default:</div>
-              <div>{isDefault ? "True" : "False"}</div>
+              <div>{this.props.isDefault ? "True" : "False"}</div>
             </div>
           </div>
         </div>
