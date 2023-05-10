6ae4a7f5f03da7cd889b0b41a579ac219c3b087f
Jim Hollenbach <james.d.hollenbach@gmail.com>
2023-05-10 12:44:29 -0700

    take out stuff that isnt just clickhouse support.

--- enterprise/app/history/history_invocation_card.tsx
+++ enterprise/app/history/history_invocation_card.tsx
@@ -273,8 +273,6 @@ export default class HistoryInvocationCardComponent extends React.Component<Prop
                 {this.props.invocation.branchName}
               </div>
             )}
-            {this.props.invocation.tags &&
-              this.props.invocation.tags.map((tag) => <span className="invocation-tag">{tag.name}</span>)}
             {!this.props.hover && this.props.invocation.commitSha && (
               <div
                 className="detail clickable"
