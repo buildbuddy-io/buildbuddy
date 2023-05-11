770f3f73d499a10f4357c351de7d0a8c8bdef5f3
Siggi Simonarson <siggisim@gmail.com>
2023-05-11 11:03:35 -0700

    Pull OpenAI specific code into its own backend

---
+++ enterprise/server/backends/openai/openai.go
@@ -0,0 +1,80 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io"
+	"net/http"
+
+	"github.com/buildbuddy-io/buildbuddy/cli/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+)
+
+var apiKey = flag.String("openai.api_key", "", "OpenAI API key")
+var Model = flag.String("openai.model", "gpt-3.5-turbo", "OpenAI model name to use. Find them here: https://platform.openai.com/docs/models")
+
+const (
+	chatCompletionsEndpoint = "https://api.openai.com/v1/chat/completions"
+)
+
+func IsConfigured() bool {
+	return *apiKey != ""
+}
+
+func GetCompletions(data *CompletionRequest) (*CompletionResponse, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	postRequest, err := http.NewRequest("POST", chatCompletionsEndpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	postRequest.Header.Set("Content-Type", "application/json")
+	postRequest.Header.Set("Authorization", "Bearer "+*apiKey)
+
+	client := &http.Client{}
+	postResp, err := client.Do(postRequest)
+	if err != nil {
+		return nil, err
+	}
+	defer postResp.Body.Close()
+
+	body, err := io.ReadAll(postResp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if postResp.StatusCode != http.StatusOK {
+		log.Debugf("%+v %+v", postResp.StatusCode, string(body))
+		return nil, status.UnavailableError("Unable to contact suggestion provider.") // todo
+	}
+
+	var response CompletionResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+type CompletionMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type CompletionRequest struct {
+	Model    string              `json:"model"`
+	Messages []CompletionMessage `json:"messages"`
+}
+
+type CompletionChoice struct {
+	Message CompletionMessage `json:"message"`
+}
+
+type CompletionResponse struct {
+	Choices []CompletionChoice `json:"choices"`
+}
