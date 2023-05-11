02a1014d54a1d755a7685bb3cd93766ee8126f2e
Siggi Simonarson <siggisim@gmail.com>
2023-05-11 15:11:41 -0700

    Add Google's VertexAI as a suggestion backend (#3954)

--- enterprise/server/suggestion/suggestion.go
+++ enterprise/server/suggestion/suggestion.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/backends/openai"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/backends/vertexai"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/eventlog"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
@@ -20,6 +21,15 @@ const (
 	// GPT 3.5 is limited to 4,096 tokens and each token is roughly 4 english characters.
 	// We limit our log inputs to roughly half that to avoid going over any input limits.
 	maxChars = 8000
+
+	prompt = "How would you fix this error?"
+
+	// Various vertex model parameters.
+	// For more information about each one, read: https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models
+	vertexTemperature     = 0.2
+	vertexMaxOutputTokens = 1024
+	vertexTopK            = 0.8
+	vertexTopP            = 40
 )
 
 type suggestionService struct {
@@ -27,7 +37,7 @@ type suggestionService struct {
 }
 
 func Register(env environment.Env) error {
-	if openai.IsConfigured() {
+	if openai.IsConfigured() || vertexai.IsConfigured() {
 		env.SetSuggestionService(New(env))
 	}
 	return nil
@@ -37,6 +47,10 @@ func New(env environment.Env) *suggestionService {
 	return &suggestionService{
 		env: env,
 	}
+}
+
+func (s *suggestionService) MultipleProvidersConfigured() bool {
+	return openai.IsConfigured() && vertexai.IsConfigured()
 }
 
 func (s *suggestionService) GetSuggestion(ctx context.Context, req *supb.GetSuggestionRequest) (*supb.GetSuggestionResponse, error) {
@@ -64,25 +78,76 @@ func (s *suggestionService) GetSuggestion(ctx context.Context, req *supb.GetSugg
 		errorMessage = errorMessage[:maxChars] // Truncate to avoid going over api input limit.
 	}
 
+	r := ""
+	if openai.IsConfigured() &&
+		(!s.MultipleProvidersConfigured() ||
+			(s.MultipleProvidersConfigured() && req.GetService() == supb.SuggestionService_OPENAI)) {
+		r, err = openaiRequest(errorMessage)
+		if err != nil {
+			return nil, err
+		}
+	} else if vertexai.IsConfigured() {
+		r, err = vertexaiRequest(ctx, errorMessage)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	res := &supb.GetSuggestionResponse{
+		Suggestion: []string{r},
+	}
+
+	return res, nil
+}
+
+func openaiRequest(input string) (string, error) {
 	data := &openai.CompletionRequest{Model: *openai.Model, Messages: []openai.CompletionMessage{
 		openai.CompletionMessage{
 			Role:    "user",
-			Content: "How would you fix this error? " + errorMessage,
+			Content: prompt + " " + input,
 		},
 	}}
 
 	completionResponse, err := openai.GetCompletions(data)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	if len(completionResponse.Choices) < 1 {
-		return nil, status.NotFoundError("No suggestions found.")
+		return "", status.NotFoundError("No suggestions found.")
 	}
 
-	res := &supb.GetSuggestionResponse{
-		Suggestion: []string{completionResponse.Choices[0].Message.Content},
+	return completionResponse.Choices[0].Message.Content, nil
+}
+
+func vertexaiRequest(ctx context.Context, input string) (string, error) {
+	data := &vertexai.PredictionRequest{Instances: []vertexai.PredictionInstance{
+		{
+			Examples: []string{},
+			Context:  "",
+			Messages: []vertexai.PredictionMessage{
+				{
+					Author:  "user",
+					Content: prompt + " " + input,
+				},
+			},
+		},
+	}, Parameters: vertexai.PredictionParameters{
+		Temperature:     vertexTemperature,
+		MaxOutputTokens: vertexMaxOutputTokens,
+		TopK:            vertexTopK,
+		TopP:            vertexTopP,
+	}}
+
+	predictionResponse, err := vertexai.GetPrediction(ctx, data)
+	if err != nil {
+		return "", err
 	}
 
-	return res, nil
+	if len(predictionResponse.Predictions) < 1 || len(predictionResponse.Predictions[0].Candidates) < 1 {
+		log.Debugf("empty response from vertexai: %+v", predictionResponse)
+		return "", status.NotFoundError("No suggestions found.")
+	}
+
+	return predictionResponse.Predictions[0].Candidates[0].Content, nil
 }
