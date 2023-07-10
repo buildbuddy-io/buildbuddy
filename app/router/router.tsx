50f21d64b07b3755f64e952f2741c81a6451adc5
Jim Hollenbach <james.d.hollenbach@gmail.com>
2023-07-10 14:58:35 -0700

    switch to CreateWorkflow instead of GetWorkflows for determining current access so that it still restricts to admins.

--- app/router/router.tsx
+++ app/router/router.tsx
@@ -301,7 +301,7 @@ class Router {
   }
 
   canAccessWorkflowsPage(user?: User) {
-    const workflowsAdmin = capabilities.workflows && Boolean(user?.canCall("getWorkflows"));
+    const workflowsAdmin = capabilities.workflows && Boolean(user?.canCall("createWorkflow"));
     const workflowsUser =
       capabilities.workflows &&
       capabilities.config.workflowHistoryEnabled &&
