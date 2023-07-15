22a88189733a2d4c959018e2edae68764ef1da24
Tyler Williams <williams.tyler@gmail.com>
2023-07-14 17:04:30 -0700

    Don't double cgroup if using runsc (#4324)

--- enterprise/server/remote_execution/containers/podman/podman.go
+++ enterprise/server/remote_execution/containers/podman/podman.go
@@ -375,6 +375,11 @@ func (c *podmanCommandContainer) getPodmanRunArgs(workDir string) []string {
 	}
 	if c.options.Runtime != "" {
 		args = append(args, "--runtime="+c.options.Runtime)
+		if filepath.Base(c.options.Runtime) == "runsc" {
+			// gVisor will attempt to setup cgroups, but podman has
+			// already done that, so tell gVisor not to.
+			args = append(args, "--runtime-flag=ignore-cgroups")
+		}
 	}
 	if c.imageIsStreamable {
 		args = append(args, enableStreamingStoreArg)
