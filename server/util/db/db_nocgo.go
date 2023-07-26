8b26acc50d54867f66e5a4d4f7c9a5bdf1cf569a
Son Luong Ngoc <sluongng@gmail.com>
2023-07-26 22:35:16 +0200

    server/util/db: allow compilation without cgo (#4286)

---
+++ server/util/db/db_nocgo.go
@@ -0,0 +1,32 @@
+//go:build !cgo
+
+package db
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+
+	gomysql "github.com/go-sql-driver/mysql"
+	gopostgres "github.com/jackc/pgx/v5/stdlib"
+)
+
+func getDriver(ds DataSource) (driver.Driver, error) {
+	switch ds.DriverName() {
+	case mysqlDriver:
+		return &gomysql.MySQLDriver{}, nil
+	case postgresDriver:
+		return &gopostgres.Driver{}, nil
+	default:
+		return nil, fmt.Errorf("unsupported database driver %s", ds.DriverName())
+	}
+}
+
+func (h *DBHandle) IsDuplicateKeyError(err error) bool {
+	var mysqlErr *gomysql.MySQLError
+	// Defined at https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html#error_er_dup_entry
+	if errors.As(err, &mysqlErr) && mysqlErr.Number == 1062 {
+		return true
+	}
+	return false
+}
