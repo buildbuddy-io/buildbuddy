88e5b8d094e71689386edae3332df60a4c2b8fa6
Maggie Lou <maggie@buildbuddy.io>
2023-08-01 15:51:40 -0700

    Move waiting for setup msg to goroutine

--- enterprise/server/remote_execution/uffd/uffd.go
+++ enterprise/server/remote_execution/uffd/uffd.go
@@ -97,14 +97,6 @@ func NewHandler() (*Handler, error) {
 // Start fulfills UFFD requests using the given memory store. If the UFFD object has not already
 // been initialized, it will also listen on the given socket path for Firecracker's UFFD initialization message
 func (h *Handler) Start(ctx context.Context, socketPath string, memoryStore *blockio.COWStore) error {
-	if h.uffd == 0 {
-		// Get uffd sent from firecracker
-		err := h.receiveSetupMsg(ctx, socketPath)
-		if err != nil {
-			return status.WrapError(err, "receive setup message from firecracker")
-		}
-	}
-
 	// Create a FD that can be used to terminate Poll early
 	pipeRead, pipeWrite, err := os.Pipe()
 	if err != nil {
@@ -114,7 +106,16 @@ func (h *Handler) Start(ctx context.Context, socketPath string, memoryStore *blo
 	h.earlyTerminationWriter = pipeWrite
 
 	go func() {
-		if err := h.handle(ctx, memoryStore); err != nil {
+		if h.uffd == 0 {
+			// Get uffd sent from firecracker
+			err = h.receiveSetupMsg(ctx, socketPath)
+			if err != nil {
+				log.CtxErrorf(ctx, "Failed to receive setup message from firecracker: %s", err)
+				return
+			}
+		}
+
+		if err = h.handle(ctx, memoryStore); err != nil {
 			log.CtxErrorf(ctx, "Failed to handle firecracker memory requests: %s", err)
 		}
 	}()
