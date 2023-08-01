8fd9b7ad2a1d733e5664875684ae0bd0aa2cdddf
James Hollenbach <jdhollen@feelings.attlocal.net>
2023-08-01 15:57:01 -0700

    ff

--- enterprise/tools/fcflakes/run.sh
+++ enterprise/tools/fcflakes/run.sh
@@ -18,7 +18,7 @@ else
   )
 fi
 
-exec bazel test //enterprise/tools/fcflakes:repro \
+exec bb test //enterprise/tools/fcflakes:repro \
   --runs_per_test=1000 \
   --test_keep_going \
   --test_output=errors \
