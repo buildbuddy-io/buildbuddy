4fe2f08e9cc95b67ad7c0ca0c8b77481326c2174
Son Luong Ngoc <sluongng@gmail.com>
2023-08-02 16:11:11 +0200

    add test

--- server/util/fastcopy/fastcopy_darwin.go
+++ server/util/fastcopy/fastcopy_darwin.go
@@ -1,28 +1,15 @@
-//go:build darwin
-// +build darwin
+//go:build darwin && !ios
 
 package fastcopy
 
-/*
-#include <stdint.h> // for uint32_t
-#include <stdlib.h> // for free
-#include <sys/clonefile.h>
-*/
-import "C"
-
 import (
-	"syscall"
-	"unsafe"
+	"errors"
+
+	"golang.org/x/sys/unix"
 )
 
 func FastCopy(source, destination string) error {
-	src := C.CString(source)
-	defer C.free(unsafe.Pointer(src))
-	dst := C.CString(destination)
-	defer C.free(unsafe.Pointer(dst))
-
-	ret, err := C.clonefile(src, dst, C.uint32_t(0))
-	if errno, ok := err.(syscall.Errno); ret == -1 && ok && errno != syscall.EEXIST {
+	if err := unix.Clonefile(source, destination, unix.CLONE_NOFOLLOW); err != nil && !errors.Is(err, unix.EEXIST) {
 		return err
 	}
 	return nil
