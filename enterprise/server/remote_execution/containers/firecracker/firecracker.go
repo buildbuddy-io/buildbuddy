168ca9f05890a2d1b730d7077a1999775f874cd9
Maggie Lou <maggie@buildbuddy.io>
2023-08-04 10:42:44 -0700

    Further increase timeout to reduce flakiness

--- enterprise/server/remote_execution/containers/firecracker/firecracker.go
+++ enterprise/server/remote_execution/containers/firecracker/firecracker.go
@@ -76,7 +76,7 @@ const (
 	firecrackerSocketWaitTimeout = 3 * time.Second
 
 	// How long to wait when dialing the vmexec server inside the VM.
-	vSocketDialTimeout = 30 * time.Second
+	vSocketDialTimeout = 50 * time.Second
 
 	// How long to wait for the jailer directory to be created.
 	jailerDirectoryCreationTimeout = 1 * time.Second
