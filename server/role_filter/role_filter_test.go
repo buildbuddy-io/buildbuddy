226ca83b54e4a34c6d464d7aa29fbd27cb3400ef
Brandon Duffany <brandon@buildbuddy.io>
2023-08-16 17:45:04 -0400

    Add missing request/response context fields (#4572)

--- server/role_filter/role_filter_test.go
+++ server/role_filter/role_filter_test.go
@@ -9,6 +9,7 @@ import (
 
 	apipb "github.com/buildbuddy-io/buildbuddy/proto/api/v1"
 	bbspb "github.com/buildbuddy-io/buildbuddy/proto/buildbuddy_service"
+	ctxpb "github.com/buildbuddy-io/buildbuddy/proto/context"
 )
 
 func TestAllRPCsHaveExplicitRolesSpecified(t *testing.T) {
@@ -37,4 +38,25 @@ func TestAllRPCsHaveExplicitRolesSpecified(t *testing.T) {
 		"All BuildBuddyService RPCs listed in role_filter.go should be valid BuildBuddy service RPCs. "+
 			"(check for typos, or if you deleted an RPC, remove it from role_filter.go)",
 	)
+}
+
+func TestBuildBuddyServiceRPCsHaveRequestAndResponseContextFields(t *testing.T) {
+	type Req interface{ GetRequestContext() *ctxpb.RequestContext }
+	type Res interface{ GetResponseContext() *ctxpb.ResponseContext }
+	reqType := reflect.TypeOf((*Req)(nil)).Elem()
+	resType := reflect.TypeOf((*Res)(nil)).Elem()
+
+	buildbuddyServiceType := reflect.TypeOf((*bbspb.BuildBuddyServiceServer)(nil)).Elem()
+	for i := 0; i < buildbuddyServiceType.NumMethod(); i++ {
+		methodFunc := buildbuddyServiceType.Method(i).Type
+		methodName := buildbuddyServiceType.Method(i).Name
+		reqMsg := methodFunc.In(1)
+		if !reqMsg.Implements(reqType) {
+			assert.Failf(t, "missing request_context field", "BuildBuddyService/%s request message %s must have a field 'context.RequestContext request_context'", methodName, reqMsg)
+		}
+		resMsg := methodFunc.Out(0)
+		if !resMsg.Implements(resType) {
+			assert.Failf(t, "missing response_context field", "BuildBuddyService/%s response message %s must have a field 'context.ResponseContext response_context'", methodName, resMsg)
+		}
+	}
 }
