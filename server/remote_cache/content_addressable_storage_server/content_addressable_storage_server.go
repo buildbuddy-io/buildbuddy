b84f64cda3a4792f42bf65ac52212e5e040d1148
Tyler Williams <williams.tyler@gmail.com>
2023-08-23 14:36:40 -0700

    Use tree cache more (#4610)

--- server/remote_cache/content_addressable_storage_server/content_addressable_storage_server.go
+++ server/remote_cache/content_addressable_storage_server/content_addressable_storage_server.go
@@ -43,7 +43,7 @@ var (
 	enableTreeCaching       = flag.Bool("cache.enable_tree_caching", true, "If true, cache GetTree responses (full and partial)")
 	treeCacheSeed           = flag.String("cache.tree_cache_seed", "treecache-03011023", "If set, hash this with digests before caching / reading from tree cache")
 	minTreeCacheLevel       = flag.Int("cache.tree_cache_min_level", 1, "The min level at which the tree may be cached. 0 is the root")
-	minTreeCacheDescendents = flag.Int("cache.tree_cache_min_descendents", 10, "The min number of descendents a node must parent in order to be cached")
+	minTreeCacheDescendents = flag.Int("cache.tree_cache_min_descendents", 3, "The min number of descendents a node must parent in order to be cached")
 	maxTreeCacheSetDuration = flag.Duration("cache.max_tree_cache_set_duration", time.Second, "The max amount of time to wait for unfinished tree cache entries to be set.")
 )
 
