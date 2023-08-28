e71b7acfa3a6a3ca94150c7eaaf5e4aca874c35c
Son Luong Ngoc <sluongng@gmail.com>
2023-08-28 10:32:42 +0200

    runner: make podman and firecracker linux only (#4382)

---
+++ enterprise/server/remote_execution/runner/runner_darwin.go
@@ -0,0 +1,33 @@
+//go:build darwin && !ios
+
+package runner
+
+import (
+	"context"
+
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/container"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/containers/bare"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/containers/sandbox"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/platform"
+)
+
+func (p *pool) initContainerProviders() error {
+	providers := make(map[platform.ContainerType]container.Provider)
+	providers[platform.SandboxContainerType] = &sandbox.Provider{}
+	providers[platform.BareContainerType] = &bare.Provider{}
+
+	p.containerProviders = providers
+	return nil
+}
+
+func (r *commandRunner) startVFS() error {
+	return nil
+}
+
+func (r *commandRunner) prepareVFS(ctx context.Context, layout *container.FileSystemLayout) error {
+	return nil
+}
+
+func (r *commandRunner) removeVFS() error {
+	return nil
+}
