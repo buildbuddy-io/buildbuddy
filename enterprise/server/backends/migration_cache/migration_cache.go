c7fe5be268b250e1d926cd7f8cd512f8ce444133
Lulu Zhang <lulu@buildbuddy.io>
2023-08-29 14:47:12 -0700

    read remaining bytes in dest cache

--- enterprise/server/backends/migration_cache/migration_cache.go
+++ enterprise/server/backends/migration_cache/migration_cache.go
@@ -442,6 +442,8 @@ type doubleReader struct {
 	doubleReadBuf []byte
 	bytesReadSrc  int
 	bytesReadDest int
+	lastSrcError  error
+	lastDestErr   error
 }
 
 func (d *doubleReader) Read(p []byte) (n int, err error) {
@@ -457,12 +459,14 @@ func (d *doubleReader) Read(p []byte) (n int, err error) {
 			var dstN int
 			dstN, dstErr = d.dest.Read(d.doubleReadBuf)
 			d.bytesReadDest += dstN
+			d.lastDestErr = dstErr
 			return nil
 		})
 	}
 
 	srcN, srcErr := d.src.Read(p)
 	d.bytesReadSrc += srcN
+	d.lastSrcError = srcErr
 
 	eg.Wait()
 	// Don't log on EOF errors when reading chunks, because the readers from different caches
@@ -478,11 +482,14 @@ func (d *doubleReader) Close() error {
 	eg := &errgroup.Group{}
 	if d.dest != nil {
 		// Don't log on byte differences for AC records, because there could be minor differences in metadata like timestamps
-		if d.r.GetCacheType() == rspb.CacheType_CAS && d.bytesReadDest != d.bytesReadSrc {
-			log.Warningf("Migration %v read err, src read %d bytes, dest read %d bytes", d.r, d.bytesReadSrc, d.bytesReadDest)
-		}
-
 		eg.Go(func() error {
+			if d.lastSrcError == io.EOF && d.lastDestErr != io.EOF {
+				destBuf, err := io.ReadAll(d.dest)
+				if err != nil {
+					log.Warningf("Migration %v read err: failed to read remaining bytes from dest cache: %s", d.r, err)
+				}
+				d.bytesReadDest += len(destBuf)
+			}
 			dstErr := d.dest.Close()
 			if dstErr != nil {
 				log.Warningf("Migration dest reader close err: %s", dstErr)
@@ -493,6 +500,12 @@ func (d *doubleReader) Close() error {
 
 	srcErr := d.src.Close()
 	eg.Wait()
+
+	if d.dest != nil {
+		if d.r.GetCacheType() == rspb.CacheType_CAS && d.bytesReadDest != d.bytesReadSrc {
+			log.Warningf("Migration %v read err, src read %d bytes, dest read %d bytes", d.r, d.bytesReadSrc, d.bytesReadDest)
+		}
+	}
 
 	return srcErr
 }
