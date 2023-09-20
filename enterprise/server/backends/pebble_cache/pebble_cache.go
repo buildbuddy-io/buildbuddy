42af53c4d373a8f7be0a65a0fdc54e3d7efab4c6
Lulu Zhang <lulu@buildbuddy.io>
2023-09-19 17:24:41 -0700

    commit then close (#4828)

--- enterprise/server/backends/pebble_cache/pebble_cache.go
+++ enterprise/server/backends/pebble_cache/pebble_cache.go
@@ -1982,12 +1982,10 @@ func (cdcw *cdcWriter) writeRawChunk(fileRecord *rfpb.FileRecord, key filestore.
 	if err != nil {
 		return err
 	}
+	defer wcm.Close()
 	_, err = wcm.Write(chunkData)
 	if err != nil {
 		return status.InternalErrorf("failed to write raw chunk: %s", err)
-	}
-	if err := wcm.Close(); err != nil {
-		return status.InternalErrorf("failed to close while writing raw chunk: %s", err)
 	}
 	if err := wcm.Commit(); err != nil {
 		return status.InternalErrorf("failed to commit while writing raw chunk: %s", err)
