fd359856d37c11131e026823a54bba8a3c431063
Jim Hollenbach <jim@buildbuddy.io>
2023-09-21 16:12:24 -0700

    remove useless added newline

--- app/router/router.tsx
+++ app/router/router.tsx
@@ -160,7 +160,6 @@ class Router {
    */
   navigateToDatePreserveHash(startTimeMillis: number, endTimeMillis?: number) {
     const url = new URL(window.location.href);
-
     url.searchParams.set(START_DATE_PARAM_NAME, String(startTimeMillis));
     if (endTimeMillis) {
       url.searchParams.set(END_DATE_PARAM_NAME, String(endTimeMillis));
