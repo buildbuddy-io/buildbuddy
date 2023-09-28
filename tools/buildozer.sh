2461cb0f5d45a4dedd680dac73d87289447e9acb
Maggie Lou <maggie@buildbuddy.io>
2023-09-28 15:53:09 -0500

    Make firecracker test script explicitly fail if buildozer is not installed

--- tools/buildozer.sh
+++ tools/buildozer.sh
@@ -3,7 +3,7 @@ set -e
 
 # TODO: provision buildozer with bazel
 if ! command -v buildozer >/dev/null; then
-  echo >&2 "Missing 'buildozer' in \$PATH. Install with:"
+  echo >&2 "Missing 'buildozer' in \$PATH. Make sure ~/go/bin is in \$PATH and install with:"
   echo >&2 "    go install github.com/bazelbuild/buildtools/buildozer@latest"
   exit 1
 fi
