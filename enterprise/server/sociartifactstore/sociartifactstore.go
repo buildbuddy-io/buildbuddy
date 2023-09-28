a7dc97c65ff1f33243ae8512cfe3738b3fc96c50
Iain Macdonald <xiainx@gmail.com>
2023-09-28 11:35:47 -0700

    Update salt because format changed

--- enterprise/server/sociartifactstore/sociartifactstore.go
+++ enterprise/server/sociartifactstore/sociartifactstore.go
@@ -48,7 +48,7 @@ import (
 
 var (
 	layerStorage       = flag.String("soci_artifact_store.layer_storage", "/tmp/", "Directory in which to store pulled container image layers for indexing by soci artifact store.")
-	sociIndexCacheSeed = flag.String("soci_artifact_store.cache_seed", "socicache-09272023", "If set, this seed is hashed with container image IDs to generate cache keys storing soci indexes.")
+	sociIndexCacheSeed = flag.String("soci_artifact_store.cache_seed", "socicache-092872023", "If set, this seed is hashed with container image IDs to generate cache keys storing soci indexes.")
 )
 
 const (
