a20b25c4313f3a94c9c54321042e0fd92a9d0ff7
Tyler Williams <williams.tyler@gmail.com>
2023-10-12 13:30:43 -0700

    Fix AC hitrate (thanks Jim)

--- server/remote_cache/digest/digest.go
+++ server/remote_cache/digest/digest.go
@@ -18,6 +18,7 @@ import (
 	"sync"
 
 	"github.com/buildbuddy-io/buildbuddy/server/util/alert"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/zeebo/blake3"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
@@ -306,7 +307,7 @@ func HashForDigestType(digestType repb.DigestFunction_Value) (hash.Hash, error) 
 	case repb.DigestFunction_BLAKE3:
 		return blake3.New(), nil
 	case repb.DigestFunction_UNKNOWN:
-		// TODO(tylerw): make this a warning when clients support this.
+		log.Warningf("Digest function was unset: defaulting to SHA256")
 		return sha256.New(), nil
 	default:
 		return nil, status.UnimplementedErrorf("No support for digest type: %s", digestType)
