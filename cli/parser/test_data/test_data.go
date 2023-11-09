c96ac79ff1791305982263ddca5d37efaa4e02b1
Brandon Duffany <brandon@buildbuddy.io>
2023-11-09 18:28:30 -0500

    cli: fix 'common' config parsing

--- cli/parser/test_data/test_data.go
+++ cli/parser/test_data/test_data.go
@@ -5,5 +5,14 @@ import _ "embed"
 //go:embed bazel5.3.1_help_startup_options.txt
 var BazelHelpStartupOptionsOutput string
 
+//go:embed bazel5.3.1_help_build.txt
+var BazelHelpBuildOutput string
+
+//go:embed bazel5.3.1_help_run.txt
+var BazelHelpRunOutput string
+
 //go:embed bazel5.3.1_help_test.txt
 var BazelHelpTestOutput string
+
+//go:embed bazel5.3.1_help_query.txt
+var BazelHelpQueryOutput string
