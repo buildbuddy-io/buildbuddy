90159796f8060e2b67807965c59600beb0e46ed8
Tyler Williams <williams.tyler@gmail.com>
2023-11-08 21:19:59 -0800

    Blocking channel

--- enterprise/server/raft/listener/listener.go
+++ enterprise/server/raft/listener/listener.go
@@ -54,12 +54,7 @@ func (rl *RaftListener) LeaderUpdated(info raftio.LeaderInfo) {
 	rl.lastLeaderInfo = &info
 
 	for _, ch := range rl.leaderChangeListeners {
-		select {
-		case ch <- info:
-			continue
-		default:
-			log.Warning("Dropped LeaderUpdate callback")
-		}
+		ch <- info
 	}
 }
 
