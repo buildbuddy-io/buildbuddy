c9b4e148631e47ac285927c08803a4c3369d5057
Jim Hollenbach <jim@buildbuddy.io>
2023-11-14 11:36:32 -0800

    make grpc use RecvBufferPools

--- server/util/grpc_client/grpc_client.go
+++ server/util/grpc_client/grpc_client.go
@@ -199,6 +199,7 @@ func CommonGRPCClientOptions() []grpc.DialOption {
 		interceptors.GetUnaryClientInterceptor(),
 		interceptors.GetStreamClientInterceptor(),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(math.MaxInt32)),
+		grpc.WithRecvBufferPool(grpc.NewSharedBufferPool()),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
 			// After a duration of this time if the client doesn't see any activity it
 			// pings the server to see if the transport is still alive.
