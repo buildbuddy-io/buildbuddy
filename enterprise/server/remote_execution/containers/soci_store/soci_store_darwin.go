aec902a5f8ad9a0e8bceb02a54051af99ff394df
Iain Macdonald <xiainx@gmail.com>
2023-11-16 16:07:46 -0800

    Move linux/darwin common stuff to common location and remove unused flag.

--- enterprise/server/remote_execution/containers/soci_store/soci_store_darwin.go
+++ enterprise/server/remote_execution/containers/soci_store/soci_store_darwin.go
@@ -4,48 +4,8 @@
 package soci_store
 
 import (
-	"context"
-
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/oci"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 )
-
-type Store interface {
-	Exists(ctx context.Context) error
-	WaitUntilExists() error
-
-	// Returns the command-line argument to pass to podman in order to stream
-	// images using this soci-store.
-	EnableStreamingStoreArg() string
-
-	GetArtifacts(ctx context.Context, env environment.Env, image string, creds oci.Credentials) error
-
-	SeedCredentials(ctx context.Context, image string, credentials oci.Credentials) error
-}
-
-// A SociStore implementation that does not start up a soci-store and does not
-// stream container images.
-type NoStore struct{}
-
-func (_ NoStore) Exists(ctx context.Context) error {
-	return nil
-}
-
-func (_ NoStore) WaitUntilExists() error {
-	return nil
-}
-
-func (_ NoStore) EnableStreamingStoreArg() string {
-	return ""
-}
-
-func (_ NoStore) GetArtifacts(ctx context.Context, env environment.Env, image string, creds oci.Credentials) error {
-	return nil
-}
-
-func (_ NoStore) SeedCredentials(ctx context.Context, image string, credentials oci.Credentials) error {
-	return nil
-}
 
 func Init(env environment.Env) (Store, error) {
 	return &NoStore{}, nil
---
+++ enterprise/server/remote_execution/containers/soci_store/soci_store_test.go
@@ -0,0 +1,13 @@
+package soci_store_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFoo(t *testing.T) {
+	require.NoError(t, nil)
+	assert.True(t, true)
+}
