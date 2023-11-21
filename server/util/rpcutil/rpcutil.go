13060909a98ab5bacca345a968bf2b9c0b290760
Vadim Berezniker <vadim@buildbuddy.io>
2023-11-21 10:28:08 -0800

    better doc string

--- server/util/rpcutil/rpcutil.go
+++ server/util/rpcutil/rpcutil.go
@@ -17,6 +17,21 @@ type RecvStream[T proto.Message] interface {
 
 // RecvChan starts a goroutine that receives messages from a stream and puts it
 // on a channel.
+//
+// Example usage:
+//
+// msgs := rpcutil.RecvChan[*bspb.ReadResponse](ctx, stream)
+//
+//	for {
+//	  select {
+//	    case msg := <-msgs:
+//	      // handle stream message/err
+//	    case <-ctx.Done():
+//	      return ctx.Err()
+//	  }
+//	}
+//
+// It's important to always include a ctx.Done() case to avoid leaks.
 func RecvChan[T proto.Message](ctx context.Context, stream RecvStream[T]) chan StreamMsg[T] {
 	streamMsgs := make(chan StreamMsg[T])
 	go func() {
