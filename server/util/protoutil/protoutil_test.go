965b0d585400c844ac02aacd0c201315653af782
Lulu Zhang <lulu@buildbuddy.io>
2023-12-04 13:18:39 -0800

    wip

--- server/util/protoutil/protoutil_test.go
+++ server/util/protoutil/protoutil_test.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	capb "github.com/buildbuddy-io/buildbuddy/proto/cache"
 	rfpb "github.com/buildbuddy-io/buildbuddy/proto/raft"
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+	rspb "github.com/buildbuddy-io/buildbuddy/proto/resource"
 	"github.com/buildbuddy-io/buildbuddy/server/util/protoutil"
+	statuspb "google.golang.org/genproto/googleapis/rpc/status"
+	gcodes "google.golang.org/grpc/codes"
 )
 
 type protoMessage interface {
@@ -33,18 +40,54 @@ func generateFileMetadata(t testing.TB) []protoMessage {
 }
 
 func generateScoreCard(t testing.TB) []protoMessage {
-	res := make([]protoMessage, 0, 100)
-	for i := 0; i < 100; i++ {
-		sc := &capb.ScoreCard{}
-		err := faker.FakeData(sc)
-		require.NoError(t, err, "unable to fake data for scoreCard")
-		//for j := 0; j < 1; j++ {
-		//   result := &capb.ScoreCard_Result{}
-		//   err := faker.FakeData(result)
-		//   //sc.Results = append(sc.Results, result)
-		//   res = append(res, result)
-		//}
-		res = append(res, sc)
+	besUpload := &capb.ScoreCard_Result{
+		ActionId:    "bes-upload",
+		Digest:      &repb.Digest{Hash: "aaa", SizeBytes: 1_000},
+		CacheType:   rspb.CacheType_CAS,
+		RequestType: capb.RequestType_WRITE,
+		Status:      &statuspb.Status{Code: int32(gcodes.OK)},
+		StartTime:   timestamppb.New(time.Unix(100, 0)),
+		Duration:    durationpb.New(300 * time.Millisecond),
+	}
+	acMiss := &capb.ScoreCard_Result{
+		ActionId:       "abc",
+		ActionMnemonic: "GoCompile",
+		TargetId:       "//foo",
+		Digest:         &repb.Digest{Hash: "abc", SizeBytes: 111},
+		CacheType:      rspb.CacheType_AC,
+		RequestType:    capb.RequestType_READ,
+		Status:         &statuspb.Status{Code: int32(gcodes.NotFound)},
+		StartTime:      timestamppb.New(time.Unix(300, 0)),
+		Duration:       durationpb.New(100 * time.Millisecond),
+	}
+	casUpload := &capb.ScoreCard_Result{
+		ActionId:       "abc",
+		ActionMnemonic: "GoCompile",
+		TargetId:       "//foo",
+		Digest:         &repb.Digest{Hash: "ccc", SizeBytes: 10_000},
+		CacheType:      rspb.CacheType_CAS,
+		RequestType:    capb.RequestType_WRITE,
+		Status:         &statuspb.Status{Code: int32(gcodes.OK)},
+		StartTime:      timestamppb.New(time.Unix(200, 0)),
+		Duration:       durationpb.New(200 * time.Millisecond),
+	}
+	casDownload := &capb.ScoreCard_Result{
+		ActionId:       "edf",
+		ActionMnemonic: "GoLink",
+		TargetId:       "//bar",
+		Digest:         &repb.Digest{Hash: "fff", SizeBytes: 100_000},
+		CacheType:      rspb.CacheType_CAS,
+		RequestType:    capb.RequestType_READ,
+		Status:         &statuspb.Status{Code: int32(gcodes.OK)},
+		StartTime:      timestamppb.New(time.Unix(400, 0)),
+		Duration:       durationpb.New(150 * time.Millisecond),
+	}
+
+	res := []protoMessage{
+		besUpload,
+		acMiss,
+		casUpload,
+		casDownload,
 	}
 	return res
 }
@@ -99,11 +142,11 @@ func BenchmarkMarshal(b *testing.B) {
 	}
 
 	marshalFns := map[string]marshalFunc{
-		"protoutil": func(v protoMessage) ([]byte, error) {
-			return v.MarshalVT()
-		},
 		"proto": func(v protoMessage) ([]byte, error) {
 			return proto.Marshal(v)
+		},
+		"protoutil": func(v protoMessage) ([]byte, error) {
+			return v.MarshalVT()
 		},
 	}
 
@@ -128,26 +171,26 @@ func BenchmarkUnmarshal(b *testing.B) {
 		"fileMetadata": unmarshalFnAndData{
 			data: generateBytes(b, mds),
 			fns: map[string]unmarshalFunc{
-				"protoutil": func(buf []byte) error {
-					v := &rfpb.FileMetadata{}
-					return protoutil.Unmarshal(buf, v)
-				},
 				"proto": func(buf []byte) error {
 					v := &rfpb.FileMetadata{}
 					return proto.Unmarshal(buf, v)
+				},
+				"protoutil": func(buf []byte) error {
+					v := &rfpb.FileMetadata{}
+					return protoutil.Unmarshal(buf, v)
 				},
 			},
 		},
 		"scoreCard": unmarshalFnAndData{
 			data: generateBytes(b, scoreCards),
 			fns: map[string]unmarshalFunc{
-				"protoutil": func(buf []byte) error {
-					v := &capb.ScoreCard{}
-					return protoutil.Unmarshal(buf, v)
-				},
 				"proto": func(buf []byte) error {
-					v := &capb.ScoreCard{}
+					v := &capb.ScoreCard_Result{}
 					return proto.Unmarshal(buf, v)
+				},
+				"protoutil": func(buf []byte) error {
+					v := &capb.ScoreCard_Result{}
+					return v.UnmarshalVT(buf)
 				},
 			},
 		},
