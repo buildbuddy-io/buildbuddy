c2a6faec1ccf88c26d096df7bb8109169e1aea21
Lulu Zhang <lulu@buildbuddy.io>
2023-12-08 09:05:53 -0800

    Add docs for exported prometheus metrics for cloud users (#5470)

--- website/sidebars.js
+++ website/sidebars.js
@@ -24,7 +24,7 @@ module.exports = {
       "enterprise-mac-rbe",
       "enterprise-api",
     ],
-    Monitoring: ["prometheus-metrics"],
+    Monitoring: ["prometheus-metrics-on-prem", "prometheus-metrics-for-cloud"],
     Architecture: [
       "architecture-overview",
       "architecture-data-storage",
