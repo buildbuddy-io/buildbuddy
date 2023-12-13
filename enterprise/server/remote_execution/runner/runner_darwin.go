3286195d447102039eced18c02656b32ecfb53a0
Maggie Lou <maggie@buildbuddy.io>
2023-12-13 16:06:12 -0600

    Fix linux-support-only firecracker import on mac/windows builds

--- enterprise/server/remote_execution/runner/runner_darwin.go
+++ enterprise/server/remote_execution/runner/runner_darwin.go
@@ -9,6 +9,8 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/containers/bare"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/containers/sandbox"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/platform"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
 
 func (p *pool) registerContainerProviders(providers map[platform.ContainerType]container.Provider, executor *platform.ExecutorProperties) error {
@@ -33,4 +35,8 @@ func (r *commandRunner) prepareVFS(ctx context.Context, layout *container.FileSy
 
 func (r *commandRunner) removeVFS() error {
 	return nil
+}
+
+func (r *commandRunner) hasMaxResourceUtilization(ctx context.Context, usageStats *repb.UsageStats) bool {
+	return false
 }
