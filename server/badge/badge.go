9430d34b8e312a84840000782514a7e63fec33cb
Lulu Zhang <lulu@buildbuddy.io>
2024-01-19 09:31:55 -0800

    backend: export display user

--- server/badge/badge.go
+++ server/badge/badge.go
@@ -5,10 +5,31 @@ import (
 
 	bpb "github.com/buildbuddy-io/buildbuddy/proto/badge"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 )
 
 func GetUserBadges(ctx context.Context, env environment.Env, req *bpb.GetUserBadgesRequest) (*bpb.GetUserBadgesResponse, error) {
+	auth := env.GetAuthenticator()
+	if auth == nil {
+		return nil, status.UnimplementedError("Not Implemented")
+	}
+	_, err := auth.AuthenticatedUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	reqUserId := req.GetUserId()
+	if len(reqUserId) == 0 {
+		reqUserId = req.GetRequestContext().GetUserId().GetId()
+	}
+
+	users, err := env.GetUserDB().GetDisplayUsers(ctx, []string{reqUserId})
+	if err != nil {
+		return nil, err
+	}
+	du := users[reqUserId]
 	res := &bpb.GetUserBadgesResponse{
+		DisplayUser: du,
 		Badges: []*bpb.Badge{
 			{
 				Description: "Earth Saver",
