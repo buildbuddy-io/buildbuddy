1c5cf6a5eb4502cdd449d7cc1d43884ad602a30c
Iain Macdonald <iain@buildbuddy.io>
2024-02-16 14:53:45 -0800

    Update enterprise/server/scheduling/scheduler_server/scheduler_server.go
    Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

--- enterprise/server/scheduling/scheduler_server/scheduler_server.go
+++ enterprise/server/scheduling/scheduler_server/scheduler_server.go
@@ -1820,8 +1820,8 @@ func (s *SchedulerServer) enqueueOnRemoteExecutor(ctx context.Context, node *exe
 		return false
 	}
 	rpcCtx, cancel := context.WithTimeout(ctx, schedulerEnqueueTaskReservationTimeout)
+	defer cancel()
 	_, err = schedulerClient.EnqueueTaskReservation(rpcCtx, request)
-	cancel()
 	if err != nil {
 		log.CtxWarningf(ctx, "EnqueueTaskReservation via scheduler target %q failed: %s", node.schedulerHostPort, err)
 		time.Sleep(schedulerEnqueueTaskReservationFailureSleep)
