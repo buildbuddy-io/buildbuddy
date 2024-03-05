cdc16ec8ee0e106f55e8a07f728d852fc8279795
Tyler Williams <williams.tyler@gmail.com>
2024-03-05 14:29:34 -0800

    BF

--- enterprise/server/raft/store/store.go
+++ enterprise/server/raft/store/store.go
@@ -184,7 +184,6 @@ func NewWithArgs(env environment.Env, rootDir string, nodeHost *dragonboat.NodeH
 	s.db = db
 	s.leaser = pebble.NewDBLeaser(db)
 
-
 	usages, err := usagetracker.New(s, gossipManager, s.NodeDescriptor(), partitions, s.AddEventListener())
 	if err != nil {
 		return nil, err
