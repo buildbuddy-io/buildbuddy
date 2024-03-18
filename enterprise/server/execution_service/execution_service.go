0486ad195335f3b44879a7a5a05c5c343849a96d
Brandon Duffany <brandon@buildbuddy.io>
2024-03-18 16:34:49 -0400

    Show error for workflows that fail before invocation is created (#6177)

--- enterprise/server/execution_service/execution_service.go
+++ enterprise/server/execution_service/execution_service.go
@@ -8,9 +8,11 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/tables"
 	"github.com/buildbuddy-io/buildbuddy/server/util/db"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/perms"
 	"github.com/buildbuddy-io/buildbuddy/server/util/query_builder"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"golang.org/x/sync/errgroup"
 
 	espb "github.com/buildbuddy-io/buildbuddy/proto/execution_stats"
 )
@@ -32,9 +34,16 @@ func checkPreconditions(req *espb.GetExecutionRequest) error {
 	return status.FailedPreconditionError("An execution lookup with invocation_id must be provided")
 }
 
-func (es *ExecutionService) queryExecutions(ctx context.Context, baseQuery *query_builder.Query) ([]*tables.Execution, error) {
+func (es *ExecutionService) getInvocationExecutions(ctx context.Context, invocationID string) ([]*tables.Execution, error) {
+	// Note: the invocation row may not be created yet because workflow
+	// invocations are created by the execution itself.
+	q := query_builder.NewQuery(`
+		SELECT e.* FROM "InvocationExecutions" ie
+		JOIN "Executions" e ON e.execution_id = ie.execution_id
+		LEFT JOIN "Invocations" i ON i.invocation_id = e.invocation_id
+	`)
+	q.AddWhereClause(`ie.invocation_id = ?`, invocationID)
 	dbh := es.env.GetDBHandle()
-	q := baseQuery
 
 	permClauses, err := perms.GetPermissionsCheckClauses(ctx, es.env, q, "e")
 	if err != nil {
@@ -45,23 +54,13 @@ func (es *ExecutionService) queryExecutions(ctx context.Context, baseQuery *quer
 	// bit. An alternative here would be to explicitly mark all child executions
 	// with OTHERS_READ, but that is somewhat complex. So we use this simple
 	// permissions inheriting approach instead.
-	permClauses.AddOr("i.perms & ? != 0", perms.OTHERS_READ)
+	permClauses.AddOr("i.perms IS NOT NULL AND i.perms & ? != 0", perms.OTHERS_READ)
 	permQuery, permArgs := permClauses.Build()
 	q.AddWhereClause("("+permQuery+")", permArgs...)
 
 	queryStr, args := q.Build()
 	rq := dbh.NewQuery(ctx, "execution_server_get_executions").Raw(queryStr, args...)
 	return db.ScanAll(rq, &tables.Execution{})
-}
-
-func (es *ExecutionService) getInvocationExecutions(ctx context.Context, invocationID string) ([]*tables.Execution, error) {
-	q := query_builder.NewQuery(`
-		SELECT e.* FROM "InvocationExecutions" ie
-		JOIN "Executions" e ON e.execution_id = ie.execution_id
-		JOIN "Invocations" i ON i.invocation_id = e.invocation_id
-	`)
-	q.AddWhereClause(`ie.invocation_id = ?`, invocationID)
-	return es.queryExecutions(ctx, q)
 }
 
 func (es *ExecutionService) GetExecution(ctx context.Context, req *espb.GetExecutionRequest) (*espb.GetExecutionResponse, error) {
@@ -86,6 +85,24 @@ func (es *ExecutionService) GetExecution(ctx context.Context, req *espb.GetExecu
 			return nil, err
 		}
 		rsp.Execution = append(rsp.Execution, protoExec)
+	}
+	if req.GetInlineExecuteResponse() {
+		// If inlined responses are requested, fetch them now.
+		var eg errgroup.Group
+		for _, ex := range rsp.Execution {
+			ex := ex
+			eg.Go(func() error {
+				res, err := execution.GetCachedExecuteResponse(ctx, es.env, ex.ExecutionId)
+				if err != nil {
+					return err
+				}
+				ex.ExecuteResponse = res
+				return nil
+			})
+		}
+		if err := eg.Wait(); err != nil {
+			log.CtxInfof(ctx, "Failed to fetch inline execution response(s): %s", err)
+		}
 	}
 	return rsp, nil
 }
