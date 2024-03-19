ce58fe8cb497abe5f058e1619e2dd2b19b065a04
Son Luong Ngoc <sluongng@gmail.com>
2024-03-19 15:06:11 +0100

    Update tools/deps/deps.go
    Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

--- tools/deps/deps.go
+++ tools/deps/deps.go
@@ -2,6 +2,7 @@
 
 // This tools/deps package keep track of Go tooling dependency that we use
 // but do not depend directly in code.
+// See https://github.com/bazelbuild/rules_go/blob/master/docs/go/core/bzlmod.md#depending-on-tools
 //
 // Importing them here helps nudge "go mod tidy" to include them inside
 // "go.mod" and "go.sum" files, which are used as source of truth for rules_go
