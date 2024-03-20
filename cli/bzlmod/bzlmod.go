cd9323ce2d45811adacf952632a14842abf752d9
Son Luong Ngoc <sluongng@gmail.com>
2024-03-20 15:04:45 +0100

    use sync.once

--- cli/bzlmod/bzlmod.go
+++ cli/bzlmod/bzlmod.go
@@ -4,45 +4,67 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/buildbuddy-io/buildbuddy/cli/bazelisk"
 )
 
+var (
+	once       sync.Once
+	useModules bool
+	infoErr    error
+)
+
 func UseModules() (bool, error) {
-	infos, err := bazelisk.BazelInfo([]string{"release", "starlark-semantics"})
-	if err != nil {
-		return false, fmt.Errorf("could not run bazel info: %w", err)
-	}
-	starlarkSemantics, ok := infos["starlark-semantics"]
-	if !ok {
-		return false, fmt.Errorf("could not find `starlark-semantics` in `bazel info` result: %v", infos)
-	}
+	once.Do(func() {
+		infos, err := bazelisk.BazelInfo([]string{"release", "starlark-semantics"})
+		if err != nil {
+			useModules = false
+			infoErr = fmt.Errorf("could not run bazel info: %w", err)
+			return
+		}
+		starlarkSemantics, ok := infos["starlark-semantics"]
+		if !ok {
+			useModules = false
+			infoErr = fmt.Errorf("could not find `starlark-semantics` in `bazel info` result: %v", infos)
+			return
+		}
 
-	// It's possible that bzlmod is enabled/disabled explicitly inside a bazelrc file.
-	// If that's the case, starlark-semantics should show the status if the value is not the default value
-	if strings.Contains(starlarkSemantics, "enable_bzlmod=true") {
-		return true, nil
-	}
-	if strings.Contains(starlarkSemantics, "enable_bzlmod=false") {
-		return false, nil
-	}
+		// It's possible that bzlmod is enabled/disabled explicitly inside a bazelrc file.
+		// If that's the case, starlark-semantics should show the status if the value is not the default value
+		if strings.Contains(starlarkSemantics, "enable_bzlmod=true") {
+			useModules = true
+			return
+		}
+		if strings.Contains(starlarkSemantics, "enable_bzlmod=false") {
+			useModules = false
+			return
+		}
 
-	release, ok := infos["release"]
-	if !ok {
-		return false, fmt.Errorf("could not find `release` in `bazel info` result: %v", infos)
-	}
-	version := strings.TrimLeft(release, "release ")
-	versionParts := strings.Split(version, ".")
-	majorVersion, err := strconv.Atoi(versionParts[0])
-	if err != nil {
-		return false, fmt.Errorf("could not parse Bazel release version: %v", err)
-	}
+		release, ok := infos["release"]
+		if !ok {
+			useModules = false
+			infoErr = fmt.Errorf("could not find `release` in `bazel info` result: %v", infos)
+			return
+		}
+		version := strings.TrimLeft(release, "release ")
+		versionParts := strings.Split(version, ".")
+		majorVersion, err := strconv.Atoi(versionParts[0])
+		if err != nil {
+			useModules = false
+			infoErr = fmt.Errorf("could not parse Bazel release version: %v", err)
+			return
+		}
 
-	// From Bazel 7 on-ward, bzlmod is enabled by default and will not be shown
-	// in starlark-semantics setting.
-	if majorVersion <= 6 {
-		// If current version is before Bazel 7, then bzlmod is disabled by default.
-		return false, nil
-	}
-	return true, nil
+		// From Bazel 7 on-ward, bzlmod is enabled by default and will not be shown
+		// in starlark-semantics setting.
+		if majorVersion <= 6 {
+			// If current version is before Bazel 7, then bzlmod is disabled by default.
+			useModules = false
+			return
+		}
+		useModules = true
+	})
+
+	return useModules, infoErr
 }
