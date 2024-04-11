2f2b1c671389aefbad077653a4c1225b4d566bd4
jim hollenbach <james.d.hollenbach@gmail.com>
2024-04-11 14:03:26 -0700

    split out data model for code review into immutable objects (#6342)

---
+++ enterprise/app/review/file_content.tsx
@@ -0,0 +1,280 @@
+import React from "react";
+import { github } from "../../../proto/github_ts_proto";
+import ReviewThreadComponent, { CommentEditHandler } from "./review_thread";
+import { CommentModel, ReviewModel, ThreadModel } from "./review_model";
+
+interface FileContentComponentProps {
+  reviewModel: ReviewModel;
+  disabled: boolean;
+  handler: CommentEditHandler;
+  viewerLogin: string;
+  owner: string;
+  repo: string;
+  pull: number;
+  path: string;
+  patch: string;
+  commitSha: string;
+}
+
+interface DiffLineInfo {
+  startLine: number;
+  lineCount: number;
+}
+
+interface SourceLine {
+  source?: string;
+  lineNumber?: number;
+}
+
+interface DiffLinePair {
+  left: SourceLine;
+  right: SourceLine;
+}
+
+interface Hunk {
+  header: string;
+  lines: DiffLinePair[];
+}
+
+export default class FileContentComponent extends React.Component<FileContentComponentProps> {
+  replyBodyTextRef: React.RefObject<HTMLTextAreaElement> = React.createRef();
+  replyApprovalCheckRef: React.RefObject<HTMLInputElement> = React.createRef();
+
+  renderThread(thread: ThreadModel) {
+    const comments = thread.getComments();
+    const draft = thread.getDraft();
+    if (comments.length < 1 && !draft) {
+      return <></>;
+    }
+    const firstComment = comments[0] ?? draft;
+    const leftSide = firstComment.getSide() === github.CommentSide.LEFT_SIDE;
+
+    return (
+      <div className="thread-block">
+        {!leftSide ? (
+          <>
+            <pre className="thread-line-number-space"> </pre>
+            <div className="thread-empty-side"> </div>
+          </>
+        ) : undefined}
+        <pre className="thread-line-number-space"> </pre>
+        <div className="thread-container">
+          <ReviewThreadComponent
+            threadId={thread.getId()}
+            reviewId={this.props.reviewModel.getDraftReviewId()}
+            viewerLogin={this.props.reviewModel.getViewerLogin()}
+            comments={comments}
+            draftComment={draft}
+            disabled={Boolean(this.props.disabled)}
+            updating={Boolean(draft && draft.isSubmittedToGithub())}
+            editing={Boolean(this.props.reviewModel.isCommentInProgress(draft?.getId()))}
+            saving={/* TODO(jdhollen */ false}
+            handler={this.props.handler}
+            activeUsername={this.props.viewerLogin}></ReviewThreadComponent>
+        </div>
+        {leftSide ? (
+          <>
+            <pre className="thread-line-number-space"> </pre>
+            <div className="thread-empty-side"> </div>
+          </>
+        ) : undefined}
+      </div>
+    );
+  }
+
+  renderComments(comments: CommentModel[]) {
+    const threads: Map<String, ThreadModel> = ThreadModel.threadsFromComments(
+      comments,
+      this.props.reviewModel.getDraftReviewId()
+    );
+    const outs: JSX.Element[] = [];
+
+    threads.forEach((t) => {
+      outs.push(this.renderThread(t));
+    });
+    return <>{outs}</>;
+  }
+
+  renderDiffHunk(hunk: Hunk, commitSha: string) {
+    const comments = this.props.reviewModel.getCommentsForFile(this.props.path, commitSha);
+
+    return (
+      <>
+        <pre className="diff-header">{hunk.header}</pre>
+        {hunk.lines.map((v) => {
+          let leftClasses =
+            "source-line left" +
+            (v.right.source === undefined ? " new" : "") +
+            (v.left.source === undefined ? " empty" : "");
+          let rightClasses =
+            "source-line right" +
+            (v.left.source === undefined ? " new" : "") +
+            (v.right.source === undefined ? " empty" : "");
+          if (v.left.source !== undefined && v.right.source !== undefined && v.left.source !== v.right.source) {
+            leftClasses += " modified";
+            rightClasses += " modified";
+          }
+          const leftLineNumber = v.left.lineNumber;
+          const rightLineNumber = v.right.lineNumber;
+          return (
+            <>
+              <div className="source-line-pair">
+                <pre className="source-line-number">{v.left.lineNumber ?? " "}</pre>
+                <pre
+                  className={leftClasses}
+                  onClick={
+                    leftLineNumber
+                      ? () =>
+                          this.props.handler.startComment(
+                            github.CommentSide.LEFT_SIDE,
+                            this.props.path,
+                            commitSha,
+                            leftLineNumber
+                          )
+                      : undefined
+                  }>
+                  {v.left.source ?? ""}
+                </pre>
+                <pre className="source-line-number">{v.right.lineNumber ?? " "}</pre>
+                <pre
+                  className={rightClasses}
+                  onClick={
+                    rightLineNumber
+                      ? () =>
+                          this.props.handler.startComment(
+                            github.CommentSide.RIGHT_SIDE,
+                            this.props.path,
+                            commitSha,
+                            rightLineNumber
+                          )
+                      : undefined
+                  }>
+                  {v.right.source ?? ""}
+                </pre>
+              </div>
+              <div className="threads">
+                {this.renderComments(
+                  comments.filter((c) => {
+                    return (
+                      c.getLine() ===
+                      (c.getSide() === github.CommentSide.LEFT_SIDE ? v.left.lineNumber : v.right.lineNumber)
+                    );
+                  })
+                )}
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  }
+
+  render(): JSX.Element[] {
+    if (this.props.patch.length === 0) {
+      return [<div>No diff info available (binary file?)</div>];
+    }
+    const out: JSX.Element[] = [];
+
+    const patchLines = this.props.patch.split("\n");
+    let currentIndex = 0;
+    while (currentIndex < patchLines.length) {
+      let hunk: Hunk;
+      if (patchLines[currentIndex].startsWith("@@")) {
+        [hunk, currentIndex] = readNextHunk(patchLines, currentIndex);
+        // TODO(jdhollen): Need to check commit sha.
+        out.push(this.renderDiffHunk(hunk, this.props.commitSha));
+      }
+    }
+
+    return out;
+  }
+}
+
+function getDiffLineInfo(input: string): DiffLineInfo {
+  const breakdown = input.slice(1).split(",");
+  return { startLine: Number(breakdown[0]) || 0, lineCount: Number(breakdown[1]) || 0 };
+}
+
+function readNextHunk(patchLines: string[], startIndex: number): [Hunk, number] {
+  // Parse the hunk start line.
+  const hunkSummary = patchLines[startIndex].split("@@");
+  if (hunkSummary.length < 3) {
+    return [{ header: "", lines: [] }, startIndex];
+  }
+  const diffLineInfo = hunkSummary[1].trim().split(" ");
+  let leftInfo: DiffLineInfo | undefined, rightInfo: DiffLineInfo | undefined;
+  for (let i = 0; i < diffLineInfo.length; i++) {
+    const value = diffLineInfo[i];
+    if (value.length < 4) {
+      continue;
+    }
+    if (value[0] === "+") {
+      rightInfo = getDiffLineInfo(value);
+    } else if (value[0] === "-") {
+      leftInfo = getDiffLineInfo(value);
+    }
+  }
+
+  let leftLinesRead = 0;
+  let rightLinesRead = 0;
+  let currentLineOffset = 0;
+  let currentIndex = startIndex + 1;
+
+  let leftLines: SourceLine[] = [];
+  let rightLines: SourceLine[] = [];
+  while (
+    (leftLinesRead < (leftInfo?.lineCount || 1) || rightLinesRead < (rightInfo?.lineCount || 1)) &&
+    currentIndex < patchLines.length
+  ) {
+    let line = patchLines[currentIndex];
+    if (line[0] === "+") {
+      rightLines.push({ source: line.slice(1), lineNumber: (rightInfo?.startLine ?? 0) + rightLinesRead });
+      rightLinesRead += 1;
+      currentLineOffset += 1;
+    } else if (line[0] === "-") {
+      leftLines.push({ source: line.slice(1), lineNumber: (leftInfo?.startLine ?? 0) + leftLinesRead });
+      leftLinesRead += 1;
+      currentLineOffset -= 1;
+    } else {
+      rightLines.push({ source: line.slice(1), lineNumber: (rightInfo?.startLine ?? 0) + rightLinesRead });
+      leftLines.push({ source: line.slice(1), lineNumber: (leftInfo?.startLine ?? 0) + leftLinesRead });
+      leftLinesRead += 1;
+      rightLinesRead += 1;
+      const arrayToGrow = currentLineOffset < 0 ? rightLines : leftLines;
+      for (let i = 0; i < Math.abs(currentLineOffset); i++) {
+        arrayToGrow.push({});
+      }
+      currentLineOffset = 0;
+    }
+    currentIndex++;
+  }
+  const finalOffset = rightLines.length - leftLines.length;
+  if (finalOffset !== 0) {
+    const arrayToGrow = finalOffset < 0 ? rightLines : leftLines;
+    for (let i = 0; i < Math.abs(finalOffset); i++) {
+      arrayToGrow.push({});
+    }
+  }
+
+  let output: DiffLinePair[] = [];
+  for (let i = rightLines.length - 1; i >= 0; i--) {
+    if (leftLines[i].source === undefined) {
+      let j = i - 1;
+      while (j >= 0) {
+        if (leftLines[j].source !== undefined) {
+          if (leftLines[j].source === rightLines[i].source) {
+            leftLines[i] = leftLines[j];
+            leftLines[j] = {};
+          }
+          break;
+        }
+        j--;
+      }
+    }
+  }
+  for (let i = 0; i < rightLines.length; i++) {
+    output.push({ left: leftLines[i], right: rightLines[i] });
+  }
+
+  return [{ header: patchLines[startIndex], lines: output }, currentIndex];
+}
---
+++ enterprise/app/review/review_model.ts
@@ -0,0 +1,457 @@
+import Long from "long";
+import { github } from "../../../proto/github_ts_proto";
+
+const FAKE_ID_PREFIX = "bb-tmp/";
+let fakeReviewIdCounter = 0;
+function newFakeId(): string {
+  fakeReviewIdCounter++;
+  return FAKE_ID_PREFIX + fakeReviewIdCounter;
+}
+
+export class CommentModel {
+  private comment: github.Comment;
+
+  private constructor(comment: github.Comment) {
+    this.comment = comment;
+  }
+
+  getId(): string {
+    return this.comment.id;
+  }
+
+  getReviewId(): string {
+    return this.comment.reviewId;
+  }
+
+  getPath(): string {
+    return this.comment.path;
+  }
+
+  getCommitSha(): string {
+    return this.comment.commitSha;
+  }
+
+  getThreadId(): string {
+    return this.comment.threadId;
+  }
+
+  getBody(): string {
+    return this.comment.body;
+  }
+
+  getCreatedAtUsec(): number {
+    return +this.comment.createdAtUsec;
+  }
+
+  getCommenter(): string {
+    return this.comment.commenter?.login || "unknown commenter";
+  }
+
+  isBot(): boolean {
+    return Boolean(this.comment.commenter?.bot);
+  }
+
+  getLine(): number {
+    return +(this.comment.position?.startLine || this.comment.position?.endLine || 0);
+  }
+
+  getSide(): github.CommentSide {
+    return this.comment.position?.side ?? github.CommentSide.RIGHT_SIDE;
+  }
+
+  isThreadSavedToGithub(): boolean {
+    return Boolean(this.comment.threadId && !this.comment.threadId.startsWith(FAKE_ID_PREFIX));
+  }
+
+  isSubmittedToGithub(): boolean {
+    return this.comment.id !== "" && !this.comment.id.startsWith(FAKE_ID_PREFIX);
+  }
+
+  // Careful! This is only a shallow copy, so if you are mucking with nested fields you need
+  // to actually set them (or else you'll mess with other people holding a ref to this proto)
+  // TODO(jdhollen): Maybe, someday, make this actually a deep copy.
+  private toProtoComment(): github.Comment {
+    return new github.Comment(this.comment);
+  }
+
+  public updateBody(newBody: string): CommentModel {
+    const proto = this.toProtoComment();
+    proto.body = newBody;
+    return new CommentModel(proto);
+  }
+
+  createReply(reviewId: string, author: string): CommentModel {
+    const newComment = this.toProtoComment();
+    newComment.id = newFakeId();
+    newComment.reviewId = reviewId;
+    newComment.body = "";
+    newComment.commenter = new github.ReviewUser({ login: author || "you" });
+    newComment.createdAtUsec = Long.fromNumber(Date.now() * 1000);
+    newComment.isResolved = false;
+
+    return CommentModel.fromComment(newComment);
+  }
+
+  static fromComment(c: github.Comment) {
+    return new CommentModel(new github.Comment(c));
+  }
+
+  static newComment(
+    reviewId: string,
+    path: string,
+    commitSha: string,
+    lineNumber: number,
+    side: github.CommentSide
+  ): CommentModel {
+    return CommentModel.fromComment(
+      new github.Comment({
+        id: newFakeId(),
+        threadId: newFakeId(),
+        reviewId,
+        parentCommentId: "",
+        body: "",
+        // TODO(jdhollen): pass user back from github.
+        commenter: new github.ReviewUser({ login: "you" }),
+        path,
+        commitSha,
+        position: new github.CommentPosition({
+          startLine: Long.fromNumber(lineNumber),
+          endLine: Long.fromNumber(lineNumber),
+          side,
+        }),
+        createdAtUsec: Long.fromNumber(Date.now() * 1000),
+        isResolved: false,
+      })
+    );
+  }
+}
+
+export class FileModel {
+  private file: github.FileSummary;
+
+  private constructor(f: github.FileSummary) {
+    this.file = f;
+  }
+
+  getFullPath(): string {
+    return this.file.name;
+  }
+
+  getPatch(): string {
+    return this.file.patch;
+  }
+
+  getAdditions(): number {
+    return +this.file.additions;
+  }
+
+  getDeletions(): number {
+    return +this.file.deletions;
+  }
+
+  getCommentCount(): number {
+    return +this.file.comments;
+  }
+
+  getCommitSha(): string {
+    return this.file.commitSha;
+  }
+
+  static fromFileSummary(f: github.FileSummary) {
+    return new FileModel(new github.FileSummary(f));
+  }
+}
+
+interface ThreadAndDraft {
+  threadId: string;
+  comments: CommentModel[];
+  draft?: CommentModel;
+}
+
+export class ThreadModel {
+  private threadId: string;
+  private comments: readonly CommentModel[];
+  private draft?: CommentModel;
+
+  private constructor({ threadId, comments, draft }: ThreadAndDraft) {
+    this.threadId = threadId;
+    this.comments = [...comments];
+    this.draft = draft;
+  }
+
+  getComments(): readonly CommentModel[] {
+    return this.comments;
+  }
+
+  hasDraft(): boolean {
+    return this.draft !== undefined;
+  }
+
+  getDraft(): CommentModel | undefined {
+    return this.draft;
+  }
+
+  getId(): string {
+    return this.threadId;
+  }
+
+  static threadsFromComments(comments: readonly CommentModel[], draftReviewId?: string): Map<string, ThreadModel> {
+    const threads: Map<string, ThreadAndDraft> = new Map();
+    comments.forEach((c) => {
+      const thread = c.getThreadId();
+      let threadAndDraft = threads.get(thread);
+      if (!threadAndDraft) {
+        threadAndDraft = {
+          threadId: thread,
+          comments: [],
+        };
+        threads.set(thread, threadAndDraft);
+      }
+      if (draftReviewId && c.getReviewId() === draftReviewId) {
+        threadAndDraft.draft = c;
+      } else {
+        threadAndDraft.comments.push(c);
+      }
+    });
+
+    const result = new Map<string, ThreadModel>();
+    threads.forEach((v, k) => result.set(k, new ThreadModel(v)));
+
+    return result;
+  }
+}
+
+// This little nightmare makes it easier to modify the otherwise-immutable
+// state of a model: we can just make a copy and replace the necessary fields
+// instead of exposing a modifiable structure to callers.
+interface State {
+  draftReviewId: string;
+  title: string;
+  body: string;
+  owner: string;
+  repo: string;
+  pullNumber: number;
+  pullId: string;
+  branch: string;
+  githubUrl: string;
+  viewerLogin: string;
+  author: string;
+  submitted: boolean;
+  mergeable: boolean;
+  createdAtUsec: number;
+  updatedAtUsec: number;
+  files: readonly FileModel[];
+  comments: readonly CommentModel[];
+  actionStatuses: readonly github.ActionStatus[];
+  reviewers: readonly github.Reviewer[];
+  inProgressComments: Set<string>;
+}
+
+export class ReviewModel {
+  private state: State;
+
+  private constructor(state: State) {
+    this.state = state;
+  }
+
+  getDraftReviewId(): string {
+    return this.state.draftReviewId;
+  }
+
+  isCommentInProgress(id?: string): boolean {
+    return Boolean(id && this.state.inProgressComments.has(id));
+  }
+
+  isReviewSavedToGithub(): boolean {
+    return this.state.draftReviewId !== "" && !this.state.draftReviewId.startsWith(FAKE_ID_PREFIX);
+  }
+  getOwner(): string {
+    return this.state.owner;
+  }
+
+  getRepo(): string {
+    return this.state.repo;
+  }
+
+  getPullNumber(): number {
+    return this.state.pullNumber;
+  }
+
+  getPullId(): string {
+    return this.state.pullId;
+  }
+
+  getGithubUrl(): string {
+    return this.state.githubUrl;
+  }
+
+  getCreatedAtUsec(): number {
+    return this.state.createdAtUsec;
+  }
+
+  getUpdatedAtUsec(): number {
+    return this.state.updatedAtUsec;
+  }
+
+  getTitle(): string {
+    return this.state.title;
+  }
+
+  getBody(): string {
+    return this.state.body;
+  }
+
+  getBranch(): string {
+    return this.state.branch;
+  }
+
+  isSubmitted(): boolean {
+    return this.state.submitted;
+  }
+
+  isMergeable(): boolean {
+    return this.state.mergeable;
+  }
+
+  getDraftReviewComments(): readonly CommentModel[] {
+    return !this.state.draftReviewId
+      ? []
+      : this.state.comments.filter((v) => v.getReviewId() === this.state.draftReviewId);
+  }
+
+  hasAnyDraftComments(): boolean {
+    return (
+      this.state.draftReviewId !== "" &&
+      this.state.comments.filter((v) => v.getReviewId() === this.state.draftReviewId).length > 0
+    );
+  }
+
+  getComment(commentId: string): CommentModel | undefined {
+    return this.state.comments.find((c) => c.getId() === commentId);
+  }
+
+  getCommentsForFile(path: string, commitSha: string): readonly CommentModel[] {
+    // TODO(jdhollen): filter on commitSha.
+    return this.state.comments.filter((v) => v.getPath() === path);
+  }
+
+  getCommentsForThread(threadId: string): readonly CommentModel[] {
+    return this.state.comments.filter((v) => v.getThreadId() === threadId);
+  }
+
+  getViewerLogin(): string {
+    return this.state.viewerLogin;
+  }
+
+  getAuthor(): string {
+    return this.state.author;
+  }
+
+  getFiles(): readonly FileModel[] {
+    return this.state.files;
+  }
+
+  // TODO(jdhollen): This needs to be fully locked down / unmodifiable.
+  getReviewers(): readonly github.Reviewer[] {
+    return this.state.reviewers;
+  }
+
+  // TODO(jdhollen): This needs to be fully locked down / unmodifiable.
+  getActionStatuses(): readonly github.ActionStatus[] {
+    return this.state.actionStatuses;
+  }
+
+  private stateCopy(): State {
+    // TODO(jdhollen): Do a better deep copy once all fields are unmodifiable.
+    const copy = { ...this.state };
+    copy.inProgressComments = new Set(copy.inProgressComments);
+    return copy;
+  }
+
+  addComment(newComment: CommentModel): ReviewModel {
+    const newState = this.stateCopy();
+    const newComments = [...newState.comments];
+    newComments.push(newComment);
+    newState.comments = newComments;
+    return new ReviewModel(newState);
+  }
+
+  updateComment(commentId: string, newComment: CommentModel): ReviewModel {
+    const newState = this.stateCopy();
+    const newComments = [...newState.comments];
+    const oldCommentIndex = newComments.findIndex((c) => c.getId() === commentId) ?? -1;
+    if (oldCommentIndex === -1) {
+      newComments.push(newComment);
+    } else {
+      newComments.splice(oldCommentIndex, 1, newComment);
+    }
+    newState.comments = newComments;
+
+    return new ReviewModel(newState);
+  }
+
+  deleteComment(commentId: string): ReviewModel {
+    const newState = this.stateCopy();
+    const newComments = [...newState.comments];
+    const oldCommentIndex = newComments.findIndex((c) => c.getId() === commentId) ?? -1;
+    if (oldCommentIndex !== -1) {
+      newComments.splice(oldCommentIndex, 1);
+    }
+    newState.comments = newComments;
+    return new ReviewModel(newState);
+  }
+
+  setCommentToPending(commentId: string): ReviewModel {
+    if (this.isCommentInProgress(commentId)) {
+      return this;
+    }
+    const newState = this.stateCopy();
+    newState.inProgressComments.add(commentId);
+    return new ReviewModel(newState);
+  }
+
+  removeCommentFromPending(commentId: string) {
+    if (!this.isCommentInProgress(commentId)) {
+      return this;
+    }
+    const newState = this.stateCopy();
+    newState.inProgressComments.delete(commentId);
+    return new ReviewModel(newState);
+  }
+
+  setDraftReviewId(draftReviewId: string) {
+    if (this.state.draftReviewId === draftReviewId) {
+      return this;
+    }
+    const newState = this.stateCopy();
+    newState.draftReviewId = draftReviewId;
+    return new ReviewModel(newState);
+  }
+
+  static fromResponse(response: github.GetGithubPullRequestDetailsResponse): ReviewModel {
+    const reviewers = [...response.reviewers];
+    reviewers.sort((a, b) => (a.login.toLowerCase() < b.login.toLowerCase() ? -1 : 1));
+    return new ReviewModel({
+      draftReviewId: response.draftReviewId ? response.draftReviewId : newFakeId(),
+      title: response.title,
+      body: response.body,
+      owner: response.owner,
+      repo: response.repo,
+      pullNumber: +response.pull,
+      pullId: response.pullId,
+      branch: response.branch,
+      submitted: response.submitted,
+      mergeable: response.mergeable,
+      createdAtUsec: +response.createdAtUsec,
+      updatedAtUsec: +response.updatedAtUsec,
+      reviewers,
+      author: response.author,
+      comments: response.comments.map(CommentModel.fromComment),
+      files: response.files.map(FileModel.fromFileSummary),
+      githubUrl: response.githubUrl,
+      viewerLogin: response.viewerLogin,
+      actionStatuses: [...response.actionStatuses],
+      inProgressComments: new Set<string>(),
+    });
+  }
+}
