15fbbd67a3e0059dc022e3f580fe604ef05e86ec
Brandon Duffany <brandon@buildbuddy.io>
2024-04-11 10:09:34 -0400

    Small tweaks to firecracker clean script

--- tools/firecracker_clean_networking.sh
+++ tools/firecracker_clean_networking.sh
@@ -6,22 +6,13 @@ if [[ "$(id -u)" != 0 ]]; then
   exit 1
 fi
 
+echo >&2 "Cleaning up any running firecracker processes"
+killall -KILL firecracker || true
+
 echo >&2 "Cleaning up network namespaces"
 ip netns list | awk '{ print $1 }' | while read -r netns; do
   [[ "$netns" == bb-executor-* ]] && (
     set -x
     ip netns delete "$netns"
   )
-done
-
-IPTABLES=$(iptables-save)
-BACKUP_FILE=$(mktemp --suffix .iptables.bak)
-cat <<<"$IPTABLES" >"$BACKUP_FILE"
-echo >&2 "Created iptables backup at $BACKUP_FILE"
-echo >&2 "Restore backup with: sudo iptables-restore < $BACKUP_FILE"
-
-echo >&2 "Cleaning up iptables veth* rules"
-grep -vP '\bveth' <<<"$IPTABLES" | uniq | iptables-restore
-
-echo >&2 "Cleaning up any remaining firecracker processes"
-killall firecracker
+done || true
