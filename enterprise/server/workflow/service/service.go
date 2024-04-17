f9895585c217031da6df1bc8c3bf279e75ce0330
Siggi Simonarson <siggi@buildbuddy.io>
2024-04-17 13:26:02 -0700

    Add `BUILDBUDDY_INVOCATION_ID` environment variable to workflows (#6399)

--- enterprise/server/workflow/service/service.go
+++ enterprise/server/workflow/service/service.go
@@ -1078,6 +1078,7 @@ func (ws *workflowService) createActionForWorkflow(ctx context.Context, wf *tabl
 		{Name: "GIT_BASE_BRANCH", Value: wd.TargetBranch},
 		{Name: "GIT_REPO_DEFAULT_BRANCH", Value: wd.TargetRepoDefaultBranch},
 		{Name: "GIT_PR_NUMBER", Value: fmt.Sprintf("%d", wd.PullRequestNumber)},
+		{Name: "BUILDBUDDY_INVOCATION_ID", Value: invocationID},
 	}
 	for k, v := range env {
 		envVars = append(envVars, &repb.Command_EnvironmentVariable{
