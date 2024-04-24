0cf8b6822c178b1eaf3765b1058d8974f71d3e49
Iain Macdonald <xiainx@gmail.com>
2024-04-24 16:25:25 -0700

    Simpler error message

--- server/util/grpc_server/grpc_server.go
+++ server/util/grpc_server/grpc_server.go
@@ -84,7 +84,7 @@ func (b *Builder) GetServer() *grpc.Server {
 func NewBuilder(env environment.Env, port int, ssl bool) (*Builder, error) {
 	b := &Builder{env: env, port: port, ssl: ssl}
 	if ssl && !env.GetSSLService().IsEnabled() {
-		return nil, status.InvalidArgumentError("GRPCS Server requested without SSL Service enabled")
+		return nil, status.InvalidArgumentError("GRPCS requires SSL Service")
 	}
 	b.hostPort = fmt.Sprintf("%s:%d", b.env.GetListenAddr(), b.port)
 
