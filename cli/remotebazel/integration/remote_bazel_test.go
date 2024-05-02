6ecd2bf182114e8d2235d0e7b66a4f2655e3a724
Maggie Lou <maggie@buildbuddy.io>
2024-05-02 16:35:16 -0500

    Run server and executor locally - uploading to BES does not work, but remote run completes

--- cli/remotebazel/integration/remote_bazel_test.go
+++ cli/remotebazel/integration/remote_bazel_test.go
@@ -2,6 +2,9 @@ package integration_test
 
 import (
 	"fmt"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/buildbuddy_enterprise"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/testexecutor"
+	akpb "github.com/buildbuddy-io/buildbuddy/proto/api_key"
 	"os"
 	"testing"
 
@@ -11,8 +14,6 @@ import (
 )
 
 func TestWithPublicRepo(t *testing.T) {
-	apiKey := os.Getenv("BB_PROD_PROBER_ORG_API_KEY")
-	fmt.Printf("Api key is %s", apiKey)
 	// Root dir is persisted on recycled runners
 	rootDir := "/root/workspace/remote-bazel-integration-test"
 	err := os.Setenv("HOME", rootDir)
@@ -31,8 +32,41 @@ func TestWithPublicRepo(t *testing.T) {
 	err = os.Chdir(fmt.Sprintf("%s/bazel-gazelle", rootDir))
 	require.NoError(t, err)
 
-	// TODO: Run a server and executor locally to run this against
-	exitCode, err := remotebazel.HandleRemoteBazel([]string{"help", fmt.Sprintf("--remote_header=x-buildbuddy-api-key=%s", apiKey)})
+	// Spinning up firecracker need iptables which is in /usr/sbin.
+	err = os.Setenv("PATH", os.Getenv("PATH")+":/usr/sbin")
+	require.NoError(t, err)
+
+	// Run a server and executor locally to run this against
+	app := buildbuddy_enterprise.Run(t)
+	_ = testexecutor.Run(
+		t,
+		testexecutor.ExecutorRunfilePath,
+		[]string{
+			"--executor.app_target=" + app.GRPCAddress(),
+			"--executor.default_isolation_type=firecracker",
+			"--executor.enable_firecracker=true",
+			"--debug_stream_command_outputs",
+		},
+	)
+	webClient := buildbuddy_enterprise.LoginAsDefaultSelfAuthUser(t, app)
+
+	rsp := &akpb.CreateApiKeyResponse{}
+	err = webClient.RPC("CreateApiKey", &akpb.CreateApiKeyRequest{
+		RequestContext: webClient.RequestContext,
+		GroupId:        webClient.RequestContext.GroupId,
+		Capability: []akpb.ApiKey_Capability{
+			akpb.ApiKey_CAS_WRITE_CAPABILITY,
+			akpb.ApiKey_CACHE_WRITE_CAPABILITY,
+			akpb.ApiKey_ORG_ADMIN_CAPABILITY,
+		},
+	}, rsp)
+	require.NoError(t, err)
+	apiKey := rsp.ApiKey.Value
+
+	exitCode, err := remotebazel.HandleRemoteBazel([]string{
+		fmt.Sprintf("--remote_runner=%s", app.GRPCAddress()),
+		"help",
+		fmt.Sprintf("--remote_header=x-buildbuddy-api-key=%s", apiKey)})
 	require.NoError(t, err)
 	require.Equal(t, 0, exitCode)
 }
