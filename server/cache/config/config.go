17373b458d89c5bb09410e6a898e18dd74f312a0
Zoey Greer <zoey@buildbuddy.io>
2024-05-14 10:47:51 -0400

    Add flags to make metrics collector expiries configurable (#6563)

--- server/cache/config/config.go
+++ server/cache/config/config.go
@@ -1,9 +1,19 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"time"
+)
 
-var cacheMaxSizeBytes = flag.Int64("cache.max_size_bytes", 10_000_000_000 /* 10 GB */, "How big to allow the cache to be (in bytes).")
+var (
+	cacheMaxSizeBytes = flag.Int64("cache.max_size_bytes", 10_000_000_000 /* 10 GB */, "How big to allow the cache to be (in bytes).")
+	countTTL          = flag.Duration("cache.count_ttl", 24*time.Hour, "How long to go without receiving any cache requests for an invocation before deleting the invocation's counts from the metrics collector.")
+)
 
 func MaxSizeBytes() int64 {
 	return *cacheMaxSizeBytes
+}
+
+func CountTTL() time.Duration {
+	return *countTTL
 }
