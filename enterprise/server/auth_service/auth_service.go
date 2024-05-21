6b1ef6aeb220cd56805c965eb27348b2c5af25e3
Iain Macdonald <xiainx@gmail.com>
2024-05-21 12:18:01 -0700

    Skeletonize auth service

--- enterprise/server/auth_service/auth_service.go
+++ enterprise/server/auth_service/auth_service.go
@@ -3,22 +3,18 @@ package auth_service
 import (
 	"context"
 
-	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
 
 	authpb "github.com/buildbuddy-io/buildbuddy/proto/auth"
 )
 
 type AuthService struct {
-	authenticator interfaces.Authenticator
 }
 
 func Register(env *real_environment.RealEnv) {
-	env.SetAuthService(AuthService{authenticator: env.GetAuthenticator()})
+	env.SetAuthService(AuthService{})
 }
 
 func (a AuthService) Authenticate(ctx context.Context, req *authpb.AuthenticateRequest) (*authpb.AuthenticateResponse, error) {
-	// TODO(iain): is this OK / check TTL.
-	jwt := a.authenticator.TrustedJWTFromAuthContext(ctx)
-	return &authpb.AuthenticateResponse{Jwt: &jwt}, nil
+	return &authpb.AuthenticateResponse{}, nil
 }
