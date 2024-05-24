1238c283b6deeb00c70b1a42a62aa09c661c82d5
Jim Hollenbach <jim@buildbuddy.io>
2024-05-24 12:55:01 -0700

    Buildfix

--- server/util/region/region.go
+++ server/util/region/region.go
@@ -24,8 +24,8 @@ func Protos() []*cfgpb.Region {
 	protos := []*cfgpb.Region{}
 	for _, r := range *regions {
 		protos = append(protos, &cfgpb.Region{
-			Name:   r.Name,
-			Server: r.Server,
+			Name:       r.Name,
+			Server:     r.Server,
 			Subdomains: r.Subdomains,
 		})
 	}
