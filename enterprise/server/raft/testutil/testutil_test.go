d9ad1877e2d11d7556d967fd07fbe7afeb682a47
Lulu Zhang <lulu@buildbuddy.io>
2024-06-05 14:26:48 -0700

    write a test in testutil

--- enterprise/server/raft/testutil/testutil_test.go
+++ enterprise/server/raft/testutil/testutil_test.go
@@ -10,7 +10,9 @@ import (
 func TestStartShard(t *testing.T) {
 	sf := testutil.NewStoreFactory(t)
 	s1 := sf.NewStore(t)
+	s2 := sf.NewStore(t)
+	s3 := sf.NewStore(t)
 	ctx := context.Background()
 
-	testutil.StartShard(t, ctx, s1)
+	testutil.StartShard(t, ctx, s1, s2, s3)
 }
