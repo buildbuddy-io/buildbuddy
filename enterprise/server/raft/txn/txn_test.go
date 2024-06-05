4ae75c5b814d218410f9914b2db237ea1d1b777b
Lulu Zhang <lulu@buildbuddy.io>
2024-06-05 12:09:40 -0700

    reduce stores

--- enterprise/server/raft/txn/txn_test.go
+++ enterprise/server/raft/txn/txn_test.go
@@ -283,10 +283,11 @@ func TestFetchTxnRecordsSkipRecent(t *testing.T) {
 func TestRunTxn(t *testing.T) {
 	sf := testutil.NewStoreFactory(t)
 	s1 := sf.NewStore(t)
-	s2 := sf.NewStore(t)
-	s3 := sf.NewStore(t)
+	//s2 := sf.NewStore(t)
+	//s3 := sf.NewStore(t)
 	ctx := context.Background()
-	testutil.StartShard(t, ctx, s1, s2, s3)
+	stores := []*testutil.TestingStore{s1}
+	testutil.StartShard(t, ctx, stores...)
 
 	batch := rbuilder.NewBatchBuilder().Add(&rfpb.DirectWriteRequest{
 		Kv: &rfpb.KV{
@@ -308,7 +309,7 @@ func TestRunTxn(t *testing.T) {
 	})
 	txnBuilder := rbuilder.NewTxn().AddStatement(&rfpb.ReplicaDescriptor{
 		ShardId:   2,
-		ReplicaId: 4,
+		ReplicaId: 2,
 	}, batch).AddStatement(&rfpb.ReplicaDescriptor{
 		ShardId:   1,
 		ReplicaId: 1,
