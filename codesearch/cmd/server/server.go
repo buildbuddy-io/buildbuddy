149d2642d903558c795177889a0ca14112df876d
Tyler Williams <williams.tyler@gmail.com>
2024-06-07 15:13:02 -0700

    Make the codesearch server real

---
+++ codesearch/cmd/server/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"net"
+
+	"github.com/buildbuddy-io/buildbuddy/codesearch/server"
+	"github.com/buildbuddy-io/buildbuddy/server/config"
+	"github.com/buildbuddy-io/buildbuddy/server/nullauth"
+	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/flag"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_server"
+	"github.com/buildbuddy-io/buildbuddy/server/util/healthcheck"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	_ "github.com/buildbuddy-io/buildbuddy/server/util/monitoring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	csspb "github.com/buildbuddy-io/buildbuddy/proto/codesearch_service"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+)
+
+var (
+	serverType = flag.String("server_type", "codesearch-server", "The server type to match on health checks")
+
+	listen     = flag.String("codesearch.listen", ":2633", "Address to listen on")
+	indexDir   = flag.String("codesearch.index_dir", "", "Directory to store index in")
+	scratchDir = flag.String("codesearch.scratch_dir", "", "Directory to store temp files in")
+)
+
+func main() {
+	flag.Parse()
+
+	if err := config.Load(); err == nil {
+		config.ReloadOnSIGHUP()
+	}
+
+	lis, err := net.Listen("tcp", *listen)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	healthChecker := healthcheck.NewHealthChecker(*serverType)
+	env := real_environment.NewRealEnv(healthChecker)
+	env.SetAuthenticator(nullauth.NewNullAuthenticator(true /*anonymousEnabled*/, ""))
+
+	css, err := server.New(*indexDir, *scratchDir)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	options := grpc_server.CommonGRPCServerOptions(env)
+	server := grpc.NewServer(options...)
+	reflection.Register(server)
+
+	grpc_prometheus.Register(server)
+	grpc_prometheus.EnableHandlingTimeHistogram()
+
+	csspb.RegisterCodesearchServiceServer(server, css)
+
+	env.GetHealthChecker().RegisterShutdownFunction(grpc_server.GRPCShutdownFunc(server))
+	go func() {
+		_ = server.Serve(lis)
+	}()
+	log.Printf("Codesearch server listening at %v", lis.Addr())
+	env.GetHealthChecker().WaitForGracefulShutdown()
+}
---
+++ codesearch/server/server.go
@@ -0,0 +1,200 @@
+package server
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/buildbuddy-io/buildbuddy/codesearch/index"
+	"github.com/buildbuddy-io/buildbuddy/codesearch/query"
+	"github.com/buildbuddy-io/buildbuddy/codesearch/searcher"
+	"github.com/buildbuddy-io/buildbuddy/codesearch/types"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/cockroachdb/pebble"
+	"github.com/gabriel-vasile/mimetype"
+	"github.com/go-enry/go-enry/v2"
+
+	inpb "github.com/buildbuddy-io/buildbuddy/proto/index"
+	srpb "github.com/buildbuddy-io/buildbuddy/proto/search"
+	xxhash "github.com/cespare/xxhash/v2"
+)
+
+// TODO(tylerw): this should come from a flag?
+var skipMime = regexp.MustCompile(`^audio/.*|video/.*|image/.*$`)
+
+const (
+	maxFileLen = 1 << 30
+
+	// The following field names are used in the indexed docs.
+	filenameField = "filename"
+	contentField  = "content"
+	languageField = "language"
+	repoField     = "repo"
+)
+
+func New(rootDirectory, scratchDirectory string) (*codesearchServer, error) {
+	db, err := pebble.Open(rootDirectory, &pebble.Options{})
+	if err != nil {
+		return nil, err
+	}
+	return &codesearchServer{
+		db:               db,
+		scratchDirectory: scratchDirectory,
+	}, nil
+}
+
+type codesearchServer struct {
+	db               *pebble.DB
+	scratchDirectory string
+}
+
+func makeDoc(name, repo string, buf []byte) (types.Document, error) {
+	// Skip long files.
+	if len(buf) > maxFileLen {
+		return nil, fmt.Errorf("%s: too long, ignoring\n", name)
+	}
+
+	// Compute a hash of the file.
+	docID := xxhash.Sum64(buf)
+
+	// Check the mimetype and skip if bad.
+	mtype, err := mimetype.DetectReader(bytes.NewReader(buf))
+	if err == nil && skipMime.MatchString(mtype.String()) {
+		return nil, fmt.Errorf("%q: skipping (invalid mime type: %q)", name, mtype.String())
+	}
+
+	// Compute filetype
+	lang := strings.ToLower(enry.GetLanguage(filepath.Base(name), buf))
+	doc := types.NewMapDocument(
+		docID,
+		map[string]types.NamedField{
+			filenameField: types.NewNamedField(types.TrigramField, filenameField, []byte(name), true /*=stored*/),
+			contentField:  types.NewNamedField(types.TrigramField, contentField, buf, true /*=stored*/),
+			languageField: types.NewNamedField(types.StringTokenField, languageField, []byte(lang), true /*=stored*/),
+			repoField:     types.NewNamedField(types.StringTokenField, repoField, []byte(repo), true /*=stored*/),
+		},
+	)
+	return doc, nil
+}
+
+func (css *codesearchServer) Index(ctx context.Context, req *inpb.IndexRequest) (*inpb.IndexResponse, error) {
+	gitRepo := req.GetGitRepo()
+	if gitRepo == nil || gitRepo.GetOwnerRepo() == "" {
+		return nil, fmt.Errorf("a git_repo must be specified")
+	}
+
+	// https://github.com/buildbuddy-io/buildbuddy/archive/1d8a3184c996c3d167a281b70a4eeccd5188e5e1.tar.gz
+	archiveURL := fmt.Sprintf("https://github.com/%s/archive/%s.zip", gitRepo.GetOwnerRepo(), gitRepo.GetCommitSha())
+	log.Debugf("archive URL is %q", archiveURL)
+
+	start := time.Now()
+	log.Printf("Started indexing %s @ %s", gitRepo.GetOwnerRepo(), gitRepo.GetCommitSha())
+
+	httpRsp, err := http.Get(archiveURL)
+	if err != nil {
+		return nil, err
+	}
+	defer httpRsp.Body.Close()
+
+	tmpFile, err := os.CreateTemp(css.scratchDirectory, "archive-*.zip")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := io.Copy(tmpFile, httpRsp.Body); err != nil {
+		return nil, err
+	}
+	log.Debugf("Copied archive to %q", tmpFile.Name())
+
+	zipReader, err := zip.OpenReader(tmpFile.Name())
+	if err != nil {
+		return nil, err
+	}
+	defer zipReader.Close()
+
+	// TODO(tylerw): pull namespace from index request.
+	namespace := ""
+	iw, err := index.NewWriter(css.db, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range zipReader.File {
+		parts := strings.Split(file.Name, string(filepath.Separator))
+		if len(parts) == 1 {
+			continue
+		}
+		filename := filepath.Join(parts[1:]...)
+
+		rc, err := file.Open()
+		if err != nil {
+			return nil, err
+		}
+		defer rc.Close()
+		buf, err := io.ReadAll(rc)
+		if err != nil {
+			return nil, err
+		}
+		doc, err := makeDoc(filename, gitRepo.GetOwnerRepo(), buf)
+		if err != nil {
+			log.Debugf(err.Error())
+			continue
+		}
+		if err := iw.AddDocument(doc); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := iw.Flush(); err != nil {
+		return nil, err
+	}
+
+	log.Printf("Finished indexing %s @ %s [%s]", gitRepo.GetOwnerRepo(), gitRepo.GetCommitSha(), time.Since(start))
+	return &inpb.IndexResponse{
+		GitRepo: &inpb.GitRepoResponse{},
+	}, nil
+}
+
+func (css *codesearchServer) Search(ctx context.Context, req *srpb.SearchRequest) (*srpb.SearchResponse, error) {
+	// TODO(tylerw): pull namespace and numResultsfrom search request.
+	namespace := ""
+	numResults := 10
+
+	codesearcher := searcher.New(index.NewReader(css.db, namespace))
+	q, err := query.NewReQuery(req.GetQuery().GetTerm(), numResults)
+	if err != nil {
+		return nil, err
+	}
+	docs, err := codesearcher.Search(q)
+	if err != nil {
+		return nil, err
+	}
+	highlighter := q.GetHighlighter()
+
+	rsp := &srpb.SearchResponse{}
+	for _, doc := range docs {
+		regions := highlighter.Highlight(doc)
+		result := &srpb.Result{
+			Repo:       string(doc.Field(repoField).Contents()),
+			Filename:   string(doc.Field(filenameField).Contents()),
+			MatchCount: int32(len(regions)),
+		}
+		for _, region := range regions {
+			result.Snippets = append(result.Snippets, &srpb.Snippet{
+				Lines: region.String(),
+			})
+		}
+		rsp.Results = append(rsp.Results, result)
+	}
+
+	return rsp, nil
+}
