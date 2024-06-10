9b0b12054d0e6aabff5791608d31a1e042151808
Brandon Duffany <brandon@buildbuddy.io>
2024-06-10 14:15:23 -0400

    Fix action cache file fetching

--- server/testutil/testcache/testcache.go
+++ server/testutil/testcache/testcache.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/action_cache_server"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/byte_stream_server"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/capabilities_server"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/content_addressable_storage_server"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	bspb "google.golang.org/genproto/googleapis/bytestream"
@@ -27,22 +29,33 @@ func Setup(t *testing.T, env *testenv.TestEnv) {
 	require.NotNil(t, env.GetGRPCServer(), "GRPC server is missing from env. Call testenv.RegisterLocalGRPCServer first")
 	require.NotNil(t, env.GetLocalBufconnListener(), "Missing bufconn listener in env. Make sure you aren't calling SetGRPCServer in the test env, and call testenv.RegisterLocalGRPCServer instead")
 
+	RegisterServers(t, env)
+
+	conn, err := testenv.LocalGRPCConn(env.GetServerContext(), env)
+	require.NoError(t, err)
+	t.Cleanup(func() { conn.Close() })
+
+	RegisterClients(env, conn)
+}
+
+func RegisterServers(t *testing.T, env *testenv.TestEnv) {
 	ac, err := action_cache_server.NewActionCacheServer(env)
 	require.NoError(t, err)
 	bs, err := byte_stream_server.NewByteStreamServer(env)
 	require.NoError(t, err)
 	cas, err := content_addressable_storage_server.NewContentAddressableStorageServer(env)
 	require.NoError(t, err)
+	caps := capabilities_server.NewCapabilitiesServer(env, true /*cas*/, true /*rbe*/, true /*zstd*/)
 
 	repb.RegisterActionCacheServer(env.GetGRPCServer(), ac)
 	bspb.RegisterByteStreamServer(env.GetGRPCServer(), bs)
 	repb.RegisterContentAddressableStorageServer(env.GetGRPCServer(), cas)
+	repb.RegisterCapabilitiesServer(env.GetGRPCServer(), caps)
+}
 
-	conn, err := testenv.LocalGRPCConn(env.GetServerContext(), env)
-	require.NoError(t, err)
-	t.Cleanup(func() { conn.Close() })
-
+func RegisterClients(env *testenv.TestEnv, conn grpc.ClientConnInterface) {
 	env.SetActionCacheClient(repb.NewActionCacheClient(conn))
 	env.SetByteStreamClient(bspb.NewByteStreamClient(conn))
 	env.SetContentAddressableStorageClient(repb.NewContentAddressableStorageClient(conn))
+	env.SetCapabilitiesClient(repb.NewCapabilitiesClient(conn))
 }
