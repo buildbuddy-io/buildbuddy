c9bf2c53c01becd4e899df383fe5612d6b479cdf
Vadim Berezniker <vadim@buildbuddy.io>
2024-06-12 16:18:26 -0700

    Add support for k8s cert generation.

--- tools/bbcert/bbcert.go
+++ tools/bbcert/bbcert.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"os"
 	"os/exec"
 	"path"
@@ -10,6 +11,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/flag"
 	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 
 	cgpb "github.com/buildbuddy-io/buildbuddy/proto/certgenerator"
 )
@@ -24,6 +26,48 @@ const (
 	defaultRSAPublicKeyFile   = "id_rsa.pub"
 	defaultRSACertificateFile = "id_rsa-cert.pub"
 )
+
+func kubectlConfigSet(ctx context.Context, key, value string) error {
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd := exec.CommandContext(ctx, "kubectl", "config", "set", key, value)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return status.UnknownErrorf("could not update kubectl configuration\n%s", stderr.String())
+	}
+	return nil
+}
+
+func updateKubectlConfig(ctx context.Context, kc *cgpb.KubernetesClusterCredentials) error {
+	// Setup cluster config.
+	clusterPrefix := "clusters.bb-" + kc.Name + "."
+	if err := kubectlConfigSet(ctx, clusterPrefix+"server", kc.Server); err != nil {
+		return err
+	}
+	if err := kubectlConfigSet(ctx, clusterPrefix+"certificate-authority-data", base64.StdEncoding.EncodeToString([]byte(kc.ServerCa))); err != nil {
+		return err
+	}
+
+	// Setup user config.
+	usersPrefix := "users.bb-" + kc.Name + "."
+	if err := kubectlConfigSet(ctx, usersPrefix+"client-certificate-data", base64.StdEncoding.EncodeToString([]byte(kc.ClientCert))); err != nil {
+		return err
+	}
+	if err := kubectlConfigSet(ctx, usersPrefix+"client-key-data", base64.StdEncoding.EncodeToString([]byte(kc.ClientCertKey))); err != nil {
+		return err
+	}
+
+	// Create context.
+	contextPrefix := "contexts.bb-" + kc.Name + "."
+	if err := kubectlConfigSet(ctx, contextPrefix+"cluster", "bb-"+kc.Name); err != nil {
+		return err
+	}
+	if err := kubectlConfigSet(ctx, contextPrefix+"user", "bb-"+kc.Name); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	flag.Parse()
@@ -77,4 +121,12 @@ func main() {
 	}
 
 	log.Infof("Wrote certificate to %q.", certFile)
+
+	for _, kc := range resp.KubernetesCredentials {
+		log.Infof("Configuring credentials for %q kubernetes cluster.", "bb-"+kc.Name)
+		if err := updateKubectlConfig(ctx, kc); err != nil {
+			log.Warningf("Could not generate kubectl config for cluster %q: %s", kc.Name, err)
+		}
+	}
+
 }
