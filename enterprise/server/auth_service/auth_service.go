1438f0db02769a6760a88b9394b8272d353b1382
Iain Macdonald <xiainx@gmail.com>
2024-06-18 10:27:04 -0700

    Implement auth service

--- enterprise/server/auth_service/auth_service.go
+++ enterprise/server/auth_service/auth_service.go
@@ -3,18 +3,31 @@ package auth_service
 import (
 	"context"
 
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/authutil"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 
 	authpb "github.com/buildbuddy-io/buildbuddy/proto/auth"
 )
 
 type AuthService struct {
+	authenticator interfaces.GRPCAuthenticator
 }
 
 func Register(env *real_environment.RealEnv) {
-	env.SetAuthService(AuthService{})
+	env.SetAuthService(AuthService{authenticator: env.GetAuthenticator()})
 }
 
 func (a AuthService) Authenticate(ctx context.Context, req *authpb.AuthenticateRequest) (*authpb.AuthenticateResponse, error) {
-	return &authpb.AuthenticateResponse{}, nil
+	ctx = a.authenticator.AuthenticatedGRPCContext(ctx)
+	err, found := authutil.AuthErrorFromContext(ctx)
+	if found {
+		return nil, err
+	}
+	jwt, ok := ctx.Value(authutil.ContextTokenStringKey).(string)
+	if ok {
+		return &authpb.AuthenticateResponse{Jwt: &jwt}, nil
+	}
+	return nil, status.UnauthenticatedError("Authentication failed")
 }
