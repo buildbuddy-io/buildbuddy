ef142b28f4a0c9f53dbae53cdcf9001132b1d032
Iain Macdonald <xiainx@gmail.com>
2024-07-01 16:18:50 -0700

    Remove one more context

--- enterprise/server/cmd/cache_proxy/cache_proxy.go
+++ enterprise/server/cmd/cache_proxy/cache_proxy.go
@@ -65,7 +65,7 @@ func main() {
 	healthChecker := healthcheck.NewHealthChecker(*serverType)
 	env := real_environment.NewRealEnv(healthChecker)
 	env.SetMux(tracing.NewHttpServeMux(http.NewServeMux()))
-	authenticator, err := remoteauth.NewRemoteAuthenticator(env.GetServerContext())
+	authenticator, err := remoteauth.NewRemoteAuthenticator()
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
