c44e27d19359c5be19318dcaed997ae52d9db7b7
Maggie Lou <maggie@buildbuddy.io>
2024-07-08 17:41:22 -0500

    Scripts for hyperfine

---
+++ metal_run_rb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+api_key=$1
+command=$2
+
+instance_name=$(date +"%Y%m%d_%H%M%S")
+bb remote --remote_runner=grpcs://cloud.metal.buildbuddy.dev --runner_exec_properties=instance_name=$instance_name --runner_exec_properties=EstimatedComputeUnits=4 --runner_exec_properties=EstimatedFreeDiskBytes=35000000000 $command --remote_header=x-buildbuddy-api-key="$api_key" --config=bare
