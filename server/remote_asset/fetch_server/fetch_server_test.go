4ab0568616c394716a67f7a18a57c613e644708a
Son Luong Ngoc <sluongng@gmail.com>
2024-07-10 16:28:45 +0200

    fetch_server: support storing blob using checksum digest func (#6997)

--- server/remote_asset/fetch_server/fetch_server_test.go
+++ server/remote_asset/fetch_server/fetch_server_test.go
@@ -331,25 +331,21 @@ func TestSubsequentRequestCacheHit(t *testing.T) {
 		name              string
 		digestFunc        repb.DigestFunction_Value
 		checksumQualifier string
-		expectCacheHit    bool
 	}{
 		{
 			name:              "sha256_digest_func__sri_sha256",
 			digestFunc:        repb.DigestFunction_SHA256,
 			checksumQualifier: sha256CRI,
-			expectCacheHit:    true,
 		},
 		{
 			name:              "blake3_digest_func__sri_sha256",
 			digestFunc:        repb.DigestFunction_BLAKE3,
 			checksumQualifier: sha256CRI,
-			expectCacheHit:    false,
 		},
 		{
 			name:              "sha256_digest_func__sri_sha512",
 			digestFunc:        repb.DigestFunction_SHA256,
 			checksumQualifier: sha512CRI,
-			expectCacheHit:    false,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -360,9 +356,9 @@ func TestSubsequentRequestCacheHit(t *testing.T) {
 			fetchClient := rapb.NewFetchClient(clientConn)
 
 			// a cache miss would translate to an incoming request handled by http test server
-			cacheMisses := 0
+			cacheMissCount := 0
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				cacheMisses += 1
+				cacheMissCount += 1
 				fmt.Fprint(w, content)
 			}))
 			defer ts.Close()
@@ -389,20 +385,18 @@ func TestSubsequentRequestCacheHit(t *testing.T) {
 				assert.Equal(t, "", resp.GetStatus().Message)
 				assert.Contains(t, resp.GetUri(), ts.URL)
 				require.NoError(t, err)
-				require.Equal(t, 1, cacheMisses)
+				require.Equal(t, 1, cacheMissCount)
 			}
 
-			{
-				// Second fetch request _should_ get cache hits, except for known cases
-				// TODO: ensure cache hits for all cases
+			for range 2 {
 				resp, err := fetchClient.FetchBlob(ctx, request)
 				assert.NoError(t, err)
 				require.NotNil(t, resp)
-				if tc.expectCacheHit {
-					require.Equal(t, 1, cacheMisses)
-				} else {
-					require.Equal(t, 2, cacheMisses)
-				}
+				require.Equal(t, 1, cacheMissCount, "subsequent fetch requests should get cached blob")
+				assert.Equal(t, int32(0), resp.GetStatus().Code)
+				assert.Equal(t, "", resp.GetStatus().Message)
+				// we are not current storing which url we got the cached blob from
+				assert.Equal(t, "", resp.Uri)
 			}
 		})
 	}
