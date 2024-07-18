7fc563a513d27356cd01c8f10ac323e573ad8bd2
Brandon Duffany <brandon@buildbuddy.io>
2024-07-18 12:23:17 -0400

    WIP

--- enterprise/vmsupport/kernel/rebuild.sh
+++ enterprise/vmsupport/kernel/rebuild.sh
@@ -5,6 +5,7 @@ set -e
 
 : "${VERSION:=v5.4}"
 : "${WORKDIR:=/tmp/linux}"
+: "${UPLOAD:=1}"
 
 cd "$(dirname "$0")"
 KERNEL_CONFIG_PATH="$PWD/microvm-kernel-x86_64.config"
@@ -23,23 +24,39 @@ SHA256=$(sha256sum "$PWD/vmlinux" | awk '{print $1}')
 NAME="vmlinux-${VERSION}-${SHA256}"
 mv vmlinux "$NAME"
 
+# Set up a bazel workspace compatible with --override_repository
+rm -rf ./bazel_override
+mkdir bazel_override
+touch bazel_override/WORKSPACE
+mkdir bazel_override/file
+ln "$NAME" bazel_override/file/downloaded
+echo 'exports_files(["downloaded"])' >bazel_override/file/BUILD
+
 echo "---"
 echo "Successfully built $PWD/$NAME"
-echo "Uploading to https://storage.googleapis.com/buildbuddy-tools/binaries/linux/$NAME ..."
 
-(
-  set -x
-  gsutil cp "$PWD/$NAME" "gs://buildbuddy-tools/binaries/linux/$NAME"
-) || {
-  echo "Upload failed; please upload manually."
-}
+echo "Bazel repository override:"
+echo "  --override_repository=org_kernel_git_linux_kernel-vmlinux=$PWD/bazel_override"
 
-echo ""
-echo "deps.bzl update is required:"
-echo ""
-echo "    http_file("
-echo "        name = \"org_kernel_git_linux_kernel-vmlinux\","
-echo "        sha256 = \"$SHA256\","
-echo "        urls = [\"https://storage.googleapis.com/buildbuddy-tools/binaries/linux/$NAME\"],"
-echo "        executable = True,"
-echo "    )"
+UPLOAD_COMMAND=(gsutil cp "$PWD/$NAME" "gs://buildbuddy-tools/binaries/linux/$NAME")
+if ((UPLOAD)); then
+  echo "Uploading to https://storage.googleapis.com/buildbuddy-tools/binaries/linux/$NAME ..."
+  (
+    echo "Running" "${UPLOAD_COMMAND[@]}"
+    "${UPLOAD_COMMAND[@]}"
+
+    echo ""
+    echo "deps.bzl update is required:"
+    echo ""
+    echo "    http_file("
+    echo "        name = \"org_kernel_git_linux_kernel-vmlinux\","
+    echo "        sha256 = \"$SHA256\","
+    echo "        urls = [\"https://storage.googleapis.com/buildbuddy-tools/binaries/linux/$NAME\"],"
+    echo "        executable = True,"
+    echo "    )"
+  ) || {
+    echo "Upload failed; please upload manually, and update deps.bzl"
+  }
+else
+  echo "To upload to gcs, run:" "${UPLOAD_COMMAND[@]}"
+fi
