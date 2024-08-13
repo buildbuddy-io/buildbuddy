ba5c3aad5c15454fdd4c98ac2e47e595e1f31ae3
Tyler Williams <williams.tyler@gmail.com>
2024-08-12 19:23:53 -0700

    Fix import

--- codesearch/dfa/dfa.go
+++ codesearch/dfa/dfa.go
@@ -9,7 +9,7 @@ import (
 	"regexp/syntax"
 	"sort"
 
-	"github.com/google/codesearch/sparse"
+	"github.com/buildbuddy-io/buildbuddy/codesearch/sparse"
 )
 
 // A matcher holds the state for running regular expression search.
