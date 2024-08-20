b68db2e6d49986c2def061e31cd2fe4b5ce46cae
Maggie Lou <maggie@buildbuddy.io>
2024-08-20 11:40:56 -0700

    Set digest_function in command line instead

--- app/invocation/invocation_model.tsx
+++ app/invocation/invocation_model.tsx
@@ -405,9 +405,6 @@ export default class InvocationModel {
   getDigestFunction() {
     const digestFnName = this.optionsMap.get("digest_function");
     if (!digestFnName) {
-      if (this.isWorkflowInvocation() || this.isHostedBazelInvocation()) {
-        return build.bazel.remote.execution.v2.DigestFunction.Value.BLAKE3;
-      }
       return build.bazel.remote.execution.v2.DigestFunction.Value.SHA256;
     }
     const digestFnEnum =
