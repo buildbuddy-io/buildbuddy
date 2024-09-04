b2fd8ec3657b3fb77815d799392323751f806d4d
Maggie Lou <maggie@buildbuddy.io>
2024-09-04 15:12:30 -0500

    Ramp up pebble eviction

--- enterprise/server/backends/pebble_cache/pebble_cache.go
+++ enterprise/server/backends/pebble_cache/pebble_cache.go
@@ -121,7 +121,7 @@ var (
 const (
 	// cutoffThreshold is the point above which a janitor thread will run
 	// and delete the oldest items from the cache.
-	JanitorCutoffThreshold = .85
+	JanitorCutoffThreshold = .65
 
 	megabyte = 1e6
 
