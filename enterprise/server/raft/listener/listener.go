130f1b9d6b35891e09ad0509eeb83e02e8045109
Lulu Zhang <lulu@buildbuddy.io>
2024-09-25 08:18:29 -0700

    [raft] Use NodeReady callback to determine whether a replica is ready

--- enterprise/server/raft/listener/listener.go
+++ enterprise/server/raft/listener/listener.go
@@ -10,43 +10,59 @@ import (
 
 var (
 	leaderUpdatedChanSize = flag.Int64("cache.raft.leader_updated_chan_size", 200, "The length of the leader updated channel")
+	nodeReadyChanSize     = flag.Int64("cache.raft.node_ready_chan_size", 200, "The length of the node ready channel")
 )
 
+type leaderUpdate struct {
+	mu        sync.Mutex
+	lastInfo  *raftio.LeaderInfo
+	listeners map[string]chan raftio.LeaderInfo
+}
+type nodeReady struct {
+	mu        sync.Mutex
+	listeners map[string]chan raftio.NodeInfo
+}
 type RaftListener struct {
-	mu                    sync.Mutex
-	log                   log.Logger
-	lastLeaderInfo        *raftio.LeaderInfo
-	leaderChangeListeners map[string]chan raftio.LeaderInfo
+	log log.Logger
+
+	leaderUpdate leaderUpdate
+	nodeReady    nodeReady
 }
 
 func NewRaftListener() *RaftListener {
 	return &RaftListener{
-		mu:                    sync.Mutex{},
-		log:                   log.NamedSubLogger("RaftCallback"),
-		leaderChangeListeners: make(map[string]chan raftio.LeaderInfo),
+		leaderUpdate: leaderUpdate{
+			mu:        sync.Mutex{},
+			listeners: make(map[string]chan raftio.LeaderInfo),
+		},
+		nodeReady: nodeReady{
+			mu:        sync.Mutex{},
+			listeners: make(map[string]chan raftio.NodeInfo),
+		},
+		log: log.NamedSubLogger("RaftCallback"),
 	}
 }
 
 func (rl *RaftListener) RemoveLeaderChangeListener(id string) {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
+	rl.leaderUpdate.mu.Lock()
+	defer rl.leaderUpdate.mu.Unlock()
 
-	ch := rl.leaderChangeListeners[id]
+	ch := rl.leaderUpdate.listeners[id]
 	close(ch)
-	delete(rl.leaderChangeListeners, id)
+	delete(rl.leaderUpdate.listeners, id)
 }
 
 // AddLeaderChangeListener returns a channel where updates to raftio.LeaderInfo
 // will be published when callbacks are received by the library.
 // Listeners *must not block* or they risk dropping updates.
 func (rl *RaftListener) AddLeaderChangeListener(id string) <-chan raftio.LeaderInfo {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
+	rl.leaderUpdate.mu.Lock()
+	defer rl.leaderUpdate.mu.Unlock()
 
 	ch := make(chan raftio.LeaderInfo, *leaderUpdatedChanSize)
-	rl.leaderChangeListeners[id] = ch
-	if rl.lastLeaderInfo != nil {
-		ch <- *rl.lastLeaderInfo
+	rl.leaderUpdate.listeners[id] = ch
+	if rl.leaderUpdate.lastInfo != nil {
+		ch <- *rl.leaderUpdate.lastInfo
 	}
 
 	return ch
@@ -57,21 +73,49 @@ func (rl *RaftListener) AddLeaderChangeListener(id string) <-chan raftio.LeaderI
 // to leaderChangeListeners.
 func (rl *RaftListener) LeaderUpdated(info raftio.LeaderInfo) {
 	rl.log.Debugf("LeaderUpdated: %+v", info)
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
-	rl.lastLeaderInfo = &info
+	rl.leaderUpdate.mu.Lock()
+	defer rl.leaderUpdate.mu.Unlock()
+	rl.leaderUpdate.lastInfo = &info
 
-	for id, ch := range rl.leaderChangeListeners {
+	for id, ch := range rl.leaderUpdate.listeners {
 		select {
 		case ch <- info:
 		default:
-			log.Warningf("dropping message for %s", id)
+			log.Warningf("dropping leaderUpdate message for %s", id)
 		}
 	}
 }
 
+func (rl *RaftListener) AddNodeReadyListener(id string) <-chan raftio.NodeInfo {
+	rl.nodeReady.mu.Lock()
+	defer rl.nodeReady.mu.Unlock()
+
+	ch := make(chan raftio.NodeInfo, *nodeReadyChanSize)
+	rl.nodeReady.listeners[id] = ch
+	return ch
+}
+
+func (rl *RaftListener) RemoveNodeReadyListener(id string) {
+	rl.nodeReady.mu.Lock()
+	defer rl.nodeReady.mu.Unlock()
+
+	ch := rl.nodeReady.listeners[id]
+	close(ch)
+	delete(rl.nodeReady.listeners, id)
+}
+
 func (rl *RaftListener) NodeReady(info raftio.NodeInfo) {
 	rl.log.Debugf("NodeReady: %+v", info)
+	rl.nodeReady.mu.Lock()
+	defer rl.nodeReady.mu.Unlock()
+
+	for id, ch := range rl.nodeReady.listeners {
+		select {
+		case ch <- info:
+		default:
+			log.Warningf("dropping nodeReady message for %s", id)
+		}
+	}
 }
 func (rl *RaftListener) NodeHostShuttingDown() {
 	rl.log.Debugf("NodeHostShuttingDown")
