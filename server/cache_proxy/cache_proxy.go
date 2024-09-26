buildbuddy-io/buildbuddy

commit f4629ae567c28c24c5d1b56cb4751b77dc8ca125

Log warning/error logs from sidecar if build fails (#7563)
Also add a sidecar flag to log slow cache uploads.

Example (everything after and including the `---` in the screenshot
below)


![image](https://github.com/user-attachments/assets/d5a4b5c2-d316-486c-a3e5-4947f4f8b221)

server/cache_proxy/cache_proxy.go
@@ -16,7 +16,9 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/cachetools"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/content_addressable_storage_server"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/bazel_request"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/proto"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -30,9 +32,10 @@ import (
 )
 
 var (
-	readThrough      = flag.Bool("read_through", true, "If true, cache remote reads locally")
-	writeThrough     = flag.Bool("write_through", true, "If true, upload writes to remote cache too")
-	synchronousWrite = flag.Bool("synchronous_write", false, "If true, wait until writes to remote cache are finished")
+	readThrough        = flag.Bool("local_cache_proxy.read_through", true, "If true, cache remote reads locally")
+	writeThrough       = flag.Bool("local_cache_proxy.write_through", true, "If true, upload writes to remote cache too")
+	synchronousWrite   = flag.Bool("local_cache_proxy.synchronous_write", false, "If true, wait until writes to remote cache are finished")
+	slowWriteThreshold = flag.Duration("local_cache_proxy.slow_write_threshold", 30*time.Second, "If greater than 0, log warnings for cache writes that take longer than this duration")
 )
 
 const (
@@ -246,8 +249,9 @@ func (p *CacheProxy) Read(req *bspb.ReadRequest, stream bspb.ByteStream_ReadServ
 }
 
 func (p *CacheProxy) Write(stream bspb.ByteStream_WriteServer) error {
+	ctx := stream.Context()
 	var wreq *bspb.WriteRequest
-	clientStream, err := p.localBSSClient.Write(stream.Context())
+	clientStream, err := p.localBSSClient.Write(ctx)
 	if err != nil {
 		return err
 	}
@@ -274,12 +278,12 @@ func (p *CacheProxy) Write(stream bspb.ByteStream_WriteServer) error {
 			}
 			if *writeThrough {
 				if *synchronousWrite {
-					if err := p.qWorker.RemoteWriteBlocked(stream.Context(), wreq); err != nil {
-						log.Errorf("Error write to remote cache: %s", err)
+					if err := p.qWorker.RemoteWriteBlocked(ctx, wreq); err != nil {
+						log.CtxErrorf(ctx, "Error write to remote cache: %s", err)
 					}
 				} else {
-					if err := p.qWorker.EnqueueRemoteWrite(stream.Context(), wreq); err != nil {
-						log.Errorf("Error enqueueing write request to remote: %s", err.Error())
+					if err := p.qWorker.EnqueueRemoteWrite(ctx, wreq); err != nil {
+						log.CtxErrorf(ctx, "Error enqueueing write request to remote: %s", err.Error())
 					}
 				}
 			}
@@ -290,6 +294,29 @@ func (p *CacheProxy) Write(stream bspb.ByteStream_WriteServer) error {
 
 func (p *CacheProxy) QueryWriteStatus(ctx context.Context, req *bspb.QueryWriteStatusRequest) (*bspb.QueryWriteStatusResponse, error) {
 	return p.localBSS.QueryWriteStatus(ctx, req)
+}
+
+func logContextFromMetadata(ctx context.Context, md metadata.MD) context.Context {
+	if md == nil {
+		return ctx
+	}
+	if iid := getInvocationID(md); iid != "" {
+		ctx = log.EnrichContext(ctx, log.InvocationIDKey, iid)
+	}
+	return ctx
+}
+
+func getInvocationID(md metadata.MD) string {
+	vals := md[bazel_request.RequestMetadataKey]
+	if len(vals) == 0 {
+		return ""
+	}
+	rmd := &repb.RequestMetadata{}
+	b := []byte(vals[0])
+	if err := proto.Unmarshal(b, rmd); err != nil {
+		return ""
+	}
+	return rmd.GetToolInvocationId()
 }
 
 type queueReq struct {
@@ -313,6 +340,7 @@ func NewQueueWorker(ctx context.Context, localClient, remoteClient bspb.ByteStre
 	qw.Start()
 	return qw
 }
+
 func (qw *queueWorker) Start() {
 	go func() {
 		for {
@@ -320,16 +348,37 @@ func (qw *queueWorker) Start() {
 			case <-qw.ctx.Done():
 				return
 			case req := <-qw.workQ:
-				if err := qw.handleWriteRequest(req); err != nil {
-					log.Printf("Error handling write request: %s", err.Error())
+				ctx := qw.ctx
+				// Reconstruct the original context, including gRPC metadata
+				// and logging context that would normally be populated via
+				// the client interceptors.
+				ctx = metadata.NewOutgoingContext(ctx, req.metadata)
+				ctx = logContextFromMetadata(ctx, req.metadata)
+
+				if err := qw.handleWriteRequest(ctx, req.writeResourceName); err != nil {
+					log.CtxErrorf(ctx, "Error handling write request: %s", err)
 				}
 			}
 		}
 	}()
 }
-func (qw *queueWorker) handleWriteRequest(qreq queueReq) error {
+
+func (qw *queueWorker) handleWriteRequest(ctx context.Context, writeResourceName string) error {
+	if *slowWriteThreshold > 0 {
+		done := make(chan struct{})
+		defer close(done)
+		go func() {
+			select {
+			case <-time.After(*slowWriteThreshold):
+				log.CtxWarningf(ctx, "Slow cache upload: %q took > %s", writeResourceName, *slowWriteThreshold)
+			case <-done:
+				return
+			}
+		}()
+	}
+
 	start := time.Now()
-	resourceName, err := digest.ParseUploadResourceName(qreq.writeResourceName)
+	resourceName, err := digest.ParseUploadResourceName(writeResourceName)
 	if err != nil {
 		return err
 	}
@@ -340,8 +389,6 @@ func (qw *queueWorker) handleWriteRequest(qreq queueReq) error {
 		return err
 	}
 	defer os.Remove(tmpFile.Name())
-	ctx := qw.ctx
-	ctx = metadata.NewOutgoingContext(ctx, qreq.metadata)
 	if err := cachetools.GetBlob(ctx, qw.localClient, resourceName, tmpFile); err != nil {
 		return err
 	}
@@ -351,7 +398,7 @@ func (qw *queueWorker) handleWriteRequest(qreq queueReq) error {
 	if _, _, err := cachetools.UploadFromReader(ctx, qw.remoteClient, resourceName, tmpFile); err != nil {
 		return err
 	}
-	log.Debugf("Handled write request: %s in %s", qreq.writeResourceName, time.Since(start))
+	log.CtxDebugf(ctx, "Wrote %q in %s", writeResourceName, time.Since(start))
 	return nil
 }
 
@@ -370,12 +417,7 @@ func (qw *queueWorker) EnqueueRemoteWrite(ctx context.Context, wreq *bspb.WriteR
 }
 
 func (qw *queueWorker) RemoteWriteBlocked(ctx context.Context, wreq *bspb.WriteRequest) error {
-	md, _ := metadata.FromIncomingContext(ctx)
-	req := queueReq{
-		metadata:          md,
-		writeResourceName: wreq.GetResourceName(),
-	}
-	return qw.handleWriteRequest(req)
+	return qw.handleWriteRequest(ctx, wreq.GetResourceName())
 }
 
 func defaultCapabilities() *repb.ServerCapabilities {

cli/cmd/bb/bb.go
@@ -33,6 +33,11 @@ var (
 	}
 )
 
+const (
+	bazelExitCodeTestFailure = 3
+	bazelExitCodeInterrupted = 8
+)
+
 func main() {
 	// If we're the sidecar (CLI server) process, run the sidecar instead of the
 	// CLI.
@@ -152,7 +157,7 @@ func handleBazelCommand(start time.Time, args []string, originalArgs []string) (
 		}
 	}()
 
-	plugins, bazelArgs, execArgs, err := setup.Setup(args, tempDir)
+	plugins, bazelArgs, execArgs, sidecar, err := setup.Setup(args, tempDir)
 	if err != nil {
 		return 1, err
 	}
@@ -198,7 +203,7 @@ func handleBazelCommand(start time.Time, args []string, originalArgs []string) (
 	}
 
 	// If the build was interrupted (Ctrl+C), don't run post-bazel plugins.
-	if exitCode == 8 /*interrupted*/ {
+	if exitCode == bazelExitCodeInterrupted {
 		return exitCode, nil
 	}
 
@@ -216,6 +221,11 @@ func handleBazelCommand(start time.Time, args []string, originalArgs []string) (
 
 	// TODO: Support post-run hooks?
 	// e.g. show a desktop notification once a k8s deploy has finished
+
+	// If bazel failed, show sidecar warnings/errors to help diagnose.
+	if exitCode != 0 && exitCode != bazelExitCodeTestFailure && sidecar != nil {
+		sidecar.PrintWarningsSince(start)
+	}
 
 	return exitCode, nil
 }

cli/setup/setup.go
@@ -11,11 +11,12 @@ import (
 
 // Setup prepares us to run a bazel command. It loads plugins, handles auth,
 // configures the sidecar, and runs pre-bazel handlers.
-func Setup(args []string, tempDir string) (plugins []*plugin.Plugin, bazelArgs []string, execArgs []string, err error) {
+// TODO: this func has too many return values - pack up into a struct.
+func Setup(args []string, tempDir string) (_ []*plugin.Plugin, bazelArgs []string, execArgs []string, _ *sidecar.Instance, _ error) {
 	// Load plugins
-	plugins, err = plugin.LoadAll(tempDir)
+	plugins, err := plugin.LoadAll(tempDir)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
 	// Parse args.
@@ -26,7 +27,7 @@ func Setup(args []string, tempDir string) (plugins []*plugin.Plugin, bazelArgs [
 	// process.
 	bazelArgs, err = parser.ExpandConfigs(bazelArgs)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
 	// Fiddle with Bazel args
@@ -34,30 +35,30 @@ func Setup(args []string, tempDir string) (plugins []*plugin.Plugin, bazelArgs [
 	bazelArgs = tooltag.ConfigureToolTag(bazelArgs)
 	bazelArgs, err = login.ConfigureAPIKey(bazelArgs)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
 	// Prepare convenience env vars for plugins
 	if err := plugin.PrepareEnv(); err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
 	// Run plugin pre-bazel hooks
 	bazelArgs, err = parser.CanonicalizeArgs(bazelArgs)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	for _, p := range plugins {
 		bazelArgs, execArgs, err = p.PreBazel(bazelArgs, execArgs)
 		if err != nil {
-			return nil, nil, nil, err
+			return nil, nil, nil, nil, err
 		}
 	}
 
 	// Note: sidecar is configured after pre-bazel plugins, since pre-bazel
 	// plugins may change the value of bes_backend, remote_cache,
 	// remote_instance_name, etc.
-	bazelArgs = sidecar.ConfigureSidecar(bazelArgs)
+	bazelArgs, sidecarInstance := sidecar.ConfigureSidecar(bazelArgs)
 
-	return plugins, bazelArgs, execArgs, nil
+	return plugins, bazelArgs, execArgs, sidecarInstance, nil
 }

cli/sidecar/sidecar.go
@@ -1,12 +1,14 @@
 package sidecar
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"hash/crc32"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"syscall"
 	"time"
@@ -77,13 +79,13 @@ func getArgsFromConfig(ctx context.Context, diskCacheDir string) ([]string, erro
 	return args, nil
 }
 
-func restartSidecarIfNecessary(ctx context.Context, bbCacheDir string, args []string) (string, error) {
+func restartSidecarIfNecessary(ctx context.Context, bbCacheDir string, args []string) (*Instance, error) {
 	// Forward args from BB_SIDECAR_ARGS env var (useful for setting debug log
 	// level, etc.)
 	rawExtraArgs := os.Getenv("BB_SIDECAR_ARGS")
 	extraArgs, err := shlex.Split(rawExtraArgs)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	args = append(args, extraArgs...)
 
@@ -97,17 +99,22 @@ func restartSidecarIfNecessary(ctx context.Context, bbCacheDir string, args []st
 	sockName := sockPrefix + sidecarID + ".sock"
 	sockPath := filepath.Join(os.TempDir(), sockName)
 
+	logPath := filepath.Join(bbCacheDir, "sidecar-"+sidecarID+".log")
+	instance := &Instance{
+		SockPath: sockPath,
+		LogPath:  logPath,
+	}
+
 	// Check if a process is already running with this sock.
 	// If one is, we're all done!
 	if pathExists(sockPath) {
 		log.Debugf("Sidecar socket %q exists.", sockPath)
-		return sockPath, nil
+		return instance, nil
 	}
 
-	logPath := filepath.Join(bbCacheDir, "sidecar-"+sidecarID+".log")
 	f, err := os.Create(logPath)
 	if err != nil {
-		return "", fmt.Errorf("failed to create sidecar log file: %s", err)
+		return nil, fmt.Errorf("failed to create sidecar log file: %s", err)
 	}
 	// Note: Not closing f since the sidecar writes to it.
 
@@ -123,9 +130,9 @@ func restartSidecarIfNecessary(ctx context.Context, bbCacheDir string, args []st
 	log.Debugf("Running sidecar cmd: %s", c.String())
 	log.Debugf("Sidecar will write logs to: %s", logPath)
 	if err := c.Start(); err != nil {
-		return "", err
+		return nil, err
 	}
-	return sockPath, nil
+	return instance, nil
 }
 
 func isFlagTrue(flag string) bool {
@@ -147,7 +154,66 @@ func isCI(args []string) bool {
 	return false
 }
 
-func ConfigureSidecar(args []string) []string {
+// Instance holds information about the running sidecar instance.
+type Instance struct {
+	// SockPath is the path to the sidecar socket.
+	SockPath string
+	// LogPath is the path to the logs for the sidecar.
+	LogPath string
+}
+
+func (i *Instance) PrintWarningsSince(t time.Time) {
+	// TODO: only look at tail of logs, to limit IO
+	f, err := os.Open(i.LogPath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	var lines []string
+	for s.Scan() {
+		line := s.Text()
+		lt, ok := parseLogTimestamp(line)
+		if !ok || !lt.After(t) {
+			continue
+		}
+		if !strings.Contains(line, "WRN") && !strings.Contains(line, "ERR") {
+			continue
+		}
+		lines = append(lines, line)
+	}
+	if len(lines) > 0 {
+		os.Stderr.WriteString("---\n")
+		log.Warnf("BuildBuddy CLI sidecar errors during this build:")
+		for _, line := range lines {
+			fmt.Print(line)
+			if !strings.HasSuffix(line, "\n") {
+				fmt.Println()
+			}
+		}
+	}
+}
+
+var logTimestampRegexp = regexp.MustCompile(`\d+/\d+/\d+ \d+:\d+:\d+\.\d+`)
+var logTimestampFormat = "2006/01/02 15:04:05.000"
+
+func parseLogTimestamp(line string) (time.Time, bool) {
+	// TODO: maybe use structured logging to avoid this parsing and to allow
+	// the CLI to present sidecar errors in its own style.
+	m := logTimestampRegexp.FindString(line)
+	if m == "" {
+		return time.Time{}, false
+	}
+	t, err := time.ParseInLocation(logTimestampFormat, m, time.Local)
+	if err != nil {
+		return time.Time{}, false
+	}
+	return t, true
+}
+
+func ConfigureSidecar(args []string) ([]string, *Instance) {
+	originalArgs := args
+
 	// Disable sidecar on CI for now since the async upload behavior can cause
 	// problems if the CI runner terminates before the uploads have completed.
 	if isCI(args) {
@@ -166,7 +232,7 @@ func ConfigureSidecar(args []string) []string {
 	cacheDir, err := storage.CacheDir()
 	if err != nil {
 		log.Warnf("Sidecar could not be initialized, continuing without sidecar: %s", err)
-		return args
+		return args, nil
 	}
 
 	// Re(Start) the sidecar if the flags set don't match.
@@ -201,11 +267,11 @@ func ConfigureSidecar(args []string) []string {
 
 	if !sidecarEnabled {
 		// Sidecar is not needed for this invocation; don't start it.
-		return args
+		return args, nil
 	}
 
 	if synchronousWriteFlag == "1" || synchronousWriteFlag == "true" {
-		sidecarArgs = append(sidecarArgs, "--synchronous_write")
+		sidecarArgs = append(sidecarArgs, "--local_cache_proxy.synchronous_write")
 		sidecarArgs = append(sidecarArgs, "--bes_synchronous")
 		args = append(args, "--bes_upload_mode=wait_for_upload_complete")
 	}
@@ -235,16 +301,16 @@ func ConfigureSidecar(args []string) []string {
 
 	var connectionErr error
 	for i := 0; i < numConnectionAttempts; i++ {
-		sidecarSocket, err := restartSidecarIfNecessary(ctx, cacheDir, sidecarArgs)
+		instance, err := restartSidecarIfNecessary(ctx, cacheDir, sidecarArgs)
 		if err != nil {
 			log.Warnf("Sidecar could not be initialized, continuing without sidecar: %s", err)
-			return args
+			return originalArgs, nil
 		}
-		if err := keepaliveSidecar(ctx, sidecarSocket); err != nil {
+		if err := keepaliveSidecar(ctx, instance.SockPath); err != nil {
 			// If we fail to connect, the sidecar might have been abruptly
 			// killed before this CLI invocation. Attempt to remove the socket
 			// and restart the sidecar before the next connection attempt.
-			if err := os.Remove(sidecarSocket); err != nil {
+			if err := os.Remove(instance.SockPath); err != nil {
 				log.Debugf("Failed to remove sidecar socket: %s", err)
 			}
 			connectionErr = err
@@ -252,19 +318,19 @@ func ConfigureSidecar(args []string) []string {
 			continue
 		}
 		if besBackendFlag != "" {
-			args = append(args, fmt.Sprintf("--bes_backend=unix://%s", sidecarSocket))
+			args = append(args, fmt.Sprintf("--bes_backend=unix://%s", instance.SockPath))
 		}
 		if remoteCacheFlag != "" && remoteExecFlag == "" {
-			args = append(args, fmt.Sprintf("--remote_cache=unix://%s", sidecarSocket))
+			args = append(args, fmt.Sprintf("--remote_cache=unix://%s", instance.SockPath))
 			// Set bytestream URI prefix to match the actual remote cache
 			// backend, rather than the sidecar socket.
 			instanceName := arg.Get(args, "remote_instance_name")
 			args = append(args, fmt.Sprintf("--remote_bytestream_uri_prefix=%s", bytestreamURIPrefix(remoteCacheFlag, instanceName)))
 		}
-		return args
+		return args, instance
 	}
 	log.Warnf("Could not connect to sidecar, continuing without sidecar: %s", connectionErr)
-	return args
+	return originalArgs, nil
 }
 
 func bytestreamURIPrefix(cacheTarget, instanceName string) string {

cli/test/integration/cli/cli_test.go
@@ -229,9 +229,12 @@ func TestBazelBuildWithBuildBuddyServices(t *testing.T) {
 
 	cmd := testcli.Command(t, ws, args...)
 
-	err = cmd.Run()
-
+	b, err := testcli.CombinedOutput(cmd)
 	require.NoError(t, err)
+
+	// Sidecar should not log any errors.
+	require.NotContains(t, string(b), "sidecar errors")
+
 	bbs := app.BuildBuddyServiceClient(t)
 
 	ctx := context.Background()

cli/versioncmd/versioncmd.go
@@ -38,7 +38,7 @@ func runBazelVersion(args []string) (int, error) {
 	// the command to bazel
 	args = append([]string{"version"}, args...)
 
-	plugins, bazelArgs, execArgs, err := setup.Setup(args, tempDir)
+	plugins, bazelArgs, execArgs, _, err := setup.Setup(args, tempDir)
 	if err != nil {
 		return 1, status.WrapError(err, "bazel setup")
 	}

server/cache_proxy/BUILD
@@ -14,7 +14,9 @@ go_library(
         "//server/remote_cache/cachetools",
         "//server/remote_cache/content_addressable_storage_server",
         "//server/remote_cache/digest",
+        "//server/util/bazel_request",
         "//server/util/log",
+        "//server/util/proto",
         "//server/util/status",
         "@org_golang_google_genproto_googleapis_bytestream//:bytestream",
         "@org_golang_google_grpc//:grpc",

commit eec83961737a91df81549f364f4d7dc26ae275f0

Fix --remote_header propagation in CLI local cache proxy (#7571)
Fixes a small regression from
https://github.com/buildbuddy-io/buildbuddy/pull/7563

The PR restructured some things so that we can use `log.Ctx*` to get
more helpful logs. But it introduced a bug - the previous code was
correctly propagating _all_ gRPC metadata to the remote cache (i.e. all
`--remote_header` values) but that PR made it so that we only propagate
the metadata allowed by the RPC interceptors (API key, bazel
requestmetadata, etc.)

server/cache_proxy/cache_proxy.go
@@ -417,6 +417,10 @@ func (qw *queueWorker) EnqueueRemoteWrite(ctx context.Context, wreq *bspb.WriteR
 }
 
 func (qw *queueWorker) RemoteWriteBlocked(ctx context.Context, wreq *bspb.WriteRequest) error {
+	// Copy all incoming metadata to outgoing RPCs.
+	md, _ := metadata.FromIncomingContext(ctx)
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
 	return qw.handleWriteRequest(ctx, wreq.GetResourceName())
 }
 