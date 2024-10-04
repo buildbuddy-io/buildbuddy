602d5c46a33137460ef31572b00600699d7d9743
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-10-04 19:42:53 +0200

    add first test

--- cli/explain/compactgraph/input_test.go
+++ cli/explain/compactgraph/input_test.go
@@ -1,6 +1,10 @@
 package compactgraph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPostOrder(t *testing.T) {
 	srcFile := &File{path: "pkg/file.txt"}
@@ -8,12 +12,28 @@ func TestPostOrder(t *testing.T) {
 	externalSrcFile := &File{path: "external/repo/pkg/file.txt"}
 	externalGenFile := &File{path: "bazel-out/k8-fastbuild/bin/external/repo/pkg/file.txt"}
 
-	set := &InputSet{
-		directEntries:  []Input{srcFile, externalGenFile},
-		transitiveSets: []*InputSet{{directEntries: []Input{genFile, externalSrcFile, srcFile, externalGenFile}}},
-	}
-	m := make(map[string]Input)
-	for runfilesPath, input := range runfilesMapping(set) {
-		m[runfilesPath] = input
+	for _, tc := range []struct {
+		name     string
+		set      depset
+		expected map[string]Input
+	}{
+		{
+			"duplicate artifact sandwiches other entry in post order",
+			&InputSet{
+				directEntries:  []Input{srcFile, externalGenFile},
+				transitiveSets: []*InputSet{{directEntries: []Input{srcFile, externalGenFile, genFile, externalSrcFile}}}},
+			map[string]Input{
+				"_main/pkg/file.txt": genFile,
+				"repo/pkg/file.txt":  externalSrcFile,
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := make(map[string]Input)
+			for runfilesPath, input := range runfilesMapping(tc.set) {
+				actual[runfilesPath] = input
+			}
+			assert.Equal(t, tc.expected, actual)
+		})
 	}
 }
