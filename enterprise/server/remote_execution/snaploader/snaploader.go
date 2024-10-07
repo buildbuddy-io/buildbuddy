53fa970b37de0a037290f6af148574e4e614dfcf
Maggie Lou <maggie@buildbuddy.io>
2024-10-07 16:32:30 -0500

    [CI] Add better snapshot fallback behavior for merge queues

--- enterprise/server/remote_execution/snaploader/snaploader.go
+++ enterprise/server/remote_execution/snaploader/snaploader.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -572,6 +573,8 @@ func (l *FileCacheLoader) UnpackSnapshot(ctx context.Context, snapshot *Snapshot
 }
 
 func (l *FileCacheLoader) CacheSnapshot(ctx context.Context, key *fcpb.SnapshotKey, opts *CacheSnapshotOptions) error {
+	key = generateMergeQueueSupportedKey(key)
+
 	vmConfig, err := anypb.New(opts.VMConfiguration)
 	if err != nil {
 		return err
@@ -657,6 +660,33 @@ func (l *FileCacheLoader) CacheSnapshot(ctx context.Context, key *fcpb.SnapshotK
 	// uploaded all snapshot related artifacts. We'll retrieve this later in
 	// order to unpack the snapshot.
 	return l.cacheActionResult(ctx, key, ar, opts)
+}
+
+// For merge queue branches, we should save the snapshot to the default branch (like `main`),
+// to support our fallback branch behavior.
+//
+// By default, if there is not a snapshot for the current branch,
+// we will fallback to a snapshot for the default branch.
+// If someone is using merge queues, they're unlikely to run CI on the default
+// branch itself, so there will never be a fallback branch hit. Instead, we should
+// fallback to the latest merge queue snapshot.
+func generateMergeQueueSupportedKey(key *fcpb.SnapshotKey) *fcpb.SnapshotKey {
+	snapshotBranch := key.GetRef()
+	if !strings.HasPrefix(snapshotBranch, "gh-readonly-queue") {
+		return key
+	}
+
+	newKey := key.CloneVT()
+	// We're expecting branches in the format `gh-readonly-queue/<default branch name>/<pr branch name>
+	splitBranch := strings.Split(snapshotBranch, "/")
+	if len(splitBranch) == 3 {
+		defaultBranch := splitBranch[1]
+		newKey.Ref = defaultBranch
+	} else {
+		log.Errorf("Unexpected merge queue branch name %s: expected 3 '/' separated parts", snapshotBranch)
+	}
+
+	return newKey
 }
 
 func (l *FileCacheLoader) cacheActionResult(ctx context.Context, key *fcpb.SnapshotKey, ar *repb.ActionResult, opts *CacheSnapshotOptions) error {
