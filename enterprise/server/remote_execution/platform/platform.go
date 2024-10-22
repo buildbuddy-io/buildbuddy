ce32b589d61810e9da153e3a41982083aaeec8bd
Zoey Greer <zoey@buildbuddy.io>
2024-10-22 08:24:41 -0700

    Use a multi-arch image for our ub22 image (#7772)

--- enterprise/server/remote_execution/platform/platform.go
+++ enterprise/server/remote_execution/platform/platform.go
@@ -44,7 +44,7 @@ const (
 
 	Ubuntu18_04WorkflowsImage     = "gcr.io/flame-public/buildbuddy-ci-runner@sha256:8cf614fc4695789bea8321446402e7d6f84f6be09b8d39ec93caa508fa3e3cfc"
 	Ubuntu20_04WorkflowsImage     = "gcr.io/flame-public/rbe-ubuntu20-04-workflows@sha256:c5092c8cb94471bb7c7fbd046cd80cb596dcc508f0833a748c035a1ba41f1681"
-	Ubuntu22_04WorkflowsImage     = "gcr.io/flame-public/rbe-ubuntu22-04@sha256:9cd09782d2c5b1949674d7f8d9cba589435b7346c1478dab62f933afba52bff9"
+	Ubuntu22_04WorkflowsImage     = "gcr.io/flame-public/rbe-ubuntu22-04@sha256:c7567f84d702b596c592d844adb0570526e262e60e2e1fe50c27177ee6d652ad"
 	Ubuntu20_04GitHubActionsImage = "gcr.io/flame-public/rbe-ubuntu20-04-github-actions@sha256:2a3b50fa1aafcb8446c94ab5707270f92fa91abd64a0e049312d4a086d0abb1c"
 
 	// overrideHeaderPrefix is a prefix used to override platform props via
