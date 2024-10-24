4acd734af225a54152cfe08abdb16de528167be9
Jim Hollenbach <jim@buildbuddy.io>
2024-10-24 10:34:13 -0700

    probably functioning, tests still broken

--- server/util/filter/filter.go
+++ server/util/filter/filter.go
@@ -5,6 +5,7 @@ import (
 	"slices"
 	"strings"
 
+	ispb "github.com/buildbuddy-io/buildbuddy/proto/invocation_status"
 	"github.com/buildbuddy-io/buildbuddy/proto/stat_filter"
 	"github.com/buildbuddy-io/buildbuddy/server/util/query_builder"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
@@ -234,6 +235,8 @@ func ValidateAndGenerateGenericFilterQueryStringAndArgs(f *stat_filter.GenericFi
 		} else {
 			return "", nil, status.InvalidArgumentErrorf("Invalid value for string filter: %s %s Value: %+v", f.GetType(), f.GetOperand(), v)
 		}
+	} else if typeOptions.GetCategory() == stat_filter.FilterCategory_STATUS_FILTER_CATEGORY {
+		return generateStatusFilterQueryStringAndArgs(f)
 	} else {
 		return "", nil, status.InternalErrorf("Unknown filter category: %s", typeOptions.GetCategory())
 	}
