a03d32a1fbc26a288b0cd62b35d46c572850e12f
Vanja Pejovic <vvaffle@gmail.com>
2024-12-02 16:15:17 -0500

    fix failing test.

--- enterprise/server/test/integration/remote_execution/remote_execution_test.go
+++ enterprise/server/test/integration/remote_execution/remote_execution_test.go
@@ -46,6 +46,7 @@ import (
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -197,9 +198,17 @@ func TestSimpleCommand_Timeout_StdoutStderrStillVisible(t *testing.T) {
 	assert.Equal(t, 1, int(taskCount-initialTaskCount), "unexpected number of tasks started")
 	execRes, err := execution.GetCachedExecuteResponse(ctx, rbe.GetActionResultStorageClient(), res.ID)
 	require.NoError(t, err)
+	// Not sure what will be in these fields, so check that they are non-nil
+	// and then ignore them.
+	assert.NotNil(t, execRes.GetResult().GetExecutionMetadata().GetIoStats())
+	assert.NotNil(t, execRes.GetResult().GetExecutionMetadata().GetUsageStats())
 	assert.Empty(
 		t,
-		cmp.Diff(res.ActionResult, execRes.GetResult(), protocmp.Transform()),
+		cmp.Diff(
+			res.ActionResult,
+			execRes.GetResult(),
+			protocmp.Transform(),
+			protocmp.IgnoreFields(new(repb.ExecutedActionMetadata), protoreflect.Name("io_stats"), protoreflect.Name("usage_stats"))),
 		"failed action result should match what was sent in the ExecuteResponse")
 }
 
