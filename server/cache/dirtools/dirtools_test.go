f66e55b1af99246e05e416dfb8797923186cb9cd
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-12-13 09:33:51 +0100

    TEST: Cause a test to fail

--- server/cache/dirtools/dirtools_test.go
+++ server/cache/dirtools/dirtools_test.go
@@ -77,7 +77,7 @@ func TestUploadTree(t *testing.T) {
 			},
 			expectedInfo: &dirtools.TransferInfo{
 				FileCount:        2,
-				BytesTransferred: 84,
+				BytesTransferred: 42,
 			},
 		},
 		{
