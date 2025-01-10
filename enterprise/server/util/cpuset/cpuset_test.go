2b37c904b580c04e7c9eebd768660e60da591b0b
Tyler Williams <williams.tyler@gmail.com>
2025-01-10 12:26:27 -0800

    Make cpuleaser numa-aware (#8156)
    Make the CPU Leaser numa aware (on linux).
    
    The leaser will prefer to assign a set of processors from the least
    loaded numa node. It will never return processors from multiple numa
    nodes in the same lease -- if more processors are requested than are
    available on a single node, it will return all the processors from the
    least loaded node.
    
    Obligatory https://www.youtube.com/watch?v=Cqd1Gvq-RBY

--- enterprise/server/util/cpuset/cpuset_test.go
+++ enterprise/server/util/cpuset/cpuset_test.go
@@ -43,7 +43,7 @@ func TestEvenDistributionUnderLoad(t *testing.T) {
 	counts := make(map[int]int, 4)
 	for i := 0; i < 100; i++ {
 		task := uuid.New()
-		cpus, cancel := cs.Acquire(3000, task)
+		cpus, cancel := cs.Acquire(1000, task)
 		for _, cpu := range cpus {
 			counts[cpu] += 1
 		}
@@ -51,7 +51,7 @@ func TestEvenDistributionUnderLoad(t *testing.T) {
 		defer cancel()
 	}
 	for _, count := range counts {
-		assert.Equal(t, 75, count)
+		assert.Equal(t, 25, count)
 	}
 }
 
@@ -103,4 +103,66 @@ func TestCPUSetDisabledManualCPUSet(t *testing.T) {
 	cpus1, cancel1 := cs.Acquire(1100, task1)
 	defer cancel1()
 	assert.Equal(t, []int{0, 1, 3}, cpus1)
+}
+
+func TestNumaNodeFairness(t *testing.T) {
+	flags.Set(t, "executor.cpu_leaser.enable", true)
+	flags.Set(t, "executor.cpu_leaser.overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.min_overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.cpuset", "0:0-1,1:4-7,0:2-3")
+
+	var allCPUs []int
+
+	cs, err := cpuset.NewLeaser()
+	require.NoError(t, err)
+	task1 := uuid.New()
+	cpus1, cancel1 := cs.Acquire(4000, task1)
+	defer cancel1()
+	allCPUs = append(allCPUs, cpus1...)
+
+	task2 := uuid.New()
+	cpus2, cancel2 := cs.Acquire(4000, task2)
+	defer cancel2()
+	allCPUs = append(allCPUs, cpus2...)
+
+	assert.ElementsMatch(t, []int{0, 1, 2, 3, 4, 5, 6, 7}, allCPUs)
+
+	task3 := uuid.New()
+	cpus3, cancel3 := cs.Acquire(8000, task3)
+	defer cancel3()
+	assert.Equal(t, len(cpus3), 4)
+}
+
+func TestNumaNodesAreNotSplit(t *testing.T) {
+	flags.Set(t, "executor.cpu_leaser.enable", true)
+	flags.Set(t, "executor.cpu_leaser.overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.min_overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.cpuset", "0:0-3,1:4-7")
+
+	cs, err := cpuset.NewLeaser()
+	require.NoError(t, err)
+	task1 := uuid.New()
+	cpus1, cancel1 := cs.Acquire(4000, task1)
+	defer cancel1()
+	assert.Equal(t, 4, len(cpus1))
+}
+
+func TestNonContiguousNumaNodes(t *testing.T) {
+	flags.Set(t, "executor.cpu_leaser.enable", true)
+	flags.Set(t, "executor.cpu_leaser.overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.min_overhead", 0)
+	flags.Set(t, "executor.cpu_leaser.cpuset", "3:0-3,9:4-7")
+
+	cs, err := cpuset.NewLeaser()
+	require.NoError(t, err)
+
+	task1 := uuid.New()
+	cpus1, cancel1 := cs.Acquire(4000, task1)
+	defer cancel1()
+
+	task2 := uuid.New()
+	cpus2, cancel2 := cs.Acquire(4000, task2)
+	defer cancel2()
+
+	assert.NotElementsMatch(t, cpus1, cpus2)
 }
