68374143923cd41392f0d39e568554a2e2107fc7
Zoey Greer <zoey@buildbuddy.io>
2025-01-15 08:37:35 -0800

    Use `flags-as-proto` to determine command line schema (take two) (#8155)
    We now use `bazel help flags-as-proto` as the primary method of
    determining the command line schema, only falling back to the
    usage-based parsing if the bazel version is too old to support
    `flags-as-proto`.
    
    Switched from using `BoolLike` to the more-specific `RequiresValue` and
    `HasNegative`, as those are what is present in the `FlagCollection`
    proto. They also allow us to tell the difference between boolean options
    (or boolean/enum options) and expansion options, which allows us more
    precision and accuracy in parsing: we can now remove values specified
    via `=` to expansion options (which improves our canonicalization).
    
    Added a test to confirm that `flags-as-proto` allows us to correctly
    handle undocumented options that are specified via `common` in a
    `bazelrc` file (which was the primary motivation for this change).
    
    Added a mock help function in the tests that uses `flags-as-proto` help
    and tested it everywhere the usage-style help was tested to ensure there
    were no regressions.
    
    Added some options to the canonicalization tests to ensure we are
    handling expansion options and options that use `BoolOrEnumConverter` in
    bazel (`--subcommands` and `--experimental_convenience_symlinks`)
    correctly.
    
    Added a test to ensure the options produced through `flags-as-proto`
    match up with those produced by parsing usage.
    
    ---------
    
    Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

--- cli/parser/test_data/test_data.go
+++ cli/parser/test_data/test_data.go
@@ -2,17 +2,20 @@ package test_data
 
 import _ "embed"
 
-//go:embed bazel5.3.1_help_startup_options.txt
+//go:embed bazel7.4.0_help_startup_options.txt
 var BazelHelpStartupOptionsOutput string
 
-//go:embed bazel5.3.1_help_build.txt
+//go:embed bazel7.4.0_help_build.txt
 var BazelHelpBuildOutput string
 
-//go:embed bazel5.3.1_help_run.txt
+//go:embed bazel7.4.0_help_run.txt
 var BazelHelpRunOutput string
 
-//go:embed bazel5.3.1_help_test.txt
+//go:embed bazel7.4.0_help_test.txt
 var BazelHelpTestOutput string
 
-//go:embed bazel5.3.1_help_query.txt
+//go:embed bazel7.4.0_help_query.txt
 var BazelHelpQueryOutput string
+
+//go:embed bazel7.4.0_help_flags-as-proto.b64
+var BazelHelpFlagsAsProtoOutput string
