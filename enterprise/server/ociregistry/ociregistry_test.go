189fb9cbdb6878290e5956af587fe5e6ad75dd32
Dan Stowell <dan@buildbuddy.io>
2025-01-22 10:09:28 -0600

    use atomic.Int32 intead of custom counter struct; v1 import alias -> gcr

--- enterprise/server/ociregistry/ociregistry_test.go
+++ enterprise/server/ociregistry/ociregistry_test.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"net/http"
 	"runtime"
-	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/ociregistry"
@@ -17,32 +17,14 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testport"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testregistry"
 	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	rgpb "github.com/buildbuddy-io/buildbuddy/proto/registry"
+	gcr "github.com/google/go-containerregistry/pkg/v1"
 )
 
-type requestCounter struct {
-	count int32
-	mu    sync.Mutex
-}
-
-func (c *requestCounter) Inc() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.count++
-}
-
-func withCounter(counter *requestCounter, handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		counter.Inc()
-		handler.ServeHTTP(w, r)
-	})
-}
-
-func runMirrorRegistry(t *testing.T, env environment.Env, counter *requestCounter) string {
+func runMirrorRegistry(t *testing.T, env environment.Env, counter *atomic.Int32) string {
 	t.Helper()
 	ocireg, err := ociregistry.New(env)
 	require.Nil(t, err)
@@ -53,7 +35,7 @@ func runMirrorRegistry(t *testing.T, env environment.Env, counter *requestCounte
 
 	listenAddr := fmt.Sprintf("localhost:%d", port)
 	server := &http.Server{Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		counter.Inc()
+		counter.Add(1)
 		ocireg.ServeHTTP(w, r)
 	})}
 	lis, err := net.Listen("tcp", listenAddr)
@@ -68,16 +50,16 @@ func runMirrorRegistry(t *testing.T, env environment.Env, counter *requestCounte
 func TestResolve(t *testing.T) {
 	te := testenv.GetTestEnv(t)
 
-	testregCounter := &requestCounter{}
+	testregCounter := atomic.Int32{}
 	testreg := testregistry.Run(t, testregistry.Opts{
 		HttpInterceptor: func(w http.ResponseWriter, r *http.Request) bool {
-			testregCounter.Inc()
+			testregCounter.Add(1)
 			return true
 		},
 	})
 	imageName, randomImage := testreg.PushRandomImage(t)
 
-	mirrorCounter := requestCounter{}
+	mirrorCounter := atomic.Int32{}
 	mirrorAddr := runMirrorRegistry(t, te, &mirrorCounter)
 
 	flags.Set(t, "executor.container_registry_mirrors", []oci.MirrorConfig{{
@@ -98,11 +80,11 @@ func TestResolve(t *testing.T) {
 	// One request to the mirror can generate multiple requests to the test registry.
 	// So at least make sure the mirror received some requests, and that the test registry
 	// received at least as many requests.
-	assert.Greater(t, mirrorCounter.count, int32(0))
-	assert.GreaterOrEqual(t, testregCounter.count, mirrorCounter.count)
+	assert.Greater(t, mirrorCounter.Load(), int32(0))
+	assert.GreaterOrEqual(t, testregCounter.Load(), mirrorCounter.Load())
 }
 
-func assertSameImages(t *testing.T, original, resolved v1.Image) {
+func assertSameImages(t *testing.T, original, resolved gcr.Image) {
 	originalImageDigest, err := original.Digest()
 	require.NoError(t, err)
 
