buildbuddy-io/buildbuddy

commit 260b7e71f375618ed539d12a23901c2e51349cab

Reland checkstyle/buildfix runfiles changes (#8226)
Reverts the revert
https://github.com/buildbuddy-io/buildbuddy/commit/d9d19f0c4bdc195611bf02638b4723abdf159246
with the following additional fixes:
* Use `bazel run` for everything in `buildfix.sh`.
* Always use a hermetic `clang-format` 15 binary (reusing the binaries
from the Angular project).
* Fix runfiles handling in prettier wrapper, which is called from
`//tools/checkstyle` after `cd`ing into a different directory.

---------

Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

tools/prettier/prettier.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Runs prettier on all files that differ from the main branch.
 
-set -e
+set -euo pipefail
+
+if [[ -z "${RUNFILES_DIR+x}" ]]; then
+  export RUNFILES_DIR="$PWD"/..
+fi
+
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY}" ]]; then
+  cd "$BUILD_WORKSPACE_DIRECTORY"
+fi
 
 : "${PRETTIER_PATH:=}"
 : "${DIFF_BASE:=}"
@@ -57,22 +65,14 @@ while read -r path; do
   paths+=("$path")
 done < <(paths_to_format)
 
-if [[ -z "${paths[*]}" ]]; then
+if [[ -z "${paths[*]+}" ]]; then
   exit 0
 fi
 
-# Run bazel quietly; see: https://github.com/bazelbuild/bazel/issues/4867#issuecomment-796208829
 if [[ "$PRETTIER_PATH" ]]; then
   PRETTIER_COMMAND=("$PRETTIER_PATH")
 else
-  tmp="$(mktemp -d)"
-  trap 'rm -rf "$tmp"' EXIT
-  bazel run @npm//prettier/bin:prettier --script_path="$tmp/run.sh" &>"$tmp/build.log" || {
-    cat "$tmp/build.log" >&2
-    exit 1
-  }
-  chmod +x "$tmp/run.sh"
-  PRETTIER_COMMAND=("$tmp/run.sh" --bazel_node_working_dir="$PWD")
+  PRETTIER_COMMAND=("${RUNFILES_DIR}/npm/prettier/bin/prettier.sh" --bazel_node_working_dir="$PWD")
 fi
 
 "${PRETTIER_COMMAND[@]}" "${paths[@]}" "$@"

.bazelrc
@@ -1,5 +1,6 @@
 common --noenable_bzlmod
 common --lockfile_mode=update
+common --nolegacy_external_runfiles
 
 common --enable_platform_specific_config=true
 

MODULE.bazel
@@ -488,6 +488,7 @@ use_repo(
     "org_kernel_git_linux_kernel-vmlinux",
     "org_kernel_git_linux_kernel-vmlinux-arm64",
     "org_llvm_llvm_clang-format_linux-x86_64",
+    "org_llvm_llvm_clang-format_macos-x86_64",
 )
 
 bazel_dep(name = "rules_oci", version = "2.0.0")

buildfix.sh
@@ -24,9 +24,6 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-c_yellow="\x1b[33m"
-c_reset="\x1b[0m"
-
 BAZEL_QUIET_FLAGS=(
   "--ui_event_filters=-info,-stdout,-stderr"
   "--noshow_progress"
@@ -43,21 +40,17 @@ while IFS= read -r line; do
 done < <(git ls-files '*.go')
 bazel run "${BAZEL_QUIET_FLAGS[@]}" //:gofmt -- -w "${GO_SRCS[@]}"
 
-if which clang-format &>/dev/null; then
-  echo "Formatting .proto files..."
-  protos=()
-  while read -r proto; do
-    protos+=("$proto")
-  done < <(git ls-files --exclude-standard | grep '\.proto$')
-  if [ ${#protos[@]} -gt 0 ]; then
-    clang-format -i --style=Google "${protos[@]}"
-  fi
-else
-  echo -e "${c_yellow}WARNING: Missing clang-format tool; will not format proto files.${c_reset}"
+echo "Formatting .proto files..."
+protos=()
+while read -r proto; do
+  protos+=("$PWD/$proto")
+done < <(git ls-files --exclude-standard | grep '\.proto$')
+if [ ${#protos[@]} -gt 0 ]; then
+  bazel run "${BAZEL_QUIET_FLAGS[@]}" //tools/clang-format -- -i --style=Google "${protos[@]}"
 fi
 
 echo "Formatting frontend and markup files with prettier..."
-./tools/prettier/prettier.sh --write
+bazel run "${BAZEL_QUIET_FLAGS[@]}" //tools/prettier -- --write
 
 if ((GO_DEPS)); then
   echo "Fixing go.mod, go.sum, deps.bzl, and MODULE.bazel..."

deps.bzl
@@ -6546,12 +6546,18 @@ def install_static_dependencies(workspace_name = "buildbuddy"):
         executable = True,
     )
 
-    # TODO: mac build
     http_file(
         name = "org_llvm_llvm_clang-format_linux-x86_64",
         executable = True,
-        sha256 = "85b1c2591274422234955e906aee39e6f793a88a74f3efc49a1852a0646ce08f",
-        urls = ["https://storage.googleapis.com/buildbuddy-tools/binaries/clang-format/clang-format-14_linux-x86_64"],
+        integrity = "sha256-BQxgAlbiJeq+lgjSj0kv6Gc8bn9d6sWcbalzIjx2TWw=",
+        urls = ["https://storage.googleapis.com/buildbuddy-tools/binaries/clang-format/clang-format-15_linux-x86_64"],
+    )
+
+    http_file(
+        name = "org_llvm_llvm_clang-format_macos-x86_64",
+        executable = True,
+        integrity = "sha256-lxFvZNl/socLSqKXWLuo+w/n87HtikvBL6qSfs/ewZY=",
+        urls = ["https://storage.googleapis.com/buildbuddy-tools/binaries/clang-format/clang-format-15_darwin-x86_64"],
     )
 
     http_file(

tools/checkstyle/BUILD
@@ -6,6 +6,6 @@ sh_binary(
         "//:gofmt",
         "//cli/cmd/bb",
         "//tools/clang-format",
-        "@npm//prettier/bin:prettier",
+        "//tools/prettier",
     ],
 )

tools/checkstyle/checkstyle.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-GO_PATH="$(readlink ./go)"
-GOFMT_PATH="$(readlink ./gofmt)"
-BB_PATH="$(readlink ./cli/cmd/bb/bb_/bb)"
-PRETTIER_PATH="$(readlink ./external/npm/prettier/bin/prettier.sh)"
-CLANG_FORMAT_PATH="$(readlink ./tools/clang-format/clang-format)"
+export RUNFILES_DIR=$(cd ../ && pwd)
+
+# Use absolute paths as we cd to the WORKSPACE directory below.
+GO_PATH="$(pwd)/go"
+GOFMT_PATH="$(pwd)/gofmt"
+BB_PATH="$(pwd)/cli/cmd/bb/bb_/bb"
+PRETTIER_PATH="$(pwd)/tools/prettier/prettier.sh"
+CLANG_FORMAT_PATH="$(pwd)/tools/clang-format/clang-format"
 
 # Make sure 'go' is in $PATH (gazelle depends on this).
 # TODO: set up the env properly to point to the bazel-provisioned SDK root
 export PATH="$PATH:$PWD"
-
-export RUNFILES_DIR=$(cd ../ && pwd)
-export RUNFILES="${RUNFILES_DIR}_manifest"
 
 cd "$BUILD_WORKSPACE_DIRECTORY"
 
@@ -58,8 +58,7 @@ run ProtoFormat \
   tools/clang-format/clang-format.sh --dry-run
 
 run PrettierFormat \
-  env PRETTIER_PATH="$PRETTIER_PATH" \
-  tools/prettier/prettier.sh --log-level=warn --check
+  "$PRETTIER_PATH" --log-level=warn --check
 
 wait
 

tools/clang-format/BUILD
@@ -1,9 +1,8 @@
 genrule(
     name = "clang-format_crossplatform",
     srcs = select({
-        # TODO: mac build
-        # "@bazel_tools//src/conditions:darwin": ["@org_llvm_llvm_clang-format_darwin-arm64//file:downloaded"],
-        "//conditions:default": ["@org_llvm_llvm_clang-format_linux-x86_64//file:downloaded"],
+        "@platforms//os:macos": ["@org_llvm_llvm_clang-format_macos-x86_64//file:downloaded"],
+        "@platforms//os:linux": ["@org_llvm_llvm_clang-format_linux-x86_64//file:downloaded"],
     }),
     outs = ["clang-format"],
     cmd_bash = "cp $(SRCS) $@",

commit 5c7671671059907205fe2101b04a59739a111d7e

Fix prettier.sh (#8303)
`prettier.sh` has been broken since 260b7e71f37 - the bash syntax `[[ -z
"${paths[*]+}" ]]` for checking whether an array is empty doesn't seem
to work.

app/invocation/cache_requests_card.tsx
@@ -462,7 +462,12 @@ export default class CacheRequestsCardComponent extends React.Component<CacheReq
                  In all other cases, this is a special cache access (e.g. for BES purposes) with
                  no link to an action.
                 */
-              if (looksLikeDigest(result.actionId) || result.actionId === "input" || result.actionId === "output" || result.actionId === "prefetcher") {
+              if (
+                looksLikeDigest(result.actionId) ||
+                result.actionId === "input" ||
+                result.actionId === "output" ||
+                result.actionId === "prefetcher"
+              ) {
                 if (groupTarget === null) {
                   name = result.targetId;
                   if (groupActionId === null) {
@@ -600,7 +605,13 @@ export default class CacheRequestsCardComponent extends React.Component<CacheReq
         {result.actionId && (
           <>
             <b>Action ID</b>
-            <span>{result.actionId === "input" ? "input (to local execution of this action)" : result.actionId === "output" ? "output (explicitly requested)" : result.actionId} </span>
+            <span>
+              {result.actionId === "input"
+                ? "input (to local execution of this action)"
+                : result.actionId === "output"
+                  ? "output (explicitly requested)"
+                  : result.actionId}{" "}
+            </span>
           </>
         )}
         {result.name ? (

enterprise/app/code/code_v2.tsx
@@ -265,21 +265,23 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
     req.decorationsRequest.diagnostics = true;
 
     let rsp = await rpcService.service.kytheProxy(req);
-    const newDecor = rsp.decorationsReply?.reference.map((x) => {
-      const startLine = x.span?.start?.lineNumber || 0;
-      const startColumn = x.span?.start?.columnOffset || 0;
-      const endLine = x.span?.end?.lineNumber || 0;
-      const endColumn = x.span?.end?.columnOffset || 0;
-      const monacoRange = new monaco.Range(startLine, startColumn + 1, endLine, endColumn + 1);
-      const displayOptions = this.getDisplayOptions(x);
-      if (displayOptions === null) {
-        return null;
-      }
-      return {
-        range: monacoRange,
-        options: displayOptions,
-      };
-    }).filter((x) => x !== null);
+    const newDecor = rsp.decorationsReply?.reference
+      .map((x) => {
+        const startLine = x.span?.start?.lineNumber || 0;
+        const startColumn = x.span?.start?.columnOffset || 0;
+        const endLine = x.span?.end?.lineNumber || 0;
+        const endColumn = x.span?.end?.columnOffset || 0;
+        const monacoRange = new monaco.Range(startLine, startColumn + 1, endLine, endColumn + 1);
+        const displayOptions = this.getDisplayOptions(x);
+        if (displayOptions === null) {
+          return null;
+        }
+        return {
+          range: monacoRange,
+          options: displayOptions,
+        };
+      })
+      .filter((x) => x !== null);
 
     // Paging @jdhollen
   }
@@ -421,7 +423,7 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
 
     if (this.currentPath()) {
       const url = new URL(window.location.href);
-      this.fetchIfNeededAndNavigate(this.currentPath(), "?"+url.searchParams.toString());
+      this.fetchIfNeededAndNavigate(this.currentPath(), "?" + url.searchParams.toString());
     } else {
       this.editor.setValue(
         ["// Welcome to BuildBuddy Code!", "", "// Click on a file to the left to get start editing."].join("\n")

enterprise/app/codesearch/codesearch.css
@@ -143,11 +143,11 @@
 }
 
 .code-search .statsForNerds {
-    color: #aaa;
-    font-size: 0.8em;
-    padding: 10px 10px 10px 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
+  color: #aaa;
+  font-size: 0.8em;
+  padding: 10px 10px 10px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 }

enterprise/app/codesearch/codesearch.tsx
@@ -40,7 +40,7 @@ export default class CodeSearchComponent extends React.Component<Props, State> {
       return;
     }
 
-    this.setState({ loading: true, response: undefined, inputText: this.getQuery()});
+    this.setState({ loading: true, response: undefined, inputText: this.getQuery() });
     rpcService.service
       .search(new search.SearchRequest({ query: new search.Query({ term: this.getQuery() }) }))
       .then((response) => {
@@ -49,10 +49,10 @@ export default class CodeSearchComponent extends React.Component<Props, State> {
       .catch((e) => {
         const parsedError = BuildBuddyError.parse(e);
         if (parsedError.code == "InvalidArgument") {
-	  this.setState({errorMessage: String(parsedError.description)});
+          this.setState({ errorMessage: String(parsedError.description) });
         } else {
           errorService.handleError(e);
-	}
+        }
       })
       .finally(() => this.setState({ loading: false }));
   }
@@ -131,7 +131,7 @@ export default class CodeSearchComponent extends React.Component<Props, State> {
           <div className="circle">
             <XCircle className="icon gray" />
             <h2>Invalid Search Query</h2>
-	    <p>{this.state.errorMessage}</p>
+            <p>{this.state.errorMessage}</p>
           </div>
         </div>
       );
@@ -162,7 +162,10 @@ export default class CodeSearchComponent extends React.Component<Props, State> {
           ))}
         </div>
         <div className="statsForNerds">
-          <span>Found {this.getStat("TOTAL_DOCS_SCORED_COUNT")} results ({(this.getStat("TOTAL_SEARCH_DURATION")/1e6).toFixed(2)}ms)</span>
+          <span>
+            Found {this.getStat("TOTAL_DOCS_SCORED_COUNT")} results (
+            {(this.getStat("TOTAL_SEARCH_DURATION") / 1e6).toFixed(2)}ms)
+          </span>
         </div>
       </div>
     );

enterprise/app/tap/flakes.tsx
@@ -366,8 +366,7 @@ export default class FlakesComponent extends React.Component<Props, State> {
                     target={targetLabel}
                     testSuite={testSuite}
                     testResult={s.event!.testResult!}
-                    dark={this.props.dark}
-                  ></TargetFlakyTestCardComponent>
+                    dark={this.props.dark}></TargetFlakyTestCardComponent>
                 );
               });
           } else if (testXmlDoc.testLogString) {
@@ -378,8 +377,7 @@ export default class FlakesComponent extends React.Component<Props, State> {
                 target={targetLabel}
                 logContents={testXmlDoc.testLogString}
                 testResult={s.event!.testResult!}
-                dark={this.props.dark}
-              ></FlakyTargetSampleLogCardComponent>
+                dark={this.props.dark}></FlakyTargetSampleLogCardComponent>
             );
           }
         })}
@@ -416,7 +414,7 @@ export default class FlakesComponent extends React.Component<Props, State> {
       );
     }
 
-    let tableData = singleTarget ? [] : this.state.tableData?.stats ?? [];
+    let tableData = singleTarget ? [] : (this.state.tableData?.stats ?? []);
     let sortFn: (a: target.AggregateTargetStats, b: target.AggregateTargetStats) => number;
     if (this.state.tableSort === "Flakes") {
       sortFn = (a, b) => {
@@ -490,8 +488,7 @@ export default class FlakesComponent extends React.Component<Props, State> {
         <TapEmptyStateComponent
           title="No flakes found!"
           message="Wow! Either you have no flaky CI tests, or no CI test data all. To see CI test data, make sure your CI tests are configured as follows:"
-          showV2Instructions={true}
-        ></TapEmptyStateComponent>
+          showV2Instructions={true}></TapEmptyStateComponent>
       );
     }
 
@@ -526,8 +523,7 @@ export default class FlakesComponent extends React.Component<Props, State> {
               }}
               formatXAxisLabel={(ts) => moment.unix(ts).format("MMM D")}
               formatHoverXAxisLabel={(ts) => moment.unix(ts).format("dddd, MMMM Do YYYY")}
-              ticks={[]}
-            ></TrendsChartComponent>
+              ticks={[]}></TrendsChartComponent>
           </div>
         </div>
         {tableData.length > 0 && (

tools/prettier/prettier.sh
@@ -3,13 +3,20 @@
 
 # --- begin runfiles.bash initialization v3 ---
 # Copy-pasted from the Bazel Bash runfiles library v3.
-set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$0.runfiles/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+set -uo pipefail
+set +e
+f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null ||
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null ||
+  source "$0.runfiles/$f" 2>/dev/null ||
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
+  {
+    echo >&2 "ERROR: cannot find $f"
+    exit 1
+  }
+f=
+set -e
 # --- end runfiles.bash initialization v3 ---
 
 if [[ -n "${BUILD_WORKSPACE_DIRECTORY}" ]]; then
@@ -58,7 +65,7 @@ function paths_to_format() {
   # not just changed files:
   # - The prettier version in yarn.lock
   # - Any prettier configuration in .prettierrc
-  if (git diff "$DIFF_BASE" -- yarn.lock | grep -q prettier) || (git diff "$DIFF_BASE" -- .prettierrc | grep -q .); then
+  if (git diff "$DIFF_BASE" -- yarn.lock | grep -q prettier) || (git diff "$DIFF_BASE" -- .prettierrc tools/prettier/prettier.sh | grep -q .); then
     git ls-files -- "${GIT_FILE_PATTERNS[@]}"
   else
     git diff --name-only --diff-filter=AMRCT "$DIFF_BASE" -- "${GIT_FILE_PATTERNS[@]}"
@@ -70,7 +77,7 @@ while read -r path; do
   paths+=("$path")
 done < <(paths_to_format)
 
-if [[ -z "${paths[*]+}" ]]; then
+if [[ ${#paths} -eq -0 ]]; then
   exit 0
 fi
 