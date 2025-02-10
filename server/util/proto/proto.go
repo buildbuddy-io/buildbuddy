d3f76ea758e0ddc4d0ab6ccafa95e869ede9c8e5
Son Luong Ngoc <sluongng@gmail.com>
2025-02-10 14:42:15 +0100

    feat: Use CodecV2 for proto encoding/decoding
    Route all proto encode/decode through encoding.CodecV2, which is
    implemented in vtprotocodec package. This means that proto encode/decode
    will attempt reuse the pre-allocated mem.DefaultBuffer as much as
    possible.
    
    Since we are copying these directly to []byte right now, there is not
    much benefit to this. We should investigate using mem.Buffer instead of
    []byte so that we can delay the materialization of the blob as late as
    possible.

--- server/util/proto/proto.go
+++ server/util/proto/proto.go
@@ -1,7 +1,12 @@
 package proto
 
 import (
+	"google.golang.org/grpc/encoding"
+	"google.golang.org/grpc/mem"
+
 	gproto "google.golang.org/protobuf/proto"
+
+	"github.com/buildbuddy-io/buildbuddy/server/util/vtprotocodec"
 )
 
 var Size = gproto.Size
@@ -20,36 +25,19 @@ var Bool = gproto.Bool
 type Message = gproto.Message
 type MarshalOptions = gproto.MarshalOptions
 
-type VTProtoMessage interface {
-	MarshalVT() ([]byte, error)
-	UnmarshalVT([]byte) error
-	CloneMessageVT() Message
-
-	// For vtprotoCodecV2
-	MarshalToSizedBufferVT(data []byte) (int, error)
-	SizeVT() int
-}
-
 func Marshal(v Message) ([]byte, error) {
-	vt, ok := v.(VTProtoMessage)
-	if ok {
-		return vt.MarshalVT()
-	}
-	return MarshalOld(v)
+	bs, err := encoding.GetCodecV2(vtprotocodec.Name).Marshal(v)
+	return bs.MaterializeToBuffer(mem.DefaultBufferPool()).ReadOnlyData(), err
 }
 
 func Unmarshal(b []byte, v Message) error {
-	vt, ok := v.(VTProtoMessage)
-	if ok {
-		return vt.UnmarshalVT(b)
-	}
-
-	return gproto.Unmarshal(b, v)
+	return encoding.
+		GetCodecV2(vtprotocodec.Name).
+		Unmarshal(mem.BufferSlice{mem.SliceBuffer(b)}, v)
 }
 
 func Clone(v Message) Message {
-	vt, ok := v.(VTProtoMessage)
-	if ok {
+	if vt, ok := v.(vtprotocodec.VTProtoMessage); ok {
 		return vt.CloneMessageVT()
 	}
 	return gproto.Clone(v)
