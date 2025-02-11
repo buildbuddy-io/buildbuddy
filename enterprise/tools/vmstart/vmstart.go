4f8515500416bf0ad9bd4463e70cb3790d237529
Brandon Duffany <brandon@buildbuddy.io>
2025-02-11 17:48:02 -0500

    Oops

--- enterprise/tools/vmstart/vmstart.go
+++ enterprise/tools/vmstart/vmstart.go
@@ -158,7 +158,7 @@ func main() {
 		log.Fatalf("Must be run as root: 'bazel run --run_under=sudo'")
 	}
 	flagutil.SetValueForFlagName("debug_enable_anonymous_runner_recycling", true, nil, false)
-	flagutil.SetValueForFlagName("debug_disable_cgroup", false, nil, false)
+	flagutil.SetValueForFlagName("debug_disable_cgroup", true, nil, false)
 	flagutil.SetValueForFlagName("executor.enable_local_snapshot_sharing", true, nil, false)
 	flagutil.SetValueForFlagName("executor.enable_remote_snapshot_sharing", true, nil, false)
 	flagutil.SetValueForFlagName("executor.remote_snapshot_readonly", true, nil, false)
