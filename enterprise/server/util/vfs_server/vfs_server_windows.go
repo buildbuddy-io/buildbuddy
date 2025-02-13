b56ee694d2f2df77bb974414c79c3baf0b92eabb
Vadim Berezniker <vadim@buildbuddy.io>
2025-02-12 16:45:20 -0800

    open some windows

--- enterprise/server/util/vfs_server/vfs_server_windows.go
+++ enterprise/server/util/vfs_server/vfs_server_windows.go
@@ -3,29 +3,15 @@
 package vfs_server
 
 import (
-	"context"
-	"io/fs"
-
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
 
-type LazyFile struct {
-	Path  string
-	Size  int64
-	Perms fs.FileMode
-}
-
-type CASLazyFileProvider struct {
-}
-
-func NewCASLazyFileProvider(env environment.Env, ctx context.Context, remoteInstanceName string, digestFunction repb.DigestFunction_Value, inputTree *repb.Tree) (*CASLazyFileProvider, error) {
-	panic("not implemented")
-}
-
 type Server struct {
 }
+
+func (s *Server) UpdateIOStats(stats *repb.IOStats) {}
 
 func New(env environment.Env, workspacePath string) *Server {
 	panic("not implemented")
