8f4b00001fced38fceac5f735ad9773f238a7a81
Brandon Duffany <brandon@buildbuddy.io>
2025-02-20 17:22:18 -0500

    Rename cache charts

--- enterprise/app/trends/trends.tsx
+++ enterprise/app/trends/trends.tsx
@@ -444,7 +444,7 @@ export default class TrendsComponent extends React.Component<Props, State> {
               />
 
               <TrendsChartComponent
-                title="Cache read throughput"
+                title="Cache download"
                 data={this.state.timeKeys}
                 dataSeries={[
                   {
@@ -479,7 +479,7 @@ export default class TrendsComponent extends React.Component<Props, State> {
               />
 
               <TrendsChartComponent
-                title="Cache write throughput"
+                title="Cache upload"
                 data={this.state.timeKeys}
                 dataSeries={[
                   {
