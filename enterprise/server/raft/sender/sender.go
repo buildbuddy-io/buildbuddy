cf7414f685a1131bbb194b5d87c2a5e1f171d8d2
Lulu Zhang <lulu@buildbuddy.io>
2025-02-21 14:26:31 -0800

    [raft] record error in tracing

--- enterprise/server/raft/sender/sender.go
+++ enterprise/server/raft/sender/sender.go
@@ -18,6 +18,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/buildbuddy-io/buildbuddy/server/util/tracing"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 
 	rfpb "github.com/buildbuddy-io/buildbuddy/proto/raft"
 	rfspb "github.com/buildbuddy-io/buildbuddy/proto/raft_service"
@@ -163,9 +164,15 @@ func (s *Sender) fetchRangeDescriptorFromMetaRange(ctx context.Context, key []by
 	return nil, status.UnavailableErrorf("Error finding range descriptor for %q", key)
 }
 
-func (s *Sender) LookupRangeDescriptor(ctx context.Context, key []byte, skipCache bool) (*rfpb.RangeDescriptor, error) {
+func (s *Sender) LookupRangeDescriptor(ctx context.Context, key []byte, skipCache bool) (returnedRD *rfpb.RangeDescriptor, returnedErr error) {
 	ctx, spn := tracing.StartSpan(ctx) // nolint:SA4006
-	defer spn.End()
+	defer func() {
+		if returnedErr != nil {
+			spn.RecordError(returnedErr)
+			spn.SetStatus(codes.Error, returnedErr.Error())
+		}
+		spn.End()
+	}()
 	if len(key) == 0 {
 		return nil, status.FailedPreconditionError("A non-nil key is required")
 	}
@@ -173,6 +180,7 @@ func (s *Sender) LookupRangeDescriptor(ctx context.Context, key []byte, skipCach
 	if rangeDescriptor == nil || skipCache {
 		rd, err := s.fetchRangeDescriptorFromMetaRange(ctx, key)
 		if err != nil {
+			spn.RecordError(err)
 			return nil, err
 		}
 		s.rangeCache.UpdateRange(rd)
@@ -269,8 +277,12 @@ func (s *Sender) TryReplicas(ctx context.Context, rd *rfpb.RangeDescriptor, fn r
 
 	logs := []string{}
 	defer func() {
-		if returnedErr != nil && len(logs) > 0 {
-			log.CtxDebug(ctx, strings.Join(logs, "\n"))
+		if returnedErr != nil {
+			spn.RecordError(returnedErr)
+			spn.SetStatus(codes.Error, returnedErr.Error())
+			if len(logs) > 0 {
+				log.CtxDebugf(ctx, "failed to TryReplicas: %s. Detailed logs: %s", returnedErr, strings.Join(logs, "\n"))
+			}
 		}
 	}()
 	for i, replica := range rd.GetReplicas() {
@@ -354,9 +366,15 @@ func WithConsistencyMode(mode rfpb.Header_ConsistencyMode) Option {
 // cache and try the fn again with the new replica information. If the
 // fn succeeds with the new replicas, the range cache will be updated with
 // the new ownership information.
-func (s *Sender) run(ctx context.Context, key []byte, fn runFunc, mods ...Option) error {
+func (s *Sender) run(ctx context.Context, key []byte, fn runFunc, mods ...Option) (returnedErr error) {
 	ctx, spn := tracing.StartSpan(ctx) // nolint:SA4006
-	defer spn.End()
+	defer func() {
+		if returnedErr != nil {
+			spn.RecordError(returnedErr)
+			spn.SetStatus(codes.Error, returnedErr.Error())
+		}
+		spn.End()
+	}()
 	opts := defaultOptions()
 	for _, mod := range mods {
 		mod(opts)
