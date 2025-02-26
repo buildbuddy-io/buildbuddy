aaafb3c21402ff3336229daeb99ee893098367a8
Son Luong Ngoc <sluongng@gmail.com>
2025-02-26 18:53:17 +0100

    pebble_cache: add a flag to enable auto rachet
    PebbleDB comes with multiple on-disk format versions(0). If we don't
    specify a FormatMajorVersion inside pebble.Options when we initialize
    the DB, the default format is FormatMostCompatible, the second oldest
    version available.
    
    To prepare for a future upgrade to PebbleV2(1), we want to upgrade all
    DBs to the latest on-disk format available.  By setting
    `FormatMostCompatible: FormatNewest`, the `pebble.Open()` func will
    automatically migrate the on disk format to the latest version.
    
    The migrations are done in a backward compatible manner (2).  If there
    are any errors during the migration, the existing DB should not be
    affected.  Despite that, safeguard the migration with a feature flag,
    disabled by default, so that we can test this in dev first.
    
    Add a test to verify the expected behvior of before/after rachetting.
    This test should be extended in the future to accomodate for the V2
    upgrade (1).
    
    (0): https://github.com/cockroachdb/pebble/blob/53918335bb8c71a6420644e86d66f4f4a3ccf38f/format_major_version.go#L50
    (1): https://github.com/buildbuddy-io/buildbuddy/pull/8488
    (2): https://github.com/cockroachdb/pebble/blob/53918335bb8c71a6420644e86d66f4f4a3ccf38f/format_major_version.go#L244

--- enterprise/server/backends/pebble_cache/pebble_cache.go
+++ enterprise/server/backends/pebble_cache/pebble_cache.go
@@ -85,6 +85,7 @@ var (
 	evictionRateLimit         = flag.Int("cache.pebble.eviction_rate_limit", 300, "Maximum number of entries to evict per second (per partition).")
 	includeMetadataSize       = flag.Bool("cache.pebble.include_metadata_size", false, "If true, include metadata size")
 	enableTableBloomFilter    = flag.Bool("cache.pebble.enable_table_bloom_filter", false, "If true, write bloom filter data with pebble SSTables.")
+	enableAutoRachet          = flag.Bool("cache.pebble.enable_auto_rachet", false, "If true, automatically upgrade on-disk format to latest version.")
 
 	activeKeyVersion  = flag.Int64("cache.pebble.active_key_version", int64(filestore.UnspecifiedKeyVersion), "The key version new data will be written with. If negative, will write to the highest existing version in the database, or the highest known version if a new database is created.")
 	migrationQPSLimit = flag.Int("cache.pebble.migration_qps_limit", 50, "QPS limit for data version migration")
@@ -487,6 +488,9 @@ func defaultPebbleOptions(mc *pebble.MetricsCollector) *pebble.Options {
 				FilterPolicy: pebble.BloomFilterPolicy(10),
 			},
 		}
+	}
+	if *enableAutoRachet {
+		opts.FormatMajorVersion = pebble.FormatNewest
 	}
 
 	// The threshold of L0 read-amplification at which compaction concurrency
