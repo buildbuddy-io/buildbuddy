14362068c0e223196d0942d16da4719b1f3a46c0
Vanja Pejovic <vanja@buildbuddy.io>
2025-03-10 10:57:41 -0400

    fix race and remove extra logging

--- enterprise/server/remote_execution/vbd/vbd.go
+++ enterprise/server/remote_execution/vbd/vbd.go
@@ -135,14 +135,7 @@ func (f *FS) Unmount(ctx context.Context) error {
 		case resultCh <- err:
 			// Since resultCh is unbuffered, this only happens when the outer
 			// function received from the channel and will return this err.
-			if err != nil {
-				log.CtxErrorf(ctx, "Failed to unmount %s: %s", f.mountPath, err)
-			} else {
-				log.CtxDebugf(ctx, "Unmounted %s", f.mountPath)
-			}
-		default:
-			// Nobodoy is listening on resultCh, which means the context
-			// expired.
+		case <-ctx.Done():
 			if err != nil {
 				log.CtxErrorf(ctx, "Failed to unmount %s in the background after context was cancelled: %s", f.mountPath, err)
 			} else {
