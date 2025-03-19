11fc6bd3ce40bc55a7b79280cbd40af9d7290e33
Jim Hollenbach <jim@buildbuddy.io>
2025-03-19 12:47:08 -0700

    this mostly works except for a failing test.  gonna change strategies

--- enterprise/server/action_cache_server_proxy/action_cache_server_proxy_test.go
+++ enterprise/server/action_cache_server_proxy/action_cache_server_proxy_test.go
@@ -2,19 +2,25 @@ package action_cache_server_proxy
 
 import (
 	"context"
+	"log"
 	"strings"
 	"testing"
 
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/action_cache_server"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testauth"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
 	"github.com/stretchr/testify/require"
 
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
 
-func runACServer(ctx context.Context, t *testing.T) repb.ActionCacheClient {
+func runACServer(ctx context.Context, t *testing.T, ta *testauth.TestAuthenticator) repb.ActionCacheClient {
 	env := testenv.GetTestEnv(t)
+	env.SetAuthenticator(ta)
+
 	acServer, err := action_cache_server.NewActionCacheServer(env)
 	require.NoError(t, err)
 	grpcServer, runFunc, lis := testenv.RegisterLocalGRPCServer(t, env)
@@ -26,8 +32,9 @@ func runACServer(ctx context.Context, t *testing.T) repb.ActionCacheClient {
 	return repb.NewActionCacheClient(conn)
 }
 
-func runACProxy(ctx context.Context, t *testing.T, client repb.ActionCacheClient) repb.ActionCacheClient {
+func runACProxy(ctx context.Context, t *testing.T, ta *testauth.TestAuthenticator, client repb.ActionCacheClient) repb.ActionCacheClient {
 	env := testenv.GetTestEnv(t)
+	env.SetAuthenticator(ta)
 	env.SetActionCacheClient(client)
 	proxyServer, err := NewActionCacheServerProxy(env)
 	require.NoError(t, err)
@@ -60,10 +67,23 @@ func get(ctx context.Context, client repb.ActionCacheClient, digest *repb.Digest
 	return resp
 }
 
+func getCached(ctx context.Context, client repb.ActionCacheClient, digest *repb.Digest, cachedValue *repb.Digest, t *testing.T) *repb.ActionResult {
+	req := &repb.GetActionResultRequest{
+		ActionDigest:             digest,
+		DigestFunction:           repb.DigestFunction_SHA256,
+		CachedActionResultDigest: cachedValue,
+	}
+	resp, err := client.GetActionResult(ctx, req)
+	require.NoError(t, err)
+	return resp
+}
+
 func TestActionCacheProxy(t *testing.T) {
+	flags.Set(t, "cache_proxy.cache_action_results", false)
 	ctx := context.Background()
-	ac := runACServer(ctx, t)
-	proxy := runACProxy(ctx, t, ac)
+	ta := testauth.NewTestAuthenticator(testauth.TestUsers())
+	ac := runACServer(ctx, t, ta)
+	proxy := runACProxy(ctx, t, ta, ac)
 
 	digestA := &repb.Digest{
 		Hash:      strings.Repeat("a", 64),
@@ -107,6 +127,75 @@ func TestActionCacheProxy(t *testing.T) {
 	update(ctx, ac, digestB, 999, t)
 	require.Equal(t, int32(999), get(ctx, ac, digestB, t).GetExitCode())
 	require.Equal(t, int32(999), get(ctx, proxy, digestB, t).GetExitCode())
+
+	// Change the action result, write it, and confirm the new result is
+	// readable from the proxy and backing cache.
+	update(ctx, ac, digestB, 998, t)
+	require.Equal(t, int32(998), get(ctx, ac, digestB, t).GetExitCode())
+	require.Equal(t, int32(998), get(ctx, proxy, digestB, t).GetExitCode())
+}
+
+func TestActionCacheProxy_CachingEnabled(t *testing.T) {
+	flags.Set(t, "cache_proxy.cache_action_results", true)
+	flags.Set(t, "cache.check_client_action_result_digests", true)
+
+	env := testenv.GetTestEnv(t)
+	ta := testauth.NewTestAuthenticator(testauth.TestUsers("user", "GR123"))
+	env.SetAuthenticator(ta)
+	ctx, err := ta.WithAuthenticatedUser(context.Background(), "user")
+	require.NoError(t, err)
+
+	ac := runACServer(ctx, t, ta)
+	proxy := runACProxy(ctx, t, ta, ac)
+
+	digestA := &repb.Digest{
+		Hash:      strings.Repeat("a", 64),
+		SizeBytes: 1024,
+	}
+	digestB := &repb.Digest{
+		Hash:      strings.Repeat("b", 64),
+		SizeBytes: 1024,
+	}
+
+	// DigestA shouldn't be present initially.
+	readReqA := &repb.GetActionResultRequest{
+		ActionDigest:   digestA,
+		DigestFunction: repb.DigestFunction_SHA256,
+	}
+	r, err := proxy.GetActionResult(ctx, readReqA)
+	log.Printf("%v", r)
+	log.Printf("%v", err)
+	require.True(t, status.IsNotFoundError(err))
+
+	// Write it through the proxy and confirm it's readable from the proxy and
+	// backing cache.
+	update(ctx, proxy, digestA, 1, t)
+	require.Equal(t, int32(1), get(ctx, ac, digestA, t).GetExitCode())
+	require.Equal(t, int32(1), get(ctx, proxy, digestA, t).GetExitCode())
+
+	// Change the action result, write it, and confirm the new result is
+	// readable from the proxy and backing cache.
+	update(ctx, proxy, digestA, 2, t)
+	require.Equal(t, int32(2), get(ctx, ac, digestA, t).GetExitCode())
+	require.Equal(t, int32(2), get(ctx, proxy, digestA, t).GetExitCode())
+
+	// DigestB shouldn't be present initially,
+	readReqB := &repb.GetActionResultRequest{
+		ActionDigest:   digestB,
+		DigestFunction: repb.DigestFunction_SHA256,
+	}
+	_, err = proxy.GetActionResult(ctx, readReqB)
+	require.True(t, status.IsNotFoundError(err))
+
+	// Cache shouldn't act like it has DigestB when specifying a cache value, either.
+
+	// Write it to the backing cache and confirm it's readable from the proxy
+	// and backing cache.
+	update(ctx, ac, digestB, 999, t)
+	require.Equal(t, int32(999), get(ctx, ac, digestB, t).GetExitCode())
+	require.Equal(t, int32(999), get(ctx, proxy, digestB, t).GetExitCode())
+	d, _ := digest.ComputeForMessage(getCached(ctx, proxy, digestB, nil, t), repb.DigestFunction_SHA256)
+	log.Printf("%v", d)
 
 	// Change the action result, write it, and confirm the new result is
 	// readable from the proxy and backing cache.
