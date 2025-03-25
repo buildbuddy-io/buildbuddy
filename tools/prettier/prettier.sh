41f8e3e665b74d10b8e599f509a45f3f7dc0d151
Son Luong Ngoc <sluongng@gmail.com>
2025-03-25 14:40:56 +0100

    Reapply "Migrate to rules_js" (#8754)
    This reverts commit b7a969c12ac18b16752815b659a5004f0b59db9d.

--- tools/prettier/prettier.sh
+++ tools/prettier/prettier.sh
@@ -86,7 +86,7 @@ fi
 if [[ "$PRETTIER_PATH" ]]; then
   PRETTIER_COMMAND=("$PRETTIER_PATH")
 else
-  PRETTIER_COMMAND=("$(rlocation npm/prettier/bin/prettier.sh)" --bazel_node_working_dir="$PWD")
+  PRETTIER_COMMAND=("$(rlocation buildbuddy/tools/prettier/prettier_bin_/prettier_bin)")
 fi
 
-"${PRETTIER_COMMAND[@]}" "${paths[@]}" "$@"
+BAZEL_BINDIR=. "${PRETTIER_COMMAND[@]}" "${paths[@]}" "$@"
