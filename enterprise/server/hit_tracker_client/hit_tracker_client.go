630fe531cd31313bd6827c2269058c35cda52025
Iain Macdonald <xiainx@gmail.com>
2025-03-27 16:33:12 -0700

    Initial implementation of the hit_tracker_client

--- enterprise/server/hit_tracker_client/hit_tracker_client.go
+++ enterprise/server/hit_tracker_client/hit_tracker_client.go
@@ -2,25 +2,44 @@ package hit_tracker_client
 
 import (
 	"context"
+	"flag"
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/durationpb"
 
+	hitpb "github.com/buildbuddy-io/buildbuddy/proto/hit_tracker"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+	rspb "github.com/buildbuddy-io/buildbuddy/proto/resource"
 )
 
+var (
+	remoteHitTrackerTarget  = flag.String("cache.hit_tracker.target", "", "The gRPC target of the remote cache-hit-tracker.")
+	remoteHitTrackerTimeout = flag.Duration("cache.hit_tracker.rpc_timeout", 5*time.Second, "The timout to use for gRPC requests to the remote cache-hit-tracker.")
+)
+
+func Register(env *real_environment.RealEnv) error {
+	conn, err := grpc_client.DialInternal(env, *remoteHitTrackerTarget)
+	if err != nil {
+		return err
+	}
+	env.SetHitTrackerFactory(newHitTrackerClient(conn))
+	return nil
+}
+
+func newHitTrackerClient(conn grpc.ClientConnInterface) *HitTrackerFactory {
+	return &HitTrackerFactory{client: hitpb.NewHitTrackerServiceClient(conn)}
+}
+
 type HitTrackerFactory struct {
-	env environment.Env
-}
-
-func Register(env *real_environment.RealEnv) {
-	env.SetHitTrackerFactory(HitTrackerFactory{})
-}
-
-// TODO(iain): hook this up with a to-be-added RPC service in the app.
-type HitTracker struct {
+	env    environment.Env
+	client hitpb.HitTrackerServiceClient
 }
 
 func (h HitTrackerFactory) NewACHitTracker(ctx context.Context, invocationID string, requestMetadata *repb.RequestMetadata) interfaces.HitTracker {
@@ -31,32 +50,129 @@ func (h HitTrackerFactory) NewCASHitTracker(ctx context.Context, invocationID st
 	return &HitTracker{}
 }
 
-func (h *HitTracker) SetExecutedActionMetadata(md *repb.ExecutedActionMetadata) {
+// For Action Cache hit-tracking, explicitly ignore everything. These cache
+// artifacts should always be served from the authoritative cache which will
+// take care of hit-tracking.
+type NoOpHitTracker struct {
 }
 
-func (h *HitTracker) TrackMiss(d *repb.Digest) error {
+func (h *NoOpHitTracker) SetExecutedActionMetadata(md *repb.ExecutedActionMetadata) {
+}
+
+func (h *NoOpHitTracker) TrackMiss(d *repb.Digest) error {
 	return nil
 }
 
-func (h *HitTracker) TrackEmptyHit() error {
+func (h *NoOpHitTracker) TrackEmptyHit() error {
+	return nil
+}
+
+func (h *NoOpHitTracker) TrackDownload(d *repb.Digest) interfaces.TransferTimer {
+	return &NoOpTransferTimer{}
+}
+
+func (h *NoOpHitTracker) TrackUpload(d *repb.Digest) interfaces.TransferTimer {
+	return &NoOpTransferTimer{}
+}
+
+type NoOpTransferTimer struct {
+}
+
+func (t *NoOpTransferTimer) CloseWithBytesTransferred(bytesTransferredCache, bytesTransferredClient int64, compressor repb.Compressor_Value, serverLabel string) error {
+	return nil
+}
+
+func (t *NoOpTransferTimer) Record(bytesTransferred int64, duration time.Duration, compressor repb.Compressor_Value) error {
+	return nil
+}
+
+// For CAS hit-tracking, use a hit-tracker that sends information about local
+// cache hits to the RPC service at the configured backend.
+type HitTrackerClient struct {
+	ctx          context.Context
+	client       hitpb.HitTrackerServiceClient
+	invocationID string
+}
+
+// This should only be used for Action Cache hit tracking.
+func (h *HitTrackerClient) SetExecutedActionMetadata(md *repb.ExecutedActionMetadata) {
+	log.Warning("Unexpected call to SetExecutedActionMetadata")
+}
+
+// Misses hit the backing cache, so no need to report on hit-tracking.
+func (h *HitTrackerClient) TrackMiss(d *repb.Digest) error {
+	return nil
+}
+
+// TODO(iain): implement some client-side batching here.
+func sendAsync(ctx context.Context, client hitpb.HitTrackerServiceClient, req *hitpb.TrackRequest) {
+	go func() {
+		ctx, cancel := context.WithTimeout(ctx, *remoteHitTrackerTimeout)
+		_, err := client.Track(ctx, req)
+		cancel()
+		if err != nil {
+			log.Infof("Error sending hit_tracker.Track RPC: %v", err)
+		}
+	}()
+}
+
+func (h *HitTrackerClient) TrackEmptyHit() error {
+	req := hitpb.TrackRequest{
+		Hits: []*hitpb.CacheHits{
+			&hitpb.CacheHits{
+				InvocationId: h.invocationID,
+				EmptyHits:    1,
+			},
+		},
+	}
+	sendAsync(h.ctx, h.client, &req)
 	return nil
 }
 
 type TransferTimer struct {
+	ctx          context.Context
+	invocationID string
+	digest       *repb.Digest
+	start        time.Time
+	client       hitpb.HitTrackerServiceClient
 }
 
 func (t *TransferTimer) CloseWithBytesTransferred(bytesTransferredCache, bytesTransferredClient int64, compressor repb.Compressor_Value, serverLabel string) error {
+	download := &hitpb.Download{
+		Resource: &rspb.ResourceName{
+			Digest:    t.digest,
+			CacheType: rspb.CacheType_CAS,
+		},
+		SizeBytes: bytesTransferredClient,
+		Duration:  durationpb.New(time.Since(t.start)),
+	}
+	req := hitpb.TrackRequest{
+		Hits: []*hitpb.CacheHits{
+			&hitpb.CacheHits{
+				InvocationId: t.invocationID,
+				Downloads:    []*hitpb.Download{download},
+			},
+		},
+	}
+	sendAsync(t.ctx, t.client, &req)
 	return nil
 }
 
 func (t *TransferTimer) Record(bytesTransferred int64, duration time.Duration, compressor repb.Compressor_Value) error {
-	return nil
+	return status.InternalError("Unxpected call to hit_tracker_client.Record()")
 }
 
-func (h *HitTracker) TrackDownload(d *repb.Digest) interfaces.TransferTimer {
-	return &TransferTimer{}
+func (h *HitTrackerClient) TrackDownload(digest *repb.Digest) interfaces.TransferTimer {
+	return &TransferTimer{
+		ctx:          h.ctx,
+		invocationID: h.invocationID,
+		digest:       digest,
+		start:        time.Now(),
+		client:       h.client,
+	}
 }
 
-func (h *HitTracker) TrackUpload(d *repb.Digest) interfaces.TransferTimer {
-	return &TransferTimer{}
+// Writes hit the backing cache, so no need to report on hit-tracking.
+func (h *HitTrackerClient) TrackUpload(digest *repb.Digest) interfaces.TransferTimer {
+	return &NoOpTransferTimer{}
 }
--- enterprise/server/hit_tracker_client/hit_tracker_client_test.go
+++ enterprise/server/hit_tracker_client/hit_tracker_client_test.go
@@ -1,15 +1,120 @@
-package hit_tracker_client_test
+package hit_tracker_client
 
 import (
+	"context"
+	"strings"
+	"sync/atomic"
 	"testing"
+	"time"
 
-	"github.com/buildbuddy-io/buildbuddy/enterprise/server/hit_tracker_client"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/stretchr/testify/require"
+
+	hitpb "github.com/buildbuddy-io/buildbuddy/proto/hit_tracker"
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 )
 
-func TestHitTrackerClient(t *testing.T) {
+var (
+	digestA = &repb.Digest{
+		Hash:      strings.Repeat("a", 64),
+		SizeBytes: 123,
+	}
+	digestB = &repb.Digest{
+		Hash:      strings.Repeat("b", 64),
+		SizeBytes: 234,
+	}
+	digestC = &repb.Digest{
+		Hash:      strings.Repeat("c", 64),
+		SizeBytes: 345,
+	}
+)
+
+type testHitTracker struct {
+	t               *testing.T
+	requests        atomic.Int64
+	emptyHits       atomic.Int64
+	downloads       atomic.Int64
+	bytesDownloaded atomic.Int64
+}
+
+func newTestHitTracker(t *testing.T) *testHitTracker {
+	return &testHitTracker{
+		t:               t,
+		requests:        atomic.Int64{},
+		emptyHits:       atomic.Int64{},
+		downloads:       atomic.Int64{},
+		bytesDownloaded: atomic.Int64{},
+	}
+}
+
+func (ht *testHitTracker) Track(ctx context.Context, req *hitpb.TrackRequest) (*hitpb.TrackResponse, error) {
+	for _, cacheHit := range req.GetHits() {
+		if cacheHit.GetInvocationId() == "" {
+			log.Warning("Skipping hit-tracking for empty invocation ID")
+			continue
+		}
+
+		ht.emptyHits.Add(cacheHit.GetEmptyHits())
+		ht.downloads.Add(int64(len(cacheHit.GetDownloads())))
+		for _, download := range cacheHit.GetDownloads() {
+			ht.bytesDownloaded.Add(download.GetSizeBytes())
+		}
+	}
+
+	// Increment this at the end so that we know when requests are finished.
+	ht.requests.Add(1)
+	return &hitpb.TrackResponse{}, nil
+}
+
+func setup(t *testing.T) (*HitTrackerFactory, *testHitTracker) {
 	te := testenv.GetTestEnv(t)
-	hit_tracker_client.Register(te)
-	require.NotNil(t, te.GetHitTrackerFactory())
+	hitTrackerService := newTestHitTracker(t)
+	grpcServer, runServer, lis := testenv.RegisterLocalGRPCServer(t, te)
+	hitpb.RegisterHitTrackerServiceServer(grpcServer, hitTrackerService)
+	go runServer()
+	conn, err := testenv.LocalGRPCConn(context.Background(), lis)
+	require.NoError(t, err)
+	t.Cleanup(func() { conn.Close() })
+	return newHitTrackerClient(conn), hitTrackerService
+}
+
+func TestACHitTracker(t *testing.T) {
+	hitTrackerFactory, hitTrackerService := setup(t)
+	ctx := context.Background()
+	hitTracker := hitTrackerFactory.NewACHitTracker(ctx, "")
+	hitTracker.TrackMiss(digestA)
+	hitTracker.TrackEmptyHit()
+	hitTracker.TrackDownload(digestB).CloseWithBytesTransferred(1000, 2000, repb.Compressor_IDENTITY, "test")
+	hitTracker.TrackUpload(digestC).CloseWithBytesTransferred(3000, 4000, repb.Compressor_IDENTITY, "test")
+
+	time.Sleep(100 * time.Millisecond)
+	require.Equal(t, int64(0), hitTrackerService.requests.Load())
+}
+
+func TestCASHitTracker(t *testing.T) {
+	hitTrackerFactory, hitTrackerService := setup(t)
+	ctx := context.Background()
+	hitTracker := hitTrackerFactory.NewCASHitTracker(ctx, "iid")
+	hitTracker.TrackMiss(digestA)
+	hitTracker.TrackEmptyHit()
+	hitTracker.TrackDownload(digestB).CloseWithBytesTransferred(1000, 2000, repb.Compressor_IDENTITY, "test")
+	hitTracker.TrackUpload(digestC).CloseWithBytesTransferred(3000, 4000, repb.Compressor_IDENTITY, "test")
+
+	expectToEqual(t, 2, &hitTrackerService.requests, "Expected 2 HitTracker.Track requests")
+	require.Equal(t, int64(1), hitTrackerService.emptyHits.Load())
+	require.Equal(t, int64(1), hitTrackerService.downloads.Load())
+	require.Equal(t, int64(2000), hitTrackerService.bytesDownloaded.Load())
+}
+
+func expectToEqual(t *testing.T, expected int64, actual *atomic.Int64, message string) {
+	backoff := time.Millisecond
+	for i := 0; i < 10; i++ {
+		if expected == actual.Load() {
+			return
+		}
+		time.Sleep(backoff)
+		backoff = backoff * 2
+	}
+	require.Fail(t, message)
 }
