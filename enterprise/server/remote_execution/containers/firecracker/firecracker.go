38015cf3ccfdf014c4da1df759b1c05fd34db8f1
Dan Stowell <dan@buildbuddy.io>
2025-04-03 15:24:54 -0500

    hook up fc to new ociconv.CreateDiskImage

--- enterprise/server/remote_execution/containers/firecracker/firecracker.go
+++ enterprise/server/remote_execution/containers/firecracker/firecracker.go
@@ -2360,7 +2360,7 @@ func (c *FirecrackerContainer) PullImage(ctx context.Context, creds oci.Credenti
 		log.CtxDebugf(ctx, "PullImage took %s", time.Since(start))
 	}()
 
-	_, err := ociconv.CreateDiskImage(ctx, c.executorConfig.CacheRoot, c.containerImage, creds)
+	_, err := ociconv.CreateDiskImage(ctx, c.env.GetActionCacheClient(), c.env.GetByteStreamClient(), c.executorConfig.CacheRoot, c.containerImage, creds)
 	if err != nil {
 		return err
 	}
