ddd2d9b2ab4d3771184563b84c932017001d626d
Vanja Pejovic <vanja@buildbuddy.io>
2025-04-04 16:41:49 -0400

    Fix a bug in  distributed.Cache.remoteGetMulti
    When using a lookaside cache, if some of the digests weren't found in the lookaside cache, we would fail to return all the ones that *were* found.

--- enterprise/server/backends/distributed/distributed.go
+++ enterprise/server/backends/distributed/distributed.go
@@ -692,16 +692,22 @@ func (c *Cache) remoteGetMulti(ctx context.Context, peer string, isolation *dcpb
 		return results, nil
 	}
 
-	results, err := c.distributedProxy.RemoteGetMulti(ctx, peer, isolation, stillMissing)
+	remoteResults, err := c.distributedProxy.RemoteGetMulti(ctx, peer, isolation, stillMissing)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, r := range stillMissing {
-		buf, ok := results[r.GetDigest()]
+		buf, ok := remoteResults[r.GetDigest()]
 		if ok {
 			c.addLookasideEntry(r, buf)
 		}
+	}
+	if len(results) == 0 {
+		return remoteResults, nil
+	}
+	for k, v := range remoteResults {
+		results[k] = v
 	}
 	return results, nil
 }
