64ef7134ef9da49fba683dde3cf4d962e36ed635
jim hollenbach <james.d.hollenbach@gmail.com>
2025-04-04 13:50:02 -0700

    don't show action / package counts when buildMetrics event is missing (#8869)

--- app/invocation/invocation_overview.tsx
+++ app/invocation/invocation_overview.tsx
@@ -133,13 +133,13 @@ export default class InvocationOverviewComponent extends React.Component<Props> 
               {this.props.model.getTargetConfiguredCount() == 1 ? "target" : "targets"}
             </div>
           )}
-          {isBazelInvocation && (
+          {isBazelInvocation && Boolean(this.props.model.buildMetrics?.actionSummary) && (
             <div title={`${this.props.model.buildMetrics?.actionSummary?.actionsCreated} created`} className="detail">
               <Activity className="icon" />
               {format.formatWithCommas(this.props.model.buildMetrics?.actionSummary?.actionsExecuted)} actions
             </div>
           )}
-          {isBazelInvocation && (
+          {isBazelInvocation && Boolean(this.props.model.buildMetrics?.packageMetrics) && (
             <div className="detail">
               <Box className="icon" />
               {format.formatWithCommas(this.props.model.buildMetrics?.packageMetrics?.packagesLoaded)} packages
