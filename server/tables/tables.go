123dd7fd6252ac8f46bdf8ee7c75af8f2f40dd2f
Maggie Lou <maggie@buildbuddy.io>
2025-04-10 11:49:27 -0500

    [WF] Add db columns to support disabling automatic workflow functionality

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -617,6 +617,18 @@ type GitRepository struct {
 	// TODO(http://go/b/3286): Remove this field after completing migration.
 	DefaultNonRootRunner bool `gorm:"not null;default:0"`
 
+	// UseDefaultWorkflowConfig determines whether workflows should use the
+	// default workflow config if there isn't a workflow config file (buildbuddy.yaml)
+	// in the workspace root. If enabled, workflows will automatically start
+	// running for a git repo when it is linked.
+	UseDefaultWorkflowConfig bool `gorm:"not null;default:0"`
+
+	// ReportCommitStatusesForCIBuilds determines whether we should automatically
+	// report commit statuses to GitHub for all builds where role is CI or CI_RUNNER.
+	// Even if enabled, this setting can be overridden by setting
+	// `--build_metadata=DISABLE_COMMIT_STATUS_REPORTING=true` on a build.
+	ReportCommitStatusesForCIBuilds bool `gorm:"not null;default:0"`
+
 	// The ID of the BuildBuddy Github app this repository was authorized for.
 	// (i.e. either the read-only or the read-write app)
 	AppID int64
