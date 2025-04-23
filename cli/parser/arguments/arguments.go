39601d74ccdcb81671b969c50ea22bc23474a10c
Zoey Greer <zoey@buildbuddy.io>
2025-04-22 23:13:51 -0400

    add comment explaining `Argument` type

--- cli/parser/arguments/arguments.go
+++ cli/parser/arguments/arguments.go
@@ -1,5 +1,12 @@
 package arguments
 
+// Argument represents parsed command-line argument (or arguments, in the case
+// of an `--option value` pair). This interface is implemented by
+// `PositionalArgument` and `options.Option`. The main benefits this interface
+// affords us are 1) being able to store `option.Options` and
+// `PositionalArguments` in the same slice in a type-safe way, and 2) being
+// able to easily `Format` a slice of `Argument`s in order to retrieve the
+// string representation of the arguments.
 type Argument interface {
 	GetValue() string
 	SetValue(value string)
