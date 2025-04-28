de72b8163a0cb6c84be50043d56304782b20fcb2
Brandon Duffany <brandon@buildbuddy.io>
2025-04-28 19:49:55 -0400

    Add gitserver util

---
+++ server/util/mockgitserver/mockgitserver_test.go
@@ -0,0 +1,138 @@
+package mockgitserver_test
+
+import (
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testfs"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testshell"
+	"github.com/buildbuddy-io/buildbuddy/server/util/mockgitserver"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockGitServerPushAndPull(t *testing.T) {
+	for _, test := range []struct {
+		name         string
+		repoIsPublic bool
+		repoTokens   []string
+		userToken    string
+
+		wantCloneError string
+		wantPushError  string
+	}{
+		{
+			name:          "can pull public repo as unauthenticated user",
+			repoIsPublic:  true,
+			wantPushError: "remote: Unauthorized",
+		},
+		{
+			name:          "can pull but not push public repo with invalid access token",
+			repoIsPublic:  true,
+			userToken:     "invalid-test-token",
+			wantPushError: "remote: Unauthorized",
+		},
+		{
+			name:         "can pull and push public repo if authenticated as repo owner",
+			repoIsPublic: true,
+			userToken:    "test-token",
+		},
+		{
+			name:           "cannot pull private repo as unauthenticated user",
+			repoIsPublic:   false,
+			wantCloneError: "remote: Unauthorized",
+		},
+		{
+			name:           "cannot pull private repo with invalid access token",
+			repoIsPublic:   false,
+			userToken:      "invalid-test-token",
+			wantCloneError: "remote: Unauthorized",
+		},
+		{
+			name:         "can pull and push private repo if authenticated as repo owner",
+			repoIsPublic: false,
+			userToken:    "test-token",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			// For the purposes of this test, set GIT_ASKPASS to a no-op
+			// executable to prevent "No such device or address" errors, and
+			// instead just tell git to send empty credentials to the server
+			// when the server asks for them.
+			t.Setenv("GIT_ASKPASS", "/usr/bin/true")
+			t.Setenv("GIT_TERMINAL_PROMPT", "0")
+			// Disable credential helpers so macOS keychain prompts do not hang
+			// the test when a remote URL contains credentials.
+			t.Setenv("GIT_CONFIG_COUNT", "1")
+			t.Setenv("GIT_CONFIG_KEY_0", "credential.helper")
+			t.Setenv("GIT_CONFIG_VALUE_0", "")
+
+			remoteRoot := testfs.MakeTempDir(t)
+
+			// Provision a bare repository for testorg/testrepo.
+			err := mockgitserver.CreateProject(remoteRoot, "testorg", "testrepo", &mockgitserver.ProjectSettings{
+				Public: test.repoIsPublic,
+			})
+			require.NoError(t, err)
+
+			// Serve the repository.
+			server := httptest.NewServer(mockgitserver.NewHandler(remoteRoot, mockgitserver.Options{
+				AccessToken: "test-token",
+			}))
+			t.Cleanup(server.Close)
+
+			// Try cloning. Note, we need to add the server's TLS certificate,
+			// otherwise we cannot use auth.
+			remoteURL := server.URL
+			if test.userToken != "" {
+				u, err := url.Parse(server.URL)
+				require.NoError(t, err)
+				u.User = url.UserPassword("x-token", test.userToken)
+				remoteURL = u.String()
+			}
+			localRoot := testfs.MakeTempDir(t)
+			_, stderr, err := testshell.Try(t, localRoot, `
+				git clone `+remoteURL+`/testorg/testrepo clonedir
+			`)
+			if test.wantCloneError != "" {
+				assert.Contains(t, stderr, test.wantCloneError)
+				require.Error(t, err, "stderr: %s", stderr)
+				return
+			} else {
+				require.NoError(t, err, "stderr: %s", stderr)
+			}
+
+			// Try pushing a commit.
+			_, stderr, err = testshell.Try(t, localRoot, `
+				cd clonedir
+				git config user.name 'Test'
+				git config user.email test@test.com
+				echo "test-readme-content" > README
+				git add .
+				git commit -m "Initial commit"
+				git push
+			`)
+			if test.wantPushError != "" {
+				assert.Contains(t, stderr, test.wantPushError)
+				require.Error(t, err, "stderr: %s", stderr)
+				return
+			} else {
+				require.NoError(t, err, "stderr: %s", stderr)
+			}
+
+			// If we got this far, we can both pull and push. Try cloning again
+			// and make sure the clone contains our new file.
+			stdout, stderr, err := testshell.Try(t, localRoot, `
+				mkdir clonedir2
+				cd clonedir2
+				git init >&2
+				cp ../clonedir/.git/config .git/config
+				git pull
+				cat README
+			`)
+			assert.Equal(t, string("test-readme-content\n"), stdout)
+			require.NoError(t, err, "stderr: %s", stderr)
+		})
+	}
+}
