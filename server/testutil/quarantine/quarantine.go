e141794db8963462c3b158e1d9b469dc8949cfab
Dan Stowell <dan@buildbuddy.io>
2025-04-29 09:55:04 -0500

    comment

--- server/testutil/quarantine/quarantine.go
+++ server/testutil/quarantine/quarantine.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// SkipQuarantinedTest skips the current test unless
+// the RUN_QUARANTINED_TESTS environment variable is set to "true".
 func SkipQuarantinedTest(t *testing.T) {
 	if os.Getenv("RUN_QUARANTINED_TESTS") != "true" {
 		t.Skip("skipping quarantined test")
