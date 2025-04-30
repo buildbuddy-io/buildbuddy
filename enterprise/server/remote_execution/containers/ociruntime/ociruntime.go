e5d3b2a3c2f33653f7f01ce7d990037a6dbd1c9c
Brandon Duffany <brandon@buildbuddy.io>
2025-04-30 12:24:11 -0400

    Log a warning if pids.max is hit (#9138)
    When the cgroup2 file `pids.events` contains a line like `max N` (where
    `N` is a number >0), this means the container tried to create a process
    but failed due to the `pids.max` limit being enforced.
    
    With this change, we'll log a warning when this happens, so that we can
    diagnose these issues better. These issues are unlikely now that #9098
    is merged, but if these do pop up in the future we'll at least be able
    to see them in the logs.
    
    Note: this change depends on
    https://github.com/buildbuddy-io/buildbuddy/pull/9113, because
    `pids.events` is only reported to the container cgroup, and is not
    bubbled up to any parent cgroups. Prior to that change, `crun` would
    immediately delete the container cgroup once the task exited, preventing
    us from detecting these events.

--- enterprise/server/remote_execution/containers/ociruntime/ociruntime.go
+++ enterprise/server/remote_execution/containers/ociruntime/ociruntime.go
@@ -724,24 +724,57 @@ func (c *ociContainer) doWithStatsTracking(ctx context.Context, invokeRuntimeFn 
 	}
 	res.UsageStats = combinedStats
 
-	// Check for oom_kill memory events in the cgroup and return a
-	// ResourceExhausted error if found. We read this from the parent cgroup,
-	// because at this point, crun will have deleted the child cgroup.
-	memoryEvents, err := cgroup.ReadMemoryEvents(c.cgroupPath())
-	if err != nil {
-		log.CtxWarningf(ctx, "Failed to get memory events: %s", err)
-	} else if memoryEvents["oom_kill"] > 0 {
-		if res.ExitCode == 0 {
-			// TODO: look into any logs that happen here, and consider whether
-			// it makes sense to make these failures.
-			log.CtxWarningf(ctx, "Task succeeded, but cgroup reported oom_kill events.")
-		} else {
-			res.ExitCode = commandutil.KilledExitCode
-			res.Error = status.UnavailableError("task process or child process killed by oom killer")
-		}
+	// If there was an oom_kill event, return an error instead of a normal exit
+	// status.
+	if err := c.checkOOMKill(ctx, res); err != nil {
+		res.ExitCode = commandutil.KilledExitCode
+		res.Error = err
+		return res
+	}
+
+	// Check whether the pid limit was exceeded, and just log it for now so that
+	// it can be diagnosed.
+	if err := c.checkPIDLimitExceeded(ctx, res); err != nil {
+		log.CtxWarning(ctx, status.Message(err))
 	}
 
 	return res
+}
+
+// checkOOMKill checks for oom_kill memory events in the cgroup and returns an
+// Unavailable error if found.
+func (c *ociContainer) checkOOMKill(ctx context.Context, res *interfaces.CommandResult) error {
+	memoryEvents, err := cgroup.ReadMemoryEvents(c.cgroupPath())
+	if err != nil {
+		log.CtxWarningf(ctx, "Failed to get memory events: %s", err)
+		return nil
+	}
+	if memoryEvents["oom_kill"] == 0 {
+		return nil
+	}
+	if res.ExitCode == 0 {
+		log.CtxWarningf(ctx, "Task succeeded, but cgroup reported oom_kill events.")
+		return nil
+	}
+	return status.UnavailableError("task process or child process killed by oom killer")
+}
+
+// checkPIDLimitExceeded checks for pid limit exceeded events in the cgroup and
+// returns an Unavailable error if found.
+func (c *ociContainer) checkPIDLimitExceeded(ctx context.Context, res *interfaces.CommandResult) error {
+	pidsEvents, err := cgroup.ReadPidsEvents(c.cgroupPath())
+	if err != nil {
+		log.CtxWarningf(ctx, "Failed to get pids events: %s", err)
+		return nil
+	}
+	if pidsEvents["max"] == 0 {
+		return nil
+	}
+	if res.ExitCode == 0 {
+		log.CtxWarningf(ctx, "Task succeeded, but cgroup reported pid limit exceeded events.")
+		return nil
+	}
+	return status.UnavailableErrorf("pid limit exceeded (maximum number of pids allowed is %d)", c.cgroupSettings.GetPidsMax())
 }
 
 func (c *ociContainer) setupCgroup(ctx context.Context) error {
