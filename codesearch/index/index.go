buildbuddy-io/buildbuddy

commit bb74200beb6ea9f5fcdc0f742c194b92cf428541

Formalize the concept of a schema in codesearch (#9255)
In codesearch, both the read and write operations require schema
knowledge in order to properly tokenize queries (read) and content
(write). This PR makes the concept of schemas more explicit.

- Introduce `FieldSchema` and `DocumentSchema` types
- Repurpose the `codesearch/schema` package to hold schema and document
related types
- Define a "default schema", which is what we use for codesearch, in the
schema package
- Documents can now only be created from a `DocumentSchema`, which can
be mildly inconvenient in testing scenarios, but makes it very hard to
ever have an invalid `Document`
- `IndexReaders` now require a `DocumentSchema`, to support loading
stored fields

Oddities / future cleanups:
- `IndexWriter` does not enforce a single schema across the entire
index. It will happily write any `Document` you send to it. However
`IndexWriter` is not built or tested to support mixed schemas. So, it
would be nice if `IndexWriter` enforced a single schema. However, given
that we currently only plan to support a single schema per index, the
enforcement would add overhead that could significantly impact indexing
time (the potential impact hasn't been measured though).
- Code related to fetching and indexing Github repos should get factored
out of `server` and `cli` and moved to the new `codesearch/github`
package (which currently only contains one helper function to extract
fields from a github-sourced file)

I'm just coming back to golang after several years away, and I've also
been poisoned by teaching Java, so please don't hold back on readability
/ style / convention suggestions.

codesearch/index/index.go
@@ -6,7 +6,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"maps"
-	"regexp"
 	"runtime"
 	"slices"
 	"strconv"
@@ -15,7 +14,6 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/codesearch/performance"
 	"github.com/buildbuddy-io/buildbuddy/codesearch/posting"
-	"github.com/buildbuddy-io/buildbuddy/codesearch/token"
 	"github.com/buildbuddy-io/buildbuddy/codesearch/types"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
@@ -26,7 +24,6 @@ import (
 )
 
 var (
-	fieldNameRegex   = regexp.MustCompile(`^([a-zA-Z0-9][a-zA-Z0-9_]*)$`)
 	nextGenerationMu = sync.Mutex{}
 )
 
@@ -41,7 +38,6 @@ type Writer struct {
 	generation        uint32
 	docIndex          uint32
 	namespace         string
-	tokenizers        map[types.FieldType]types.Tokenizer
 	fieldPostingLists map[string]postingLists
 	deletes           posting.List
 	batch             *pebble.Batch
@@ -60,7 +56,6 @@ func NewWriter(db *pebble.DB, namespace string) (*Writer, error) {
 		generation:        generation,
 		docIndex:          0,
 		namespace:         namespace,
-		tokenizers:        make(map[types.FieldType]types.Tokenizer),
 		fieldPostingLists: make(map[string]postingLists),
 		deletes:           posting.NewList(),
 		batch:             db.NewBatch(),
@@ -283,30 +278,14 @@ func (w *Writer) AddDocument(doc types.Document) error {
 	w.batch.Set(idKey, Uint64ToBytes(docID), nil)
 
 	for _, fieldName := range doc.Fields() {
-		if !fieldNameRegex.MatchString(fieldName) {
-			return status.InvalidArgumentErrorf("Invalid field name %q", fieldName)
-		}
 		field := doc.Field(fieldName)
 		if _, ok := w.fieldPostingLists[field.Name()]; !ok {
 			w.fieldPostingLists[field.Name()] = make(postingLists, 0)
 		}
 		postingLists := w.fieldPostingLists[field.Name()]
 
-		// Lookup the tokenizer to use; if one has not already been
-		// created for this field type then make it.
-		if _, ok := w.tokenizers[field.Type()]; !ok {
-			switch field.Type() {
-			case types.SparseNgramField:
-				w.tokenizers[field.Type()] = token.NewSparseNgramTokenizer(token.WithMaxNgramLength(6))
-			case types.TrigramField:
-				w.tokenizers[field.Type()] = token.NewTrigramTokenizer()
-			case types.KeywordField:
-				w.tokenizers[field.Type()] = token.NewWhitespaceTokenizer()
-			default:
-				return status.InternalErrorf("No tokenizer known for field type: %q", field.Type())
-			}
-		}
-		tokenizer := w.tokenizers[field.Type()]
+		tokenizer := field.Schema().Tokenizer()
+		// TODO(jdelfino): not threadsafe
 		tokenizer.Reset(bytes.NewReader(field.Contents()))
 
 		for tokenizer.Next() == nil {
@@ -317,7 +296,7 @@ func (w *Writer) AddDocument(doc types.Document) error {
 			postingLists[ngram].Add(docID)
 		}
 
-		if field.Stored() {
+		if field.Schema().Stored() {
 			storedFieldKey := w.storedFieldKey(docID, field.Name())
 			w.batch.Set(storedFieldKey, field.Contents(), nil)
 		}
@@ -394,18 +373,20 @@ func (w *Writer) Flush() error {
 }
 
 type Reader struct {
-	ctx context.Context
-	db  pebble.Reader
-	log log.Logger
+	ctx    context.Context
+	db     pebble.Reader
+	schema types.DocumentSchema // TODO(jdelfino): Could store this in the index, in theory
+	log    log.Logger
 
 	namespace string
 }
 
-func NewReader(ctx context.Context, db pebble.Reader, namespace string) *Reader {
+func NewReader(ctx context.Context, db pebble.Reader, namespace string, schema types.DocumentSchema) *Reader {
 	subLog := log.NamedSubLogger(fmt.Sprintf("reader-%s", namespace))
 	return &Reader{
 		ctx:       ctx,
 		db:        db,
+		schema:    schema,
 		log:       subLog,
 		namespace: namespace,
 	}
@@ -469,7 +450,7 @@ func (r *Reader) recordIterStats(iter *pebble.Iterator, kt indexKeyType) {
 	}
 }
 
-func (r *Reader) getStoredFields(docID uint64, fieldNames ...string) (map[string]types.NamedField, error) {
+func (r *Reader) getStoredFields(docID uint64, fieldNames ...string) (map[string]types.Field, error) {
 	docIDStart := r.storedFieldKey(docID, "")
 	iter, err := r.db.NewIter(&pebble.IterOptions{
 		LowerBound: docIDStart,
@@ -495,7 +476,7 @@ func (r *Reader) getStoredFields(docID uint64, fieldNames ...string) (map[string
 		return false
 	}
 
-	fields := make(map[string]types.NamedField, 0)
+	fields := make(map[string]types.Field, 0)
 	k := key{}
 	for iter.First(); iter.Valid(); iter.Next() {
 		if err := k.FromBytes(iter.Key()); err != nil {
@@ -511,7 +492,7 @@ func (r *Reader) getStoredFields(docID uint64, fieldNames ...string) (map[string
 		}
 		fieldVal := make([]byte, len(iter.Value()))
 		copy(fieldVal, iter.Value())
-		fields[k.field] = types.NewNamedField(types.TrigramField, k.field, fieldVal, true /*=stored*/)
+		fields[k.field] = r.schema.Field(k.field).MakeField(fieldVal)
 	}
 	return fields, nil
 }
@@ -700,7 +681,7 @@ type lazyDoc struct {
 	r *Reader
 
 	id     uint64
-	fields map[string]types.NamedField
+	fields map[string]types.Field
 }
 
 func (d lazyDoc) ID() uint64 {
@@ -713,7 +694,12 @@ func (d lazyDoc) Field(name string) types.Field {
 	}
 	fm, err := d.r.getStoredFields(d.id, name)
 	if err == nil {
-		d.fields[name] = fm[name]
+		field, ok := fm[name]
+		if !ok {
+			d.fields[name] = d.r.schema.Field(name).MakeField(nil)
+		} else {
+			d.fields[name] = field
+		}
 	}
 	return d.fields[name]
 }
@@ -726,7 +712,7 @@ func (r *Reader) newLazyDoc(docid uint64) *lazyDoc {
 	return &lazyDoc{
 		r:      r,
 		id:     docid,
-		fields: make(map[string]types.NamedField, 0),
+		fields: make(map[string]types.Field, 0),
 	}
 }
 

codesearch/schema/BUILD
@@ -1,4 +1,4 @@
-load("@io_bazel_rules_go//go:def.bzl", "go_library")
+load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
 
 go_library(
     name = "schema",
@@ -6,10 +6,18 @@ go_library(
     importpath = "github.com/buildbuddy-io/buildbuddy/codesearch/schema",
     visibility = ["//visibility:public"],
     deps = [
+        "//codesearch/token",
         "//codesearch/types",
-        "//server/util/git",
-        "@com_github_cespare_xxhash_v2//:xxhash",
-        "@com_github_gabriel_vasile_mimetype//:mimetype",
-        "@com_github_go_enry_go_enry_v2//:go-enry",
+        "//server/util/status",
+    ],
+)
+
+go_test(
+    name = "schema_test",
+    srcs = ["schema_test.go"],
+    deps = [
+        ":schema",
+        "//codesearch/token",
+        "//codesearch/types",
     ],
 )

codesearch/schema/schema.go
@@ -1,28 +1,16 @@
 package schema
 
 import (
-	"bytes"
 	"fmt"
-	"path/filepath"
 	"regexp"
-	"strings"
-	"unicode/utf8"
 
+	"github.com/buildbuddy-io/buildbuddy/codesearch/token"
 	"github.com/buildbuddy-io/buildbuddy/codesearch/types"
-	"github.com/buildbuddy-io/buildbuddy/server/util/git"
-
-	"github.com/gabriel-vasile/mimetype"
-	"github.com/go-enry/go-enry/v2"
-
-	xxhash "github.com/cespare/xxhash/v2"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 )
 
-const (
-	maxFileLen = 10_000_000
-
-	// The maximum amount of bytes from a file to use for language and
-	// mimetype detection.
-	detectionBufferSize = 1000
+var (
+	fieldNameRegex = regexp.MustCompile(`^([a-zA-Z0-9][a-zA-Z0-9_]*)$`)
 
 	// The following field names are used in the indexed docs.
 	IDField       = "id"
@@ -32,49 +20,164 @@ const (
 	OwnerField    = "owner"
 	RepoField     = "repo"
 	SHAField      = "sha"
+
+	defaultSchema = NewDocumentSchema([]types.FieldSchema{
+		MustFieldSchema(types.KeywordField, IDField, false),
+		MustFieldSchema(types.TrigramField, FilenameField, true),
+		MustFieldSchema(types.SparseNgramField, ContentField, true),
+		MustFieldSchema(types.KeywordField, LanguageField, true),
+		MustFieldSchema(types.KeywordField, OwnerField, true),
+		MustFieldSchema(types.KeywordField, RepoField, true),
+		MustFieldSchema(types.KeywordField, SHAField, true),
+	})
 )
 
-// TODO(tylerw): this should come from a flag?
-var skipMime = regexp.MustCompile(`^audio/.*|video/.*|image/.*|application/gzip$`)
+func DefaultSchema() types.DocumentSchema {
+	return defaultSchema
+}
 
-func MakeDocument(name, commitSha string, repoURL *git.RepoURL, buf []byte) (types.Document, error) {
-	// Skip long files.
-	if len(buf) > maxFileLen {
-		return nil, fmt.Errorf("skipping %s (file too long)", name)
+// Satisfies the types.FieldSchema interface.
+type fieldSchema struct {
+	ftype     types.FieldType
+	name      string
+	stored    bool
+	tokenizer types.Tokenizer
+}
+
+func (f fieldSchema) Type() types.FieldType      { return f.ftype }
+func (f fieldSchema) Name() string               { return f.name }
+func (f fieldSchema) Stored() bool               { return f.stored }
+func (f fieldSchema) Tokenizer() types.Tokenizer { return f.tokenizer }
+
+func (f fieldSchema) MakeField(buf []byte) types.Field {
+	return documentField{
+		buf:    buf,
+		schema: f,
+	}
+}
+
+func NewFieldSchema(ftype types.FieldType, name string, stored bool) (types.FieldSchema, error) {
+	var tokenizer types.Tokenizer = nil
+
+	if !fieldNameRegex.MatchString(name) {
+		return nil, status.InvalidArgumentErrorf("Invalid field name %q", name)
 	}
 
-	var shortBuf []byte
-	if len(buf) > detectionBufferSize {
-		shortBuf = buf[:detectionBufferSize]
+	switch ftype {
+	case types.SparseNgramField:
+		// TODO(jdelfino): old code used lowercase on query side but not on index side - was it a bug?
+		tokenizer = token.NewSparseNgramTokenizer(token.WithMaxNgramLength(6), token.WithLowerCase(true))
+	case types.TrigramField:
+		tokenizer = token.NewTrigramTokenizer()
+	case types.KeywordField:
+		tokenizer = token.NewWhitespaceTokenizer()
+	default:
+		return nil, status.InvalidArgumentErrorf("Invalid field type %q", ftype)
+	}
+
+	return fieldSchema{
+		ftype:     ftype,
+		name:      name,
+		stored:    stored,
+		tokenizer: tokenizer,
+	}, nil
+}
+
+// Schemas are typically static and const, so in the most common situations, it's reasonable to
+// panic if creation fails.
+func MustFieldSchema(ftype types.FieldType, name string, stored bool) types.FieldSchema {
+	fieldSchema, err := NewFieldSchema(ftype, name, stored)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create field schema for %s: %v", name, err))
+	}
+	return fieldSchema
+}
+
+func (f fieldSchema) String() string {
+	return fmt.Sprintf("FieldSchema<type: %v, name: %q, stored: %v>", f.ftype, f.name, f.stored)
+}
+
+// Satisfies the types.DocumentSchema interface.
+type documentSchema map[string]types.FieldSchema
+
+func (d documentSchema) Fields() []types.FieldSchema {
+	fieldSchemas := make([]types.FieldSchema, 0, len(d))
+	for _, field := range d {
+		fieldSchemas = append(fieldSchemas, field)
+	}
+	return fieldSchemas
+}
+
+func (d documentSchema) Field(name string) types.FieldSchema {
+	if schema, ok := d[name]; ok {
+		return schema
+	}
+	return nil
+}
+
+func (d documentSchema) String() string {
+	schemas := make([]string, 0, len(d))
+	for _, field := range d {
+		schemas = append(schemas, field.String())
+	}
+	return fmt.Sprintf("DocumentSchema<fields: [%v]>", schemas)
+}
+
+func (d documentSchema) MakeDocument(data map[string][]byte) (types.Document, error) {
+	fields := make(map[string]types.Field, len(data))
+	for name, field_data := range data {
+		fieldSchema, ok := d[name]
+		if !ok {
+			return nil, status.InvalidArgumentErrorf("Invalid field name %q", name)
+		}
+		fields[name] = fieldSchema.MakeField(field_data)
+	}
+	return document(fields), nil
+}
+
+func NewDocumentSchema(schemas []types.FieldSchema) types.DocumentSchema {
+	fieldMap := make(map[string]types.FieldSchema, len(schemas))
+	for _, schema := range schemas {
+		fieldMap[schema.Name()] = schema
+	}
+	return documentSchema(fieldMap)
+}
+
+// Satisfies the types.DocumentField interface.
+type documentField struct {
+	buf    []byte
+	schema types.FieldSchema
+}
+
+func (f documentField) Schema() types.FieldSchema { return f.schema }
+func (f documentField) Name() string              { return f.schema.Name() }
+func (f documentField) Type() types.FieldType     { return f.schema.Type() }
+func (f documentField) Contents() []byte          { return f.buf }
+func (f documentField) String() string {
+	var snippet string
+	if len(f.buf) < 10 {
+		snippet = string(f.buf)
 	} else {
-		shortBuf = buf
+		snippet = string(f.buf[:10])
 	}
+	return fmt.Sprintf("field<type: %v, name: %q, buf: %q>", f.schema.Type(), f.schema.Name(), snippet)
+}
 
-	// Check the mimetype and skip if bad.
-	mtype, err := mimetype.DetectReader(bytes.NewReader(shortBuf))
-	if err == nil && skipMime.MatchString(mtype.String()) {
-		return nil, fmt.Errorf("skipping %s (invalid mime type: %q)", name, mtype.String())
+func NewDocField(schema types.FieldSchema, buf []byte) documentField {
+	return documentField{
+		buf:    buf,
+		schema: schema,
 	}
+}
 
-	// Skip non-utf8 encoded files.
-	if !utf8.Valid(buf) {
-		return nil, fmt.Errorf("skipping %s (non-utf8 content)", name)
+// Satisfies the types.Document interface.
+type document map[string]types.Field
+
+func (d document) Field(name string) types.Field { return d[name] }
+func (d document) Fields() []string {
+	fieldNames := make([]string, 0, len(d))
+	for name := range d {
+		fieldNames = append(fieldNames, name)
 	}
-
-	uniqueID := xxhash.Sum64String(repoURL.Owner + repoURL.Repo + name)
-	idBytes := []byte(fmt.Sprintf("%d", uniqueID))
-	// Compute filetype
-	lang := strings.ToLower(enry.GetLanguage(filepath.Base(name), shortBuf))
-	doc := types.NewMapDocument(
-		map[string]types.NamedField{
-			IDField:       types.NewNamedField(types.KeywordField, IDField, idBytes, false /*=stored*/),
-			FilenameField: types.NewNamedField(types.TrigramField, FilenameField, []byte(name), true /*=stored*/),
-			ContentField:  types.NewNamedField(types.SparseNgramField, ContentField, buf, true /*=stored*/),
-			LanguageField: types.NewNamedField(types.KeywordField, LanguageField, []byte(lang), true /*=stored*/),
-			OwnerField:    types.NewNamedField(types.KeywordField, OwnerField, []byte(repoURL.Owner), true /*=stored*/),
-			RepoField:     types.NewNamedField(types.KeywordField, RepoField, []byte(repoURL.Repo), true /*=stored*/),
-			SHAField:      types.NewNamedField(types.KeywordField, SHAField, []byte(commitSha), true /*=stored*/),
-		},
-	)
-	return doc, nil
+	return fieldNames
 }

codesearch/types/types.go
@@ -1,7 +1,6 @@
 package types
 
 import (
-	"fmt"
 	"io"
 )
 
@@ -29,11 +28,26 @@ func (ft FieldType) String() string {
 	}
 }
 
-type Field interface {
+type FieldSchema interface {
 	Type() FieldType
 	Name() string
-	Contents() []byte
 	Stored() bool
+	Tokenizer() Tokenizer
+	MakeField([]byte) Field
+	String() string
+}
+
+type DocumentSchema interface {
+	Fields() []FieldSchema
+	Field(name string) FieldSchema
+	MakeDocument(map[string][]byte) (Document, error)
+}
+
+type Field interface {
+	Contents() []byte
+	Type() FieldType
+	Name() string
+	Schema() FieldSchema
 }
 
 type Document interface {
@@ -97,51 +111,4 @@ type Query interface {
 
 type Searcher interface {
 	Search(q Query, numResults, offset int) ([]Document, error)
-}
-
-// TODO(tylerw): move these structs somewhere else. This file should only
-// contain interface definitions.
-type NamedField struct {
-	ftype  FieldType
-	name   string
-	buf    []byte
-	stored bool
-}
-
-func (f NamedField) Type() FieldType  { return f.ftype }
-func (f NamedField) Name() string     { return f.name }
-func (f NamedField) Contents() []byte { return f.buf }
-func (f NamedField) Stored() bool     { return f.stored }
-func (f NamedField) String() string {
-	var snippet string
-	if len(f.buf) < 10 {
-		snippet = string(f.buf)
-	} else {
-		snippet = string(f.buf[:10])
-	}
-	return fmt.Sprintf("field<type: %v, name: %q, buf: %q>", f.ftype, f.name, snippet)
-}
-
-func NewNamedField(ftype FieldType, name string, buf []byte, stored bool) NamedField {
-	return NamedField{
-		ftype:  ftype,
-		name:   name,
-		buf:    buf,
-		stored: stored,
-	}
-}
-
-type MapDocument map[string]NamedField
-
-func (d MapDocument) Field(name string) Field { return d[name] }
-func (d MapDocument) Fields() []string {
-	fieldNames := make([]string, 0, len(d))
-	for name := range d {
-		fieldNames = append(fieldNames, name)
-	}
-	return fieldNames
-}
-
-func NewMapDocument(fieldMap map[string]NamedField) MapDocument {
-	return MapDocument(fieldMap)
 }

commit d36a03572f953f07ceedc31f82ccef70c0faf978

Fix crash due to thread-unsafe usage of tokenizers in codesearch (#9277)
Tokenizers are not threadsafe. In this change, Tokenizers are now
created on-demand and cached on Writer instances. This avoids reuse of a
single tokenizer instance across multiple Writers. Writers themselves
are not shared across threads currently. This crash was introduced in
#9255.

Note that there is a larger issue here around updating the same document
concurrently with multiple writers, but this change is intended to get
us back to a non-crashing state before addressing this larger issue.

codesearch/index/index.go
@@ -31,6 +31,12 @@ const batchFlushSizeBytes = 1_000_000_000 // flush batch every 1G
 
 type postingLists map[string]posting.List
 
+// Writer is not thread-safe. A single instance should not be used concurrently.
+// Multiple instances can be used concurrently without crashing, however CRUD operations are
+// not atomic, so index corruption can occur if multiple writers are used to modify the same
+// documents at the same time.
+// TODO(jdelfino): The server currently does not guarantee that multiple Writers won't be used to
+// update the same documents at the same time.
 type Writer struct {
 	db  *pebble.DB
 	log log.Logger
@@ -41,6 +47,7 @@ type Writer struct {
 	fieldPostingLists map[string]postingLists
 	deletes           posting.List
 	batch             *pebble.Batch
+	tokenizers        map[string]types.Tokenizer
 }
 
 func NewWriter(db *pebble.DB, namespace string) (*Writer, error) {
@@ -59,6 +66,7 @@ func NewWriter(db *pebble.DB, namespace string) (*Writer, error) {
 		fieldPostingLists: make(map[string]postingLists),
 		deletes:           posting.NewList(),
 		batch:             db.NewBatch(),
+		tokenizers:        make(map[string]types.Tokenizer),
 	}, nil
 }
 
@@ -284,8 +292,12 @@ func (w *Writer) AddDocument(doc types.Document) error {
 		}
 		postingLists := w.fieldPostingLists[field.Name()]
 
-		tokenizer := field.Schema().Tokenizer()
-		// TODO(jdelfino): not threadsafe
+		// Tokenizers are not thread-safe, so the writer must create its own instances.
+		if _, ok := w.tokenizers[field.Name()]; !ok {
+			w.tokenizers[field.Name()] = field.Schema().MakeTokenizer()
+		}
+		tokenizer := w.tokenizers[field.Name()]
+
 		tokenizer.Reset(bytes.NewReader(field.Contents()))
 
 		for tokenizer.Next() == nil {

codesearch/schema/BUILD
@@ -19,5 +19,6 @@ go_test(
         ":schema",
         "//codesearch/token",
         "//codesearch/types",
+        "@com_github_stretchr_testify//assert",
     ],
 )

codesearch/schema/schema.go
@@ -38,16 +38,16 @@ func DefaultSchema() types.DocumentSchema {
 
 // Satisfies the types.FieldSchema interface.
 type fieldSchema struct {
-	ftype     types.FieldType
-	name      string
-	stored    bool
-	tokenizer types.Tokenizer
+	ftype   types.FieldType
+	name    string
+	stored  bool
+	makeTok func() types.Tokenizer
 }
 
-func (f fieldSchema) Type() types.FieldType      { return f.ftype }
-func (f fieldSchema) Name() string               { return f.name }
-func (f fieldSchema) Stored() bool               { return f.stored }
-func (f fieldSchema) Tokenizer() types.Tokenizer { return f.tokenizer }
+func (f fieldSchema) Type() types.FieldType          { return f.ftype }
+func (f fieldSchema) Name() string                   { return f.name }
+func (f fieldSchema) Stored() bool                   { return f.stored }
+func (f fieldSchema) MakeTokenizer() types.Tokenizer { return f.makeTok() }
 
 func (f fieldSchema) MakeField(buf []byte) types.Field {
 	return documentField{
@@ -57,7 +57,7 @@ func (f fieldSchema) MakeField(buf []byte) types.Field {
 }
 
 func NewFieldSchema(ftype types.FieldType, name string, stored bool) (types.FieldSchema, error) {
-	var tokenizer types.Tokenizer = nil
+	var makeTok func() types.Tokenizer = nil
 
 	if !fieldNameRegex.MatchString(name) {
 		return nil, status.InvalidArgumentErrorf("Invalid field name %q", name)
@@ -66,20 +66,26 @@ func NewFieldSchema(ftype types.FieldType, name string, stored bool) (types.Fiel
 	switch ftype {
 	case types.SparseNgramField:
 		// TODO(jdelfino): old code used lowercase on query side but not on index side - was it a bug?
-		tokenizer = token.NewSparseNgramTokenizer(token.WithMaxNgramLength(6), token.WithLowerCase(true))
+		makeTok = func() types.Tokenizer {
+			return token.NewSparseNgramTokenizer(token.WithMaxNgramLength(6), token.WithLowerCase(true))
+		}
 	case types.TrigramField:
-		tokenizer = token.NewTrigramTokenizer()
+		makeTok = func() types.Tokenizer {
+			return token.NewTrigramTokenizer()
+		}
 	case types.KeywordField:
-		tokenizer = token.NewWhitespaceTokenizer()
+		makeTok = func() types.Tokenizer {
+			return token.NewWhitespaceTokenizer()
+		}
 	default:
 		return nil, status.InvalidArgumentErrorf("Invalid field type %q", ftype)
 	}
 
 	return fieldSchema{
-		ftype:     ftype,
-		name:      name,
-		stored:    stored,
-		tokenizer: tokenizer,
+		ftype:   ftype,
+		name:    name,
+		stored:  stored,
+		makeTok: makeTok,
 	}, nil
 }
 

codesearch/types/types.go
@@ -32,7 +32,7 @@ type FieldSchema interface {
 	Type() FieldType
 	Name() string
 	Stored() bool
-	Tokenizer() Tokenizer
+	MakeTokenizer() Tokenizer
 	MakeField([]byte) Field
 	String() string
 }

codesearch/schema/schema_test.go
@@ -6,46 +6,30 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/codesearch/schema"
 	"github.com/buildbuddy-io/buildbuddy/codesearch/token"
 	"github.com/buildbuddy-io/buildbuddy/codesearch/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMakeField(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(types.KeywordField, "test", true)
-	if err != nil {
-		t.Fatalf("Failed to create field schema: %v", err)
-	}
-	if fschema.Type() != types.KeywordField {
-		t.Errorf("Expected field type %v, got %v", types.KeywordField, fschema.Type())
-	}
-	if fschema.Name() != "test" {
-		t.Errorf("Expected field name %q, got %q", "test", fschema.Name())
-	}
-	if fschema.Stored() != true {
-		t.Errorf("Expected field stored %v, got %v", true, fschema.Stored())
-	}
-	if fschema.Tokenizer() == nil {
-		// Tokenizer type mappings covered in other tests below.
-		t.Errorf("Expected field tokenizer %v, got %v", nil, fschema.Tokenizer())
-	}
+	assert.Nil(t, err, "Expected no error creating field schema")
+
+	assert.Equal(t, fschema.Type(), types.KeywordField)
+	assert.Equal(t, fschema.Name(), "test")
+	assert.Equal(t, fschema.Stored(), true)
+	// Tokenizer type mappings covered in other tests below.
+	assert.NotNil(t, fschema.MakeTokenizer())
 }
 
 func testInvalidName(t *testing.T, name string) {
 	fschema, err := schema.NewFieldSchema(types.KeywordField, name, true)
-	if err == nil {
-		t.Fatalf("Expected error for invalid field name %q, got %v", name, fschema)
-	}
-	if fschema != nil {
-		t.Errorf("Expected nil field schema for invalid name %q, got %v", name, fschema)
-	}
+	assert.NotNilf(t, err, "Expected error for invalid field name %q", name)
+	assert.Nil(t, fschema, "Expected nil field schema for invalid name %q", name)
 }
 
 func testValidName(t *testing.T, name string) {
 	fschema, err := schema.NewFieldSchema(types.KeywordField, name, true)
-	if err != nil {
-		t.Fatalf("Expected no error for valid field name %q, got %v", name, err)
-	}
-	if fschema == nil {
-		t.Errorf("Expected non-nil field schema for valid name %q, got %v", name, fschema)
-	}
+	assert.Nil(t, err, "Expected no error creating field schema for valid name %q", name)
+	assert.NotNil(t, fschema, "Expected non-nil field schema for valid name %q", name)
 }
 
 func TestFieldNamePatterns(t *testing.T) {
@@ -61,66 +45,39 @@ func TestFieldNamePatterns(t *testing.T) {
 
 func TestNgramTokenizer(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(types.SparseNgramField, "test", true)
-	if err != nil {
-		t.Fatalf("Failed to create field schema: %v", err)
-	}
-	tok := fschema.Tokenizer()
-	if tok == nil {
-		t.Errorf("Expected non-nil tokenizer, got nil")
-	}
-	// Check that the tokenizer is of the expected type.
-	if _, ok := tok.(*token.SparseNgramTokenizer); !ok {
-		t.Errorf("Expected SparseNgramTokenizer, got %T", tok)
-	}
+	assert.Nil(t, err, "Expected no error creating field schema")
+
+	tok := fschema.MakeTokenizer()
+	assert.NotNil(t, tok, "Expected non-nil tokenizer")
+	assert.IsType(t, &token.SparseNgramTokenizer{}, tok)
 }
 
 func TestTrigramTokenizer(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(types.TrigramField, "test", true)
-	if err != nil {
-		t.Fatalf("Failed to create field schema: %v", err)
-	}
-	tok := fschema.Tokenizer()
-	if tok == nil {
-		t.Errorf("Expected non-nil tokenizer, got nil")
-	}
-	// Check that the tokenizer is of the expected type.
-	if _, ok := tok.(*token.TrigramTokenizer); !ok {
-		t.Errorf("Expected TrigramTokenizer, got %T", tok)
-	}
+	assert.Nil(t, err, "Expected no error creating field schema")
+
+	tok := fschema.MakeTokenizer()
+	assert.NotNil(t, tok, "Expected non-nil tokenizer")
+	assert.IsType(t, &token.TrigramTokenizer{}, tok)
 }
 
 func TestKeywordTokenizer(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(types.KeywordField, "test", true)
-	if err != nil {
-		t.Fatalf("Failed to create field schema: %v", err)
-	}
-	tok := fschema.Tokenizer()
-	if tok == nil {
-		t.Errorf("Expected non-nil tokenizer, got nil")
-	}
-	// Check that the tokenizer is of the expected type.
-	if _, ok := tok.(*token.WhitespaceTokenizer); !ok {
-		t.Errorf("Expected WhitespaceTokenizer, got %T", tok)
-	}
+	assert.Nil(t, err, "Expected no error creating field schema")
+
+	tok := fschema.MakeTokenizer()
+	assert.NotNil(t, tok, "Expected non-nil tokenizer")
+	assert.IsType(t, &token.WhitespaceTokenizer{}, tok)
 }
 
 func TestInvalidTokenizer(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(100, "test", true)
-	if err == nil {
-		t.Fatalf("Expected error for invalid field type, got %v", fschema)
-	}
-	if fschema != nil {
-		t.Errorf("Expected nil field schema for invalid type, got %v", fschema)
-	}
+	assert.NotNil(t, err)
+	assert.Nil(t, fschema)
 }
 
 func TestFieldSchemaString(t *testing.T) {
 	fschema, err := schema.NewFieldSchema(types.KeywordField, "test", true)
-	if err != nil {
-		t.Fatalf("Failed to create field schema: %v", err)
-	}
-	expected := `FieldSchema<type: KeywordToken, name: "test", stored: true>`
-	if fschema.String() != expected {
-		t.Errorf("Expected %q, got %q", expected, fschema.String())
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, `FieldSchema<type: KeywordToken, name: "test", stored: true>`, fschema.String())
 }