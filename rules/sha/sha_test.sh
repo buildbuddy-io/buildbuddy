d0aca7369a10f31bc2a24e75dd8cac38fde63668
Brandon Duffany <brandon@buildbuddy.io>
2025-05-14 17:56:03 -0400

    Don't generate .list.sum file (#9333)
    This breaks the gcs deployment because the target label is now ambiguous
    as to which sum file it refers to.
    
    The .list.sum file was only needed for debugging. Instead add an env var
    that prints out the .list.sum file contents to stderr and fails.

--- rules/sha/sha_test.sh
+++ rules/sha/sha_test.sh
@@ -9,15 +9,6 @@ EXPECTED_HASHES=$(cat <<EOF
 797f24c0a77095eb03f5a1f183245eb371d6db00  rules/sha/testdata/dir.ln/child.txt
 EOF
 )
-ACTUAL_HASHES=$(cat "$TESTDATA_LIST_SUM")
-if [ "$ACTUAL_HASHES" != "$EXPECTED_HASHES" ]; then
-  echo >&2 "Unexpected .list.sum contents:"
-  echo >&2 "$ACTUAL_HASHES"
-  echo >&2 "Expected:"
-  echo >&2 "$EXPECTED_HASHES"
-  exit 1
-fi
-
 # This expected hash was computed by copying the "EXPECTED_HASHES" declaration
 # above, then running:
 # echo "$EXPECTED_HASHES" | shasum
@@ -25,5 +16,6 @@ EXPECTED_HASH='1c385057a72428ea0499c51856a90e638c9dd7df'
 ACTUAL_HASH=$(cat "$TESTDATA_SUM")
 if [ "$ACTUAL_HASH" != "$EXPECTED_HASH" ]; then
   echo >&2 "Unexpected .sum contents: '$ACTUAL_HASH' does not match expected hash '$EXPECTED_HASH'"
+  echo >&2 "Re-run with --action_env=BB_RULES_SHA_DEBUG=1 to see the intermediate file hashes."
   exit 1
 fi
