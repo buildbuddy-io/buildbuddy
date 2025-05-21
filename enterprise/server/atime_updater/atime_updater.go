4c1b466231a072d93368d56ec33460f7d4c13d29
Iain Macdonald <iain@buildbuddy.io>
2025-05-21 09:42:34 -0700

    Remove digest.Is*ResourceName functions (#9414)
    These functions fully parse the digest name, which is kind of expensive,
    and the only callers later call the corresponding `Parse` function.
    Change the callers to just call `Parse` and check for errors.

--- enterprise/server/atime_updater/atime_updater.go
+++ enterprise/server/atime_updater/atime_updater.go
@@ -241,14 +241,10 @@ func (u *atimeUpdater) Enqueue(ctx context.Context, instanceName string, digests
 }
 
 func (u *atimeUpdater) EnqueueByResourceName(ctx context.Context, downloadString string) {
-	if digest.IsActionCacheResourceName(downloadString) {
-		// ActionCache entries are always read authoritatively, so no need to
-		// enqueue an atime update.
-		return
-	}
 	rn, err := digest.ParseDownloadResourceName(downloadString)
 	if err != nil {
-		log.Warningf("Skipping remote atime update for malformed download resource name: %s [%s]", downloadString, err)
+		// Could be an ActionCache digest, or malformed.
+		log.Infof("Skipping remote atime update for malformed download resource name: %s [%s]", downloadString, err)
 		return
 	}
 	u.Enqueue(ctx, rn.GetInstanceName(), []*repb.Digest{rn.GetDigest()}, rn.GetDigestFunction())
