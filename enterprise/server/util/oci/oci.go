03374af425c2a6077ad9b5ca5a776ff641caaf00
Dan Stowell <dan@buildbuddy.io>
2025-05-22 20:00:31 -0500

    Revert "[oci resolver] only make extra HEAD request if reading manifest from … (#9454)"
    This reverts commit 44565e7984a3cf01cb4cac4727abfdd79743e740.

--- enterprise/server/util/oci/oci.go
+++ enterprise/server/util/oci/oci.go
@@ -374,15 +374,15 @@ func updateResolveEventMetric(host string) {
 }
 
 func (r *Resolver) fetchRawManifestFromCacheOrRemote(ctx context.Context, digestOrTagRef gcrname.Reference, remoteOpts []remote.Option) (*gcr.Hash, []byte, bool, error) {
-	if *readManifestsFromCache {
-		headDesc, err := remote.Head(digestOrTagRef, remoteOpts...)
-		if err != nil {
-			if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
-				return nil, nil, false, status.PermissionDeniedErrorf("could not retrieve image manifest: %s", err)
-			}
-			return nil, nil, false, status.UnavailableErrorf("could not retrieve manifest from remote: %s", err)
+	headDesc, err := remote.Head(digestOrTagRef, remoteOpts...)
+	if err != nil {
+		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
+			return nil, nil, false, status.PermissionDeniedErrorf("could not retrieve image manifest: %s", err)
 		}
+		return nil, nil, false, status.UnavailableErrorf("could not retrieve manifest from remote: %s", err)
+	}
 
+	if *readManifestsFromCache {
 		mc, err := ocicache.FetchManifestFromAC(ctx, r.env.GetActionCacheClient(), digestOrTagRef, headDesc.Digest)
 		if err == nil {
 			return &headDesc.Digest, mc.Raw, true, nil
@@ -392,7 +392,8 @@ func (r *Resolver) fetchRawManifestFromCacheOrRemote(ctx context.Context, digest
 		}
 	}
 
-	getDesc, err := remote.Get(digestOrTagRef, remoteOpts...)
+	manifestDigest := digestOrTagRef.Context().Digest(headDesc.Digest.String())
+	getDesc, err := remote.Get(manifestDigest, remoteOpts...)
 	if err != nil {
 		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
 			return nil, nil, false, status.PermissionDeniedErrorf("could not retrieve image manifest: %s", err)
