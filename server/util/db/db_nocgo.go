25deaa355bb3f8ad5170289dcd4b426316e29fb8
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-05-24 09:07:50 +0200

    Add sqlite3 driver for Windows

--- server/util/db/db_nocgo.go
+++ server/util/db/db_nocgo.go
@@ -9,10 +9,13 @@ import (
 
 	gomysql "github.com/go-sql-driver/mysql"
 	gopostgres "github.com/jackc/pgx/v5/stdlib"
+	gosqlite "github.com/ncruces/go-sqlite3/driver"
 )
 
 func getDriver(ds DataSource) (driver.Driver, error) {
 	switch ds.DriverName() {
+	case sqliteDriver:
+		return &gosqlite.SQLite{}, nil
 	case mysqlDriver:
 		return &gomysql.MySQLDriver{}, nil
 	case postgresDriver:
