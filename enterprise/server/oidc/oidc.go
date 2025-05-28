buildbuddy-io/buildbuddy

commit badda845c695638038b0434a098e1494c5098aae

Generalize SSO check for group joins. (#9465)

enterprise/server/oidc/oidc.go
@@ -643,6 +643,9 @@ func (a *OpenIDAuthenticator) authenticateUser(w http.ResponseWriter, r *http.Re
 	// the token below.
 	if ut, err := auth.verifyTokenAndExtractUser(ctx, jwt, true /*=checkExpiry*/); err == nil {
 		claims, err := claims.ClaimsFromSubID(ctx, a.env, ut.GetSubID())
+		if auth.getSlug() != "" {
+			claims.CustomerSSO = true
+		}
 		return claims, ut, err
 	}
 
@@ -686,6 +689,9 @@ func (a *OpenIDAuthenticator) authenticateUser(w http.ResponseWriter, r *http.Re
 
 	cookie.SetLoginCookie(w, jwt, issuer, sessionID, newToken.Expiry.Unix())
 	claims, err := claims.ClaimsFromSubID(ctx, a.env, ut.GetSubID())
+	if auth.getSlug() != "" {
+		claims.CustomerSSO = true
+	}
 	return claims, ut, err
 }
 

enterprise/server/backends/userdb/userdb.go
@@ -466,7 +466,7 @@ func (d *UserDB) RequestToJoinGroup(ctx context.Context, groupID string) (grpb.G
 		// If the org has an owned domain that matches the user's email,
 		// the user can join directly as a member.
 		membershipStatus = grpb.GroupMembershipStatus_REQUESTED
-		if !u.IsSAML() && group.OwnedDomain != "" && group.OwnedDomain == getEmailDomain(tu.Email) {
+		if !u.IsCustomerSSO() && group.OwnedDomain != "" && group.OwnedDomain == getEmailDomain(tu.Email) {
 			membershipStatus = grpb.GroupMembershipStatus_MEMBER
 			return d.addUserToGroup(ctx, tx, userID, groupID)
 		}

enterprise/server/backends/userdb/userdb_test.go
@@ -1500,7 +1500,7 @@ func TestRequestToJoinGroup_AlreadyInGroup_GetAlreadyExists(t *testing.T) {
 	require.Equal(t, role.Admin, role.Role(getGroupRole(t, ctx1, env, "GR1").Role))
 }
 
-func TestRequestToJoinGroup_NoAutoJoinForSAMLUser(t *testing.T) {
+func TestRequestToJoinGroup_NoAutoJoinForSSOUser(t *testing.T) {
 	ctx := context.Background()
 	env := newTestEnv(t)
 	udb := env.GetUserDB()
@@ -1516,7 +1516,7 @@ func TestRequestToJoinGroup_NoAutoJoinForSAMLUser(t *testing.T) {
 			return nil, err
 		}
 		c := ui.(*claims.Claims)
-		c.SAML = true
+		c.CustomerSSO = true
 		return c, nil
 	}
 	createUser(t, ctx, env, "US2", "org1.io")

enterprise/server/saml/saml.go
@@ -329,6 +329,7 @@ func (a *SAMLAuthenticator) AuthenticatedUser(ctx context.Context) (interfaces.U
 			return nil, status.UnauthenticatedErrorf(authutil.UserNotFoundMsg)
 		}
 		claims.SAML = true
+		claims.CustomerSSO = true
 		return claims, nil
 	}
 	return nil, status.UnauthenticatedError("No SAML User found")

server/interfaces/interfaces.go
@@ -117,7 +117,8 @@ type UserInfo interface {
 	GetUseGroupOwnedExecutors() bool
 	GetCacheEncryptionEnabled() bool
 	GetEnforceIPRules() bool
-	IsSAML() bool
+	// IsCustomerSSO indicates whether the user logged in via a customer SSO integration (SAML/OIDC).
+	IsCustomerSSO() bool
 }
 
 // Authenticator constants

server/tables/tables.go
@@ -294,8 +294,11 @@ type User struct {
 	// Profile information etc.
 	FirstName string
 	LastName  string
-	Email     string
-	ImageURL  string
+	// Email provided by the login provider.
+	// SSO integrations can send arbitrary e-mails so this should be
+	// used with care in the context of auth.
+	Email    string
+	ImageURL string
 
 	// GitHub token used for all non-login related GitHub features.
 	// Can be for the read-only or read-write BuildBuddy GitHub app.

server/util/claims/claims.go
@@ -54,7 +54,9 @@ type Claims struct {
 	UseGroupOwnedExecutors bool                          `json:"use_group_owned_executors,omitempty"`
 	CacheEncryptionEnabled bool                          `json:"cache_encryption_enabled,omitempty"`
 	EnforceIPRules         bool                          `json:"enforce_ip_rules,omitempty"`
-	SAML                   bool                          `json:"saml,omitempty"`
+	// TODO(vadim): remove this field
+	SAML        bool `json:"saml,omitempty"`
+	CustomerSSO bool `json:"customer_sso,omitempty"`
 }
 
 func (c *Claims) GetAPIKeyID() string {
@@ -108,6 +110,10 @@ func (c *Claims) GetEnforceIPRules() bool {
 
 func (c *Claims) IsSAML() bool {
 	return c.SAML
+}
+
+func (c *Claims) IsCustomerSSO() bool {
+	return c.SAML || c.CustomerSSO
 }
 
 func ParseClaims(token string) (*Claims, error) {

commit 32bd0e0786f9b71e2c9e38d86384cb535419105d

Fix panic when auth fails (#9479)
I think this was introduced in
https://github.com/buildbuddy-io/buildbuddy/pull/9465. Not sure whether
this would happen in practice, but I hit this while trying to test Entra
+ OIDC locally.

enterprise/server/oidc/oidc.go
@@ -643,7 +643,7 @@ func (a *OpenIDAuthenticator) authenticateUser(w http.ResponseWriter, r *http.Re
 	// the token below.
 	if ut, err := auth.verifyTokenAndExtractUser(ctx, jwt, true /*=checkExpiry*/); err == nil {
 		claims, err := claims.ClaimsFromSubID(ctx, a.env, ut.GetSubID())
-		if auth.getSlug() != "" {
+		if claims != nil && auth.getSlug() != "" {
 			claims.CustomerSSO = true
 		}
 		return claims, ut, err
@@ -689,7 +689,7 @@ func (a *OpenIDAuthenticator) authenticateUser(w http.ResponseWriter, r *http.Re
 
 	cookie.SetLoginCookie(w, jwt, issuer, sessionID, newToken.Expiry.Unix())
 	claims, err := claims.ClaimsFromSubID(ctx, a.env, ut.GetSubID())
-	if auth.getSlug() != "" {
+	if claims != nil && auth.getSlug() != "" {
 		claims.CustomerSSO = true
 	}
 	return claims, ut, err