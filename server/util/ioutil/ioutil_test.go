eb68d205cece139358d84097db768790e1ef760b
Dan Stowell <dan@buildbuddy.io>
2025-06-05 17:20:17 -0500

    [ioutil] TeeReadCacher

--- server/util/ioutil/ioutil_test.go
+++ server/util/ioutil/ioutil_test.go
@@ -2,10 +2,12 @@ package ioutil_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testdigest"
 	"github.com/buildbuddy-io/buildbuddy/server/util/ioutil"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,4 +27,39 @@ func TestCustomCommitWriteCloser_SecondCommitFails(t *testing.T) {
 
 	err = cwc.Close()
 	require.NoError(t, err)
+}
+
+func TestTeeReadCacher(t *testing.T) {
+	w := &bytes.Buffer{}
+	committed := false
+	cache := ioutil.NewCustomCommitWriteCloser(w)
+	cache.CommitFn = func(_ int64) error {
+		committed = true
+		return nil
+	}
+	_, buf := testdigest.RandomCASResourceBuf(t, 1024)
+
+	rc := io.NopCloser(bytes.NewReader(buf))
+
+	{
+		tee, err := ioutil.TeeReadCacher(nil, cache)
+		require.Error(t, err)
+		require.Nil(t, tee)
+	}
+
+	{
+		tee, err := ioutil.TeeReadCacher(rc, nil)
+		require.Error(t, err)
+		require.Nil(t, tee)
+	}
+
+	tee, err := ioutil.TeeReadCacher(rc, cache)
+	require.NoError(t, err)
+	out, err := io.ReadAll(tee)
+	require.NoError(t, err)
+	require.Len(t, out, 1024)
+	require.Empty(t, cmp.Diff(buf, out))
+	require.True(t, committed)
+	require.Equal(t, 1024, w.Len())
+	require.Empty(t, cmp.Diff(buf, w.Bytes()))
 }
