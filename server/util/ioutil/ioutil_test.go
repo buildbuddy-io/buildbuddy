4a23320ea47d9819c265b19ebc2934da73ab907d
Dan Stowell <dan@buildbuddy.io>
2025-06-05 14:37:16 -0500

    [ioutil] ensure CustomWriteCommitCloser can only commit once; add a test (#9573)
    `CustomWriteCommitCloser` sets a `committed` field when called, but
    never checks that field.
    This change makes it so `CustomWriteCommitCloser.Commit()` can only be
    called once. Any further `Commit` calls will return an error.

---
+++ server/util/ioutil/ioutil_test.go
@@ -0,0 +1,28 @@
+package ioutil_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testdigest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/ioutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCustomCommitWriteCloser_SecondCommitFails(t *testing.T) {
+	w := &bytes.Buffer{}
+	cwc := ioutil.NewCustomCommitWriteCloser(w)
+	_, buf := testdigest.RandomCASResourceBuf(t, 1024)
+	written, err := cwc.Write(buf)
+	require.NoError(t, err)
+	require.Equal(t, 1024, written)
+
+	err = cwc.Commit()
+	require.NoError(t, err)
+
+	err = cwc.Commit()
+	require.Error(t, err)
+
+	err = cwc.Close()
+	require.NoError(t, err)
+}
