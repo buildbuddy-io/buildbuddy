54872507efcfdd05d789c946fca861ef2f44cb92
Joe Delfino <joe@buildbuddy.io>
2025-06-16 18:04:09 -0400

    cleanup

--- enterprise/app/code/code_v2.tsx
+++ enterprise/app/code/code_v2.tsx
@@ -1992,8 +1992,7 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                   } else {
                     rpcService.downloadBytestreamFile(this.props.search.get("filename") || "", bsUrl, invocationId);
                   }
-                }}
-              >
+                }}>
                 <Download /> Download File
               </OutlinedButton>
             </div>
@@ -2275,8 +2274,7 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                     onClick={() =>
                       window.open(applicableInstallation?.url + `/permissions/update`, "_blank") &&
                       this.updateState({ installationsResponse: undefined })
-                    }
-                  >
+                    }>
                     <Key className="icon white" /> Permissions
                   </FilledButton>
                 )}
