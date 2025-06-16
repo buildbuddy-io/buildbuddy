1ffacde8b0e511ed2c8aa8820b93d79029f3de1e
Joe Delfino <joe@buildbuddy.io>
2025-06-16 17:34:16 -0400

    ui stuff

--- enterprise/app/code/code_v2.tsx
+++ enterprise/app/code/code_v2.tsx
@@ -144,6 +144,17 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
 
   editor: monaco.editor.IStandaloneCodeEditor | undefined;
   diffEditor: monaco.editor.IDiffEditor | undefined;
+  decorIdToData: Map<string, kythe.proto.DecorationsReply.Reference> = new Map();
+
+  // Note that these decoration collections are automatically cleared when the model is changed.
+  kytheDecorations: monaco.editor.IEditorDecorationsCollection | undefined;
+  searchDecorations: monaco.editor.IEditorDecorationsCollection | undefined;
+  lcovDecorations: monaco.editor.IEditorDecorationsCollection | undefined;
+
+  findRefsKey?: monaco.editor.IContextKey<boolean>;
+  goToDefKey?: monaco.editor.IContextKey<boolean>;
+  pendingXrefsRequest?: CancelablePromise<search.KytheResponse>;
+  mousedownTarget?: monaco.Position;
 
   // Note that these decoration collections are automatically cleared when the model is changed.
   kytheDecorations: monaco.editor.IEditorDecorationsCollection | undefined;
@@ -1902,7 +1913,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                 height="56"
                 viewBox="0 0 68 56"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
                   d="M62.8577 29.2897C61.8246 27.7485 60.4825 26.5113 58.8722 25.5604C59.7424 24.8245 60.493 23.998 61.1109 23.0756C62.5071 21.0593 63.1404 18.6248 63.1404 15.8992C63.1404 13.4509 62.7265 11.2489 61.7955 9.37839C60.9327 7.5562 59.6745 6.07124 58.0282 4.96992C56.4328 3.85851 54.5665 3.09733 52.4736 2.64934C50.4289 2.21166 48.1997 2 45.7961 2H4H2V4V52V54H4H46.4691C48.7893 54 51.0473 53.7102 53.2377 53.1272C55.5055 52.5357 57.5444 51.6134 59.3289 50.3425C61.2008 49.0417 62.6877 47.3709 63.7758 45.3524L63.7808 45.3431L63.7857 45.3338C64.9054 43.2032 65.4286 40.7655 65.4286 38.084C65.4286 34.7488 64.6031 31.7823 62.8577 29.2897Z"
                   stroke-width="4"
@@ -1972,7 +1984,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                   } else {
                     rpcService.downloadBytestreamFile(this.props.search.get("filename") || "", bsUrl, invocationId);
                   }
-                }}>
+                }}
+              >
                 <Download /> Download File
               </OutlinedButton>
             </div>
@@ -1990,7 +2003,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                 <OutlinedButton
                   disabled={this.state.requestingReview}
                   className="request-review-button"
-                  onClick={this.handleShowReviewModalClicked.bind(this)}>
+                  onClick={this.handleShowReviewModalClicked.bind(this)}
+                >
                   {this.state.requestingReview ? (
                     <>
                       <Spinner className="icon" /> Requesting...
@@ -2006,7 +2020,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                 <OutlinedButton
                   disabled={this.state.updatingPR}
                   className="request-review-button"
-                  onClick={this.handleUpdatePR.bind(this)}>
+                  onClick={this.handleUpdatePR.bind(this)}
+                >
                   {this.state.updatingPR ? (
                     <>
                       <Spinner className="icon" /> Updating...
@@ -2070,7 +2085,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                 {[...this.state.tabs.keys()].reverse().map((t) => (
                   <div
                     className={`code-viewer-tab ${t == this.currentPath() ? "selected" : ""}`}
-                    onClick={this.handleTabClicked.bind(this, t)}>
+                    onClick={this.handleTabClicked.bind(this, t)}
+                  >
                     <span>{t.split("/").pop() || "Untitled"}</span>
                     <XCircle
                       onClick={(e) => {
@@ -2145,7 +2161,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                     <div
                       className={`code-diff-viewer-item-path${
                         this.state.changes.get(fullPath)?.changeType == workspace.ChangeType.DELETED ? " deleted" : ""
-                      }${this.state.changes.get(fullPath)?.changeType == workspace.ChangeType.ADDED ? " added" : ""}`}>
+                      }${this.state.changes.get(fullPath)?.changeType == workspace.ChangeType.ADDED ? " added" : ""}`}
+                    >
                       {fullPath}
                     </div>
                     {this.state.mergeConflicts.has(fullPath) && fullPath != this.currentPath() && (
@@ -2155,7 +2172,8 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                           this,
                           fullPath,
                           this.state.mergeConflicts.get(fullPath)!
-                        )}>
+                        )}
+                      >
                         View Conflict
                       </span>
                     )}
@@ -2204,20 +2222,24 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
             <div
               className="context-menu"
               onClick={this.clearContextMenu.bind(this)}
-              style={{ top: this.state.contextMenuY, left: this.state.contextMenuX }}>
+              style={{ top: this.state.contextMenuY, left: this.state.contextMenuX }}
+            >
               <div
-                onClick={() => this.handleNewFileClicked(this.state.contextMenuFile!, this.state.contextMenuFullPath!)}>
+                onClick={() => this.handleNewFileClicked(this.state.contextMenuFile!, this.state.contextMenuFullPath!)}
+              >
                 New file
               </div>
               <div
                 onClick={() =>
                   this.handleNewFolderClicked(this.state.contextMenuFile!, this.state.contextMenuFullPath!)
-                }>
+                }
+              >
                 New folder
               </div>
               <div onClick={() => this.handleRenameClicked(this.state.contextMenuFullPath!)}>Rename</div>
               <div
-                onClick={() => this.handleDeleteClicked(this.state.contextMenuFullPath!, this.state.contextMenuFile!)}>
+                onClick={() => this.handleDeleteClicked(this.state.contextMenuFullPath!, this.state.contextMenuFile!)}
+              >
                 Delete
               </div>
             </div>
@@ -2242,14 +2264,16 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
                     onClick={() =>
                       window.open(applicableInstallation?.url + `/permissions/update`, "_blank") &&
                       this.updateState({ installationsResponse: undefined })
-                    }>
+                    }
+                  >
                     <Key className="icon white" /> Permissions
                   </FilledButton>
                 )}
                 <FilledButton
                   disabled={this.state.requestingReview || applicableInstallation?.permissions?.pullRequests == "read"}
                   className="code-request-review-button"
-                  onClick={this.handleReviewClicked.bind(this)}>
+                  onClick={this.handleReviewClicked.bind(this)}
+                >
                   {this.state.requestingReview ? (
                     <>
                       <Spinner className="icon white" /> Sending...
