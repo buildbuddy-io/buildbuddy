f2bdcad22d9131f3bb61dffc6b0e573bfcf0e588
Lulu Zhang <lulu@buildbuddy.io>
2025-06-27 07:36:48 -0700

    add namespace to the warning log to investigate why group id or ip is not set in context

--- enterprise/server/quota/quota_manager.go
+++ enterprise/server/quota/quota_manager.go
@@ -336,7 +336,7 @@ func (qm *QuotaManager) findBucket(nsName string, key string) Bucket {
 func (qm *QuotaManager) Allow(ctx context.Context, namespace string, quantity int64) (bool, error) {
 	key, err := quota.GetKey(ctx, qm.env)
 	if err != nil {
-		log.Warningf("Failed to get quota key: %s", err)
+		log.Warningf("Failed to get quota key for namespace %s: %s", namespace, err)
 		return true, nil
 	}
 	b := qm.findBucket(namespace, key)
