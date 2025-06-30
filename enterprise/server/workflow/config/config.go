11e7d809838213daa7844dccc857b21e4254e23a
Joe Delfino <joe@buildbuddy.io>
2025-06-30 11:12:56 -0400

    bump to kythe v0.0.77 to pick up vname fixes

--- enterprise/server/workflow/config/config.go
+++ enterprise/server/workflow/config/config.go
@@ -178,7 +178,7 @@ func NewConfig(r io.Reader) (*BuildBuddyConfig, error) {
 	return cfg, nil
 }
 
-const kytheDownloadURL = "https://storage.googleapis.com/buildbuddy-tools/archives/kythe-v0.0.76-buildbuddy.tar.gz"
+const kytheDownloadURL = "https://storage.googleapis.com/buildbuddy-tools/archives/kythe-v0.0.77-buildbuddy.tar.gz"
 
 func checkoutKythe(dirName, downloadURL string) string {
 	buf := fmt.Sprintf(`
