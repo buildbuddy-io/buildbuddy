07bf6f42736f8355e16dc0d196bf60d534b32162
Joe Delfino <joe@buildbuddy.io>
2025-06-30 10:37:41 -0400

    The code browser documentation pop-up was a little too forceful in asserting the type (#9797)

--- enterprise/app/code/code_v2.tsx
+++ enterprise/app/code/code_v2.tsx
@@ -496,7 +496,7 @@ export default class CodeComponentV2 extends React.Component<Props, State> {
         }
         break;
       default:
-        description = kind.toUpperCase();
+        description = kind.charAt(0).toUpperCase() + kind.slice(1);
         break;
     }
     return description;
