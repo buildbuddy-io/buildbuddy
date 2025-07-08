e5a6ccc3a1289d6b36dc2a63f1b22197d69c991f
Lulu Zhang <lulu@buildbuddy.io>
2025-07-08 10:09:19 -0700

    ./buildfix.sh -a

--- enterprise/app/quota/bucket_form.tsx
+++ enterprise/app/quota/bucket_form.tsx
@@ -98,7 +98,11 @@ export default class BucketForm extends React.Component<BucketFormProps, State> 
         {this.props.namespaceEditable && (
           <div className="labeled-input">
             <label>Namespace name (required)</label>
-            <this.Field name="namespace" initialValue={this.props.initialValues?.namespace} placeholder="rpc:/service/method" />
+            <this.Field
+              name="namespace"
+              initialValue={this.props.initialValues?.namespace}
+              placeholder="rpc:/service/method"
+            />
             <div className="validation-message">Required.</div>
           </div>
         )}
