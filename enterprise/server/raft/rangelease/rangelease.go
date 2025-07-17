buildbuddy-io/buildbuddy

commit 0a067115de518ad6283912a7f03ee238328ae8a4

[raft] Fix timeout when we stop leasekeeper  (#9356)
https://github.com/buildbuddy-io/buildbuddy-internal/issues/4974

also unquarantine TestCleanupExpiredSessions:

https://app.buildbuddy.io/invocation/aa6f25d1-4bfc-4a3e-a325-a2203cf623a5
and

https://app.buildbuddy.io/invocation/2b95abbc-ea60-471a-b5e1-4ba92f72a8ca

enterprise/server/raft/rangelease/rangelease.go
@@ -52,11 +52,14 @@ type Lease struct {
 	leaseRecord *rfpb.RangeLeaseRecord
 
 	timeUntilLeaseRenewal time.Duration
-	stopped               bool
-	quitLease             chan struct{}
+
+	ctxMutex sync.Mutex
+	ctx      context.Context
+	cancel   context.CancelFunc
 }
 
 func New(nodeHost client.NodeHost, session *client.Session, log log.Logger, liveness *nodeliveness.Liveness, rangeID uint64, r *replica.Replica) *Lease {
+	ctx, cancelFunc := context.WithCancel(context.Background())
 	return &Lease{
 		nodeHost:              nodeHost,
 		log:                   log,
@@ -69,7 +72,8 @@ func New(nodeHost client.NodeHost, session *client.Session, log log.Logger, live
 		mu:                    sync.RWMutex{},
 		leaseRecord:           &rfpb.RangeLeaseRecord{},
 		timeUntilLeaseRenewal: time.Duration(math.MaxInt64),
-		stopped:               true,
+		ctx:                   ctx,
+		cancel:                cancelFunc,
 	}
 }
 
@@ -93,13 +97,21 @@ func (l *Lease) Release(ctx context.Context) error {
 	return err
 }
 
+func (l *Lease) isStopped() bool {
+	select {
+	case <-l.ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
 func (l *Lease) Stop() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
+	l.ctxMutex.Lock()
+	defer l.ctxMutex.Unlock()
 	// close the background lease-renewal thread.
-	if !l.stopped {
-		l.stopped = true
-		close(l.quitLease)
+	if !l.isStopped() {
+		l.cancel()
 	}
 }
 
@@ -298,7 +310,6 @@ func (l *Lease) renewLeaseUntilValid(ctx context.Context) (returnedRecord *rfpb.
 		span.End()
 	}()
 	l.mu.Lock()
-	defer l.mu.Unlock()
 
 	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
@@ -306,10 +317,12 @@ func (l *Lease) renewLeaseUntilValid(ctx context.Context) (returnedRecord *rfpb.
 	for {
 		select {
 		case <-ctx.Done():
+			l.mu.Unlock()
 			return nil, ctx.Err()
 		default: // continue with for loop
 		}
 		if err := l.renewLease(ctx); err != nil {
+			l.mu.Unlock()
 			return nil, status.InternalErrorf("failed to renew lease: %s", err)
 		}
 		if err := l.verifyLease(ctx, l.leaseRecord); err == nil {
@@ -317,31 +330,38 @@ func (l *Lease) renewLeaseUntilValid(ctx context.Context) (returnedRecord *rfpb.
 		}
 	}
 
+	leaseRecord := l.leaseRecord
+	l.mu.Unlock()
+
+	l.ctxMutex.Lock()
+	defer l.ctxMutex.Unlock()
+
 	// We just renewed the lease. If there isn't already a background
 	// thread running to keep it renewed, start one now.
-	if l.stopped {
-		l.stopped = false
-		l.quitLease = make(chan struct{})
-		if l.leaseRecord.GetReplicaExpiration().GetExpiration() != 0 {
+	if l.isStopped() {
+		ctx, cancel := context.WithCancel(context.Background())
+		l.ctx = ctx
+		l.cancel = cancel
+		if leaseRecord.GetReplicaExpiration().GetExpiration() != 0 {
 			// Only start the renew-goroutine for time-based
 			// leases which need periodic renewal.
-			go l.keepLeaseAlive(l.quitLease)
+			go l.keepLeaseAlive(l.ctx)
 		}
 	}
-	return l.leaseRecord, nil
+	return leaseRecord, nil
 }
 
-func (l *Lease) keepLeaseAlive(quit chan struct{}) {
+func (l *Lease) keepLeaseAlive(ctx context.Context) {
 	for {
 		l.mu.RLock()
 		timeUntilRenewal := l.timeUntilLeaseRenewal
 		l.mu.RUnlock()
 
 		select {
-		case <-quit:
+		case <-ctx.Done():
 			return
 		case <-time.After(timeUntilRenewal):
-			ctx, spn := tracing.StartSpan(context.Background())
+			ctx, spn := tracing.StartSpan(ctx)
 			_, err := l.renewLeaseUntilValid(ctx)
 			if err != nil {
 				log.Errorf("failed to ensure valid lease for c%dn%d: %s", l.replica.RangeID(), l.replica.ReplicaID(), err)

enterprise/server/raft/store/store_test.go
@@ -1023,8 +1023,10 @@ func readSessionIDs(t *testing.T, ctx context.Context, rangeID uint64, store *te
 }
 
 func TestCleanupExpiredSessions(t *testing.T) {
-	quarantine.SkipQuarantinedTest(t)
 	flags.Set(t, "cache.raft.client_session_ttl", 5*time.Hour)
+	flags.Set(t, "cache.raft.enable_driver", false)
+	flags.Set(t, "cache.raft.enable_txn_cleanup", false)
+	flags.Set(t, "cache.raft.zombie_node_scan_interval", 0)
 	clock := clockwork.NewFakeClock()
 
 	sf := testutil.NewStoreFactoryWithClock(t, clock)

commit 509029dc288e278b90013241a50687b1ed8be80c

raft fix a rangelease bug (#9920)
The bug is introduced in
https://github.com/buildbuddy-io/buildbuddy/pull/9356

Originally, the stopped var is initialized to true. When we remove the
var and
use ctx instead, the l.isStopped() always return false so the
keepLeaseAlive
loop is not run.

enterprise/server/raft/rangelease/rangelease.go
@@ -59,7 +59,6 @@ type Lease struct {
 }
 
 func New(nodeHost client.NodeHost, session *client.Session, log log.Logger, liveness *nodeliveness.Liveness, rangeID uint64, r *replica.Replica) *Lease {
-	ctx, cancelFunc := context.WithCancel(context.Background())
 	return &Lease{
 		nodeHost:              nodeHost,
 		log:                   log,
@@ -72,8 +71,6 @@ func New(nodeHost client.NodeHost, session *client.Session, log log.Logger, live
 		mu:                    sync.RWMutex{},
 		leaseRecord:           &rfpb.RangeLeaseRecord{},
 		timeUntilLeaseRenewal: time.Duration(math.MaxInt64),
-		ctx:                   ctx,
-		cancel:                cancelFunc,
 	}
 }
 
@@ -98,6 +95,9 @@ func (l *Lease) Release(ctx context.Context) error {
 }
 
 func (l *Lease) isStopped() bool {
+	if l.ctx == nil {
+		return true
+	}
 	select {
 	case <-l.ctx.Done():
 		return true