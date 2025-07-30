39ea891a622a2a0d1103f5889b2ab20e272d2a89
Zoey Greer <zoey@buildbuddy.io>
2025-07-30 15:34:46 -0400

    unify testcases

--- server/util/terminal/terminal_test.go
+++ server/util/terminal/terminal_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/server/util/random"
 	"github.com/buildbuddy-io/buildbuddy/server/util/terminal"
+	"github.com/buildbuddy-io/buildbuddy/server/util/terminal/testdata"
 	"github.com/stretchr/testify/require"
 )
 
@@ -38,161 +39,45 @@ func TestTruncation(t *testing.T) {
 }
 
 func TestComplexScreenWriting(t *testing.T) {
-	type testCase struct {
-		name       string
-		screenRows int
-		screenCols int
-		write      []string
-		wantLog    string
-	}
-	basicTestCases := []testCase{
-		{
-			name:    "single write with space",
-			write:   []string{" "},
-			wantLog: " ",
-		},
-		{
-			name:    "single blankline write",
-			write:   []string{"\n"},
-			wantLog: "\n",
-		},
-		{
-			name:    "multiple blankline writes",
-			write:   []string{"\n", "\n"},
-			wantLog: "\n\n",
-		},
-		{
-			name:    "multiple writes with multiple lines",
-			write:   []string{"1\n", "2\n"},
-			wantLog: "1\n2\n",
-		},
-		{
-			name:    "single write with multiple lines",
-			write:   []string{"1\n2\n"},
-			wantLog: "1\n2\n",
-		},
-		{
-			name:    "multiple writes with multiple lines and double-newline",
-			write:   []string{"1\n\n", "2\n"},
-			wantLog: "1\n\n2\n",
-		},
-		{
-			name:    "single writes with multiple lines and double-newline",
-			write:   []string{"1\n\n2\n"},
-			wantLog: "1\n\n2\n",
-		},
-		{
-			name:    "multiple writes with mix of trailing and leading whitespace",
-			write:   []string{"1", "2\n", "3\n ", "4\n\n", "5 \n", " 6"},
-			wantLog: "12\n3\n 4\n\n5\n 6",
-		},
-	}
 	// Run each of the basic test cases both with a window height of 0 (no
 	// curses) and 1 (with curses).
-	var testCases []testCase
+	var testCases []testdata.TestCase
 	for _, windowHeight := range []int{0, 1, 2, 3} {
-		for _, tc := range basicTestCases {
-			testCases = append(testCases, testCase{
-				name:       fmt.Sprintf("WindowHeight=%d/%s", windowHeight, tc.name),
-				write:      tc.write,
-				wantLog:    tc.wantLog,
-				screenRows: windowHeight,
+		for _, tc := range testdata.BasicScreenWritingTestcases {
+			testCases = append(testCases, testdata.TestCase{
+				Name:       fmt.Sprintf("WindowHeight=%d/%s", windowHeight, tc.Name),
+				Write:      tc.Write,
+				WantLog:    tc.WantLog,
+				ScreenRows: windowHeight,
 			})
 		}
 	}
 	// Add some advanced test cases which use cursor positioning or which
 	// are specifically testing certain screen dimensions.
-	const (
-		CUU  = "\x1b[A"  // Cursor Up
-		CHA  = "\x1b[G"  // Cursor Horizontal Absolute (go to column 1)
-		EL_2 = "\x1b[2K" // Erase in line; 2="Entire line"
-		ED_2 = "\x1b[2J" // Erase in display; 2="Entire display"
-		CUP  = "\x1b[H"  // Cursor Position (go to row 1, column 1)
-	)
-	testCases = append(testCases, []testCase{
-		{
-			name:       "scrollout of single line wrapped to multiple rows",
-			screenCols: 1,
-			screenRows: 2,
-			// The first line "ab" should be split and wrapped to the second row
-			// since maxCols is 1. We should get "ab" back as a single line;
-			// it should not be artificially wrapped.
-			write:   []string{"ab\n", "c\n", "d"},
-			wantLog: "ab\nc\nd",
-		},
-		{
-			name:       "overwrite line contents",
-			write:      []string{"123456789" + EL_2 + CHA + "ABC"},
-			wantLog:    "ABC",
-			screenRows: 1,
-		},
-		{
-			name:       "overwrite line contents with newline",
-			write:      []string{"123456789" + EL_2 + CHA + "ABC\n"},
-			wantLog:    "ABC\n",
-			screenRows: 1,
-		},
-		{
-			name: "overwrite multiple line contents",
-			write: []string{
-				// Write two lines
-				"123456789\n",
-				"ABCDEFG",
-				// Go back up to the first line
-				CUU + CUU,
-				// Clear the current line and write "Hello" on its own line
-				EL_2 + CHA + "Hello\n",
-				// Clear the current line and write "World" on its own line
-				EL_2 + CHA + "World\n"},
-			wantLog:    "Hello\nWorld\n",
-			screenRows: 2,
-		},
-		{
-			name: "overwrite multiple line contents via screen clearing",
-			write: []string{
-				// Write two lines
-				"1234",
-				"56789\n",
-				"ABCDEFG",
-				// Clear screen and position cursor at top-left
-				ED_2 + CUP,
-				// Write "Hello" on its own line
-				"Hello\n",
-				// Write "World" on its own line
-				"World\n"},
-			wantLog:    "Hello\nWorld\n",
-			screenRows: 2,
-		},
-		{
-			name:       "reset sequence at end of line",
-			write:      []string{"\x1b[32mINFO: ...\x1b[0m\n"},
-			wantLog:    "\x1b[32mINFO: ...\n",
-			screenRows: 1,
-		},
-	}...)
+	testCases = append(testCases, testdata.AdvancedScreenWritingTestcases...)
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(tc.Name, func(t *testing.T) {
 			log.SetOutput(&testLogOutput{t: t})
 			// Disable timestamps in logs
 			log.SetFlags(0)
-			log.Printf("Running test case %q", tc.name)
-			screen, err := terminal.NewScreenWriter(tc.screenRows)
+			log.Printf("Running test case %q", tc.Name)
+			screen, err := terminal.NewScreenWriter(tc.ScreenRows)
 			require.NoError(t, err)
-			if tc.screenCols > 0 {
-				if tc.screenRows > 0 {
-					screen.Screen.SetSize(tc.screenCols, tc.screenRows)
+			if tc.ScreenCols > 0 {
+				if tc.ScreenRows > 0 {
+					screen.Screen.SetSize(tc.ScreenCols, tc.ScreenRows)
 				} else {
-					screen.Screen.SetSize(tc.screenCols, terminal.Lines)
+					screen.Screen.SetSize(tc.ScreenCols, terminal.Lines)
 				}
 			}
-			for _, s := range tc.write {
+			for _, s := range tc.Write {
 				_, err = screen.Write([]byte(s))
 				require.NoError(t, err)
 				require.NoError(t, screen.WriteErr)
 			}
 			require.Equalf(
 				t,
-				ansiDebugString(tc.wantLog),
+				ansiDebugString(tc.WantLog),
 				ansiDebugString(screen.OutputAccumulator.String()+screen.Render()),
 				"scrollout was:\n--------\n%s\n--------\nrender was:\n--------\n%s\n--------\n",
 				screen.OutputAccumulator.String(),
