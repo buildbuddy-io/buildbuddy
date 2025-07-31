d532834060786f9ed65838cebdfa6cd0ac43ecc8
Tyler Williams <williams.tyler@gmail.com>
2025-07-31 13:34:03 -0700

    Use cadvisor which has numa info in useful format

--- enterprise/server/util/cpuset/numcpu_linux.go
+++ enterprise/server/util/cpuset/numcpu_linux.go
@@ -3,30 +3,28 @@
 package cpuset
 
 import (
-	"strconv"
-
-	"github.com/prometheus/procfs"
+	"github.com/google/cadvisor/utils/sysfs"
+	"github.com/google/cadvisor/utils/sysinfo"
 )
 
 func GetCPUs() ([]CPUInfo, error) {
-	fs, err := procfs.NewDefaultFS()
-	if err != nil {
-		return nil, err
-	}
-	cpuInfos, err := fs.CPUInfo()
+	sys := sysfs.NewRealSysFs()
+	cpuInfos, _, err := sysinfo.GetNodesInfo(sys)
 	if err != nil {
 		return nil, err
 	}
 
-	nodes := make([]CPUInfo, len(cpuInfos))
-	for i, info := range cpuInfos {
-		c := CPUInfo{
-			Processor: int(info.Processor),
+	nodes := make([]CPUInfo, 0)
+	for _, nodeInfo := range cpuInfos {
+		for _, coreInfo := range nodeInfo.Cores {
+			for _, thread := range coreInfo.Threads {
+				c := CPUInfo{
+					Processor: int(thread),
+					NumaNode: int(nodeInfo.Id),
+				}
+				nodes = append(nodes, c)
+			}
 		}
-		if i, err := strconv.Atoi(info.PhysicalID); err == nil {
-			c.PhysicalID = int(i)
-		}
-		nodes[i] = c
 	}
 	return nodes, nil
 }
