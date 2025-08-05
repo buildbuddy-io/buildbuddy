e29eeb04df3d9d2699d6276f2dff6e84e9a420ed
Zoey Greer <zoey@buildbuddy.io>
2025-08-05 14:29:39 -0400

    fix frontend multi-line ansi

--- app/terminal/ansi.tsx
+++ app/terminal/ansi.tsx
@@ -1,15 +1,15 @@
 const ANSI_CODES_REGEX = /\x1b\[[\d;]*?m/g;
 
-export type AnsiTextSpan = {
-  /** Text in this span, with ANSI escape sequences removed. */
-  text: string;
+export type AnsiOffsetTag = {
+  /** Length of the plaintext in this tag. */
+  length: number;
   /** Parsed ANSI style. */
-  style: AnsiStyle;
+  style?: AnsiStyle;
   /** Parsed link, if any.  */
-  link: string;
+  link?: string;
 };
 
-type AnsiStyle = {
+export type AnsiStyle = {
   foreground?: string;
   background?: string;
   bold?: boolean;
@@ -19,73 +19,80 @@ type AnsiStyle = {
 
 const colors = ["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"];
 
-function applyCode(span: AnsiTextSpan, code: number) {
+function applyCode(style: AnsiStyle | undefined, code: number) {
+  if (style === undefined) {
+    return;
+  }
   if (code === 0) {
     // Reset style
-    span.style = {};
+    delete style.foreground;
+    delete style.background;
+    delete style.bold;
+    delete style.italic;
+    delete style.underline;
     return;
   }
   if (code >= 30 && code <= 37) {
     // Foreground color
-    span.style.foreground = colors[code - 30];
+    style.foreground = colors[code - 30];
     return;
   }
   if (code === 39) {
-    delete span.style.foreground;
+    delete style.foreground;
     return;
   }
   if (code >= 40 && code <= 47) {
     // Background color
-    span.style.background = colors[code - 40];
+    style.background = colors[code - 40];
     return;
   }
   if (code === 49) {
-    delete span.style.background;
+    delete style.background;
     return;
   }
   if (code === 90) {
     // 90 is technically "bright black fg color" but just treat it as grey.
-    span.style.foreground = "grey";
+    style.foreground = "grey";
     return;
   }
   if (code === 100) {
     // 100 is technically "bright black bg color" but just treat it as grey.
-    span.style.background = "grey";
+    style.background = "grey";
     return;
   }
   if (code >= 90 && code <= 97) {
     // "Bright" foreground color (treat the same as non-bright for now)
-    span.style.foreground = colors[code - 90];
+    style.foreground = colors[code - 90];
     return;
   }
   if (code >= 100 && code <= 107) {
     // "Bright" background color (treat the same as non-bright for now)
-    span.style.background = colors[code - 100];
+    style.background = colors[code - 100];
     return;
   }
 
   if (code === 1) {
-    span.style.bold = true;
+    style.bold = true;
     return;
   }
   if (code === 22) {
-    span.style.bold = false;
+    style.bold = false;
     return;
   }
   if (code === 3) {
-    span.style.italic = true;
+    style.italic = true;
     return;
   }
   if (code === 23) {
-    span.style.italic = false;
+    style.italic = false;
     return;
   }
   if (code === 4) {
-    span.style.underline = true;
+    style.underline = true;
     return;
   }
   if (code === 24) {
-    span.style.underline = false;
+    style.underline = false;
     return;
   }
 }
@@ -102,10 +109,14 @@ const cursorEscapeCharacters = [
   "u", // restore cursor position
 ];
 
-export default function parseAnsi(text: string): AnsiTextSpan[] {
-  let span = { text: "", style: {}, link: "" };
-  const spans: AnsiTextSpan[] = [];
+// parseAnsi parses text into plaintext and offset tags that describe the ANSI
+// and link properties of the plaintext. If the passed style is null or
+// undefined, the tags array will be undefined.
+export default function parseAnsi(text: string, style?: AnsiStyle): [string, AnsiOffsetTag[]?] {
+  let plaintext: string = "";
+  const tags: AnsiOffsetTag[] = [];
   let code = "";
+  let tag: AnsiOffsetTag = { length: 0 };
 
   // rules_go produces test logs containing 0x16 ("synchronous idle") bytes at
   // the start of the line for some reason. Just ignore these for now -
@@ -121,7 +132,7 @@ export default function parseAnsi(text: string): AnsiTextSpan[] {
     if (inEscapeSequence) {
       if (char === "m") {
         // Commit the current sequence code.
-        applyCode(span, Number(code || 0));
+        applyCode(style, Number(code || 0));
         code = "";
         // Escape sequence has ended.
         inEscapeSequence = false;
@@ -134,7 +145,7 @@ export default function parseAnsi(text: string): AnsiTextSpan[] {
       }
       if (char === ";") {
         // Commit the current sequence code.
-        applyCode(span, Number(code || 0));
+        applyCode(style, Number(code || 0));
         code = "";
         continue;
       }
@@ -158,49 +169,51 @@ export default function parseAnsi(text: string): AnsiTextSpan[] {
       // Skip the "[" on the next iteration, to effectively consume the start
       // sequence "\x1b[" as a logical unit.
       i++;
-      // Begin a new span only if the current one already has text.
-      // Preserve styles from the current span.
-      if (span.text) {
-        span = { text: "", style: { ...span.style }, link: "" };
+      // Begin a new tag only if the current one already has text.
+      if (style !== undefined && tag.length != 0) {
+        tag.style = { ...style };
+        tags.push(...parseLinks(plaintext.substring(plaintext.length - tag.length, plaintext.length), tag));
+        tag = { length: 0 };
       }
       continue;
     }
 
-    // Accumulate text into the current span.
-    span.text += char;
-    if (spans[spans.length - 1] !== span) {
-      spans.push(span);
-    }
+    // Accumulate text into the current tag.
+    tag.length++;
+    plaintext += char;
   }
-  let linkSpans = [];
-  for (let span of spans) {
-    linkSpans.push(...parseLinks(span));
+  if (style === undefined) {
+    return [plaintext, undefined];
   }
-  return linkSpans;
+  if (tag.length != 0) {
+    tag.style = { ...style };
+    tags.push(...parseLinks(plaintext.substring(plaintext.length - tag.length, plaintext.length), tag));
+  }
+  return [plaintext, tags];
 }
 
 const LINK_REGEX =
   /(http(s)?:\/\/)(www\.)?([-a-zA-Z0-9@:%._\+~#=]{2,256})(\.[a-z]{2,6}|:[0-9]+)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
-function parseLinks(span: AnsiTextSpan): AnsiTextSpan[] {
-  let matches = [...span.text.matchAll(LINK_REGEX)];
+function parseLinks(text: string, tag: AnsiOffsetTag): AnsiOffsetTag[] {
+  let matches = [...text.matchAll(LINK_REGEX)];
   if (matches.length == 0) {
-    return [span];
+    return [tag];
   }
-  let spans = [];
+  let tags: AnsiOffsetTag[] = [];
   for (let match of matches) {
-    // If there's any text before the link, create a span for that.
+    // If there's any text before the link, create a tag for that.
     if (match.index && match.index > 0) {
-      spans.push({ text: span.text.substr(0, match.index), style: span.style, link: "" });
+      tags.push({ length: match.index, style: tag.style });
     }
-    // Create a link span for the link itself.
-    spans.push({ text: match[0], style: { ...span.style, underline: true }, link: match[0] });
-    // If there's any text after the link, create a span for that.
-    if (match.index && match.index + match[0].length < span.text.length) {
-      spans.push({ text: span.text.substr(match.index + match[0].length), style: span.style, link: "" });
+    // Create a link tag for the link itself.
+    tags.push({ length: match[0].length, style: { ...tag.style, underline: true }, link: match[0] });
+    // If there's any text after the link, create a tag for that.
+    if (match.index && match.index + match[0].length < tag.length) {
+      tags.push({ length: tag.length - (match.index + match[0].length), style: tag.style });
     }
   }
-  return spans;
+  return tags;
 }
 
 /** Strips ANSI codes from text. */
