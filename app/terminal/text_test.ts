9e4bbc360000d084877008b07ca95bc5f55e6f1a
Zoey Greer <zoey@buildbuddy.io>
2025-08-07 11:38:14 -0400

    Add tests for ANSI SGR state that crosses line boundaries

--- app/terminal/text_test.ts
+++ app/terminal/text_test.ts
@@ -1,4 +1,4 @@
-import { normalizeSpace } from "./text";
+import { getContent, normalizeSpace } from "./text";
 
 describe("normalizeSpace", () => {
   it("should handle a single tabstop in a single line of plaintext", () => {
@@ -21,5 +21,81 @@ describe("normalizeSpace", () => {
 
   it("should handle tabstops in lines containing ANSI codes", () => {
     expect(normalizeSpace("\x1b[33m\t12345\x1b[0m\t12345")).toEqual("\x1b[33m        12345\x1b[0m   12345");
+  });
+});
+
+describe("getContent", () => {
+  it("should handle ANSI SGR state that persists across lines", () => {
+    expect(getContent("\x1b[32mMulti-line\nColor\x1b[m\nReset", "", 0).rows).toEqual([
+      {
+        plaintext: "Multi-line",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 10,
+            style: { foreground: "green" },
+          },
+        ],
+      },
+      {
+        plaintext: "Color",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 5,
+            style: { foreground: "green" },
+          },
+        ],
+      },
+      {
+        plaintext: "Reset",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 5,
+            style: {},
+          },
+        ],
+      },
+    ]);
+
+    expect(getContent("\x1b[32mMulti-line\n\x1b[32mColor\x1b[m\nReset", "", 0).rows).toEqual([
+      {
+        plaintext: "Multi-line",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 10,
+            style: { foreground: "green" },
+          },
+        ],
+      },
+      {
+        plaintext: "Color",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 5,
+            style: { foreground: "green" },
+          },
+        ],
+      },
+      {
+        plaintext: "Reset",
+        matchStartIndex: null,
+        wrapOffset: 0,
+        tags: [
+          {
+            length: 5,
+            style: {},
+          },
+        ],
+      },
+    ]);
   });
 });
