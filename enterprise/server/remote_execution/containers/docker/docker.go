c70f0b676f163876d0e7245c96b48f5be2921caf
Son Luong Ngoc <sluongng@gmail.com>
2025-08-07 10:48:31 +0200

    commandutil: allow disabling stdout/stderr limits
    Introduces a new `DisableOutputLimits` flag to `interfaces.Stdio`.
    When set to true, this flag prevents the application of stdout/stderr
    output size limits. This allows specific components, such as persistent
    workers, to bypass these limits for their internal command executions.

--- enterprise/server/remote_execution/containers/docker/docker.go
+++ enterprise/server/remote_execution/containers/docker/docker.go
@@ -449,8 +449,10 @@ func copyOutputs(reader io.Reader, result *interfaces.CommandResult, stdio *inte
 	if *commandutil.DebugStreamCommandOutputs {
 		stdout, stderr = io.MultiWriter(stdout, os.Stdout), io.MultiWriter(stderr, os.Stderr)
 	}
-	stdout = commandutil.LimitStdOutErrWriter(stdout)
-	stderr = commandutil.LimitStdOutErrWriter(stderr)
+	if !stdio.DisableOutputLimits {
+		stdout = commandutil.LimitStdOutErrWriter(stdout)
+		stderr = commandutil.LimitStdOutErrWriter(stderr)
+	}
 
 	_, err := stdcopy.StdCopy(stdout, stderr, reader)
 	result.Stdout = stdoutBuf.Bytes()
