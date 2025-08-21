8330fe6be76b20bfb8dbead01f398a939a9c5667
Maggie Lou <margaretlou2019@u.northwestern.edu>
2025-08-21 14:48:22 -0400

    Add spotlight to config

--- enterprise/app/product_spotlight/spotlight_configs.tsx
+++ enterprise/app/product_spotlight/spotlight_configs.tsx
@@ -3,4 +3,8 @@ export const SPOTLIGHT_CONFIGS = [
     id: "mastering-tests-dashboard",
     filename: "mastering-tests-dashboard.md",
   },
+  {
+    id: "enhanced-ui-metrics-test-execution-remote-runner-monitoring",
+    filename: "enhanced-ui-metrics-test-execution-remote-runner-monitoring.md"
+  },
 ]
