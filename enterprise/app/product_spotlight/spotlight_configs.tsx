85b7e5cf42a129fe7fad3fa08af7f0e804cfb9b4
Maggie Lou <margaretlou2019@u.northwestern.edu>
2025-08-21 14:48:05 -0400

    Add spotlight to config

--- enterprise/app/product_spotlight/spotlight_configs.tsx
+++ enterprise/app/product_spotlight/spotlight_configs.tsx
@@ -3,4 +3,8 @@ export const SPOTLIGHT_CONFIGS = [
     id: "mastering-tests-dashboard",
     filename: "mastering-tests-dashboard.md",
   },
+  {
+    id: "enhanced-usage-charts-now-available",
+    filename: "enhanced-usage-charts-now-available.md"
+  },
 ]
