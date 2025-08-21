9cdee38737460be4988fc99f9d02f0f6dd872d59
Maggie Lou <margaretlou2019@u.northwestern.edu>
2025-08-21 15:09:29 -0400

    Add spotlight to config

--- enterprise/app/product_spotlight/spotlight_configs.tsx
+++ enterprise/app/product_spotlight/spotlight_configs.tsx
@@ -1,5 +1,9 @@
 export const SPOTLIGHT_CONFIGS = [
   {
+    id: "prometheus-monitoring-integration-custom-scraping",
+    filename: "prometheus-monitoring-integration-custom-scraping.md"
+  },
+{
     id: "mastering-tests-dashboard",
     filename: "mastering-tests-dashboard.md",
   },
