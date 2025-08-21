d22baf0bd4d8c46586f1bd7be15f9dcf836517af
Maggie Lou <margaretlou2019@u.northwestern.edu>
2025-08-21 14:47:53 -0400

    Add spotlight to config

--- enterprise/app/product_spotlight/spotlight_configs.tsx
+++ enterprise/app/product_spotlight/spotlight_configs.tsx
@@ -3,4 +3,8 @@ export const SPOTLIGHT_CONFIGS = [
     id: "mastering-tests-dashboard",
     filename: "mastering-tests-dashboard.md",
   },
+  {
+    id: "oci-image-caching-live-remote-executors",
+    filename: "oci-image-caching-live-remote-executors.md"
+  },
 ]
