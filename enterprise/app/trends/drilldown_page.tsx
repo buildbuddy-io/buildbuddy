505429b9eccb749a7ddbe1894bbc5b82dd3045bd
Jim Hollenbach <jim@buildbuddy.io>
2025-08-26 21:33:01 -0700

    fix invoc list type stuff

--- enterprise/app/trends/drilldown_page.tsx
+++ enterprise/app/trends/drilldown_page.tsx
@@ -396,7 +396,7 @@ export default class DrilldownPageComponent extends React.Component<Props, State
       .finally(() => this.setState({ loadingEvents: false }));
   }
 
-  fetchInvocationList(groupId: string, heatmapSelection: HeatmapSelection) {
+  fetchInvocationList(groupId: string, heatmapSelection?: HeatmapSelection) {
     this.setState({
       loadingEvents: true,
       eventsFailed: false,
@@ -420,7 +420,7 @@ export default class DrilldownPageComponent extends React.Component<Props, State
         updatedAfter: filterParams.updatedAfter,
         updatedBefore: filterParams.updatedBefore,
         status: filterParams.status || [],
-        filter: this.toStatFilterList(heatmapSelection),
+        filter: heatmapSelection ? this.toStatFilterList(heatmapSelection) : [],
         dimensionFilter: filterParams.dimensionFilters,
         genericFilters: filterParams.genericFilters,
       }),
@@ -794,7 +794,8 @@ export default class DrilldownPageComponent extends React.Component<Props, State
         <FilledButton
           className="square drilldown-page-zoom-button"
           title={"Clear zoom"}
-          onClick={() => this.handleClearZoom()}>
+          onClick={() => this.handleClearZoom()}
+        >
           <X className="icon white" />
         </FilledButton>
       </div>
@@ -843,7 +844,8 @@ export default class DrilldownPageComponent extends React.Component<Props, State
             <Select
               className="drilldown-page-select"
               onChange={this.handleMetricChange.bind(this)}
-              value={this.selectedMetric.name}>
+              value={this.selectedMetric.name}
+            >
               {METRIC_OPTIONS.map(
                 (o) =>
                   o.name && (
@@ -868,7 +870,8 @@ export default class DrilldownPageComponent extends React.Component<Props, State
                   valueFormatter={(v) => this.renderBucketValue(v)}
                   selectionCallback={(s) => this.handleHeatmapSelection(s)}
                   zoomCallback={(s) => this.handleHeatmapZoom(s)}
-                  selectedData={this.currentHeatmapSelection}></HeatmapComponent>
+                  selectedData={this.currentHeatmapSelection}
+                ></HeatmapComponent>
                 <div className="trend-chart">
                   <div className="trend-chart-title">{this.getDrilldownChartsTitle()}</div>
                   {this.state.loadingDrilldowns && <div className="loading"></div>}
@@ -898,7 +901,8 @@ export default class DrilldownPageComponent extends React.Component<Props, State
                                   width={300}
                                   height={200}
                                   data={chart.entry}
-                                  onClick={this.handleBarClick.bind(this, chart.drilldownType)}>
+                                  onClick={this.handleBarClick.bind(this, chart.drilldownType)}
+                                >
                                   <CartesianGrid strokeDasharray="3 3" />
                                   <XAxis
                                     interval="preserveStart"
@@ -948,7 +952,8 @@ export default class DrilldownPageComponent extends React.Component<Props, State
                   {this.state.eventData?.executions && (
                     <InvocationExecutionTable
                       executions={this.state.eventData.executions.map((e) => e.execution as execution_stats.Execution)}
-                      invocationIdProvider={(e) => this.getInvocationIdForExecution(e)}></InvocationExecutionTable>
+                      invocationIdProvider={(e) => this.getInvocationIdForExecution(e)}
+                    ></InvocationExecutionTable>
                   )}
                 </div>
               </>
