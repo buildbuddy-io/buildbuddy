7c94d7b9da680fd24cbbbbe70361aa28563018fa
Vadim Berezniker <vadim@buildbuddy.io>
2025-09-03 11:59:39 -0700

    Set a default value cache.client.input_tree_setup_parallelism. (#10122)
    An optimal value is not easy to figure out but we can set a
    conservatively high upper bound to prevent the executors from blowing
    up.
    
    At 1000 concurrent ops, an executor can perform 20 million fs ops per
    second assuming 50 micros per op. Don't think anyone is going to be
    hitting this any time soon.
    
    Fixes https://github.com/buildbuddy-io/buildbuddy-internal/issues/5050

--- server/cache/dirtools/dirtools.go
+++ server/cache/dirtools/dirtools.go
@@ -42,7 +42,7 @@ import (
 var (
 	enableDownloadCompresssion = flag.Bool("cache.client.enable_download_compression", true, "If true, enable compression of downloads from remote caches")
 	linkParallelism            = flag.Int("cache.client.filecache_link_parallelism", 0, "Number of goroutines to use when linking inputs from filecache. If 0 uses the value of GOMAXPROCS.")
-	inputTreeSetupParallelism  = flag.Int("cache.client.input_tree_setup_parallelism", -1, "Maximum number of concurrent filesystem operations to perform across all tasks when setting up the input tree structure. -1 means no limit.")
+	inputTreeSetupParallelism  = flag.Int("cache.client.input_tree_setup_parallelism", 1000, "Maximum number of concurrent filesystem operations to perform across all tasks when setting up the input tree structure. -1 means no limit.")
 
 	initInputTreeWrangler     sync.Once
 	inputTreeWranglerInstance *inputTreeWrangler
