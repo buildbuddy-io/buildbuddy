f1fb14bf37ea44846fa79294b7b828ada39991f3
vadim <vadim@buildbuddy.io>
2025-09-05 16:59:20 -0700

    buildfix harder

--- app/router/router.tsx
+++ app/router/router.tsx
@@ -1,5 +1,5 @@
-import { grp } from "../../proto/group_ts_proto";
 import { capability } from "../../proto/capability_ts_proto";
+import { grp } from "../../proto/group_ts_proto";
 import { user as user_proto } from "../../proto/user_ts_proto";
 import { User } from "../auth/user";
 import capabilities from "../capabilities/capabilities";
