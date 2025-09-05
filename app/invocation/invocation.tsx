941fdd14ce7a2178af82cf55cd60af35b8b1d87d
Siggi Simonarson <siggi@buildbuddy.io>
2025-09-05 10:57:39 -0700

    Make sure we don't drop out of the `scheduleRefetch` loop (#10217)
    Currently depending on ordering of componentDidUpdate calls, we have a
    chance of dropping out of this loop and stopping our periodic UI
    refreshes.

--- app/invocation/invocation.tsx
+++ app/invocation/invocation.tsx
@@ -300,6 +300,9 @@ export default class InvocationComponent extends React.Component<Props, State> {
 
       await this.fetchInvocation();
       this.timeoutRef = undefined;
+      if (this.state.model?.isInProgress() || this.isQueued()) {
+        this.scheduleRefetch();
+      }
     }, 3000);
   }
 
@@ -344,6 +347,10 @@ export default class InvocationComponent extends React.Component<Props, State> {
       .then((response) => {
         const runnerExecution = response.execution?.[response.execution.length - 1] ?? undefined;
         this.setState({ runnerExecution });
+      })
+      .catch((e) => {
+        console.error("Failed to fetch runner execution", e);
+        this.setState({ runnerExecution: undefined });
       });
     return this.runnerExecutionRPC;
   }
