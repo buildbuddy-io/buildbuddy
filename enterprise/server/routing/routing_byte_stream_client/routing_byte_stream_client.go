2fbf8832d6f5a6b6a134af9b05710e1568c89392
Jim Hollenbach <jim@buildbuddy.io>
2025-09-11 15:00:37 -0700

    skeleton code, no operands

--- enterprise/server/routing/routing_byte_stream_client/routing_byte_stream_client.go
+++ enterprise/server/routing/routing_byte_stream_client/routing_byte_stream_client.go
@@ -2,11 +2,16 @@ package routing_byte_stream_client
 
 import (
 	"context"
+	"math/rand/v2"
 
 	"google.golang.org/grpc"
 
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/batch_operator"
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 
 	bspb "google.golang.org/genproto/googleapis/bytestream"
@@ -15,6 +20,9 @@ import (
 type RoutingByteStreamClient struct {
 	acClient map[string]bspb.ByteStreamClient
 	router   interfaces.CacheRoutingService
+	copyOp   batch_operator.BatchDigestOperator
+	readOp   batch_operator.BatchDigestOperator
+	verifyOp batch_operator.BatchDigestOperator
 }
 
 func New(env environment.Env) (bspb.ByteStreamClient, error) {
@@ -41,7 +49,46 @@ func (r *RoutingByteStreamClient) Read(ctx context.Context, req *bspb.ReadReques
 	if err != nil {
 		return nil, status.InternalErrorf("Failed to get primary AC client: %s", err)
 	}
-	return primaryClient.Read(ctx, req, opts...)
+	rsp, err := primaryClient.Read(ctx, req, opts...)
+	if err != nil {
+		return rsp, err
+	}
+
+	c, err := r.router.GetCacheRoutingConfig(ctx)
+	if err != nil {
+		log.CtxWarningf(ctx, "Failed to fetch routing config: %s", err)
+		return rsp, nil
+	}
+	singleRandValue := rand.Float32()
+	if singleRandValue < c.GetBackgroundReadVerifyFraction() {
+		if rn, err := digest.ParseDownloadResourceName(req.GetResourceName()); err == nil {
+			r.verifyOp.Enqueue(ctx, rn.GetInstanceName(), []*repb.Digest{rn.GetDigest()}, rn.GetDigestFunction())
+		}
+	} else if singleRandValue < c.GetBackgroundReadFraction() {
+		if rn, err := digest.ParseDownloadResourceName(req.GetResourceName()); err == nil {
+			r.readOp.Enqueue(ctx, rn.GetInstanceName(), []*repb.Digest{rn.GetDigest()}, rn.GetDigestFunction())
+		}
+	} else if (singleRandValue) < c.GetBackgroundCopyFraction() {
+		if rn, err := digest.ParseDownloadResourceName(req.GetResourceName()); err == nil {
+			r.copyOp.Enqueue(ctx, rn.GetInstanceName(), []*repb.Digest{rn.GetDigest()}, rn.GetDigestFunction())
+		}
+	}
+	return rsp, nil
+}
+
+type wrappedWriteStream struct {
+	ctx    context.Context
+	copyOp batch_operator.BatchDigestOperator
+	bspb.ByteStream_WriteClient
+}
+
+func (w *wrappedWriteStream) Send(req *bspb.WriteRequest) error {
+	if req.GetResourceName() != "" {
+		if rn, err := digest.ParseDownloadResourceName(req.GetResourceName()); err == nil {
+			w.copyOp.Enqueue(w.ctx, rn.GetInstanceName(), []*repb.Digest{rn.GetDigest()}, rn.GetDigestFunction())
+		}
+	}
+	return w.ByteStream_WriteClient.Send(req)
 }
 
 func (r *RoutingByteStreamClient) Write(ctx context.Context, opts ...grpc.CallOption) (bspb.ByteStream_WriteClient, error) {
@@ -49,5 +96,23 @@ func (r *RoutingByteStreamClient) Write(ctx context.Context, opts ...grpc.CallOp
 	if err != nil {
 		return nil, status.InternalErrorf("Failed to get primary AC client: %s", err)
 	}
-	return primaryClient.Write(ctx, opts...)
+	rsp, err := primaryClient.Write(ctx, opts...)
+	if err != nil {
+		return rsp, err
+	}
+
+	c, err := r.router.GetCacheRoutingConfig(ctx)
+	if err != nil {
+		log.CtxWarningf(ctx, "Failed to fetch routing config: %s", err)
+		return rsp, nil
+	}
+	singleRandValue := rand.Float32()
+	if (singleRandValue) < c.GetBackgroundCopyFraction() {
+		return &wrappedWriteStream{
+			ctx:                    ctx,
+			copyOp:                 r.copyOp,
+			ByteStream_WriteClient: rsp,
+		}, nil
+	}
+	return rsp, nil
 }
