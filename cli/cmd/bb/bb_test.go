4d50416daf10f25a5837c2b88b183c9482baa9c7
Dan Stowell <dan@buildbuddy.io>
2025-09-15 12:39:32 -0500

    test: add bb command login test and update imports

--- cli/cmd/bb/bb_test.go
+++ cli/cmd/bb/bb_test.go
@@ -1,16 +1,17 @@
-package bb
+package bb_test
 
 import (
 	"testing"
 
-	register_cli_commands "github.com/buildbuddy-io/buildbuddy/cli/cli_command/register"
+	"github.com/buildbuddy-io/buildbuddy/cli/cli_command/register"
+	"github.com/buildbuddy-io/buildbuddy/cli/cmd/bb"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 )
 
 func init() {
 	// Ensure CLI commands are registered for testing
-	register_cli_commands.Register()
+	register.Register()
 }
 
 type TestResult struct {
@@ -25,34 +26,6 @@ func TestInterpretAsBBCliCommand(t *testing.T) {
 		args     []string
 		expected TestResult
 	}{
-		{
-			name: "valid bb command without options",
-			args: []string{"login"},
-			expected: TestResult{
-				OptionNames:   []string{},
-				CommandName:   "login",
-				TruncatedArgs: []string{"login"},
-			},
-		},
-		{
-			name: "valid bb command with options",
-			args: []string{"--verbose", "login"},
-			expected: TestResult{
-				OptionNames:   []string{"verbose"},
-				CommandName:   "login",
-				TruncatedArgs: []string{"login"},
-			},
-		},
-		{
-			name: "bazel command (not bb command)",
-			args: []string{"build", "//..."},
-			expected: TestResult{
-				OptionNames:   []string{},
-				CommandName:   "",
-				TruncatedArgs: []string{"build", "//..."},
-			},
-		},
-		// Bazel commands from bb-help.txt
 		{
 			name: "bazel command: analyze-profile",
 			args: []string{"analyze-profile"},
@@ -69,6 +42,15 @@ func TestInterpretAsBBCliCommand(t *testing.T) {
 				OptionNames:   []string{},
 				CommandName:   "",
 				TruncatedArgs: []string{"aquery"},
+			},
+		},
+		{
+			name: "bazel command: build",
+			args: []string{"build"},
+			expected: TestResult{
+				OptionNames:   []string{},
+				CommandName:   "",
+				TruncatedArgs: []string{"build"},
 			},
 		},
 		{
@@ -307,6 +289,15 @@ func TestInterpretAsBBCliCommand(t *testing.T) {
 			},
 		},
 		{
+			name: "bb command: login",
+			args: []string{"login"},
+			expected: TestResult{
+				OptionNames:   []string{},
+				CommandName:   "login",
+				TruncatedArgs: []string{"login"},
+			},
+		},
+		{
 			name: "bb command: logout",
 			args: []string{"logout"},
 			expected: TestResult{
@@ -409,7 +400,7 @@ func TestInterpretAsBBCliCommand(t *testing.T) {
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			opts, command, truncatedArgs := InterpretAsBBCliCommand(tc.args)
+			opts, command, truncatedArgs := bb.InterpretAsBBCliCommand(tc.args)
 			optionNames := []string{}
 			for _, opt := range opts {
 				optionNames = append(optionNames, opt.Name())
