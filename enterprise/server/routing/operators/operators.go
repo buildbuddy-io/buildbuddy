9f971cb2668cea53271c03fc38b0c7c3913b673a
Jim Hollenbach <jim@buildbuddy.io>
2025-09-22 16:09:16 -0700

    code complete.. ish

--- enterprise/server/routing/operators/operators.go
+++ enterprise/server/routing/operators/operators.go
@@ -3,12 +3,15 @@ package operators
 import (
 	"context"
 	"io"
+	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/batch_operator"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/flag"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/buildbuddy-io/buildbuddy/server/util/usageutil"
 	bspb "google.golang.org/genproto/googleapis/bytestream"
@@ -16,6 +19,24 @@ import (
 
 const maxSizeForBatching = 1024 * 1024 * 2      // 2 MB, half of the typical max gRPC message size.
 const maxSizeForBatchRequests = 1024 * 1024 * 4 // 4 MB, the typical max gRPC message size.
+
+var (
+	migrationFMQueueSize        = flag.Int("cache_proxy.migration.findmissing_queue_size", 1_000_000, "The length of the holding queue from which we create AC migration batches (shared across all group IDs).")
+	migrationFMDigestsPerBatch  = flag.Int("cache_proxy.migration.findmissing_digests_per_batch", 10_000, "The number of digests to flush out to the AC migration operator in a single batch, per (group ID, instance name) tuple.")
+	migrationFMBatchesPerGroup  = flag.Int("cache_proxy.migration.findmissing_batches_per_group", 50, "The number of batches to hold for the AC migration operator, per (group ID, instance name) tuple.")
+	migrationFMDigestsPerGroup  = flag.Int("cache_proxy.migration.findmissing_digests_per_group", 200_000, "The number of digests that we're willing to hold in batches per Group ID (across all instance names).")
+	migrationFMBatchInterval    = flag.Duration("cache_proxy.migration.cas_batch_interval", 10*time.Second, "The time interval to wait between flushing AC batches, one batch (group ID, instance name) tuple per flush.")
+	migrationCASQueueSize       = flag.Int("cache_proxy.migration.cas_queue_size", 1_000_000, "The length of the holding queue from which we create AC migration batches (shared across all group IDs).")
+	migrationCASDigestsPerBatch = flag.Int("cache_proxy.migration.cas_digests_per_batch", 1_000, "The number of digests to flush out to the AC migration operator in a single batch, per (group ID, instance name) tuple.")
+	migrationCASBatchesPerGroup = flag.Int("cache_proxy.migration.cas_batches_per_group", 500, "The number of batches to hold for the AC migration operator, per (group ID, instance name) tuple.")
+	migrationCASDigestsPerGroup = flag.Int("cache_proxy.migration.cas_digests_per_group", 500_000, "The number of digests that we're willing to hold in batches per Group ID (across all instance names).")
+	migrationCASBatchInterval   = flag.Duration("cache_proxy.migration.cas_batch_interval", 10*time.Second, "The time interval to wait between flushing AC batches, one batch (group ID, instance name) tuple per flush.")
+	migrationBSQueueSize        = flag.Int("cache_proxy.migration.bs_queue_size", 1_000_000, "The length of the holding queue from which we create AC migration batches (shared across all group IDs).")
+	migrationBSDigestsPerBatch  = flag.Int("cache_proxy.migration.bs_digests_per_batch", 100, "The number of digests to flush out to the AC migration operator in a single batch, per (group ID, instance name) tuple.")
+	migrationBSBatchesPerGroup  = flag.Int("cache_proxy.migration.bs_batches_per_group", 10_000, "The number of batches to hold for the AC migration operator, per (group ID, instance name) tuple.")
+	migrationBSDigestsPerGroup  = flag.Int("cache_proxy.migration.bs_digests_per_group", 30_000, "The number of digests that we're willing to hold in batches per Group ID (across all instance names).")
+	migrationBSBatchInterval    = flag.Duration("cache_proxy.migration.bs_batch_interval", 10*time.Millisecond, "The time interval to wait between flushing bytestream batches, one batch (group ID, instance name) tuple per flush.")
+)
 
 // Creates a composite operator that copies digests from one cache to another.
 // The flow of this operator is as follows:
@@ -46,16 +67,16 @@ func NewCopyOperator(env environment.Env) (batch_operator.BatchDigestOperator, e
 }
 
 func byteStreamCopy(ctx context.Context, router interfaces.CacheRoutingService, groupID string, b *batch_operator.DigestBatch) error {
-	ctx = usageutil.DisableUsageTracking(ctx)
+	ctx, cancel := context.WithCancel(usageutil.DisableUsageTracking(ctx))
+	defer cancel()
 	primary, secondary, err := router.GetBSClients(ctx)
 	if err != nil {
 		return err
 	}
 
 	for _, d := range b.Digests {
-		// XXX: compression?! This defaults to IDENTITY.
+		// TODO(jdhollen): This should be using compression, when available.
 		r := digest.NewCASResourceName(d, b.InstanceName, b.DigestFunction)
-		// XXX: Closing streams etc.
 		readStream, err := primary.Read(ctx, &bspb.ReadRequest{ResourceName: r.DownloadString()})
 		if err != nil {
 			return err
@@ -77,6 +98,9 @@ func byteStreamCopy(ctx context.Context, router interfaces.CacheRoutingService, 
 			if err == io.EOF {
 				break
 			}
+			if err != nil {
+				return err
+			}
 			err = writeClient.Send(&bspb.WriteRequest{
 				Data:        res.Data,
 				WriteOffset: offset,
@@ -90,6 +114,9 @@ func byteStreamCopy(ctx context.Context, router interfaces.CacheRoutingService, 
 			FinishWrite: true,
 			WriteOffset: offset,
 		})
+		if _, writeErr := writeClient.CloseAndRecv(); writeErr != nil {
+			return writeErr
+		}
 	}
 	return nil
 }
@@ -102,8 +129,11 @@ func NewByteStreamCopyOperator(env environment.Env, router interfaces.CacheRouti
 			return byteStreamCopy(ctx, router, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1,
-			MaxBatchesPerGroup: 1000,
+			QueueSize:          *migrationBSQueueSize,
+			BatchInterval:      *migrationBSBatchInterval,
+			MaxDigestsPerGroup: *migrationBSDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationBSDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationBSBatchesPerGroup,
 		},
 	)
 }
@@ -119,7 +149,7 @@ func casBatchCopy(ctx context.Context, router interfaces.CacheRoutingService, gr
 
 	flush := func() error {
 		currentBatchSize = 0
-		// XXX: Compression?!
+		// TODO(jdhollen): this should use compression when possible.
 		data, err := primary.BatchReadBlobs(ctx, &repb.BatchReadBlobsRequest{
 			InstanceName:   b.InstanceName,
 			DigestFunction: b.DigestFunction,
@@ -162,8 +192,11 @@ func NewCASBatchCopyOperator(env environment.Env, router interfaces.CacheRouting
 			return casBatchCopy(ctx, router, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1000,
-			MaxBatchesPerGroup: 100,
+			QueueSize:          *migrationCASQueueSize,
+			BatchInterval:      *migrationCASBatchInterval,
+			MaxDigestsPerGroup: *migrationCASDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationCASDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationCASBatchesPerGroup,
 		},
 	)
 }
@@ -193,16 +226,23 @@ func findMissingCheckAndDirect(ctx context.Context, router interfaces.CacheRouti
 		}
 	}
 
+	enqueueErr := error(nil)
 	if len(smallStuff) > 0 {
-		// XXX: Log on enqueue failure.
-		casOperator.Enqueue(ctx, b.InstanceName, smallStuff, b.DigestFunction)
+		success := casOperator.Enqueue(ctx, b.InstanceName, smallStuff, b.DigestFunction)
+		if !success {
+			log.CtxWarningf(ctx, "Failed to enqueue cas sync operations for group %s", groupID)
+			enqueueErr = status.ResourceExhaustedErrorf("Failed to enqueue CAS sync")
+		}
+
 	}
 	if len(bigStuff) > 0 {
-		// XXX: Log on enqueue failure.
-		bsOperator.Enqueue(ctx, b.InstanceName, bigStuff, b.DigestFunction)
+		success := bsOperator.Enqueue(ctx, b.InstanceName, bigStuff, b.DigestFunction)
+		if !success {
+			log.CtxWarningf(ctx, "Failed to enqueue bytestream sync operations for group %s", groupID)
+			enqueueErr = status.ResourceExhaustedErrorf("Failed to enqueue bytestream sync")
+		}
 	}
-	// XXX: Should this care about errors above?
-	return nil
+	return enqueueErr
 }
 
 func NewFindMissingAndCopyOperator(env environment.Env, router interfaces.CacheRoutingService, casOperator batch_operator.BatchDigestOperator, bsOperator batch_operator.BatchDigestOperator) (batch_operator.BatchDigestOperator, error) {
@@ -213,8 +253,11 @@ func NewFindMissingAndCopyOperator(env environment.Env, router interfaces.CacheR
 			return findMissingCheckAndDirect(ctx, router, casOperator, bsOperator, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1000,
-			MaxBatchesPerGroup: 100,
+			QueueSize:          *migrationFMQueueSize,
+			BatchInterval:      *migrationFMBatchInterval,
+			MaxDigestsPerGroup: *migrationFMDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationFMDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationFMBatchesPerGroup,
 		},
 	)
 }
@@ -245,8 +288,11 @@ func NewFindMissingOperator(env environment.Env) (batch_operator.BatchDigestOper
 			return findMissing(ctx, router, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1000,
-			MaxBatchesPerGroup: 100,
+			QueueSize:          *migrationFMQueueSize,
+			BatchInterval:      *migrationFMBatchInterval,
+			MaxDigestsPerGroup: *migrationFMDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationFMDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationFMBatchesPerGroup,
 		},
 	)
 }
@@ -262,7 +308,7 @@ func readAndVerifyCASBatch(ctx context.Context, router interfaces.CacheRoutingSe
 
 	flush := func() error {
 		currentBatchSize = 0
-		// XXX: Compression?!
+		// TODO(jdhollen): this should use compression and decompress after the fact.
 		data, err := secondary.BatchReadBlobs(ctx, &repb.BatchReadBlobsRequest{
 			InstanceName:   b.InstanceName,
 			DigestFunction: b.DigestFunction,
@@ -271,20 +317,21 @@ func readAndVerifyCASBatch(ctx context.Context, router interfaces.CacheRoutingSe
 		if err != nil {
 			return err
 		}
+		anyErr := error(nil)
 		for _, r := range data.GetResponses() {
-			// Decompress (if needed) and verify
 			if r.GetCompressor() == repb.Compressor_IDENTITY {
 				if len(r.GetData()) != int(r.GetDigest().GetSizeBytes()) {
-					// Uh.. shouldn't happen.
-					// XXX: Log error.
-					return nil
+					anyErr = status.InternalErrorf("Size mismatch for digest %s: expected %d, got %d", r.GetDigest().GetHash(), r.GetDigest().GetSizeBytes(), len(r.GetData()))
+					log.CtxWarningf(ctx, "Read validation error for group %s: %s", groupID, anyErr)
+					// Allow check to continue -- would be good to know if there are lots.
 				}
 			} else {
-				// XXX: Log error.
-				return nil
+				// TODO(jdhollen): Decompress (if needed) and verify
+				log.CtxWarningf(ctx, "Unexpected compressed blob in read validation.")
+				return status.InternalErrorf("Unexpected compressed blob when performing read validation for %s", groupID)
 			}
 		}
-		return err
+		return anyErr
 	}
 
 	for _, d := range b.Digests {
@@ -310,6 +357,7 @@ func readAndVerifyByteStream(ctx context.Context, router interfaces.CacheRouting
 	if err != nil {
 		return err
 	}
+	anyErr := error(nil)
 	for _, d := range b.Digests {
 		r := digest.NewCASResourceName(d, b.InstanceName, b.DigestFunction)
 		readStream, err := secondary.Read(ctx, &bspb.ReadRequest{ResourceName: r.DownloadString()})
@@ -328,11 +376,11 @@ func readAndVerifyByteStream(ctx context.Context, router interfaces.CacheRouting
 			bytesRead += int64(len(res.Data))
 		}
 		if bytesRead != d.GetSizeBytes() {
-			// XXX: Log ~!
-			return nil
+			anyErr = status.InternalErrorf("Size mismatch for digest %s: expected %d, got %d", d.GetHash(), d.GetSizeBytes(), bytesRead)
+			log.CtxWarningf(ctx, "Read validation error for group %s: %s", groupID, anyErr)
 		}
 	}
-	return nil
+	return anyErr
 }
 
 type ReadOperator struct {
@@ -354,11 +402,9 @@ func (r *ReadOperator) Enqueue(ctx context.Context, instanceName string, digests
 
 	success := true
 	if len(smallStuff) > 0 {
-		// XXX: Log on enqueue failure.
 		success = success && r.casOperator.Enqueue(ctx, instanceName, smallStuff, digestFunction)
 	}
 	if len(bigStuff) > 0 {
-		// XXX: Log on enqueue failure.
 		success = success && r.bsOperator.Enqueue(ctx, instanceName, bigStuff, digestFunction)
 	}
 	return success
@@ -401,16 +447,18 @@ func NewReadOperator(env environment.Env) (batch_operator.BatchDigestOperator, e
 	if router == nil {
 		return nil, status.FailedPreconditionError("No routing service registered!")
 	}
-	name := "cas_read_and_verify"
 	casOperator, err := batch_operator.New(
 		env,
-		name,
+		"cas_read_and_verify",
 		func(ctx context.Context, groupID string, b *batch_operator.DigestBatch) error {
 			return readAndVerifyCASBatch(ctx, router, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1000,
-			MaxBatchesPerGroup: 100,
+			QueueSize:          *migrationCASQueueSize,
+			BatchInterval:      *migrationCASBatchInterval,
+			MaxDigestsPerGroup: *migrationCASDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationCASDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationCASBatchesPerGroup,
 		},
 	)
 	if err != nil {
@@ -418,13 +466,16 @@ func NewReadOperator(env environment.Env) (batch_operator.BatchDigestOperator, e
 	}
 	bsOperator, err := batch_operator.New(
 		env,
-		name,
+		"byte_stream_read_and_verify",
 		func(ctx context.Context, groupID string, b *batch_operator.DigestBatch) error {
 			return readAndVerifyByteStream(ctx, router, groupID, b)
 		},
 		batch_operator.BatchDigestOperatorConfig{
-			MaxDigestsPerBatch: 1000,
-			MaxBatchesPerGroup: 100,
+			QueueSize:          *migrationBSQueueSize,
+			BatchInterval:      *migrationBSBatchInterval,
+			MaxDigestsPerGroup: *migrationBSDigestsPerGroup,
+			MaxDigestsPerBatch: *migrationBSDigestsPerBatch,
+			MaxBatchesPerGroup: *migrationBSBatchesPerGroup,
 		},
 	)
 	if err != nil {
