acf39c9b9fb4a03cba0239d538193dbdfc443878
Iain Macdonald <xiainx@gmail.com>
2025-10-01 13:49:24 -0700

    Add a skeleton remote_crypter

---
+++ enterprise/server/remote_crypter/remote_crypter.go
@@ -0,0 +1,39 @@
+package remote_crypter
+
+import (
+	"context"
+	"io"
+
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+
+	enpb "github.com/buildbuddy-io/buildbuddy/proto/encryption"
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+	sgpb "github.com/buildbuddy-io/buildbuddy/proto/storage"
+)
+
+type RemoteCrypter struct{}
+
+func (c *RemoteCrypter) SetEncryptionConfig(ctx context.Context, req *enpb.SetEncryptionConfigRequest) (*enpb.SetEncryptionConfigResponse, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
+
+func (c *RemoteCrypter) GetEncryptionConfig(ctx context.Context, req *enpb.GetEncryptionConfigRequest) (*enpb.GetEncryptionConfigResponse, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
+
+func (c *RemoteCrypter) ActiveKey(ctx context.Context) (*sgpb.EncryptionMetadata, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
+
+func (c *RemoteCrypter) NewEncryptor(ctx context.Context, d *repb.Digest, w interfaces.CommittedWriteCloser) (interfaces.Encryptor, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
+
+func (c *RemoteCrypter) NewDecryptor(ctx context.Context, d *repb.Digest, r io.ReadCloser, em *sgpb.EncryptionMetadata) (interfaces.Decryptor, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
+
+func (c *RemoteCrypter) GetEncryptionKey(ctx context.Context, req *enpb.GetEncryptionKeyRequest) (*enpb.GetEncryptionKeyResponse, error) {
+	return nil, status.UnimplementedError("unimplemented")
+}
---
+++ enterprise/server/remote_crypter/remote_crypter_test.go
@@ -0,0 +1,11 @@
+package remote_crypter_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSkeleton(t *testing.T) {
+	require.Equal(t, 2, 1+1)
+}
