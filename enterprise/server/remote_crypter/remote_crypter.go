9c9da1fb27dba5f7ae883a4b83b21408c1bcbd5a
Iain Macdonald <xiainx@gmail.com>
2025-10-02 15:34:16 -0700

    Implement

--- enterprise/server/remote_crypter/remote_crypter.go
+++ enterprise/server/remote_crypter/remote_crypter.go
@@ -2,13 +2,17 @@ package remote_crypter
 
 import (
 	"context"
-	"flag"
 	"io"
 
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/crypter"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/crypter_key_cache"
+	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
+	"github.com/buildbuddy-io/buildbuddy/server/util/flag"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/jonboulle/clockwork"
 
 	enpb "github.com/buildbuddy-io/buildbuddy/proto/encryption"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
@@ -20,34 +24,113 @@ var (
 )
 
 type RemoteCrypter struct {
-	cache *crypter_key_cache.KeyCache
+	env    environment.Env
+	client enpb.EncryptionServiceClient
+	cache  *crypter_key_cache.KeyCache
 }
 
-func Register(env *real_environment.RealEnv) {
-	crypter := &RemoteCrypter{}
-	env.SetCrypter(crypter)
+func Register(env *real_environment.RealEnv) error {
+	if *target == "" {
+		return nil
+	}
+
+	rc, err := new(env, env.GetClock())
+	if err != nil {
+		return err
+	}
+	env.SetCrypter(rc)
+	return nil
+}
+
+func new(env environment.Env, clock clockwork.Clock) (*RemoteCrypter, error) {
+	conn, err := grpc_client.DialSimple(*target)
+	if err != nil {
+		return nil, err
+	}
+
+	client := enpb.NewEncryptionServiceClient(conn)
+
+	refreshFn := func(ctx context.Context, ck crypter_key_cache.CacheKey) ([]byte, *sgpb.EncryptionMetadata, error) {
+		return refreshKey(ctx, ck, client)
+	}
+
+	cache := crypter_key_cache.New(env, refreshFn, clock)
+	quitChan := make(chan struct{})
+	cache.StartRefresher(quitChan)
+
+	env.GetHealthChecker().RegisterShutdownFunction(func(ctx context.Context) error {
+		close(quitChan)
+		return nil
+	})
+
+	return &RemoteCrypter{
+		env:    env,
+		client: client,
+		cache:  cache,
+	}, nil
+}
+
+func refreshKey(ctx context.Context, ck crypter_key_cache.CacheKey, client enpb.EncryptionServiceClient) ([]byte, *sgpb.EncryptionMetadata, error) {
+	req := &enpb.GetEncryptionKeyRequest{
+		Metadata: &enpb.EncryptionKeyMetadata{
+			Id:      ck.KeyID,
+			Version: int64(ck.Version),
+		},
+	}
+
+	resp, err := client.GetEncryptionKey(ctx, req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	md := &sgpb.EncryptionMetadata{
+		EncryptionKeyId: resp.GetKey().GetMetadata().GetId(),
+		Version:         resp.GetKey().GetMetadata().GetVersion(),
+	}
+
+	return resp.GetKey().GetKey(), md, nil
 }
 
 func (c *RemoteCrypter) SetEncryptionConfig(ctx context.Context, req *enpb.SetEncryptionConfigRequest) (*enpb.SetEncryptionConfigResponse, error) {
-	return nil, status.UnimplementedError("RemoteCrypter.SetEncryptionConfig unsupported")
+	return nil, status.UnimplementedError("RemoteCrypter.SetEncryptionConfig() unsupported")
 }
 
 func (c *RemoteCrypter) GetEncryptionConfig(ctx context.Context, req *enpb.GetEncryptionConfigRequest) (*enpb.GetEncryptionConfigResponse, error) {
-	return nil, status.UnimplementedError("RemoteCrypter.GetEncryptionConfig unsupported")
+	return nil, status.UnimplementedError("RemoteCrypter.GetEncryptionConfig() unsupported")
 }
 
 func (c *RemoteCrypter) ActiveKey(ctx context.Context) (*sgpb.EncryptionMetadata, error) {
-	return nil, status.UnimplementedError("unimplemented")
+	loadedKey, err := c.cache.EncryptionKey(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return loadedKey.Metadata, nil
 }
 
 func (c *RemoteCrypter) NewEncryptor(ctx context.Context, d *repb.Digest, w interfaces.CommittedWriteCloser) (interfaces.Encryptor, error) {
-	return nil, status.UnimplementedError("unimplemented")
+	u, err := c.env.GetAuthenticator().AuthenticatedUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	loadedKey, err := c.cache.EncryptionKey(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return crypter.NewEncryptor(ctx, loadedKey, d, w, u.GetGroupID(), crypter.PlainTextChunkSize)
 }
 
 func (c *RemoteCrypter) NewDecryptor(ctx context.Context, d *repb.Digest, r io.ReadCloser, em *sgpb.EncryptionMetadata) (interfaces.Decryptor, error) {
-	return nil, status.UnimplementedError("unimplemented")
+	u, err := c.env.GetAuthenticator().AuthenticatedUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	loadedKey, err := c.cache.DecryptionKey(ctx, em)
+	if err != nil {
+		return nil, err
+	}
+	return crypter.NewDecryptor(ctx, loadedKey, d, r, em, u.GetGroupID(), crypter.PlainTextChunkSize)
 }
 
 func (c *RemoteCrypter) GetEncryptionKey(ctx context.Context, req *enpb.GetEncryptionKeyRequest) (*enpb.GetEncryptionKeyResponse, error) {
-	return nil, status.UnimplementedError("unimplemented")
+	return nil, status.UnimplementedError("RemoteCrypter.GetEncryptionKey() unsupported")
 }
