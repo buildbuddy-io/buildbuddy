b9807457f12edd8bef03bdec62423ae8f324b3ed
Lulu Zhang <lulu@buildbuddy.io>
2025-10-03 11:16:15 -0700

    retry.go: fix logging non-retryable error (#10414)
    Right now we are logging the last error (which can be retryable) when we
    log the
    line "attempt failed and could not be retried due to a non-retryable
    error: "
    
    https://github.com/buildbuddy-io/buildbuddy-internal/issues/5752

--- server/util/retry/retry.go
+++ server/util/retry/retry.go
@@ -3,6 +3,7 @@ package retry
 
 import (
 	"context"
+	"fmt"
 	"math"
 	"time"
 
@@ -236,7 +237,7 @@ func Do[T any](ctx context.Context, opts *Options, fn func(ctx context.Context) 
 			return rsp, nil
 		}
 		if _, ok := err.(*nonRetryableError); ok {
-			logFailedAttempt(lastError, " and could not be retried due to a non-retryable error")
+			logFailedAttempt(err, fmt.Sprintf(" (last error: %s) and could not be retried due to a non-retryable error", lastError))
 			return rsp, err
 		}
 		lastError = err
