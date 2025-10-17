baf0ee8a5604256a19aa465339fbd9315aa4e0d7
Maggie Lou <maggie@buildbuddy.io>
2025-10-17 10:17:13 -0500

    Add debug log for VMConfiguration

--- enterprise/server/remote_execution/containers/firecracker/firecracker.go
+++ enterprise/server/remote_execution/containers/firecracker/firecracker.go
@@ -766,6 +766,7 @@ func NewContainer(ctx context.Context, env environment.Env, task *repb.Execution
 		if err != nil {
 			return nil, err
 		}
+		log.Infof("Computed SHA256 %s for VMConfiguration: %+v", cd.GetHash(), c.vmConfig)
 
 		runnerID := c.id
 		if snaputil.IsChunkedSnapshotSharingEnabled() {
