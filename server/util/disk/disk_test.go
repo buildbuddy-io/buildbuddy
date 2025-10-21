89614c4a1ac6daf56580b2f8e1a6f33e7769b40f
Vanja Pejovic <vanja@buildbuddy.io>
2025-10-21 18:20:57 -0400

    add commit tests

--- server/util/disk/disk_test.go
+++ server/util/disk/disk_test.go
@@ -27,26 +27,36 @@ func TestGetDirUsage(t *testing.T) {
 func TestWriteMover_CloseCleansUp(t *testing.T) {
 	for _, shouldCancel := range []bool{true, false} {
 		t.Run(fmt.Sprintf("cancel=%v", shouldCancel), func(t *testing.T) {
-			dir := testfs.MakeTempDir(t)
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-			path := filepath.Join(dir, "testfile")
-			w, err := disk.FileWriter(ctx, path)
-			require.NoError(t, err)
-			_, err = w.Write([]byte("hello"))
-			require.NoError(t, err)
+			for _, shouldCommit := range []bool{true, false} {
+				t.Run(fmt.Sprintf("commit=%v", shouldCommit), func(t *testing.T) {
+					dir := testfs.MakeTempDir(t)
+					ctx, cancel := context.WithCancel(context.Background())
+					defer cancel()
+					path := filepath.Join(dir, "testfile")
+					w, err := disk.FileWriter(ctx, path)
+					require.NoError(t, err)
+					_, err = w.Write([]byte("hello"))
+					require.NoError(t, err)
 
-			if shouldCancel {
-				// Cancel the context to simulate a timeout or RPC cancellation.
-				// This should cause w.Close to fail to get writer quota, but it
-				// shoud still clean up the temp file.
-				cancel()
-			}
-			w.Close()
-			entries, err := os.ReadDir(dir)
-			require.NoError(t, err)
-			for _, ent := range entries {
-				t.Fatalf("Unexpected file %v", ent.Name())
+					if shouldCommit {
+						require.NoError(t, w.Commit())
+					}
+
+					if shouldCancel {
+						// Cancel the context to simulate a timeout or RPC cancellation.
+						// This should cause w.Close to fail to get writer quota, but it
+						// shoud still clean up the temp file.
+						cancel()
+					}
+					w.Close()
+					entries, err := os.ReadDir(dir)
+					require.NoError(t, err)
+					for _, ent := range entries {
+						if !shouldCommit || ent.Name() != "testfile" {
+							t.Fatalf("Unexpected file %v", ent.Name())
+						}
+					}
+				})
 			}
 		})
 	}
