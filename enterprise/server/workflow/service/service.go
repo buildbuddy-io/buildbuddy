2f13c008fa60a57d65a7c26b919bd76c29483e06
Maggie Lou <maggie@buildbuddy.io>
2025-11-03 11:24:39 -0600

    Add parentheses

--- enterprise/server/workflow/service/service.go
+++ enterprise/server/workflow/service/service.go
@@ -833,7 +833,7 @@ func (ws *workflowService) GetWorkflowHistory(ctx context.Context) (*wfpb.GetWor
 		actionQStr, actionQArgs := q.Build()
 		actionHistoryQArgs = append(actionHistoryQArgs, actionQArgs...)
 		// Wrap as a subquery to support ORDER BY and LIMIT in SQLite UNION queries.
-		actionHistoryQStrs = append(actionHistoryQStrs, "SELECT * FROM ("+actionQStr+")")
+		actionHistoryQStrs = append(actionHistoryQStrs, "(SELECT * FROM ("+actionQStr+"))")
 		summary := &wfpb.ActionHistory_Summary{
 			TotalRuns:       row.TotalRuns,
 			SuccessfulRuns:  row.SuccessfulRuns,
