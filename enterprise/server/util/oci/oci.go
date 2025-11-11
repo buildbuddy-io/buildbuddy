7c0a7ada7716f264e0d29744b5a2ec8c0250a89f
Dan Stowell <dan@buildbuddy.io>
2025-11-11 08:33:14 -0500

    Revert "```"
    This reverts commit 5113330f07b159ad68fa0991769dd28a219bfc41.

--- enterprise/server/util/oci/oci.go
+++ enterprise/server/util/oci/oci.go
@@ -22,6 +22,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/util/claims"
 	"github.com/buildbuddy-io/buildbuddy/server/util/flag"
 	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/lru"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/buildbuddy-io/buildbuddy/server/util/tracing"
 	"github.com/distribution/reference"
@@ -30,12 +31,19 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 	"github.com/google/go-containerregistry/pkg/v1/types"
+	"github.com/jonboulle/clockwork"
 
 	rgpb "github.com/buildbuddy-io/buildbuddy/proto/registry"
 	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
 	gcrname "github.com/google/go-containerregistry/pkg/name"
 	gcr "github.com/google/go-containerregistry/pkg/v1"
 	bspb "google.golang.org/genproto/googleapis/bytestream"
+)
+
+const (
+	// resolveImageDigestLRUMaxEntries limits the number of entries in the image-tag-to-digest cache.
+	resolveImageDigestLRUMaxEntries = 1000
+	resolveImageDigestLRUDuration   = 15 * time.Minute
 )
 
 var (
@@ -247,6 +255,10 @@ type Resolver struct {
 	env environment.Env
 
 	allowedPrivateIPs []*net.IPNet
+
+	mu                  sync.Mutex
+	imageTagToDigestLRU *lru.LRU[tagToDigestEntry]
+	clock               clockwork.Clock
 }
 
 func NewResolver(env environment.Env) (*Resolver, error) {
@@ -258,9 +270,18 @@ func NewResolver(env environment.Env) (*Resolver, error) {
 		}
 		allowedPrivateIPNets = append(allowedPrivateIPNets, ipNet)
 	}
+	imageTagToDigestLRU, err := lru.NewLRU[tagToDigestEntry](&lru.Config[tagToDigestEntry]{
+		SizeFn:  func(_ tagToDigestEntry) int64 { return 1 },
+		MaxSize: int64(resolveImageDigestLRUMaxEntries),
+	})
+	if err != nil {
+		return nil, err
+	}
 	return &Resolver{
-		env:               env,
-		allowedPrivateIPs: allowedPrivateIPNets,
+		env:                 env,
+		imageTagToDigestLRU: imageTagToDigestLRU,
+		allowedPrivateIPs:   allowedPrivateIPNets,
+		clock:               env.GetClock(),
 	}, nil
 }
 
@@ -280,12 +301,7 @@ func (r *Resolver) AuthenticateWithRegistry(ctx context.Context, imageName strin
 	log.CtxInfof(ctx, "Authenticating with registry %q", imageRef.Context().RegistryStr())
 
 	remoteOpts := r.getRemoteOpts(ctx, platform, credentials)
-	// Authenticate doesn't need caching, just needs to verify access
-	fetcher, err := ocicache.NewReadThroughFetcher(r.env, false, remoteOpts...)
-	if err != nil {
-		return status.InternalErrorf("error creating fetcher: %s", err)
-	}
-	_, err = fetcher.Head(ctx, imageRef)
+	_, err = remote.Head(imageRef, remoteOpts...)
 	if err != nil {
 		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
 			return status.PermissionDeniedErrorf("not authorized to access image manifest: %s", err)
@@ -299,7 +315,7 @@ func (r *Resolver) AuthenticateWithRegistry(ctx context.Context, imageName strin
 // If the input image name includes a digest, a canonicalized version of the name is returned.
 // If the input image name refers to a tag (either explictly or implicity), ResolveImageDigest
 // will make a HEAD request to the remote registry.
-// The ReadThroughFetcher keeps an in-memory cache that maps between canonical image names with tags
+// ResolveImageDigest keeps an LRU cache that maps between canonical image names with tags
 // to image names with digests, to reduce the number of HEAD requests.
 func (r *Resolver) ResolveImageDigest(ctx context.Context, imageName string, platform *rgpb.Platform, credentials Credentials) (string, error) {
 	if imageRefWithDigest, err := gcrname.NewDigest(imageName); err == nil {
@@ -310,20 +326,36 @@ func (r *Resolver) ResolveImageDigest(ctx context.Context, imageName string, pla
 		return "", status.InvalidArgumentErrorf("invalid image name %q", imageName)
 	}
 
-	remoteOpts := r.getRemoteOpts(ctx, platform, credentials)
-	// ResolveImageDigest just needs to lookup tag->digest mapping, doesn't need AC/CAS caching
-	fetcher, err := ocicache.NewReadThroughFetcher(r.env, false, remoteOpts...)
-	if err != nil {
-		return "", status.InternalErrorf("error creating fetcher: %s", err)
+	r.mu.Lock()
+	entry, ok := r.imageTagToDigestLRU.Get(tagRef.String())
+	r.mu.Unlock()
+	if ok {
+		if entry.expiration.After(r.clock.Now()) {
+			return entry.nameWithDigest, nil
+		}
+		// Expired; evict and refresh via remote.Head below.
+		r.mu.Lock()
+		r.imageTagToDigestLRU.Remove(tagRef.String())
+		r.mu.Unlock()
 	}
-	desc, err := fetcher.Head(ctx, tagRef)
+
+	remoteOpts := r.getRemoteOpts(ctx, platform, credentials)
+	desc, err := remote.Head(tagRef, remoteOpts...)
 	if err != nil {
 		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
 			return "", status.PermissionDeniedErrorf("not authorized to access image manifest: %s", err)
 		}
 		return "", status.UnavailableErrorf("could not authorize to remote registry: %s", err)
 	}
-	return tagRef.Context().Digest(desc.Digest.String()).String(), nil
+	imageNameWithDigest := tagRef.Context().Digest(desc.Digest.String()).String()
+	entryToAdd := tagToDigestEntry{
+		nameWithDigest: imageNameWithDigest,
+		expiration:     r.clock.Now().Add(resolveImageDigestLRUDuration),
+	}
+	r.mu.Lock()
+	r.imageTagToDigestLRU.Add(tagRef.String(), entryToAdd)
+	r.mu.Unlock()
+	return imageNameWithDigest, nil
 }
 
 func (r *Resolver) Resolve(ctx context.Context, imageName string, platform *rgpb.Platform, credentials Credentials) (gcr.Image, error) {
