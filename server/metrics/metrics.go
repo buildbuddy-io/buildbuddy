906db9410abe82e012e826352b378cd4b27c2a19
Brandon Duffany <brandon@buildbuddy.io>
2025-11-14 13:06:35 -0500

    Add more filecache latency buckets

--- server/metrics/metrics.go
+++ server/metrics/metrics.go
@@ -1657,7 +1657,7 @@ var (
 		Subsystem: "remote_execution",
 		Name:      "file_cache_op_latency_usec",
 		Help:      "Latency of individual file cache operations.",
-		Buckets:   durationUsecBuckets(1*time.Microsecond, 1*time.Second, 10),
+		Buckets:   durationUsecBuckets(1*time.Microsecond, 1*time.Hour, 2),
 	}, []string{
 		OpLabel,
 	})
