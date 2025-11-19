fa6259fe89687dd2d577412d2be1c8d1ac7c05bd
Dan Stowell <dan@buildbuddy.io>
2025-11-19 12:08:26 -0600

    "Add OCI fetcher and registry fetcher implementations"

---
+++ enterprise/server/remote_execution/ocirefactor/fetch/fetch.go
@@ -0,0 +1,433 @@
+package fetch
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/cachetools"
+	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
+	"github.com/buildbuddy-io/buildbuddy/server/util/hash"
+	"github.com/buildbuddy-io/buildbuddy/server/util/log"
+	"github.com/buildbuddy-io/buildbuddy/server/util/proto"
+	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/google/go-containerregistry/pkg/authn"
+	gcrname "github.com/google/go-containerregistry/pkg/name"
+	gcr "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+
+	ocipb "github.com/buildbuddy-io/buildbuddy/proto/ociregistry"
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+	rgpb "github.com/buildbuddy-io/buildbuddy/proto/registry"
+	bspb "google.golang.org/genproto/googleapis/bytestream"
+)
+
+const (
+	// Instance name prefix for OCI cache entries
+	actionResultInstanceName    = interfaces.OCIImageInstanceNamePrefix
+	manifestContentInstanceName = interfaces.OCIImageInstanceNamePrefix + "_manifest_content_"
+
+	// Cache digest function (always SHA256 for OCI)
+	cacheDigestFunction = repb.DigestFunction_SHA256
+
+	// Output file paths for blob metadata in ActionResult
+	blobOutputFilePath         = "_bb_ociregistry_blob_"
+	blobMetadataOutputFilePath = "_bb_ociregistry_blob_metadata_"
+)
+
+// Fetcher is an interface for fetching OCI blobs and manifests.
+type Fetcher interface {
+	// FetchBlob fetches a blob by OCI reference (e.g., gcr.io/foo/bar@sha256:xxx)
+	// Returns an io.ReadCloser that yields compressed bytes (as stored in registry/CAS)
+	FetchBlob(ctx context.Context, ref string, creds *rgpb.Credentials) (io.ReadCloser, error)
+
+	// FetchBlobMetadata returns the size and media type for a blob
+	FetchBlobMetadata(ctx context.Context, ref string, creds *rgpb.Credentials) (int64, string, error)
+
+	// FetchManifest fetches raw manifest bytes for the given reference
+	// Returns whatever manifest exists at the ref (image index or single manifest)
+	// Platform is used for logging/potential future use, but caller is responsible
+	// for platform selection from image indices
+	FetchManifest(ctx context.Context, ref string, platform *repb.Platform, creds *rgpb.Credentials) ([]byte, error)
+}
+
+// parseDigestRef parses an OCI reference that must contain a digest.
+// Returns an error if the reference doesn't contain a digest.
+func parseDigestRef(ref string) (gcrname.Digest, error) {
+	digestRef, err := gcrname.NewDigest(ref)
+	if err != nil {
+		return gcrname.Digest{}, status.InvalidArgumentErrorf("ref must contain digest, got %q: %s", ref, err)
+	}
+	return digestRef, nil
+}
+
+// convertCredentials converts proto credentials to an authn.Authenticator
+func convertCredentials(creds *rgpb.Credentials) authn.Authenticator {
+	if creds == nil || (creds.GetUsername() == "" && creds.GetPassword() == "") {
+		return authn.Anonymous
+	}
+	return &authn.Basic{
+		Username: creds.GetUsername(),
+		Password: creds.GetPassword(),
+	}
+}
+
+// manifestACKey generates an AC key for a manifest, following the pattern from ocicache.
+// Uses hash of (registry, repo, MANIFEST, algorithm, hex, secret) as the digest.
+func manifestACKey(repo gcrname.Repository, refhash gcr.Hash, secret string) (*digest.ACResourceName, error) {
+	s := hash.Strings(
+		repo.RegistryStr(),
+		repo.RepositoryStr(),
+		ocipb.OCIResourceType_MANIFEST.String(),
+		refhash.Algorithm,
+		refhash.Hex,
+		secret,
+	)
+	arDigest, err := digest.Compute(bytes.NewBufferString(s), cacheDigestFunction)
+	if err != nil {
+		return nil, err
+	}
+	return digest.NewACResourceName(
+		arDigest,
+		manifestContentInstanceName,
+		cacheDigestFunction,
+	), nil
+}
+
+// blobMetadataACKey generates an AC key for blob metadata.
+// Uses proto-marshaled OCIActionResultKey as the digest input.
+func blobMetadataACKey(repo gcrname.Repository, hash gcr.Hash) (*digest.ACResourceName, error) {
+	arKey := &ocipb.OCIActionResultKey{
+		Registry:      repo.RegistryStr(),
+		Repository:    repo.RepositoryStr(),
+		ResourceType:  ocipb.OCIResourceType_BLOB,
+		HashAlgorithm: hash.Algorithm,
+		HashHex:       hash.Hex,
+	}
+	arKeyBytes, err := proto.Marshal(arKey)
+	if err != nil {
+		return nil, err
+	}
+	arDigest, err := digest.Compute(bytes.NewReader(arKeyBytes), cacheDigestFunction)
+	if err != nil {
+		return nil, err
+	}
+	return digest.NewACResourceName(
+		arDigest,
+		actionResultInstanceName,
+		cacheDigestFunction,
+	), nil
+}
+
+// blobCASResourceName generates a CAS resource name for a blob.
+// Blobs are stored compressed with ZSTD in the CAS.
+func blobCASResourceName(hash gcr.Hash, contentLength int64) *digest.CASResourceName {
+	blobDigest := &repb.Digest{
+		Hash:      hash.Hex,
+		SizeBytes: contentLength,
+	}
+	rn := digest.NewCASResourceName(
+		blobDigest,
+		"",
+		cacheDigestFunction,
+	)
+	rn.SetCompressor(repb.Compressor_ZSTD)
+	return rn
+}
+
+// CacheFetcher fetches OCI artifacts from BuildBuddy's Action Cache and CAS.
+type CacheFetcher struct {
+	acClient repb.ActionCacheClient
+	bsClient bspb.ByteStreamClient
+	secret   string // Secret to add to cache keys
+}
+
+// NewCacheFetcher creates a new CacheFetcher.
+func NewCacheFetcher(acClient repb.ActionCacheClient, bsClient bspb.ByteStreamClient, secret string) *CacheFetcher {
+	return &CacheFetcher{
+		acClient: acClient,
+		bsClient: bsClient,
+		secret:   secret,
+	}
+}
+
+// FetchManifest fetches a manifest from the Action Cache.
+func (f *CacheFetcher) FetchManifest(ctx context.Context, ref string, platform *repb.Platform, creds *rgpb.Credentials) ([]byte, error) {
+	digestRef, err := parseDigestRef(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	repo := digestRef.Context()
+	hash, err := gcr.NewHash(digestRef.DigestStr())
+	if err != nil {
+		return nil, status.InvalidArgumentErrorf("invalid digest in ref %q: %s", ref, err)
+	}
+
+	arRN, err := manifestACKey(repo, hash, f.secret)
+	if err != nil {
+		log.CtxWarningf(ctx, "Error creating key for manifest %s@%s: %s", repo, hash, err)
+		return nil, err
+	}
+
+	ar, err := cachetools.GetActionResult(ctx, f.acClient, arRN)
+	if err != nil {
+		if !status.IsNotFoundError(err) {
+			log.CtxWarningf(ctx, "Error getting action result for manifest %s@%s: %s", repo, hash, err)
+		}
+		return nil, err
+	}
+
+	meta := ar.GetExecutionMetadata()
+	if meta == nil {
+		log.CtxWarningf(ctx, "Missing execution metadata for manifest %s@%s", repo, hash)
+		return nil, status.InternalErrorf("missing execution metadata for manifest in %q", repo)
+	}
+
+	aux := meta.GetAuxiliaryMetadata()
+	if aux == nil || len(aux) != 1 {
+		log.CtxWarningf(ctx, "Missing auxiliary metadata for manifest %s@%s", repo, hash)
+		return nil, status.InternalErrorf("missing auxiliary metadata for manifest %s@%s", repo, hash)
+	}
+
+	var mc ocipb.OCIManifestContent
+	if err := aux[0].UnmarshalTo(&mc); err != nil {
+		log.CtxWarningf(ctx, "Error unmarshalling manifest content %s@%s: %s", repo, hash, err)
+		return nil, status.InternalErrorf("could not unmarshal metadata for manifest %s@%s: %s", repo, hash, err)
+	}
+
+	log.CtxInfof(ctx, "Cache hit for manifest %s@%s", repo, hash)
+	return mc.GetRaw(), nil
+}
+
+// FetchBlobMetadata fetches blob metadata (size and media type) from the Action Cache.
+func (f *CacheFetcher) FetchBlobMetadata(ctx context.Context, ref string, creds *rgpb.Credentials) (int64, string, error) {
+	digestRef, err := parseDigestRef(ref)
+	if err != nil {
+		return 0, "", err
+	}
+
+	repo := digestRef.Context()
+	hash, err := gcr.NewHash(digestRef.DigestStr())
+	if err != nil {
+		return 0, "", status.InvalidArgumentErrorf("invalid digest in ref %q: %s", ref, err)
+	}
+
+	arRN, err := blobMetadataACKey(repo, hash)
+	if err != nil {
+		return 0, "", err
+	}
+
+	ar, err := cachetools.GetActionResult(ctx, f.acClient, arRN)
+	if err != nil {
+		return 0, "", err
+	}
+
+	// Find the blob metadata digest in the output files
+	var blobMetadataCASDigest *repb.Digest
+	for _, outputFile := range ar.GetOutputFiles() {
+		if outputFile.GetPath() == blobMetadataOutputFilePath {
+			blobMetadataCASDigest = outputFile.GetDigest()
+			break
+		}
+	}
+
+	if blobMetadataCASDigest == nil {
+		return 0, "", status.NotFoundErrorf("missing blob metadata digest for %s", ref)
+	}
+
+	blobMetadataRN := digest.NewCASResourceName(
+		blobMetadataCASDigest,
+		"",
+		cacheDigestFunction,
+	)
+
+	var blobMetadata ocipb.OCIBlobMetadata
+	if err := cachetools.GetBlobAsProto(ctx, f.bsClient, blobMetadataRN, &blobMetadata); err != nil {
+		return 0, "", err
+	}
+
+	log.CtxDebugf(ctx, "Cache hit for blob metadata %s", ref)
+	return blobMetadata.GetContentLength(), blobMetadata.GetContentType(), nil
+}
+
+// FetchBlob fetches a blob from the CAS.
+// Returns a ReadCloser that streams the compressed blob data.
+func (f *CacheFetcher) FetchBlob(ctx context.Context, ref string, creds *rgpb.Credentials) (io.ReadCloser, error) {
+	// First get the metadata to find the content length
+	contentLength, _, err := f.FetchBlobMetadata(ctx, ref, creds)
+	if err != nil {
+		return nil, err
+	}
+
+	digestRef, err := parseDigestRef(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	hash, err := gcr.NewHash(digestRef.DigestStr())
+	if err != nil {
+		return nil, status.InvalidArgumentErrorf("invalid digest in ref %q: %s", ref, err)
+	}
+
+	blobRN := blobCASResourceName(hash, contentLength)
+
+	// Create a pipe to stream the blob data
+	pr, pw := io.Pipe()
+
+	go func() {
+		defer pw.Close()
+		if err := cachetools.GetBlob(ctx, f.bsClient, blobRN, pw); err != nil {
+			pw.CloseWithError(err)
+			return
+		}
+	}()
+
+	log.CtxDebugf(ctx, "Cache hit for blob %s", ref)
+	return pr, nil
+}
+
+// RegistryFetcher fetches OCI artifacts from upstream OCI registries.
+type RegistryFetcher struct {
+	// Optional transport for custom HTTP behavior (mirrors, etc.)
+	transport http.RoundTripper
+}
+
+// NewRegistryFetcher creates a new RegistryFetcher.
+func NewRegistryFetcher(transport http.RoundTripper) *RegistryFetcher {
+	return &RegistryFetcher{
+		transport: transport,
+	}
+}
+
+// getRemoteOpts builds options for go-containerregistry remote operations.
+func (f *RegistryFetcher) getRemoteOpts(ctx context.Context, platform *repb.Platform, creds *rgpb.Credentials) []remote.Option {
+	opts := []remote.Option{
+		remote.WithContext(ctx),
+		remote.WithAuth(convertCredentials(creds)),
+	}
+
+	if platform != nil {
+		// Convert repb.Platform to gcr.Platform by extracting arch/os from properties
+		// For now, we assume simple platform format. In production, you'd parse
+		// platform.Properties to extract "OSFamily", "Arch", etc.
+		opts = append(opts, remote.WithPlatform(gcr.Platform{
+			Architecture: "amd64", // TODO: extract from platform.Properties
+			OS:           "linux",
+		}))
+	}
+
+	if f.transport != nil {
+		opts = append(opts, remote.WithTransport(f.transport))
+	}
+
+	return opts
+}
+
+// FetchManifest fetches a manifest from an upstream OCI registry.
+func (f *RegistryFetcher) FetchManifest(ctx context.Context, ref string, platform *repb.Platform, creds *rgpb.Credentials) ([]byte, error) {
+	imageRef, err := gcrname.ParseReference(ref)
+	if err != nil {
+		return nil, status.InvalidArgumentErrorf("invalid image ref %q: %s", ref, err)
+	}
+
+	log.CtxInfof(ctx, "Fetching manifest from registry for %s", imageRef)
+
+	remoteOpts := f.getRemoteOpts(ctx, platform, creds)
+	puller, err := remote.NewPuller(remoteOpts...)
+	if err != nil {
+		return nil, status.InternalErrorf("error creating puller: %s", err)
+	}
+
+	desc, err := puller.Get(ctx, imageRef)
+	if err != nil {
+		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
+			return nil, status.PermissionDeniedErrorf("not authorized to retrieve manifest: %s", err)
+		}
+		return nil, status.UnavailableErrorf("could not retrieve manifest from registry: %s", err)
+	}
+
+	manifestBytes, err := desc.RawManifest()
+	if err != nil {
+		return nil, status.InternalErrorf("could not get raw manifest bytes: %s", err)
+	}
+
+	log.CtxInfof(ctx, "Successfully fetched manifest from registry for %s (size: %d bytes)", imageRef, len(manifestBytes))
+	return manifestBytes, nil
+}
+
+// FetchBlobMetadata fetches blob metadata from an upstream OCI registry using HEAD request.
+func (f *RegistryFetcher) FetchBlobMetadata(ctx context.Context, ref string, creds *rgpb.Credentials) (int64, string, error) {
+	digestRef, err := parseDigestRef(ref)
+	if err != nil {
+		return 0, "", err
+	}
+
+	log.CtxInfof(ctx, "Fetching blob metadata from registry for %s", digestRef)
+
+	remoteOpts := f.getRemoteOpts(ctx, nil, creds)
+
+	// Use go-containerregistry to get the layer
+	puller, err := remote.NewPuller(remoteOpts...)
+	if err != nil {
+		return 0, "", status.InternalErrorf("error creating puller: %s", err)
+	}
+
+	layer, err := puller.Layer(ctx, digestRef)
+	if err != nil {
+		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
+			return 0, "", status.PermissionDeniedErrorf("not authorized to retrieve blob: %s", err)
+		}
+		return 0, "", status.UnavailableErrorf("could not retrieve blob metadata from registry: %s", err)
+	}
+
+	size, err := layer.Size()
+	if err != nil {
+		return 0, "", status.InternalErrorf("could not get blob size: %s", err)
+	}
+
+	mediaType, err := layer.MediaType()
+	if err != nil {
+		return 0, "", status.InternalErrorf("could not get blob media type: %s", err)
+	}
+
+	log.CtxInfof(ctx, "Successfully fetched blob metadata from registry for %s (size: %d)", digestRef, size)
+	return size, string(mediaType), nil
+}
+
+// FetchBlob fetches a blob from an upstream OCI registry.
+func (f *RegistryFetcher) FetchBlob(ctx context.Context, ref string, creds *rgpb.Credentials) (io.ReadCloser, error) {
+	digestRef, err := parseDigestRef(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	log.CtxInfof(ctx, "Fetching blob from registry for %s", digestRef)
+
+	remoteOpts := f.getRemoteOpts(ctx, nil, creds)
+
+	puller, err := remote.NewPuller(remoteOpts...)
+	if err != nil {
+		return nil, status.InternalErrorf("error creating puller: %s", err)
+	}
+
+	layer, err := puller.Layer(ctx, digestRef)
+	if err != nil {
+		if t, ok := err.(*transport.Error); ok && t.StatusCode == http.StatusUnauthorized {
+			return nil, status.PermissionDeniedErrorf("not authorized to retrieve blob: %s", err)
+		}
+		return nil, status.UnavailableErrorf("could not retrieve blob from registry: %s", err)
+	}
+
+	// Get the compressed data (as stored in the registry)
+	rc, err := layer.Compressed()
+	if err != nil {
+		return nil, status.InternalErrorf("could not get compressed blob data: %s", err)
+	}
+
+	log.CtxInfof(ctx, "Successfully fetched blob from registry for %s", digestRef)
+	return rc, nil
+}
