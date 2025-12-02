3bcd7a571f06845ef41fbd4bef301ff57630e574
Dan Stowell <dan@buildbuddy.io>
2025-12-02 10:17:25 -0600

    Update QA test configuration and runner script

--- tools/dev_qa_test/dev_qa_test_runner.sh
+++ tools/dev_qa_test/dev_qa_test_runner.sh
@@ -113,7 +113,7 @@ build:dev_qa_test --build_metadata=TAGS=qa-integration-test
 build:dev_qa_test --workspace_status_command=
 build:dev_qa_test --compilation_mode=fastbuild
 build:dev_qa_test --host_compilation_mode=fastbuild
-build:dev_qa_test --test_tag_filters=-block-network
+build:dev_qa_test --test_tag_filters=-block-network,-performance,-webdriver,-docker,-bare
 EOF
 
 cat >> .bazelrc <<EOF
