4193d71aff12ccefa3c032c7a62f468b5f080ea3
Dan Stowell <dan@buildbuddy.io>
2025-12-04 15:36:45 -0600

    Add helper functions and refactor tests for OCI fetcher

--- enterprise/server/oci/ocifetcher/ocifetcher_test.go
+++ enterprise/server/oci/ocifetcher/ocifetcher_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/testregistry"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
 	"github.com/google/go-cmp/cmp"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/stretchr/testify/require"
 
 	ofpb "github.com/buildbuddy-io/buildbuddy/proto/oci_fetcher"
@@ -24,11 +25,42 @@ func localhostIPs(t *testing.T) []*net.IPNet {
 	return []*net.IPNet{ipv4Net, ipv6Net}
 }
 
-func TestFetchManifestMetadata_NoAuth(t *testing.T) {
-	reg := testregistry.Run(t, testregistry.Opts{})
-	imageName, img := reg.PushNamedImage(t, "test-image", nil)
+func newTestClient(t *testing.T) *ocifetcher.Client {
+	return ocifetcher.NewClient(localhostIPs(t), nil)
+}
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+func pushTestImage(t *testing.T, opts testregistry.Opts, pushCreds *testregistry.BasicAuthCreds) (string, v1.Image) {
+	reg := testregistry.Run(t, opts)
+	imageName, img := reg.PushNamedImage(t, "test-image", pushCreds)
+	return imageName, img
+}
+
+func firstLayer(t *testing.T, img v1.Image) v1.Layer {
+	layers, err := img.Layers()
+	require.NoError(t, err)
+	require.NotEmpty(t, layers)
+	return layers[0]
+}
+
+func blobRefForLayer(t *testing.T, imageName string, layer v1.Layer) string {
+	layerDigest, err := layer.Digest()
+	require.NoError(t, err)
+	return imageName + "@" + layerDigest.String()
+}
+
+func layerBytes(t *testing.T, layer v1.Layer) []byte {
+	layerReader, err := layer.Compressed()
+	require.NoError(t, err)
+	data, err := io.ReadAll(layerReader)
+	require.NoError(t, err)
+	require.NoError(t, layerReader.Close())
+	return data
+}
+
+func TestFetchManifestMetadata_NoAuth(t *testing.T) {
+	imageName, img := pushTestImage(t, testregistry.Opts{}, nil)
+
+	client := newTestClient(t)
 	resp, err := client.FetchManifestMetadata(context.Background(), &ofpb.FetchManifestMetadataRequest{
 		Ref: imageName,
 	})
@@ -46,12 +78,11 @@ func TestFetchManifestMetadata_WithValidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, img := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, img := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	resp, err := client.FetchManifestMetadata(context.Background(), &ofpb.FetchManifestMetadataRequest{
 		Ref: imageName,
 		Credentials: &rgpb.Credentials{
@@ -73,12 +104,11 @@ func TestFetchManifestMetadata_WithInvalidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, _ := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, _ := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	_, err := client.FetchManifestMetadata(context.Background(), &ofpb.FetchManifestMetadataRequest{
 		Ref: imageName,
 		Credentials: &rgpb.Credentials{
@@ -95,12 +125,11 @@ func TestFetchManifestMetadata_MissingCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, _ := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, _ := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	_, err := client.FetchManifestMetadata(context.Background(), &ofpb.FetchManifestMetadataRequest{
 		Ref: imageName,
 		// No credentials provided
@@ -110,10 +139,9 @@ func TestFetchManifestMetadata_MissingCredentials(t *testing.T) {
 }
 
 func TestFetchManifest_NoAuth(t *testing.T) {
-	reg := testregistry.Run(t, testregistry.Opts{})
-	imageName, img := reg.PushNamedImage(t, "test-image", nil)
+	imageName, img := pushTestImage(t, testregistry.Opts{}, nil)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	resp, err := client.FetchManifest(context.Background(), &ofpb.FetchManifestRequest{
 		Ref: imageName,
 	})
@@ -132,12 +160,11 @@ func TestFetchManifest_WithValidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, img := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, img := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	resp, err := client.FetchManifest(context.Background(), &ofpb.FetchManifestRequest{
 		Ref: imageName,
 		Credentials: &rgpb.Credentials{
@@ -160,12 +187,11 @@ func TestFetchManifest_WithInvalidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, _ := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, _ := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
 
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	_, err := client.FetchManifest(context.Background(), &ofpb.FetchManifestRequest{
 		Ref: imageName,
 		Credentials: &rgpb.Credentials{
@@ -178,25 +204,12 @@ func TestFetchManifest_WithInvalidCredentials(t *testing.T) {
 }
 
 func TestFetchBlob_NoAuth(t *testing.T) {
-	reg := testregistry.Run(t, testregistry.Opts{})
-	imageName, img := reg.PushNamedImage(t, "test-image", nil)
+	imageName, img := pushTestImage(t, testregistry.Opts{}, nil)
+	layer := firstLayer(t, img)
+	blobRef := blobRefForLayer(t, imageName, layer)
+	expectedData := layerBytes(t, layer)
 
-	layers, err := img.Layers()
-	require.NoError(t, err)
-	require.NotEmpty(t, layers)
-
-	layerDigest, err := layers[0].Digest()
-	require.NoError(t, err)
-
-	blobRef := imageName + "@" + layerDigest.String()
-
-	layerReader, err := layers[0].Compressed()
-	require.NoError(t, err)
-	expectedData, err := io.ReadAll(layerReader)
-	require.NoError(t, err)
-	require.NoError(t, layerReader.Close())
-
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	stream, err := client.FetchBlob(context.Background(), &ofpb.FetchBlobRequest{
 		Ref: blobRef,
 	})
@@ -218,25 +231,12 @@ func TestFetchBlob_NoAuth(t *testing.T) {
 }
 
 func TestReadBlob_NoAuth(t *testing.T) {
-	reg := testregistry.Run(t, testregistry.Opts{})
-	imageName, img := reg.PushNamedImage(t, "test-image", nil)
+	imageName, img := pushTestImage(t, testregistry.Opts{}, nil)
+	layer := firstLayer(t, img)
+	blobRef := blobRefForLayer(t, imageName, layer)
+	expectedData := layerBytes(t, layer)
 
-	layers, err := img.Layers()
-	require.NoError(t, err)
-	require.NotEmpty(t, layers)
-
-	layerDigest, err := layers[0].Digest()
-	require.NoError(t, err)
-
-	blobRef := imageName + "@" + layerDigest.String()
-
-	layerReader, err := layers[0].Compressed()
-	require.NoError(t, err)
-	expectedData, err := io.ReadAll(layerReader)
-	require.NoError(t, err)
-	require.NoError(t, layerReader.Close())
-
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	rc, err := ocifetcher.ReadBlob(context.Background(), client, blobRef, nil)
 	require.NoError(t, err)
 	defer rc.Close()
@@ -254,22 +254,12 @@ func TestReadBlob_WithValidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, img := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, img := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
+	blobRef := blobRefForLayer(t, imageName, firstLayer(t, img))
 
-	layers, err := img.Layers()
-	require.NoError(t, err)
-	require.NotEmpty(t, layers)
-
-	layerDigest, err := layers[0].Digest()
-	require.NoError(t, err)
-
-	// Construct blob reference
-	blobRef := imageName + "@" + layerDigest.String()
-
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	rc, err := ocifetcher.ReadBlob(context.Background(), client, blobRef, &rgpb.Credentials{
 		Username: "testuser",
 		Password: "testpass",
@@ -286,22 +276,12 @@ func TestReadBlob_WithInvalidCredentials(t *testing.T) {
 		Username: "testuser",
 		Password: "testpass",
 	}
-	reg := testregistry.Run(t, testregistry.Opts{
+	imageName, img := pushTestImage(t, testregistry.Opts{
 		Creds: creds,
-	})
-	imageName, img := reg.PushNamedImage(t, "test-image", creds)
+	}, creds)
+	blobRef := blobRefForLayer(t, imageName, firstLayer(t, img))
 
-	// Get the layer digest from the image
-	layers, err := img.Layers()
-	require.NoError(t, err)
-	require.NotEmpty(t, layers)
-
-	layerDigest, err := layers[0].Digest()
-	require.NoError(t, err)
-
-	blobRef := imageName + "@" + layerDigest.String()
-
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 	_, err = ocifetcher.ReadBlob(context.Background(), client, blobRef, &rgpb.Credentials{
 		Username: "wronguser",
 		Password: "wrongpass",
@@ -311,25 +291,12 @@ func TestReadBlob_WithInvalidCredentials(t *testing.T) {
 }
 
 func TestReadBlob_BufferSizes(t *testing.T) {
-	reg := testregistry.Run(t, testregistry.Opts{})
-	imageName, img := reg.PushNamedImage(t, "test-image", nil)
+	imageName, img := pushTestImage(t, testregistry.Opts{}, nil)
+	layer := firstLayer(t, img)
+	blobRef := blobRefForLayer(t, imageName, layer)
+	expectedData := layerBytes(t, layer)
 
-	layers, err := img.Layers()
-	require.NoError(t, err)
-	require.NotEmpty(t, layers)
-
-	layerDigest, err := layers[0].Digest()
-	require.NoError(t, err)
-
-	blobRef := imageName + "@" + layerDigest.String()
-
-	layerReader, err := layers[0].Compressed()
-	require.NoError(t, err)
-	expectedData, err := io.ReadAll(layerReader)
-	require.NoError(t, err)
-	layerReader.Close()
-
-	client := ocifetcher.NewClient(localhostIPs(t), nil)
+	client := newTestClient(t)
 
 	for _, readSize := range []int{
 		1,           // tiny reads
