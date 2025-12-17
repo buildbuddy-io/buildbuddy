dd91dc079d8b7934857fc058120398b823b82259
Iain Macdonald <xiainx@gmail.com>
2025-12-17 13:00:40 -0800

    Implement AuthService.GetPublicKeys()

--- enterprise/server/auth_service/auth_service_test.go
+++ enterprise/server/auth_service/auth_service_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testgrpc"
 	"github.com/buildbuddy-io/buildbuddy/server/util/authutil"
 	"github.com/buildbuddy-io/buildbuddy/server/util/status"
+	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/metadata"
 
 	authpb "github.com/buildbuddy-io/buildbuddy/proto/auth"
@@ -36,4 +38,41 @@ func TestAuthenticateWrongCreds(t *testing.T) {
 	service := AuthService{authenticator: testauth.NewTestAuthenticator(testauth.TestUsers("foo", "bar"))}
 	_, err := service.Authenticate(contextWithApiKey(t, "baz"), &authpb.AuthenticateRequest{})
 	assert.True(t, status.IsUnauthenticatedError(err))
+}
+
+func TestGetPublicKeys_NoKeys(t *testing.T) {
+	service := AuthService{}
+	resp, err := service.GetPublicKeys(t.Context(), &authpb.GetPublicKeysRequest{})
+	require.NoError(t, err)
+	assert.Empty(t, resp.PublicKeys)
+}
+
+func TestGetPublicKeys_OnlyOldKey(t *testing.T) {
+	flags.Set(t, "auth.jwt_rsa_public_key", "old-public-key")
+	service := AuthService{}
+	resp, err := service.GetPublicKeys(t.Context(), &authpb.GetPublicKeysRequest{})
+	require.NoError(t, err)
+	require.Len(t, resp.PublicKeys, 1)
+	assert.Equal(t, "old-public-key", resp.PublicKeys[0].GetKey())
+}
+
+func TestGetPublicKeys_OnlyNewKey(t *testing.T) {
+	flags.Set(t, "auth.new_jwt_rsa_public_key", "new-public-key")
+	service := AuthService{}
+	resp, err := service.GetPublicKeys(t.Context(), &authpb.GetPublicKeysRequest{})
+	require.NoError(t, err)
+	require.Len(t, resp.PublicKeys, 1)
+	assert.Equal(t, "new-public-key", resp.PublicKeys[0].GetKey())
+}
+
+func TestGetPublicKeys_BothKeys(t *testing.T) {
+	flags.Set(t, "auth.jwt_rsa_public_key", "old-public-key")
+	flags.Set(t, "auth.new_jwt_rsa_public_key", "new-public-key")
+	service := AuthService{}
+	resp, err := service.GetPublicKeys(t.Context(), &authpb.GetPublicKeysRequest{})
+	require.NoError(t, err)
+	require.Len(t, resp.PublicKeys, 2)
+	// New key should come first
+	assert.Equal(t, "new-public-key", resp.PublicKeys[0].GetKey())
+	assert.Equal(t, "old-public-key", resp.PublicKeys[1].GetKey())
 }
