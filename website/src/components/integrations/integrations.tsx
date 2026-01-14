95a74f7d6c142a253627e1b21db036828ab5118e
Zoey Greer <zoey@buildbuddy.io>
2026-01-14 12:29:44 -0500

    don't need width and height specified in html

--- website/src/components/integrations/integrations.tsx
+++ website/src/components/integrations/integrations.tsx
@@ -16,7 +16,7 @@ function Component() {
           <Image
             alt="Github Integration Octocat"
             className={styles.logo}
-            style={{ marginRight: "-32px", width: "72px", height: "100px", padding: "20px 0" }}
+            style={{ marginRight: "-22px", padding: "20px 0" }}
             img={require("../../../static/img/github-image.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
@@ -24,17 +24,15 @@ function Component() {
           <Image
             alt="Github Integration"
             className={styles.logo}
-            style={{ width: "146px", height: "100px", padding: "20px 0" }}
+            style={{ padding: "20px 0" }}
             img={require("../../../static/img/github-text.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
           />
-          <img alt="Slack Integration" width="244px" height="100px" className={styles.logo} src="/img/slack.svg" />
-          <img alt="Gitlab Integration" width="226px" height="100px" className={styles.logo} src="/img/gitlab.svg" />
+          <img alt="Slack Integration" className={styles.logo} src="/img/slack.svg" />
+          <img alt="Gitlab Integration" className={styles.logo} src="/img/gitlab.svg" />
           <img
             alt="Buildkite Integration"
-            width="247px"
-            height="100px"
             className={styles.logo}
             style={{ padding: "28px 0" }}
             src="/img/buildkite.svg"
@@ -42,7 +40,7 @@ function Component() {
           <Image
             alt="CircleCI Integration"
             className={styles.logo}
-            style={{ width: "240px", height: "100px", padding: "16px 0" }}
+            style={{ padding: "16px 0" }}
             img={require("../../../static/img/circleci.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
@@ -50,15 +48,13 @@ function Component() {
           <Image
             alt="Travis Integration"
             className={styles.logo}
-            style={{ width: "243px", height: "100px", padding: "12px 0" }}
+            style={{ padding: "12px 0" }}
             img={require("../../../static/img/travis.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
           />
           <img
             alt="Jenkins Integration"
-            width="211px"
-            height="100px"
             className={styles.logo}
             style={{ padding: "16px 0" }}
             src="/img/jenkins.svg"
@@ -66,7 +62,6 @@ function Component() {
           <Image
             alt="GCP Integration"
             className={styles.logo}
-            style={{ width: "392px", height: "100px" }}
             img={require("../../../static/img/gcp.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
@@ -74,7 +69,7 @@ function Component() {
           <Image
             alt="AWS Integration"
             className={styles.logo}
-            style={{ width: "87px", height: "100px", padding: "24px 0" }}
+            style={{ padding: "24px 0" }}
             img={require("../../../static/img/aws.png")}
             shouldAutoDownload={() => true}
             threshold={10000}
