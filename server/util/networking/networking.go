59daa8adc706027850e3733795a3d324f4917158
Iain Macdonald <xiainx@gmail.com>
2026-01-20 18:13:34 -0800

    Make TCPMSS claming optional (not all kernels support it)

--- server/util/networking/networking.go
+++ server/util/networking/networking.go
@@ -928,17 +928,20 @@ func CreateVMNetwork(ctx context.Context, tapDeviceName, tapAddr, vmIP string) (
 	}
 
 	// Rewrite SYN packets sent through the host to have an MTU equal to the
-	// path MTU because GCP machines have smaller MTUs.
+	// path MTU because GCP machines have smaller MTUs. This requires the
+	// xt_TCPMSS kernel module, which may not be available in all environments.
 	if err := runCommand(ctx, "iptables", "-t", "mangle", "-A", "FORWARD", "-p",
 		"tcp", "--tcp-flags", "SYN,RST", "SYN", "-j", "TCPMSS",
 		"--clamp-mss-to-pmtu"); err != nil {
-		return nil, status.WrapError(err, "set up TCPMSS clamping")
+		log.CtxWarningf(ctx, "Failed to set up TCPMSS clamping (xt_TCPMSS "+
+			"module may not be available): %s", err)
+	} else {
+		cleanupStack = append(cleanupStack, func(ctx context.Context) error {
+			return runCommand(ctx, "iptables", "-t", "mangle", "-D", "FORWARD",
+				"-p", "tcp", "--tcp-flags", "SYN,RST", "SYN", "-j", "TCPMSS",
+				"--clamp-mss-to-pmtu")
+		})
 	}
-	cleanupStack = append(cleanupStack, func(ctx context.Context) error {
-		return runCommand(ctx, "iptables", "-t", "mangle", "-D", "FORWARD",
-			"-p", "tcp", "--tcp-flags", "SYN,RST", "SYN", "-j", "TCPMSS",
-			"--clamp-mss-to-pmtu")
-	})
 
 	v := &VMNetwork{
 		netns:    netns,
