d7a9566a26f1ccd9dee6082eab31d0d9e1d31eaa
Dan Stowell <dan@buildbuddy.io>
2026-02-09 16:42:28 +0000

    register ocifetcher after local clients

--- enterprise/server/cmd/server/main.go
+++ enterprise/server/cmd/server/main.go
@@ -302,9 +302,6 @@ func main() {
 	if err := ociregistry.Register(realEnv); err != nil {
 		log.Fatalf("%v", err)
 	}
-	if err := ocifetcher.RegisterServer(realEnv); err != nil {
-		log.Fatalf("%v", err)
-	}
 
 	executionService := execution_service.NewExecutionService(realEnv)
 	realEnv.SetExecutionService(executionService)
@@ -331,6 +328,10 @@ func main() {
 	}
 
 	libmain.RegisterLocalServersAndClients(realEnv)
+
+	if err := ocifetcher.RegisterServer(realEnv); err != nil {
+		log.Fatalf("%v", err)
+	}
 
 	libmain.StartAndRunServices(realEnv) // Returns after graceful shutdown
 }
