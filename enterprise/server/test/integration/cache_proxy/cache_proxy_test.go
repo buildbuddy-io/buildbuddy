9cd3d7840d233062db7c049072245f5e3a6de135
Iain Macdonald <xiainx@gmail.com>
2026-02-17 13:42:52 -0800

    Actually capture the JWT

--- enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
+++ enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
@@ -1,14 +1,19 @@
 package cache_proxy_test
 
 import (
+	"context"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/test/integration/remote_execution/rbetest"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testkeys"
 	"github.com/buildbuddy-io/buildbuddy/server/util/claims"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_server"
 	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 
 	enpb "github.com/buildbuddy-io/buildbuddy/proto/encryption"
@@ -31,14 +36,31 @@ func enableEncryption(t *testing.T, rbe *rbetest.Env, userID string) {
 	require.True(t, getResp.Enabled, "Encryption should be enabled")
 }
 
-func TestFindMissing_ES256Auth(t *testing.T) {
+func TestES256Auth(t *testing.T) {
 	keyPair := testkeys.GenerateES256KeyPair(t)
 	flags.Set(t, "auth.jwt_es256_private_key", keyPair.PrivateKeyPEM)
 	flags.Set(t, "auth.remote.use_es256_jwts", true)
 	require.NoError(t, claims.Init())
 
+	var mu sync.Mutex
+	var capturedJWT string
+	jwtInterceptor := func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		if md, ok := metadata.FromIncomingContext(ctx); ok {
+			if vals := md.Get("x-buildbuddy-jwt"); len(vals) > 0 {
+				mu.Lock()
+				capturedJWT = vals[len(vals)-1]
+				mu.Unlock()
+			}
+		}
+		return handler(ctx, req)
+	}
+
 	rbe := rbetest.NewRBETestEnv(t)
-	rbe.AddBuildBuddyServer()
+	rbe.AddBuildBuddyServerWithOptions(&rbetest.BuildBuddyServerOptions{
+		GRPCServerConfig: grpc_server.GRPCServerConfig{
+			ExtraChainedUnaryInterceptors: []grpc.UnaryServerInterceptor{jwtInterceptor},
+		},
+	})
 
 	cas := rbe.AddCacheProxy().GetContentAddressableStorageClient()
 	req := repb.FindMissingBlobsRequest{
@@ -51,30 +73,15 @@ func TestFindMissing_ES256Auth(t *testing.T) {
 	resp, err := cas.FindMissingBlobs(ctx, &req)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(resp.MissingBlobDigests))
-}
 
-func TestFindMissing_ES256Auth_Encryption(t *testing.T) {
-	keyPair := testkeys.GenerateES256KeyPair(t)
-	flags.Set(t, "auth.jwt_es256_private_key", keyPair.PrivateKeyPEM)
-	flags.Set(t, "auth.remote.use_es256_jwts", true)
-	require.NoError(t, claims.Init())
+	mu.Lock()
+	jwtStr := capturedJWT
+	mu.Unlock()
+	require.NotEmpty(t, jwtStr, "expected a JWT to be forwarded to the backend")
 
-	rbe := rbetest.NewRBETestEnv(t)
-	rbe.AddBuildBuddyServer()
-
-	enableEncryption(t, rbe, rbe.UserID1)
-
-	cas := rbe.AddCacheProxy().GetContentAddressableStorageClient()
-	req := repb.FindMissingBlobsRequest{
-		BlobDigests: []*repb.Digest{
-			{Hash: strings.Repeat("a", 64), SizeBytes: 1},
-		},
-	}
-
-	ctx := metadata.AppendToOutgoingContext(t.Context(), "x-buildbuddy-api-key", rbe.APIKey1)
-	resp, err := cas.FindMissingBlobs(ctx, &req)
+	token, _, err := new(jwt.Parser).ParseUnverified(jwtStr, &claims.Claims{})
 	require.NoError(t, err)
-	require.Equal(t, 1, len(resp.MissingBlobDigests))
+	require.Equal(t, "ES256", token.Method.Alg())
 }
 
 func TestFindMissing_Encryption(t *testing.T) {
