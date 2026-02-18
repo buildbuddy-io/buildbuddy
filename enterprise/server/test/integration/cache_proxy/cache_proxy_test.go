050adc3da051efd7e27eba5a2e47fc234d5c1386
Iain Macdonald <xiainx@gmail.com>
2026-02-17 18:20:31 -0800

    Disable JWT reparsing in other tests too

--- enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
+++ enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
@@ -44,6 +44,7 @@ func TestES256Auth(t *testing.T) {
 	keyPair := testkeys.GenerateES256KeyPair(t)
 	flags.Set(t, "auth.jwt_es256_private_key", keyPair.PrivateKeyPEM)
 	flags.Set(t, "auth.remote.use_es256_jwts", true)
+	flags.Set(t, "auth.reparse_jwts", false)
 	require.NoError(t, claims.Init())
 
 	var mu sync.Mutex
