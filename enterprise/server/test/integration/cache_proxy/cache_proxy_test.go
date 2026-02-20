f5f7650d5d6388dc3295e985957be2b079fe4317
Iain Macdonald <iain@buildbuddy.io>
2026-02-20 12:45:00 -0800

    Add a test for RBE with executors talking to a Cache Proxy (#11342)
    Related issues:
    https://github.com/buildbuddy-io/buildbuddy-internal/issues/4539

--- enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
+++ enterprise/server/test/integration/cache_proxy/cache_proxy_test.go
@@ -2,13 +2,17 @@ package cache_proxy_test
 
 import (
 	"context"
+	"fmt"
+	"runtime"
 	"strings"
 	"sync"
 	"testing"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/test/integration/remote_execution/rbetest"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testkeys"
+	"github.com/buildbuddy-io/buildbuddy/server/util/authutil"
 	"github.com/buildbuddy-io/buildbuddy/server/util/claims"
+	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_client"
 	"github.com/buildbuddy-io/buildbuddy/server/util/grpc_server"
 	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
 	"github.com/golang-jwt/jwt/v4"
@@ -40,13 +44,14 @@ func TestES256Auth(t *testing.T) {
 	keyPair := testkeys.GenerateES256KeyPair(t)
 	flags.Set(t, "auth.jwt_es256_private_key", keyPair.PrivateKeyPEM)
 	flags.Set(t, "auth.remote.use_es256_jwts", true)
+	flags.Set(t, "auth.reparse_jwts", false)
 	require.NoError(t, claims.Init())
 
 	var mu sync.Mutex
 	var capturedJWT string
 	jwtInterceptor := func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		if md, ok := metadata.FromIncomingContext(ctx); ok {
-			if vals := md.Get("x-buildbuddy-jwt"); len(vals) > 0 {
+			if vals := md.Get(authutil.ContextTokenStringKey); len(vals) > 0 {
 				mu.Lock()
 				capturedJWT = vals[len(vals)-1]
 				mu.Unlock()
@@ -69,7 +74,7 @@ func TestES256Auth(t *testing.T) {
 		},
 	}
 
-	ctx := metadata.AppendToOutgoingContext(t.Context(), "x-buildbuddy-api-key", rbe.APIKey1)
+	ctx := metadata.AppendToOutgoingContext(t.Context(), authutil.APIKeyHeader, rbe.APIKey1)
 	resp, err := cas.FindMissingBlobs(ctx, &req)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(resp.MissingBlobDigests))
@@ -82,6 +87,42 @@ func TestES256Auth(t *testing.T) {
 	token, _, err := new(jwt.Parser).ParseUnverified(jwtStr, &claims.Claims{})
 	require.NoError(t, err)
 	require.Equal(t, "ES256", token.Method.Alg())
+}
+
+func TestES256Auth_RemoteExecution(t *testing.T) {
+	// TODO(http://go/b/4539): enable this test
+	t.Skip()
+
+	keyPair := testkeys.GenerateES256KeyPair(t)
+	flags.Set(t, "auth.jwt_es256_private_key", keyPair.PrivateKeyPEM)
+	flags.Set(t, "auth.remote.use_es256_jwts", true)
+	flags.Set(t, "auth.reparse_jwts", false)
+	require.NoError(t, claims.Init())
+
+	rbe := rbetest.NewRBETestEnv(t)
+	rbe.AddBuildBuddyServer()
+	proxy := rbe.AddCacheProxy()
+	conn, err := grpc_client.DialSimple(
+		fmt.Sprintf("grpc://localhost:%d", proxy.Port))
+	require.NoError(t, err)
+	rbe.AddExecutorWithOptions(t, &rbetest.ExecutorOptions{
+		Name:      "executor",
+		CacheConn: conn,
+	})
+
+	cmd := rbe.Execute(&repb.Command{
+		Arguments: []string{"sh", "-c", "echo hello"},
+		Platform: &repb.Platform{
+			Properties: []*repb.Platform_Property{
+				{Name: "container-image", Value: "none"},
+				{Name: "OSFamily", Value: runtime.GOOS},
+				{Name: "Arch", Value: runtime.GOARCH},
+			},
+		},
+	}, &rbetest.ExecuteOpts{APIKey: rbe.APIKey1})
+	res := cmd.Wait()
+	require.Equal(t, 0, res.ExitCode)
+	require.Equal(t, "hello\n", res.Stdout)
 }
 
 func TestFindMissing_Encryption(t *testing.T) {
@@ -97,7 +138,7 @@ func TestFindMissing_Encryption(t *testing.T) {
 		},
 	}
 
-	ctx := metadata.AppendToOutgoingContext(t.Context(), "x-buildbuddy-api-key", rbe.APIKey1)
+	ctx := metadata.AppendToOutgoingContext(t.Context(), authutil.APIKeyHeader, rbe.APIKey1)
 	resp, err := cas.FindMissingBlobs(ctx, &req)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(resp.MissingBlobDigests))
