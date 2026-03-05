9764e1ec4bc32e381f0863149191ddb6d1e87a65
Vadim Berezniker <vadim@buildbuddy.io>
2026-03-05 11:23:11 -0800

    get rid of dead flag

--- enterprise/server/backends/userdb/userdb_test.go
+++ enterprise/server/backends/userdb/userdb_test.go
@@ -1142,13 +1142,13 @@ func TestCreateAndGetAPIKey(t *testing.T) {
 	adminOnlyKey, err := adb.CreateAPIKey(
 		ctx1, groupID1, "Admin-only key",
 		[]cappb.Capability{cappb.Capability_CACHE_WRITE},
-		0, /*=expiresIn*/
+		0,    /*=expiresIn*/
 		false /*=visibleToDevelopers*/)
 	require.NoError(t, err)
 	developerKey, err := adb.CreateAPIKey(
 		ctx1, groupID1, "Developer key",
 		[]cappb.Capability{cappb.Capability_CAS_WRITE},
-		0, /*=expiresIn*/
+		0,   /*=expiresIn*/
 		true /*=visibleToDevelopers*/)
 	require.NoError(t, err)
 
@@ -1195,7 +1195,7 @@ func TestCreateAndGetAPIKey(t *testing.T) {
 	_, err = adb.CreateAPIKey(
 		ctx2, groupID1, "test-label-2",
 		[]cappb.Capability{cappb.Capability_CACHE_WRITE},
-		0, /*=expiresIn*/
+		0,    /*=expiresIn*/
 		false /*=visibleToDevelopers*/)
 	require.Truef(
 		t, status.IsPermissionDeniedError(err),
@@ -1205,7 +1205,7 @@ func TestCreateAndGetAPIKey(t *testing.T) {
 	_, err = adb.CreateAPIKey(
 		ctx3, groupID1, "test-label-3",
 		[]cappb.Capability{cappb.Capability_CACHE_WRITE},
-		0, /*=expiresIn*/
+		0,    /*=expiresIn*/
 		false /*=visibleToDevelopers*/)
 	require.Truef(
 		t, status.IsPermissionDeniedError(err),
@@ -2162,7 +2162,6 @@ func TestGroupMembershipAuditLogs(t *testing.T) {
 }
 
 func TestUserListAuditLogs(t *testing.T) {
-	flags.Set(t, "database.create_user_list_tables", true)
 	env := newTestEnv(t)
 	al := testauditlog.New(t)
 	env.SetAuditLogger(al)
@@ -2225,7 +2224,6 @@ func TestUserListAuditLogs(t *testing.T) {
 }
 
 func TestUserListMembershipAuditLogs(t *testing.T) {
-	flags.Set(t, "database.create_user_list_tables", true)
 	env := newTestEnv(t)
 	al := testauditlog.New(t)
 	env.SetAuditLogger(al)
@@ -2375,7 +2373,7 @@ func TestChildGroupAuth(t *testing.T) {
 	key1, err := env.GetAuthDB().CreateAPIKey(
 		ctx1, us1Group.GroupID, "admin",
 		[]cappb.Capability{cappb.Capability_ORG_ADMIN},
-		0, /*=expiresIn*/
+		0,    /*=expiresIn*/
 		false /*=visibleToDevelopers*/)
 	require.NoError(t, err)
 	adminCtx1 := env.GetAuthenticator().AuthContextFromAPIKey(ctx, key1.Value)
@@ -2386,7 +2384,7 @@ func TestChildGroupAuth(t *testing.T) {
 	key2, err := env.GetAuthDB().CreateAPIKey(
 		ctx2, us2Group.GroupID, "admin",
 		[]cappb.Capability{cappb.Capability_ORG_ADMIN},
-		0, /*=expiresIn*/
+		0,    /*=expiresIn*/
 		false /*=visibleToDevelopers*/)
 	require.NoError(t, err)
 
