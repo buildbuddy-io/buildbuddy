47465fa2acf827859316860f724b1dea1d5a44e1
Tyler Williams <williams.tyler@gmail.com>
2026-03-16 10:17:57 -0700

    Set BUILDBUDDY_RUN_ID env var (#11584)
    I think this should be set in the env so that non-bazel tools can pick
    it up and send it back via PARENT_RUN_ID metadata to show up as linked
    workflow child invocations. LMK if there's a better way / something I'm
    missing here?

--- enterprise/server/util/ci_runner_env/ci_runner_env.go
+++ enterprise/server/util/ci_runner_env/ci_runner_env.go
@@ -3,6 +3,7 @@ package ci_runner_env
 const (
 	BuildBuddyAPIKeyEnvVarName       = "BUILDBUDDY_API_KEY"
 	BuildBuddyInvocationIDEnvVarName = "BUILDBUDDY_INVOCATION_ID"
+	BuildBuddyRunIDEnvVarName        = "BUILDBUDDY_RUN_ID"
 
 	BBGrpcClientOriginEnvVarName   = "BB_GRPC_CLIENT_ORIGIN"
 	BBGrpcClientIdentityEnvVarName = "BB_GRPC_CLIENT_IDENTITY"
