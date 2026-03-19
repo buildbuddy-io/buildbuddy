0aa527812f93f8fd649a7667a918e9f10b7cde87
Dan Stowell <dan@buildbuddy.io>
2026-03-19 15:49:04 +0000

    Revert "cli_login_test: unquarantine TestCLILoginWebFlow_SingleOrg_PersonalKeysEnabled TestCLILoginWebFlow_ZeroOrgs_CreateOrgFlow (both passed 16 times just now)"
    This reverts commit 2f363e178f98fa8fb8eebbafdffed7f2c1ec9834.

--- enterprise/server/test/webdriver/cli_login/cli_login_test.go
+++ enterprise/server/test/webdriver/cli_login/cli_login_test.go
@@ -25,6 +25,7 @@ import (
 )
 
 func TestCLILoginWebFlow_SingleOrg_PersonalKeysEnabled(t *testing.T) {
+	quarantine.SkipQuarantinedTest(t)
 	buildbuddy_enterprise.MarkTestLocalOnly(t)
 
 	app := buildbuddy_enterprise.SetupWebTarget(t)
@@ -98,6 +99,7 @@ func TestCLILoginWebFlow_MultipleOrgs_ChooseOrgWithoutPersonalKeysEnabled(t *tes
 }
 
 func TestCLILoginWebFlow_ZeroOrgs_CreateOrgFlow(t *testing.T) {
+	quarantine.SkipQuarantinedTest(t)
 	buildbuddy_enterprise.MarkTestLocalOnly(t)
 
 	app := buildbuddy_enterprise.SetupWebTarget(t)
