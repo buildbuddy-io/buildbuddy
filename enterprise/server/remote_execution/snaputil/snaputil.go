3af3c95ea62be1fbf8dc5ec81a62cce8c1c0bdb6
Vanja Pejovic <vanja@buildbuddy.io>
2026-03-20 11:28:50 -0400

    Add flag to disable compression of snapshot chunk remote uploads
    Right now, we compress this data on the executor, then send it to the proxy, which decompresses it so it can validate the digest, and then it compresses it again.
    
    Since we have proxies really close to executors in some locations, it's probably better to just send the data uncompressed, and save time and CPU on both the executor and proxy.

--- enterprise/server/remote_execution/snaputil/snaputil.go
+++ enterprise/server/remote_execution/snaputil/snaputil.go
@@ -30,6 +30,7 @@ var (
 	EnableBalloon                    = flag.Bool("executor.firecracker_enable_balloon", false, "Enable memory balloon support when snapshotting firecracker VMs.")
 	VerboseLogging                   = flag.Bool("executor.verbose_snapshot_logs", false, "Enables extra-verbose snapshot logs (even at debug log level)")
 	storeSnapshotsInLocalClusterOnly = flag.Bool("executor.store_snapshots_in_local_cluster_only", false, "If true, snapshots are only stored in the cache proxy in the cluster where this executor is running.")
+	disableUploadCompresssion        = flag.Bool("executor.disable_snapshot_chunk_upload_compression", false, "If true, snapshot chunks will be sent to the remote cache uncompressed.")
 )
 
 const (
@@ -181,7 +182,9 @@ func Cache(ctx context.Context, localCache interfaces.FileCache, bsClient bytest
 	}
 
 	rn := digest.NewCASResourceName(d, remoteInstanceName, repb.DigestFunction_BLAKE3)
-	rn.SetCompressor(repb.Compressor_ZSTD)
+	if !*disableUploadCompresssion {
+		rn.SetCompressor(repb.Compressor_ZSTD)
+	}
 	file, err := os.Open(path)
 	if err != nil {
 		return 0, err
