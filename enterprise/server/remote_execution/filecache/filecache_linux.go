41bb0339aa040abba2d029c932463884a782966e
Tyler Williams <williams.tyler@gmail.com>
2026-03-30 14:40:31 -0700

    Do a little platform dance to sync the FS (part2).

---
+++ enterprise/server/remote_execution/filecache/filecache_linux.go
@@ -0,0 +1,18 @@
+//go:build linux
+
+package filecache
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+func syncFilesystem(path string) error {
+	dir, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer dir.Close()
+	return unix.Syncfs(int(dir.Fd()))
+}
