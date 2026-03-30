fca051e1bcf104a42d0850f8fac13acabcafdf23
Lulu Zhang <lulu@buildbuddy.io>
2026-03-30 14:28:16 -0700

    update the test so that we are doing multiple rounds of stopping and restarting stores at random intervals

--- enterprise/server/raft/testutil/testutil.go
+++ enterprise/server/raft/testutil/testutil.go
@@ -92,6 +92,13 @@ func (nrf nodeRegistryFactory) Create(nhid string, streamConnections uint64, v d
 func (sf *StoreFactory) RecreateStore(t *testing.T, ts *TestingStore) {
 	require.Nil(t, disk.EnsureDirectoryExists(ts.RootDir))
 
+	if ts.closed {
+		gm, err := gossip.NewWithArgs("name-"+ts.GossipAddress, ts.GossipAddress, sf.gossipAddrs)
+		require.NoError(t, err)
+		ts.gm = gm
+		ts.closed = false
+	}
+
 	te := testenv.GetTestEnv(t)
 	te.SetClock(sf.clock)
 
@@ -150,10 +157,6 @@ func (sf *StoreFactory) RecreateStore(t *testing.T, ts *TestingStore) {
 	store.StartReplicaJanitor()
 	ts.Store = store
 	ts.leaser = store.LeaserForTest()
-
-	t.Cleanup(func() {
-		ts.Stop()
-	})
 }
 
 func (sf *StoreFactory) NewStore(t *testing.T) *TestingStore {
@@ -165,14 +168,18 @@ func (sf *StoreFactory) NewStore(t *testing.T) *TestingStore {
 	require.NoError(t, err)
 
 	ts := &TestingStore{
-		t:           t,
-		gm:          gm,
-		RaftAddress: localAddr(t),
-		GRPCAddress: localAddr(t),
-		RootDir:     filepath.Join(sf.rootDir, fmt.Sprintf("store-%d", len(sf.gossipAddrs))),
-		nhid:        id.String(),
+		t:             t,
+		gm:            gm,
+		RaftAddress:   localAddr(t),
+		GRPCAddress:   localAddr(t),
+		GossipAddress: nodeAddr,
+		RootDir:       filepath.Join(sf.rootDir, fmt.Sprintf("store-%d", len(sf.gossipAddrs))),
+		nhid:          id.String(),
 	}
 	sf.RecreateStore(t, ts)
+	t.Cleanup(func() {
+		ts.Stop()
+	})
 	return ts
 }
 
@@ -195,12 +202,13 @@ type TestingStore struct {
 	leaser pebble.Leaser
 	nhid   string
 
-	gm          *gossip.GossipManager
-	Registry    registry.NodeRegistry
-	RootDir     string
-	RaftAddress string
-	GRPCAddress string
-	closed      bool
+	gm            *gossip.GossipManager
+	Registry      registry.NodeRegistry
+	RootDir       string
+	RaftAddress   string
+	GRPCAddress   string
+	GossipAddress string
+	closed        bool
 }
 
 func (ts *TestingStore) DB() pebble.IPebbleDB {
