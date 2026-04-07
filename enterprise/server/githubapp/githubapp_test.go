buildbuddy-io/buildbuddy

commit 79ed48d26d039e0359eb19c233686655d03d33d7

[WF] Refresh GH token for remote runners at lease time (#11583)
Customer requests:
https://buildbuddy-corp.slack.com/archives/C04S4M5U3BK/p1772821963095989

https://buildbuddy-corp.slack.com/archives/C0777L0C2UW/p1773351690815529?thread_ts=1773345341.240749&cid=C0777L0C2UW

enterprise/server/githubapp/githubapp.go
@@ -559,6 +559,11 @@ func (a *GitHubApp) maybeTriggerBuildBuddyWorkflow(ctx context.Context, eventTyp
 		ctx, row.GitRepository, wd, tok.GetToken())
 }
 
+// GetInstallationTokenForStatusReportingOnly returns an installation token for the given owner.
+// It does not authorize the user, because we don't have an authenticated context when handling
+// webhooks, so should be used for status reporting only.
+//
+// Use GetRepositoryInstallationToken in other cases that should authorize the user.
 func (a *GitHubApp) GetInstallationTokenForStatusReportingOnly(ctx context.Context, owner string) (*github.InstallationToken, error) {
 	var installation tables.GitHubAppInstallation
 	err := a.env.GetDBHandle().NewQuery(ctx, "githubapp_get_installation_token_for_status").Raw(`

enterprise/server/hostedrunner/hostedrunner.go
@@ -367,6 +367,7 @@ func (r *runnerService) credentialEnvOverrides(ctx context.Context, req *rnpb.Ru
 	}
 
 	// Use env override headers for credentials.
+	// TODO(Maggie): Remove REPO_TOKEN once the leaser fetches the token.
 	envOverrides := []string{
 		ci_runner_env.BuildBuddyAPIKeyEnvVarName + "=" + apiKey.Value,
 		"REPO_USER=" + req.GetGitRepo().GetUsername(),

enterprise/server/scheduling/scheduler_server/BUILD
@@ -56,11 +56,14 @@ go_test(
     deps = [
         "//enterprise/server/backends/redis_execution_collector",
         "//enterprise/server/experiments",
+        "//enterprise/server/githubapp",
         "//enterprise/server/remote_execution/execution_server",
         "//enterprise/server/tasksize",
         "//enterprise/server/testutil/enterprise_testauth",
         "//enterprise/server/testutil/enterprise_testenv",
         "//enterprise/server/testutil/testredis",
+        "//enterprise/server/util/ci_runner_env",
+        "//enterprise/server/util/ci_runner_util",
         "//proto:context_go_proto",
         "//proto:remote_execution_go_proto",
         "//proto:scheduler_go_proto",

enterprise/server/scheduling/scheduler_server/scheduler_server.go
@@ -1948,7 +1948,7 @@ func (s *SchedulerServer) LeaseTask(stream scpb.Scheduler_LeaseTaskServer) error
 					log.CtxWarningf(ctx, "Could not remove task from unclaimed list: %s", err)
 				}
 			}
-			task.serializedTask = s.modifyTaskForExperiments(ctx, req.GetExecutorHostname(), task.serializedTask)
+			task.serializedTask = s.modifyTaskForLease(ctx, req.GetExecutorHostname(), task.serializedTask, task.metadata.GetTaskGroupId())
 
 			// Prometheus: observe queue wait time.
 			ageInMillis := time.Since(task.queuedTimestamp).Milliseconds()
@@ -2041,16 +2041,36 @@ func (s *SchedulerServer) LeaseTask(stream scpb.Scheduler_LeaseTaskServer) error
 	return nil
 }
 
-func (s *SchedulerServer) modifyTaskForExperiments(ctx context.Context, executorHostname string, task []byte) []byte {
-	fp := s.env.GetExperimentFlagProvider()
-	if fp == nil {
-		return task
-	}
-
+// modifyTaskForLease applies lease-time mutations to the serialized task.
+//
+// We do this at lease time so each execution attempt gets the latest values
+// for things like experiments and token refreshes.
+// This is important for tasks that are queued for a long time or are retried
+// after some time, as values computed at initial enqueue can be stale.
+func (s *SchedulerServer) modifyTaskForLease(ctx context.Context, executorHostname string, task []byte, taskGroupID string) []byte {
 	taskProto := &repb.ExecutionTask{}
 	if err := proto.Unmarshal(task, taskProto); err != nil {
 		log.CtxWarningf(ctx, "Failed to unmarshal ExecutionTask: %s", err)
 		return task
+	}
+
+	taskProto = s.modifyTaskForExperiments(ctx, executorHostname, taskProto)
+	if err := ci_runner_util.SetTaskRepositoryToken(ctx, s.env, taskProto, taskGroupID); err != nil {
+		log.CtxWarningf(ctx, "Failed to set repository token: %s", err)
+	}
+
+	if newTask, err := proto.Marshal(taskProto); err != nil {
+		log.CtxWarningf(ctx, "Failed to marshal updated ExecutionTask: %s", err)
+		return task
+	} else {
+		return newTask
+	}
+}
+
+func (s *SchedulerServer) modifyTaskForExperiments(ctx context.Context, executorHostname string, taskProto *repb.ExecutionTask) *repb.ExecutionTask {
+	fp := s.env.GetExperimentFlagProvider()
+	if fp == nil {
+		return taskProto
 	}
 
 	selfHosted := false
@@ -2092,17 +2112,11 @@ func (s *SchedulerServer) modifyTaskForExperiments(ctx context.Context, executor
 		taskProto.Experiments = append(taskProto.Experiments, "upgrade-fc-guest-kernel")
 	}
 
-	if newTask, err := proto.Marshal(taskProto); err != nil {
-		log.CtxWarningf(ctx, "Failed to marshal ExecutionTask: %s", err)
-		return task
-	} else {
-		return newTask
-	}
+	return taskProto
 }
 
 func getWorkflowName(task *repb.ExecutionTask) string {
-	args := task.GetCommand().GetArguments()
-	if len(args) < 2 || args[0] != "./"+ci_runner_util.ExecutableName {
+	if !(ci_runner_util.IsRemoteRunnerTask(task)) {
 		return ""
 	}
 	for _, arg := range task.GetCommand().GetArguments() {

enterprise/server/scheduling/scheduler_server/scheduler_server_test.go
@@ -12,11 +12,14 @@ import (
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/backends/redis_execution_collector"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/experiments"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/githubapp"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/remote_execution/execution_server"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/tasksize"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/enterprise_testauth"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/enterprise_testenv"
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/testredis"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/ci_runner_env"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/ci_runner_util"
 	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testauth"
@@ -838,6 +841,62 @@ func TestLeaseExpiration(t *testing.T) {
 	// Lease renewal should fail as the stream should be broken.
 	err = lease.Renew()
 	require.ErrorIs(t, io.EOF, err)
+}
+
+func TestLeaseTask_RefreshToken_FailureDoesNotFailLease(t *testing.T) {
+	env, ctx := getEnv(t, &schedulerOpts{}, "user1")
+	fe := newFakeExecutor(ctx, t, env.GetSchedulerClient())
+	fe.Register()
+
+	gh, err := githubapp.NewAppService(env, nil, nil)
+	require.NoError(t, err)
+	env.SetGitHubAppService(gh)
+
+	// Simulate an auth error for token refresh.
+	env.GetAuthenticator().(*testauth.TestAuthenticator).APIKeyProvider = func(ctx context.Context, apiKey string) (interfaces.UserInfo, error) {
+		return nil, status.UnauthenticatedError("invalid API key")
+	}
+
+	overrides := ci_runner_env.BuildBuddyAPIKeyEnvVarName + "=INVALID_API_KEY"
+	task := &repb.ExecutionTask{
+		ExecutionId: "task1",
+		Command: &repb.Command{
+			Arguments: []string{"./" + ci_runner_util.ExecutableName, "--pushed_repo_url=https://github.com/acme-inc/repo"},
+		},
+		PlatformOverrides: &repb.Platform{
+			Properties: []*repb.Platform_Property{
+				{
+					Name:  platform.EnvOverridesPropertyName,
+					Value: overrides,
+				},
+			},
+		},
+	}
+	taskBytes, err := proto.Marshal(task)
+	require.NoError(t, err)
+
+	req := &scpb.ScheduleTaskRequest{
+		TaskId:         "task1",
+		SerializedTask: taskBytes,
+		Metadata: &scpb.SchedulingMetadata{
+			Os:          defaultOS,
+			Arch:        defaultArch,
+			TaskGroupId: "group1",
+			TaskSize: &scpb.TaskSize{
+				EstimatedMemoryBytes:   100,
+				EstimatedMilliCpu:      100,
+				EstimatedFreeDiskBytes: 100,
+			},
+		},
+	}
+
+	_, err = env.GetSchedulerService().ScheduleTask(ctx, req)
+	require.NoError(t, err)
+	fe.WaitForTask(req.GetTaskId())
+	lease := fe.Claim(req.GetTaskId())
+	defer lease.Finalize()
+
+	require.Equal(t, task, lease.task)
 }
 
 func TestSchedulingDelay_NoDelay(t *testing.T) {

enterprise/server/util/ci_runner_util/BUILD
@@ -1,4 +1,4 @@
-load("@io_bazel_rules_go//go:def.bzl", "go_library")
+load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
 
 package(default_visibility = ["//enterprise:__subpackages__"])
 
@@ -8,7 +8,10 @@ go_library(
     importpath = "github.com/buildbuddy-io/buildbuddy/enterprise/server/util/ci_runner_util",
     deps = [
         "//enterprise/server/cmd/ci_runner/bundle",
+        "//enterprise/server/githubapp",
+        "//enterprise/server/util/ci_runner_env",
         "//proto:remote_execution_go_proto",
+        "//server/environment",
         "//server/interfaces",
         "//server/remote_cache/cachetools",
         "//server/remote_cache/digest",
@@ -17,5 +20,27 @@ go_library(
         "//server/util/status",
         "@org_golang_google_genproto_googleapis_bytestream//:bytestream",
         "@org_golang_x_sync//errgroup",
+    ],
+)
+
+go_test(
+    name = "ci_runner_util_test",
+    size = "small",
+    srcs = ["ci_runner_util_test.go"],
+    embed = [":ci_runner_util"],
+    deps = [
+        "//enterprise/server/githubapp",
+        "//enterprise/server/testutil/enterprise_testenv",
+        "//enterprise/server/util/ci_runner_env",
+        "//proto:remote_execution_go_proto",
+        "//server/environment",
+        "//server/interfaces",
+        "//server/tables",
+        "//server/testutil/testauth",
+        "//server/testutil/testenv",
+        "//server/testutil/testgit",
+        "//server/util/platform",
+        "//server/util/testing/flags",
+        "@com_github_stretchr_testify//require",
     ],
 )

enterprise/server/util/ci_runner_util/ci_runner_util.go
@@ -5,9 +5,13 @@ import (
 	_ "embed"
 	"flag"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/cmd/ci_runner/bundle"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/githubapp"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/ci_runner_env"
+	"github.com/buildbuddy-io/buildbuddy/server/environment"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/cachetools"
 	"github.com/buildbuddy-io/buildbuddy/server/remote_cache/digest"
@@ -99,4 +103,103 @@ func UploadInputRoot(ctx context.Context, bsClient bspb.ByteStreamClient, cache 
 		return cachetools.UploadProtoToCAS(ctx, cache, instanceName, repb.DigestFunction_BLAKE3, dir)
 	}
 	return digest.ComputeForMessage(&repb.Directory{}, repb.DigestFunction_BLAKE3)
+}
+
+// SetTaskRepositoryToken sets the GitHub repository token for a trusted remote runner task.
+// It mutates the original task.
+func SetTaskRepositoryToken(ctx context.Context, env environment.Env, task *repb.ExecutionTask, groupID string) error {
+	if !(IsRemoteRunnerTask(task)) {
+		return nil
+	}
+	if groupID == "" {
+		return status.FailedPreconditionError("missing group ID")
+	}
+
+	envOverrides := envOverridesHeader(task)
+
+	// Untrusted workflows won't have repo credentials. Trusted
+	// workflows have BUILDBUDDY_API_KEY set by the workflow service.
+	apiKey := envOverrides[ci_runner_env.BuildBuddyAPIKeyEnvVarName]
+	if apiKey == "" {
+		return nil
+	}
+
+	repoURL, err := remoteRunBaseRepoURL(task.GetCommand().GetArguments())
+	if err != nil {
+		return err
+	}
+	authCtx := env.GetAuthenticator().AuthContextFromAPIKey(ctx, apiKey)
+	token, err := githubapp.GetRepositoryInstallationToken(authCtx, env, groupID, repoURL)
+	if err != nil {
+		return status.WrapError(err, "failed to refresh remote runner git token")
+	}
+	envOverrides["REPO_TOKEN"] = token
+	applyEnvOverrides(task, envOverrides)
+	return nil
+}
+
+func envOverridesHeader(task *repb.ExecutionTask) map[string]string {
+	if task.GetPlatformOverrides() == nil {
+		return nil
+	}
+	overrides := make(map[string]string)
+	for _, prop := range task.GetPlatformOverrides().GetProperties() {
+		if strings.EqualFold(prop.GetName(), platform.EnvOverridesPropertyName) {
+			for override := range strings.SplitSeq(prop.GetValue(), ",") {
+				k, v, ok := strings.Cut(strings.TrimSpace(override), "=")
+				if ok {
+					overrides[k] = v
+				}
+			}
+		}
+	}
+	return overrides
+}
+
+func remoteRunBaseRepoURL(args []string) (string, error) {
+	pushedRepoURL, _ := commandArgValue(args, "--pushed_repo_url")
+	targetRepoURL, _ := commandArgValue(args, "--target_repo_url")
+	if targetRepoURL != "" && targetRepoURL != pushedRepoURL {
+		return targetRepoURL, nil
+	}
+	if pushedRepoURL != "" {
+		return pushedRepoURL, nil
+	}
+	if targetRepoURL != "" {
+		return targetRepoURL, nil
+	}
+	return "", status.FailedPreconditionError("could not parse repo URL from command arguments")
+}
+
+func commandArgValue(args []string, name string) (string, bool) {
+	for i, arg := range args {
+		if arg == name && i+1 < len(args) {
+			return args[i+1], true
+		}
+		prefix := name + "="
+		if trimmed, hasPrefix := strings.CutPrefix(arg, prefix); hasPrefix {
+			return trimmed, true
+		}
+	}
+	return "", false
+}
+
+func applyEnvOverrides(task *repb.ExecutionTask, envOverrides map[string]string) {
+	assignments := make([]string, 0, len(envOverrides))
+	for k, v := range envOverrides {
+		assignments = append(assignments, k+"="+v)
+	}
+	overridesVal := strings.Join(assignments, ",")
+
+	for _, prop := range task.PlatformOverrides.Properties {
+		if strings.EqualFold(prop.GetName(), platform.EnvOverridesPropertyName) {
+			prop.Value = overridesVal
+			return
+		}
+	}
+}
+
+func IsRemoteRunnerTask(task *repb.ExecutionTask) bool {
+	args := task.GetCommand().GetArguments()
+	return len(args) > 0 && args[0] == "./"+ExecutableName
 }

enterprise/server/util/ci_runner_util/ci_runner_util_test.go
@@ -1,0 +1,110 @@
+package ci_runner_util
+
+import (
+	"context"
+	"testing"
+
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/githubapp"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/testutil/enterprise_testenv"
+	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/ci_runner_env"
+	"github.com/buildbuddy-io/buildbuddy/server/environment"
+	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
+	"github.com/buildbuddy-io/buildbuddy/server/tables"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testauth"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
+	"github.com/buildbuddy-io/buildbuddy/server/testutil/testgit"
+	"github.com/buildbuddy-io/buildbuddy/server/util/platform"
+	"github.com/buildbuddy-io/buildbuddy/server/util/testing/flags"
+	"github.com/stretchr/testify/require"
+
+	repb "github.com/buildbuddy-io/buildbuddy/proto/remote_execution"
+)
+
+const (
+	mockGithubAppID = 1234567890
+
+	owner   = "acme-inc"
+	repoURL = "https://github.com/acme-inc/repo"
+)
+
+func TestSetTaskRepositoryToken(t *testing.T) {
+	env := getEnv(t, "fresh-repo-token")
+	ctx := context.Background()
+
+	createLinkedRepo(t, ctx, env)
+
+	task := newRemoteRunnerTask(repoURL, "workflow-api-key", "stale-repo-token")
+	err := SetTaskRepositoryToken(ctx, env, task, "group1")
+	require.NoError(t, err)
+
+	envOverrides := envOverridesHeader(task)
+	require.Equal(t, "fresh-repo-token", envOverrides["REPO_TOKEN"])
+	require.Equal(t, "workflow-api-key", envOverrides[ci_runner_env.BuildBuddyAPIKeyEnvVarName])
+}
+
+func TestSetTaskRepositoryToken_Failure(t *testing.T) {
+	env := getEnv(t, "fresh-repo-token")
+	ctx := context.Background()
+
+	// Don't create a repo for the group ID, so the token refresh fails.
+
+	task := newRemoteRunnerTask("https://github.com/acme-inc/repo", "workflow-api-key", "stale-repo-token")
+	err := SetTaskRepositoryToken(ctx, env, task, "group1")
+	require.Error(t, err)
+
+	envOverrides := envOverridesHeader(task)
+	// If token refresh fails, the existing token should be left unchanged.
+	require.Equal(t, "stale-repo-token", envOverrides["REPO_TOKEN"])
+}
+
+func getEnv(t *testing.T, mockToken string) *testenv.TestEnv {
+	flags.Set(t, "github.read_only_app.enabled", true)
+	env := enterprise_testenv.New(t)
+	env.SetAuthenticator(testauth.NewTestAuthenticator(t, map[string]interfaces.UserInfo{
+		"user1":            testauth.User("user1", "group1"),
+		"workflow-api-key": testauth.User("user1", "group1"),
+	}))
+	gh, err := githubapp.NewAppService(env, nil, &testgit.FakeGitHubApp{Token: mockToken, MockAppID: mockGithubAppID})
+	require.NoError(t, err)
+	env.SetGitHubAppService(gh)
+	return env
+}
+
+func createLinkedRepo(t *testing.T, ctx context.Context, env environment.Env) {
+	err := env.GetDBHandle().NewQuery(ctx, "ci_runner_util_test_create_linked_repo").Create(&tables.GitRepository{
+		RepoURL: repoURL,
+		UserID:  "user1",
+		GroupID: "group1",
+		Perms:   1,
+	})
+	require.NoError(t, err)
+	err = env.GetDBHandle().NewQuery(ctx, "ci_runner_util_test_create_app_installation").Create(&tables.GitHubAppInstallation{
+		GroupID: "group1",
+		AppID:   mockGithubAppID,
+		Owner:   owner,
+	})
+	require.NoError(t, err)
+}
+
+func newRemoteRunnerTask(repoURL, apiKey, repoToken string) *repb.ExecutionTask {
+	overrides := ci_runner_env.BuildBuddyAPIKeyEnvVarName + "=" + apiKey
+	if repoToken != "" {
+		overrides += ",REPO_TOKEN=" + repoToken
+	}
+	return &repb.ExecutionTask{
+		Command: &repb.Command{
+			Arguments: []string{
+				"./" + ExecutableName,
+				"--pushed_repo_url=" + repoURL,
+			},
+		},
+		PlatformOverrides: &repb.Platform{
+			Properties: []*repb.Platform_Property{
+				{
+					Name:  platform.EnvOverridesPropertyName,
+					Value: overrides,
+				},
+			},
+		},
+	}
+}

commit 45e932c3b708896a3194a9319ef509146a47c36a

Normalize github url when fetching token (#11803)
Fixes a bug introduced in
https://github.com/buildbuddy-io/buildbuddy/pull/11583

enterprise/server/githubapp/githubapp.go
@@ -589,14 +589,19 @@ func (a *GitHubApp) GetRepositoryInstallationToken(ctx context.Context, groupID,
 		return "", err
 	}
 
+	parsedRepoURL, err := gitutil.ParseGitHubRepoURL(repoURL)
+	if err != nil {
+		return "", status.InvalidArgumentErrorf("invalid repo URL %s: %s", repoURL, err)
+	}
+
 	// Validate that the repo was imported to BB.
 	gitRepository := &tables.GitRepository{}
-	err := a.env.GetDBHandle().NewQuery(ctx, "githubapp_get_repo_for_token").Raw(`
+	err = a.env.GetDBHandle().NewQuery(ctx, "githubapp_get_repo_for_token").Raw(`
 		SELECT *
 		FROM "GitRepositories"
 		WHERE group_id = ?
 		AND repo_url = ?
-	`, groupID, repoURL).Take(gitRepository)
+	`, groupID, parsedRepoURL.String()).Take(gitRepository)
 	if err != nil {
 		if db.IsRecordNotFound(err) {
 			return "", status.NotFoundErrorf("repo %s not found", repoURL)
@@ -604,10 +609,6 @@ func (a *GitHubApp) GetRepositoryInstallationToken(ctx context.Context, groupID,
 		return "", status.InternalErrorf("failed to look up repo %s: %s", repoURL, err)
 	}
 
-	parsedRepoURL, err := gitutil.ParseGitHubRepoURL(repoURL)
-	if err != nil {
-		return "", err
-	}
 	var installation tables.GitHubAppInstallation
 	err = a.env.GetDBHandle().NewQuery(ctx, "githubapp_get_installation_token").Raw(`
 		SELECT *
@@ -645,7 +646,7 @@ func GetRepositoryInstallationToken(ctx context.Context, env environment.Env, gr
 	if err != nil {
 		return "", status.WrapError(err, "get GitHub app for owner")
 	}
-	return app.GetRepositoryInstallationToken(ctx, groupID, repoURL)
+	return app.GetRepositoryInstallationToken(ctx, groupID, parsedRepoURL.String())
 }
 
 // LinkGitHubAppInstallation imports an installed GitHub app to BuildBuddy.

enterprise/server/githubapp/githubapp_test.go
@@ -103,6 +103,22 @@ func TestGetRepositoryInstallationToken(t *testing.T) {
 	require.Equal(t, 1, client.createTokenCalls)
 }
 
+func TestGetRepositoryInstallationToken_NormalizesRepoURL(t *testing.T) {
+	te, ctx := setupEnv(t)
+	insertInstallation(t, te, ctx)
+	insertRepo(t, te, ctx)
+	client := &fakeAppClient{
+		t:                  t,
+		wantInstallationID: testInstallationID,
+	}
+	app := newTestApp(te, client)
+
+	tok, err := app.GetRepositoryInstallationToken(ctx, testGroupID, testRepoURL+".git")
+	require.NoError(t, err)
+	require.Equal(t, fakeToken, tok)
+	require.Equal(t, 1, client.createTokenCalls)
+}
+
 func TestGetRepositoryInstallationToken_Unauthorized(t *testing.T) {
 	te, _ := setupEnv(t)
 