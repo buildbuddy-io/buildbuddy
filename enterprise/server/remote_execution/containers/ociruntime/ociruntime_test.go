b24b09ff33df6765e47675bf078741d03eee29f5
Dan Stowell <dan@buildbuddy.io>
2026-04-13 19:47:47 +0000

    Unquarantine TestCreateFailureHasStderr and fix missing PullImage call
    The test was failing because it called Create() without first calling
    PullImage(), which resulted in:
      'attempted to create rootfs before pulling image'
    instead of the expected error containing 'nonexistent'.
    
    Add the missing PullImage() call before Create() to match the pattern
    used by all other tests in the suite, and remove the quarantine skip.
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- enterprise/server/remote_execution/containers/ociruntime/ociruntime_test.go
+++ enterprise/server/remote_execution/containers/ociruntime/ociruntime_test.go
@@ -29,7 +29,6 @@ import (
 	"github.com/buildbuddy-io/buildbuddy/enterprise/server/util/oci"
 	"github.com/buildbuddy-io/buildbuddy/server/interfaces"
 	"github.com/buildbuddy-io/buildbuddy/server/real_environment"
-	"github.com/buildbuddy-io/buildbuddy/server/testutil/quarantine"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testenv"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testfs"
 	"github.com/buildbuddy-io/buildbuddy/server/testutil/testnetworking"
@@ -913,8 +912,6 @@ func TestCreateExecPauseUnpause(t *testing.T) {
 }
 
 func TestCreateFailureHasStderr(t *testing.T) {
-	// TODO: https://github.com/buildbuddy-io/buildbuddy-internal/issues/6878
-	quarantine.SkipQuarantinedTest(t)
 	setupNetworking(t)
 
 	image := busyboxImage(t)
@@ -945,6 +942,8 @@ func TestCreateFailureHasStderr(t *testing.T) {
 		require.NoError(t, err)
 	})
 
+	require.NoError(t, err)
+	err = c.PullImage(ctx, oci.Credentials{})
 	require.NoError(t, err)
 	err = c.Create(ctx, wd+"nonexistent")
 	require.ErrorContains(t, err, "nonexistent")
