e943ce201b7700620eb2f8500e1ad6f3282114ca
Dan Stowell <dan@buildbuddy.io>
2026-04-20 16:52:54 +0000

    Address review feedback: use TracedCommandContainer.ImageSizeBytes()
    - runner.go: Call r.Container.ImageSizeBytes() instead of reaching
      through to r.Container.Delegate for the ImageSizer type assertion.
      TracedCommandContainer already handles this delegation.
    - docker.go, firecracker.go: Add comments explaining the intentional
      use of context.TODO() since ImageSizer interface doesn't take a ctx.
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- enterprise/server/remote_execution/containers/firecracker/firecracker.go
+++ enterprise/server/remote_execution/containers/firecracker/firecracker.go
@@ -2077,6 +2077,8 @@ func (c *FirecrackerContainer) ImageSizeBytes() int64 {
 	if !c.pulled {
 		return 0
 	}
+	// ImageSizer interface doesn't take a context; this is only called once
+	// per execution so context.TODO() is acceptable.
 	imagePath, err := ociconv.CachedDiskImagePath(context.TODO(), c.executorConfig.CacheRoot, c.containerImage)
 	if err != nil || imagePath == "" {
 		return 0
