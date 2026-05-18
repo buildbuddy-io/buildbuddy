daa1d516c06937da9cc97626e05d5792f9a419b7
Dan Stowell <dan@buildbuddy.io>
2026-05-18 18:58:36 +0000

    Clarify existing OCI image lock handling

--- enterprise/server/remote_execution/containers/ociruntime/ociruntime.go
+++ enterprise/server/remote_execution/containers/ociruntime/ociruntime.go
@@ -657,13 +657,13 @@ func (c *ociContainer) IsImageCached(ctx context.Context) (bool, error) {
 }
 
 func (c *ociContainer) PullImage(ctx context.Context, creds oci.Credentials) error {
-	previousLockedImage := c.lockedImage
+	existingLockedImage := c.lockedImage
 	lockedImage, err := c.imageStore.PullAndLockImage(ctx, c.imageRef, creds, c.useOCIFetcher)
 	if err != nil {
 		return status.WrapError(err, "pull OCI image")
 	}
-	if previousLockedImage != nil {
-		previousLockedImage.Unlock()
+	if existingLockedImage != nil {
+		existingLockedImage.Unlock()
 	}
 	c.lockedImage = lockedImage
 	return nil
