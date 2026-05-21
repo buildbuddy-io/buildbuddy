5473dcdb4a5a079bfb50a1b3ee0613ad8d5a8a9f
Tyler French <tfrench@buildbuddy.io>
2026-05-21 09:37:39 -0400

    fix(cas): add size validation to BatchRead chunked blob reassembly (#12227)
    Fixes https://github.com/buildbuddy-io/buildbuddy-internal/issues/7366

--- server/remote_cache/content_addressable_storage_server/content_addressable_storage_server_test.go
+++ server/remote_cache/content_addressable_storage_server/content_addressable_storage_server_test.go
@@ -1308,6 +1308,69 @@ func TestBatchReadBlobsWithChunkedBlob(t *testing.T) {
 	}
 }
 
+func TestBatchReadBlobsWithMismatchedChunkedManifest(t *testing.T) {
+	testProvider := memprovider.NewInMemoryProvider(map[string]memprovider.InMemoryFlag{
+		"cache.chunking_enabled": {
+			State:          memprovider.Enabled,
+			DefaultVariant: "true",
+			Variants: map[string]any{
+				"true":  true,
+				"false": false,
+			},
+		},
+	})
+	require.NoError(t, openfeature.SetProviderAndWait(testProvider))
+
+	fp, err := experiments.NewFlagProvider(t.Name())
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	te := testenv.GetTestEnv(t)
+	te.SetExperimentFlagProvider(fp)
+
+	ctx, err = prefix.AttachUserPrefixToContext(ctx, te.GetAuthenticator())
+	require.NoError(t, err)
+
+	clientConn := runCASServer(ctx, t, te)
+	casClient := repb.NewContentAddressableStorageClient(clientConn)
+	cache := te.GetCache()
+
+	chunkRN, chunk := testdigest.RandomCASResourceBuf(t, 1024)
+	require.NoError(t, cache.Set(ctx, chunkRN, chunk))
+
+	corruptDigest, _ := testdigest.NewReader(t, 3*1024*1024)
+	manifest := &chunking.Manifest{
+		BlobDigest:     corruptDigest,
+		ChunkDigests:   []*repb.Digest{chunkRN.GetDigest()},
+		InstanceName:   "",
+		DigestFunction: repb.DigestFunction_SHA256,
+	}
+	require.NoError(t, manifest.StoreWithoutVerification(ctx, cache))
+
+	readResp, err := casClient.BatchReadBlobs(ctx, &repb.BatchReadBlobsRequest{
+		Digests: []*repb.Digest{corruptDigest},
+	})
+	require.NoError(t, err)
+	require.Len(t, readResp.GetResponses(), 1)
+	require.Equal(t, int32(gcodes.NotFound), readResp.GetResponses()[0].GetStatus().GetCode())
+}
+
+func TestBatchReadBlobsRejectsOversizedRequest(t *testing.T) {
+	ctx := context.Background()
+	te := testenv.GetTestEnv(t)
+	clientConn := runCASServer(ctx, t, te)
+	casClient := repb.NewContentAddressableStorageClient(clientConn)
+
+	_, err := casClient.BatchReadBlobs(ctx, &repb.BatchReadBlobsRequest{
+		Digests: []*repb.Digest{
+			{Hash: strings.Repeat("a", 64), SizeBytes: 3 * 1000 * 1000},
+			{Hash: strings.Repeat("b", 64), SizeBytes: 2 * 1000 * 1000},
+		},
+	})
+	require.Error(t, err)
+	require.Equal(t, gcodes.InvalidArgument, gstatus.Code(err))
+}
+
 func TestSpliceBlobReadOnlyKey(t *testing.T) {
 	ctx := context.Background()
 	te := testenv.GetTestEnv(t)
