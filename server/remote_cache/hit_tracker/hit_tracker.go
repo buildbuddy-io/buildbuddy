10ab7ec3a4aeeff69f03a1087748a769c8b21a3f
Brandon Duffany <brandon@buildbuddy.io>
2026-06-10 18:53:11 -0400

    Suppress negative cached build duration alerts

--- server/remote_cache/hit_tracker/hit_tracker.go
+++ server/remote_cache/hit_tracker/hit_tracker.go
@@ -709,8 +709,12 @@ func (h *hitTracker) recordCacheUsage(ctx context.Context, d *repb.Digest, actio
 				execStartTime := md.GetExecutionStartTimestamp().AsTime()
 				execEndTime := md.GetExecutionCompletedTimestamp().AsTime()
 				execDuration := execEndTime.Sub(execStartTime)
-				c.TotalCachedActionExecUsec = execDuration.Microseconds()
-				skuCounts[sku.RemoteCacheACCachedExecDurationNanos] = execDuration.Nanoseconds()
+				if execDuration > 0 {
+					c.TotalCachedActionExecUsec = execDuration.Microseconds()
+					skuCounts[sku.RemoteCacheACCachedExecDurationNanos] = execDuration.Nanoseconds()
+				} else if execDuration < 0 {
+					log.CtxInfof(ctx, "Serving ActionResult with negative cached exec duration: executor_host_id=%q", md.GetWorker())
+				}
 			}
 		} else {
 			c.CASCacheHits = 1
