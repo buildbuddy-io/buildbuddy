41cc0b0b4f8001d03a15a1ae29f8cc99008013ac
Vadim Berezniker <vadim@buildbuddy.io>
2026-07-01 19:43:01 -0700

    Add flag to control gcs atime update separately.
    The GCS bucket has a "defensive" TTL to make sure we don't keep any
    objects around if we fail to properly evict them. The TTL is generally
    set much higher than the atime threshold.
    
    To that end, there's no need to update the GCS atime as frequently as
    long as we update it before the bucket TTL.
    
    GCS updates are also charged as "class A" operations which is the same
    as a "write".

--- server/testutil/mockgcs/mockgcs.go
+++ server/testutil/mockgcs/mockgcs.go
@@ -30,10 +30,19 @@ func New(clock clockwork.Clock) *mockGCS {
 // N.B. This implementation only mocks out the bits of GCS needed
 // to implement the pebble.PebbleGCSStorage interface.
 type mockGCS struct {
-	clock     clockwork.Clock
-	ageInDays int64
-	items     map[string]*timestampedBlob
-	mu        sync.Mutex
+	clock                     clockwork.Clock
+	ageInDays                 int64
+	items                     map[string]*timestampedBlob
+	mu                        sync.Mutex
+	updateCustomTimeCallCount int
+}
+
+// UpdateCustomTimeCallCount returns the number of times UpdateCustomTime has
+// been called. It is intended for use by tests.
+func (m *mockGCS) UpdateCustomTimeCallCount() int {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.updateCustomTimeCallCount
 }
 
 func (m *mockGCS) expired(blobName string) bool {
@@ -107,6 +116,7 @@ func (m *mockGCS) DeleteBlob(ctx context.Context, blobName string) error {
 func (m *mockGCS) UpdateCustomTime(ctx context.Context, blobName string, t time.Time) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
+	m.updateCustomTimeCallCount++
 	blob, ok := m.items[blobName]
 	if !ok {
 		return status.NotFoundError("mock gcs blob not found")
