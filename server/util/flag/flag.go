7e3bc520d94128e1d06aa0f98f1658a1ba081337
Iain Macdonald <xiainx@gmail.com>
2026-07-22 07:45:19 -0700

    Show the cache proxy's full command line on the cache proxy page

--- server/util/flag/flag.go
+++ server/util/flag/flag.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"log"
 	"net/url"
+	"reflect"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/buildbuddy-io/buildbuddy/server/util/flagutil/common"
@@ -30,6 +33,48 @@ var Args = flag.Args
 
 type Flag = flag.Flag
 type FlagSet = flag.FlagSet
+
+// IsSecret returns whether the given flag's value may contain secrets,
+// meaning it should never be displayed or logged: either the flag was
+// declared with the Secret tag, or its type contains a struct field tagged
+// config:"secret" (at any depth). Note that for the latter, YAML config
+// export redacts just the secret fields (see flagyaml.RedactSecrets);
+// callers of this coarser check should redact the whole value.
+func IsSecret(flg *flag.Flag) bool {
+	if s, ok := flg.Value.(common.Secretable); ok && s.IsSecret() {
+		return true
+	}
+	t, err := common.GetTypeForFlagValue(flg.Value)
+	if err != nil {
+		// Unrecognized flag value type; assume it may contain secrets.
+		return true
+	}
+	return typeContainsSecrets(t, map[reflect.Type]bool{})
+}
+
+func typeContainsSecrets(t reflect.Type, seen map[reflect.Type]bool) bool {
+	if t == nil || seen[t] {
+		return false
+	}
+	seen[t] = true
+	switch t.Kind() {
+	case reflect.Pointer, reflect.Slice, reflect.Array:
+		return typeContainsSecrets(t.Elem(), seen)
+	case reflect.Map:
+		return typeContainsSecrets(t.Key(), seen) || typeContainsSecrets(t.Elem(), seen)
+	case reflect.Struct:
+		for i := 0; i < t.NumField(); i++ {
+			field := t.Field(i)
+			if slices.Contains(strings.Split(field.Tag.Get("config"), ","), "secret") {
+				return true
+			}
+			if typeContainsSecrets(field.Type, seen) {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func New[T any](flagset *flag.FlagSet, name string, defaultValue T, usage string, tags ...flagtags.Taggable) *T {
 	return autoflags.New[T](flagset, name, defaultValue, usage, tags...)
