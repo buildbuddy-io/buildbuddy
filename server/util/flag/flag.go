bb0692cc3fe845a3286764f48631d793f2f0d8cc
Maggie Lou <maggie@buildbuddy.io>
2026-07-28 15:34:20 -0500

    Test

--- server/util/flag/flag.go
+++ server/util/flag/flag.go
@@ -40,7 +40,7 @@ func Parse() {
 }
 
 func String(name string, value string, usage string, tags ...flagtags.Taggable) *string {
-	return autoflags.New(common.DefaultFlagSet, name, value, usage, tags...)
+	return auxtoflags.New(common.DefaultFlagSet, name, value, usage, tags...)
 }
 
 func Bool(name string, value bool, usage string, tags ...flagtags.Taggable) *bool {
