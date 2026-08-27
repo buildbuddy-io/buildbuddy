2bdeb8a24f76737bd21c1a53160ce50353179710
Maggie Lou <maggie@buildbuddy.io>
2026-08-27 09:58:21 -0700

    [B] Add table to track last successful run time of billing export cron (#12398)
    The cron will use this to calculate the next window that should be used
    for the billing export
    
    ---------
    
    Co-authored-by: Tyler French <tfrench@buildbuddy.io>

--- server/tables/tables.go
+++ server/tables/tables.go
@@ -1035,6 +1035,21 @@ func (*IPRule) TableName() string {
 	return "IPRules"
 }
 
+// MetronomeBillingExportDestination is the BillingExportState primary key used
+// by the Metronome usage export cron.
+const MetronomeBillingExportDestination = "metronome"
+
+type BillingExportState struct {
+	Model
+	// Destination is the billing system usage is exported to, e.g. "metronome".
+	Destination                 string `gorm:"primaryKey"`
+	LastSuccessfulPeriodEndUsec int64
+}
+
+func (*BillingExportState) TableName() string {
+	return "BillingExportState"
+}
+
 type PostAutoMigrateLogic func() error
 
 // Manual migration called before auto-migration.
@@ -1521,6 +1536,7 @@ func RegisterTables() {
 	// Keep these sorted by two-letter prefix (and when adding new tables,
 	// use a unique prefix if possible):
 	registerTable("AK", &APIKey{})
+	registerTable("BE", &BillingExportState{})
 	registerTable("CA", &CacheEntry{})
 	registerTable("CL", &CacheLog{})
 	registerTable("EK", &EncryptionKey{})
