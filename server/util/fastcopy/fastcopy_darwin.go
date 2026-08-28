9227a6be91877c8a95a3be0e83b62f3a057e7e30
Keith Smiley <keithbsmiley@gmail.com>
2026-08-28 13:07:28 -0700

    Add option for hardlinks on macOS (#13098)
    Using copy-on-write is preferred but it turns out that those reflinks
    end up in extra security scans by macOS (unless you run under a trusted
    "developer tool" which the executor does not). We're a bit worried there
    are some potential macOS bugs here when you create many of these, but
    it's worth a try if the security scanning is a notable slowdown. For
    large dylibs the scan can easily be 4 seconds when launching a binary,
    and it happens per action x per dylib since the scan result is stored
    per inode.

--- server/util/fastcopy/fastcopy_darwin.go
+++ server/util/fastcopy/fastcopy_darwin.go
@@ -4,17 +4,26 @@ package fastcopy
 
 import (
 	"errors"
+	"flag"
 
 	"golang.org/x/sys/unix"
 )
 
-func Clone(source, destination string) error {
-	return FastCopy(source, destination)
-}
+var localCacheUseHardlinks = flag.Bool("executor.local_cache_use_hardlinks", false, "If true, use hardlinks instead of copy-on-write clones")
 
-func FastCopy(source, destination string) error {
+func Clone(source, destination string) error {
 	if err := unix.Clonefile(source, destination, unix.CLONE_NOFOLLOW); err != nil && !errors.Is(err, unix.EEXIST) {
 		return err
 	}
 	return nil
+}
+
+func FastCopy(source, destination string) error {
+	if *localCacheUseHardlinks {
+		if err := unix.Linkat(unix.AT_FDCWD, source, unix.AT_FDCWD, destination, 0); err != nil && !errors.Is(err, unix.EEXIST) {
+			return err
+		}
+		return nil
+	}
+	return Clone(source, destination)
 }
