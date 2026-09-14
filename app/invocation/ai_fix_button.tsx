c1141bb30f82804b1b5f483669440aab5dbd8ffb
Maggie Lou <maggie@buildbuddy.io>
2026-09-14 15:28:29 -0500

    Remove info button from AI button component

--- app/invocation/ai_fix_button.tsx
+++ app/invocation/ai_fix_button.tsx
@@ -3,16 +3,6 @@ import alertService from "../alert/alert_service";
 import { User } from "../auth/user";
 import capabilities from "../capabilities/capabilities";
 import AIButton from "../components/button/ai_button";
-import Button from "../components/button/button";
-import Dialog, {
-  DialogBody,
-  DialogFooter,
-  DialogFooterButtons,
-  DialogHeader,
-  DialogTitle,
-} from "../components/dialog/dialog";
-import { TextLink } from "../components/link/link";
-import Modal from "../components/modal/modal";
 import errorService from "../errors/error_service";
 import { copyToClipboard } from "../util/clipboard";
 import { RemoteRunnerAgent, supportsRemoteRun, triggerRemoteRun } from "../util/remote_runner";
@@ -23,15 +13,7 @@ export interface AIFixButtonProps {
   user: User | undefined;
 }
 
-type State = {
-  isDialogOpen: boolean;
-};
-
-export default class AIFixButton extends React.Component<AIFixButtonProps, State> {
-  state: State = {
-    isDialogOpen: false,
-  };
-
+export default class AIFixButton extends React.Component<AIFixButtonProps> {
   private getCommand(agent: RemoteRunnerAgent) {
     return `bb agent fix --agent=${agent} ${this.props.model.getInvocationId()}`;
   }
@@ -79,55 +61,13 @@ export default class AIFixButton extends React.Component<AIFixButtonProps, State
     }
 
     return (
-      <>
-        <AIButton
-          label="Fix"
-          loadingLabel="Starting..."
-          onClick={this.fixWithAI.bind(this)}
-          onCopyCommand={this.copyCommand.bind(this)}
-          onInfoClick={() => this.setState({ isDialogOpen: true })}
-        />
-        <Modal isOpen={this.state.isDialogOpen} onRequestClose={() => this.setState({ isDialogOpen: false })}>
-          <Dialog>
-            <DialogHeader>
-              <DialogTitle>Fix this build with AI</DialogTitle>
-            </DialogHeader>
-            <DialogBody>
-              <p>
-                <span className="inline-code">bb agent fix</span> uses AI to reproduce the failure, edit the working
-                tree with a fix, and verify the fix.
-              </p>
-              <p>
-                <b>Run from the UI</b>
-                <br />
-                When triggered from the UI, the command will be run on a remote runner. The generated patch will be
-                uploaded to the invocation under the 'Artifacts' tab. The runner requires an{" "}
-                <TextLink href="/settings/org/secrets" target="_blank">
-                  agent API-key stored as a BuildBuddy secret
-                </TextLink>
-                .
-              </p>
-              <p>
-                <b>Run locally</b>
-                <br />
-                The adjacent menu has a button to copy the command to run locally. On your local machine, the command
-                can use your existing agent sign-in.
-              </p>
-              <p>
-                Relevant profile data is sent to the AI provider, and provider charges may apply.{" "}
-                <TextLink href="https://www.buildbuddy.io/docs/cli-commands#bb-agent" target="_blank">
-                  See the docs for more info.
-                </TextLink>
-              </p>
-            </DialogBody>
-            <DialogFooter>
-              <DialogFooterButtons>
-                <Button onClick={() => this.setState({ isDialogOpen: false })}>Done</Button>
-              </DialogFooterButtons>
-            </DialogFooter>
-          </Dialog>
-        </Modal>
-      </>
+      <AIButton
+        label="Fix"
+        loadingLabel="Starting..."
+        onClick={this.fixWithAI.bind(this)}
+        onCopyCommand={this.copyCommand.bind(this)}
+        docsUrl="https://www.buildbuddy.io/docs/cli-commands#bb-agent"
+      />
     );
   }
 }
