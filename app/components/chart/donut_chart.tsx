103df54719de034946bf0272e122ce3f20f3ef56
jim hollenbach <jim@buildbuddy.io>
2026-09-17 11:48:19 -0700

    support consistent color mappings and title on donut charts (#13378)
    Co-authored-by: Brandon Duffany <brandon@buildbuddy.io>

--- app/components/chart/donut_chart.tsx
+++ app/components/chart/donut_chart.tsx
@@ -1,18 +1,51 @@
 import React from "react";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
-import { getChartColor } from "../../util/color";
+import { getChartColor, getUniformBrightnessColor } from "../../util/color";
 import format from "../../format/format";
 
-interface Props {
-  data: any[] | undefined;
+export interface NamedValue {
+  name: string;
+  value: number;
 }
 
+interface Props {
+  title?: string;
+  subtitle?: string;
+  data: NamedValue[] | undefined;
+
+  /** Returns a valid css color string (e.g., #ffff00) given a key from data. */
+  colorPicker?: (k: string) => string;
+
+  /** Returns a human-readable string for a given value in the data. */
+  valueFormatter?: (v: number) => string;
+}
+
+const OTHER_LABEL = "&&__Other__&&";
 const MAX_LEGEND_ENTRIES = 5;
 
+/**
+ * A little function that converts an ordered list of name keys into a color
+ * mapping.  This is useful for staying consistent when showing multiple donuts
+ * with the same keys in close proximity to each other.
+ */
+export function makeColorPicker(values: string[]): (name: string) => string {
+  const deduped: Set<string> = new Set(values);
+  const dedupedArray = [...deduped];
+  if (deduped.size > MAX_LEGEND_ENTRIES && !deduped.has(OTHER_LABEL)) {
+    dedupedArray.splice(MAX_LEGEND_ENTRIES, 0, OTHER_LABEL);
+  }
+
+  const mapped = new Map(dedupedArray.map((v, i) => [v, getChartColor(i)]));
+  return (n: string) => mapped.get(n) ?? getUniformBrightnessColor(n);
+}
 export default class DonutChart extends React.Component<Props> {
   render() {
-    let data = this.props.data?.filter((d) => d.value > 0).sort((a, b) => b.value - a.value);
-    const sum = data?.reduce(
+    const valueFormatter = this.props.valueFormatter ?? format.formatWithCommas;
+    const colorPicker = (nv: NamedValue, i: number) =>
+      this.props.colorPicker ? this.props.colorPicker(nv.name) : getChartColor(i);
+
+    let data = this.props.data?.filter((d) => d.value > 0).sort((a, b) => b.value - a.value) ?? [];
+    const sum = data.reduce(
       (prev, current) => {
         return { name: "Sum", value: prev.value + current.value };
       },
@@ -25,45 +58,60 @@ export default class DonutChart extends React.Component<Props> {
       for (let i = MAX_LEGEND_ENTRIES; i < data.length; i++) {
         other += data[i].value;
         otherLabels.push(
-          `${format.formatWithCommas(data[i].value)} ${data[i].name} (${format.percent(data[i].value / sum.value)}%)`
+          `${valueFormatter(data[i].value)} ${data[i].name} (${format.percent(data[i].value / sum.value)}%)`
         );
       }
     }
 
     data = data?.splice(0, MAX_LEGEND_ENTRIES);
     if (other > 0) {
-      data?.push({ name: "Other", value: other });
+      data?.push({ name: OTHER_LABEL, value: other });
     }
 
-    return (
+    const donut = (
       <div className="donut-chart">
         <ResponsiveContainer width={80} height={80}>
           <PieChart accessibilityLayer={false}>
             <Pie data={data} dataKey="value" outerRadius={40} innerRadius={20}>
-              {data?.map((_, index) => <Cell key={`cell-${index}`} fill={getChartColor(index)} />)}
+              {data?.map((entry, index) => <Cell key={`cell-${index}`} fill={colorPicker(entry, index)} />)}
             </Pie>
           </PieChart>
         </ResponsiveContainer>
         <div>
-          {data?.map((entry, index) => (
-            <div className="donut-chart-label">
-              <span className="donut-chart-swatch" style={{ backgroundColor: getChartColor(index) }}></span>
-              <span>
-                <span className="donut-chart-legend-value">{format.formatWithCommas(entry.value)}</span>{" "}
-                <span
-                  className="donut-chart-legend-desc"
-                  title={
-                    other > 0 && index == MAX_LEGEND_ENTRIES
-                      ? otherLabels.join(", ")
-                      : `${entry.name} (${format.percent(entry.value / sum.value)}%)`
-                  }>
-                  {entry.name} ({format.percent(entry.value / sum.value)}%)
+          {data?.map((entry, index) => {
+            const label = entry.name === OTHER_LABEL ? "Other" : entry.name;
+            return (
+              <div key={entry.name} className="donut-chart-label">
+                <span className="donut-chart-swatch" style={{ backgroundColor: colorPicker(entry, index) }}></span>
+                <span>
+                  <span className="donut-chart-legend-value">{valueFormatter(entry.value)}</span>{" "}
+                  <span
+                    className="donut-chart-legend-desc"
+                    title={
+                      other > 0 && index == MAX_LEGEND_ENTRIES
+                        ? otherLabels.join(", ")
+                        : `${entry.name} (${format.percent(entry.value / sum.value)}%)`
+                    }>
+                    {label} ({format.percent(entry.value / sum.value)}%)
+                  </span>
                 </span>
-              </span>
-            </div>
-          ))}
+              </div>
+            );
+          })}
         </div>
       </div>
     );
+
+    if (this.props.title) {
+      return (
+        <div className="donut-chart-title-block">
+          <div className="donut-chart-title">{this.props.title}</div>
+          {Boolean(this.props.subtitle) && <div className="donut-chart-subtitle">{this.props.subtitle}</div>}
+          {donut}
+        </div>
+      );
+    } else {
+      return donut;
+    }
   }
 }
