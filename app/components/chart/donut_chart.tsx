62965619cdda0b8df8f5ac05b4b27dd52c9515e6
Jim Hollenbach <jim@buildbuddy.io>
2026-09-17 12:52:41 -0700

    put donut chart outside of title block. oops.

--- app/components/chart/donut_chart.tsx
+++ app/components/chart/donut_chart.tsx
@@ -104,11 +104,13 @@ export default class DonutChart extends React.Component<Props> {
 
     if (this.props.title) {
       return (
-        <div className="donut-chart-title-block">
-          <div className="donut-chart-title">{this.props.title}</div>
-          {Boolean(this.props.subtitle) && <div className="donut-chart-subtitle">{this.props.subtitle}</div>}
+        <>
+          <div className="donut-chart-title-block">
+            <div className="donut-chart-title">{this.props.title}</div>
+            {Boolean(this.props.subtitle) && <div className="donut-chart-subtitle">{this.props.subtitle}</div>}
+          </div>
           {donut}
-        </div>
+        </>
       );
     } else {
       return donut;
