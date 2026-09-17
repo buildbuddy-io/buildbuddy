21eda33f63ad6fd24a7836bed1ab3160ef29d7ad
Vanja Pejovic <vanja@buildbuddy.io>
2026-09-17 17:11:27 -0400

    Fix help string for file_download_duration_usec metric
    This metric is not per-file. It's per execution.

--- server/metrics/metrics.go
+++ server/metrics/metrics.go
@@ -1784,7 +1784,7 @@ var (
 		Subsystem: "remote_execution",
 		Name:      "file_download_duration_usec",
 		Buckets:   prometheus.ExponentialBuckets(1, 10, 9),
-		Help:      "Per-file download duration during remote execution, in **microseconds**.",
+		Help:      "Time spent downloading files during remote execution, in **microseconds**.",
 	})
 
 	FileUploadCount = promauto.NewHistogram(prometheus.HistogramOpts{
